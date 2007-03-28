a0607cbfe47086d14a79cf09d39a4d91d25f68ea
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2007-03-28 04:20:37 +0000

    Overhauled interface between HHChannel and HHGate. Did unit tests for the Reinit function on HHChannel, which looks up stuff from HHGate and therefore tests a lot of the functioning.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@56 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: d4e760ee80987e447867c191056cc3e7518162cd

--- biophysics/HHGate.h
+++ biophysics/HHGate.h
@@ -14,58 +14,14 @@ class HHGate
 	friend class HHGateWrapper;
 	public:
 		HHGate()
-		{
-			takePower_ = power0;
-			instant_ = 0;
-		}
+		{ ; }
 
 		static void gateFunc(
-				const Conn& c, double v, double state, double dt );
+				const Conn& c, double v );
 		static void postCreate( const Conn& c );
 
-		static void setPower( const Conn& c, double power );
-		void innerSetPower( double power );
-		static double getPower( const Element* e );
-
-		static void setState( const Conn& c, double state );
-		static double getState( const Element* e );
-
-		static void setInstant( const Conn& c, int instant );
-		static int getInstant( const Element* e );
-
 	private:
-		double power_;
-		double state_;
-		int instant_;
 		Interpol A_;
 		Interpol B_;
-		double g_;	
-		bool useConcentration_;	
-		static double power0( double x ) {
-			return 1.0;
-		}
-		static double power1( double x ) {
-			return x;
-		}
-		static double power2( double x ) {
-			return x * x;
-		}
-		static double power3( double x ) {
-			return x * x * x;
-		}
-		static double power4( double x ) {
-			return power2( x * x );
-		}
-		static double powerN( double x ) {
-				cout << "Error: Cannot handle powerN\n";
-			return 0.0;
-		}
-		double ( *takePower_ )( double );
-
-
-
-		void innerGateFunc(
-				const Conn& c, double v, double state, double dt );
-
 };
 #endif // _HHGate_h
