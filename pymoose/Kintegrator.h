2621dbe4cc87b0d8ee5c8f69b38e5136c65f2b27
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2007-06-09 06:42:55 +0000

    Added some missing source files for Kinetics in pymoose
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@128 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ pymoose/Kintegrator.h
@@ -0,0 +1,21 @@
+#ifndef _pymoose_Kintegrator_h
+#define _pymoose_Kintegrator_h
+#include "PyMooseBase.h"
+class Kintegrator : public PyMooseBase
+{    public:
+        static const std::string className;
+        Kintegrator(Id id);
+        Kintegrator(std::string path);
+        Kintegrator(std::string name, Id parentId);
+        Kintegrator(std::string name, PyMooseBase* parent);
+        ~Kintegrator();
+        const std::string& getType();
+        bool __get_isInitiatilized() const;
+        void __set_isInitiatilized(bool isInitiatilized);
+//         string __get_method() const;
+//         void __set_method(string method);
+     std::string imethod() const;
+     std::string imethod(std::string);
+    
+};
+#endif
---
+++ pymoose/Reaction.h
@@ -0,0 +1,22 @@
+#ifndef _pymoose_Reaction_h
+#define _pymoose_Reaction_h
+#include "PyMooseBase.h"
+class Reaction : public PyMooseBase
+{    public:
+        static const std::string className;
+        Reaction(Id id);
+        Reaction(std::string path);
+        Reaction(std::string name, Id parentId);
+        Reaction(std::string name, PyMooseBase* parent);
+        ~Reaction();
+        const std::string& getType();
+        double __get_kf() const;
+        void __set_kf(double kf);
+        double __get_kb() const;
+        void __set_kb(double kb);
+        double __get_scaleKf() const;
+        void __set_scaleKf(double scaleKf);
+        double __get_scaleKb() const;
+        void __set_scaleKb(double scaleKb);
+};
+#endif
