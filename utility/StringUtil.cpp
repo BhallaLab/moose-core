ee7cfe0a9f750a078bd254ef69a814ebdc82f754
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2007-10-08 13:36:49 +0000

    Added property file handler, configuration handler and implemented Cell class to automate solver creation.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@201 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: cc1e88347daffc65411c1513cb7082ccd23dff3d

---
+++ utility/StringUtil.cpp
@@ -0,0 +1,76 @@
+/*******************************************************************
+ * File:            StringUtil.cpp
+ * Description:      
+ * Author:          Subhasis Ray
+ * E-mail:          ray.subhasis@gmail.com
+ * Created:         2007-09-25 12:12:10
+ ********************************************************************/
+#include <string>
+#include <iostream>
+#include "StringUtil.h"
+
+using namespace std;
+std::string trim(const std::string& myString)
+{
+    if (myString.length() == 0 )
+    {
+        return myString;        
+    }
+    
+    string::size_type  end = myString.find_last_not_of(DELIMITERS);
+    string::size_type begin = myString.find_first_not_of(DELIMITERS);
+
+    if(begin != string::npos)
+    {
+        return std::string(myString,begin, end-begin+1);
+    }
+    
+    return "";    
+}
+
+int testTrim()
+{
+    std::string testStrings [] = 
+        {
+            " space at beginning",
+            "space at end ",
+            " space at both sides ",
+            "\ttab at beginning",
+            "tab at end\t",
+            "\ttab at both sides\t",
+            "\nnewline at beginning",
+            "newline at end\n",
+            "\nnewline at both sides\n",
+            "\n\tnewline and tab at beginning",
+            "space and tab at end \t",
+            "   \rtab and return at both sides \r"
+        };
+    
+    std::string results[] = 
+        {
+            "space at beginning",
+            "space at end",
+            "space at both sides",
+            "tab at beginning",
+            "tab at end",
+            "tab at both sides",
+            "newline at beginning",
+            "newline at end",
+            "newline at both sides",
+            "newline and tab at beginning",
+            "space and tab at end",
+            "tab and return at both sides"
+        };
+    
+    bool success = true;
+    
+    for ( int i = 0; i < sizeof(testStrings)/sizeof(*testStrings); ++i )
+    {
+        std::string trimmed = trim(testStrings[i]);
+        success = (results[i].compare(trimmed)==0);
+        
+        cout << "'" << trimmed << "'" << (success ?" SUCCESS":"FAILED") << endl;
+    }
+    return success?1:0;    
+}
+
