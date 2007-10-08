86c216c3a0a4846d01e6ff498521fa7c9168d209
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2007-10-08 13:36:49 +0000

    Added property file handler, configuration handler and implemented Cell class to automate solver creation.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@201 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: ee7cfe0a9f750a078bd254ef69a814ebdc82f754 [formerly ee7cfe0a9f750a078bd254ef69a814ebdc82f754 [formerly cc1e88347daffc65411c1513cb7082ccd23dff3d]]
    Former-commit-id: cdf3eafca6d51f7948b2ee950962e2baabff2d61
    Former-commit-id: c305401f1e107f83c8ad22135d184d4e642cea49

---
+++ maindir/init.h
@@ -0,0 +1,26 @@
+/*******************************************************************
+ * File:            init.h
+ * Description:     Functions to do initialization for moose
+ * Author:          Subhasis Ray
+ * E-mail:          ray.subhasis@gmail.com
+ * Created:         2007-09-25 14:52:47
+ ********************************************************************/
+#ifndef _INIT_H
+#define _INIT_H
+#include <iostream>
+#include <basecode/header.h>
+#include <basecode/moose.h>
+#include <element/Neutral.h>
+#include <basecode/IdManager.h>
+#ifdef USE_MPI
+#include <mpi.h>
+#endif
+int mooseInit(std::string configFile);
+void setupDefaultSchedule(
+    Element* t0, Element* t1, 
+    Element* t2, Element* t3, 
+    Element* t4, Element* t5,
+    Element* cj);
+void initSolvers(Element *clockJob);
+
+#endif
