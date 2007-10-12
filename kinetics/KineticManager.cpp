2e8e6229f2e29c2111860ca81e9e75300868482c
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2007-10-12 17:15:42 +0000

    Implemented shell of KineticManager class, designed to manage the solution engine for kinetic simulations.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@204 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 56197aa6c32eb0d65aa479df40e823e3c6092cd6

---
+++ kinetics/KineticManager.cpp
@@ -0,0 +1,301 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment,
+** also known as GENESIS 3 base code.
+**           copyright (C) 2003-2007 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+
+#include "moose.h"
+#include <math.h>
+#include "KineticManager.h"
+
+const Cinfo* initKineticManagerCinfo()
+{
+	/*
+	static Finfo* processShared[] =
+	{
+		new DestFinfo( "process", Ftype1< ProcInfo >::global(),
+			RFCAST( &Molecule::processFunc ) ),
+		new DestFinfo( "reinit", Ftype1< ProcInfo >::global(),
+			RFCAST( &Molecule::reinitFunc ) ),
+	};
+	static Finfo* process = new SharedFinfo( "process", processShared,
+		sizeof( processShared ) / sizeof( Finfo* ) );
+
+	static Finfo* reacShared[] =
+	{
+		new DestFinfo( "reac", Ftype2< double, double >::global(),
+			RFCAST( &Molecule::reacFunc ) ),
+		new SrcFinfo( "n", Ftype1< double >::global() )
+	};
+	*/
+
+	static Finfo* kineticManagerFinfos[] =
+	{
+	///////////////////////////////////////////////////////
+	// Field definitions
+	///////////////////////////////////////////////////////
+		new ValueFinfo( "auto", 
+			ValueFtype1< bool >::global(),
+			GFCAST( &KineticManager::getAuto ), 
+			RFCAST( &KineticManager::setAuto ) 
+		),
+		new ValueFinfo( "stochastic", 
+			ValueFtype1< bool >::global(),
+			GFCAST( &KineticManager::getStochastic ), 
+			RFCAST( &KineticManager::setStochastic )
+		),
+		new ValueFinfo( "spatial", 
+			ValueFtype1< bool >::global(),
+			GFCAST( &KineticManager::getSpatial ), 
+			RFCAST( &KineticManager::setSpatial )
+		),
+		new ValueFinfo( "method", 
+			ValueFtype1< string >::global(),
+			GFCAST( &KineticManager::getMethod ), 
+			RFCAST( &KineticManager::setMethod )
+		),
+	///////////////////////////////////////////////////////
+	// MsgSrc definitions
+	///////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////
+	// MsgDest definitions
+	///////////////////////////////////////////////////////
+	
+	///////////////////////////////////////////////////////
+	// Synapse definitions
+	///////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////
+	// Shared definitions
+	///////////////////////////////////////////////////////
+		/*
+		process,
+		new SharedFinfo( "reac", reacShared,
+			sizeof( reacShared ) / sizeof( Finfo* ) ),
+		*/
+	};
+
+	// Schedule molecules stage 0.
+	// static SchedInfo schedInfo[] = { { process, 1, 0 } };
+	
+	static Cinfo kineticManagerCinfo(
+		"KineticManager",
+		"Upinder S. Bhalla, 2007, NCBS",
+		"Kinetic Manager: Handles integration methods for kinetic simulations.",
+		initNeutralCinfo(),
+		kineticManagerFinfos,
+		sizeof( kineticManagerFinfos )/sizeof(Finfo *),
+		ValueFtype1< KineticManager >::global()
+	);
+
+	return &kineticManagerCinfo;
+}
+
+static const Cinfo* kineticManagerCinfo = initKineticManagerCinfo();
+
+static const unsigned int reacSlot =
+	initKineticManagerCinfo()->getSlotIndex( "reac.n" );
+static const unsigned int nSlot =
+	initKineticManagerCinfo()->getSlotIndex( "nSrc" );
+
+///////////////////////////////////////////////////
+// Class function definitions
+///////////////////////////////////////////////////
+
+KineticManager::KineticManager()
+	:
+	auto_( 1 ), 
+	stochastic_( 0 ),
+	spatial_( 0 ),
+	method_( "RK5" )
+{
+		;
+}
+
+///////////////////////////////////////////////////
+// Field function definitions
+///////////////////////////////////////////////////
+
+void KineticManager::setAuto( const Conn& c, bool value )
+{
+	static_cast< KineticManager* >( c.data() )->auto_ = value;
+}
+
+bool KineticManager::getAuto( const Element* e )
+{
+	return static_cast< KineticManager* >( e->data() )->auto_;
+}
+
+void KineticManager::setStochastic( const Conn& c, bool value )
+{
+	static_cast< KineticManager* >( c.data() )->stochastic_ = value;
+}
+
+bool KineticManager::getStochastic( const Element* e )
+{
+	return static_cast< KineticManager* >( e->data() )->stochastic_;
+}
+
+void KineticManager::setSpatial( const Conn& c, bool value )
+{
+	static_cast< KineticManager* >( c.data() )->spatial_ = value;
+}
+
+bool KineticManager::getSpatial( const Element* e )
+{
+	return static_cast< KineticManager* >( e->data() )->spatial_;
+}
+
+void KineticManager::setMethod( const Conn& c, string value )
+{
+	static_cast< KineticManager* >( c.data() )->method_ = value;
+}
+
+string KineticManager::getMethod( const Element* e )
+{
+	return static_cast< KineticManager* >( e->data() )->method_;
+}
+
+///////////////////////////////////////////////////
+// Dest function definitions
+///////////////////////////////////////////////////
+
+
+/*
+
+void KineticManager::sumProcessFuncLocal( )
+{
+		n_ = total_;
+		total_ = 0.0;
+}
+void KineticManager::sumProcessFunc( const Conn& c, ProcInfo info )
+{
+	static_cast< KineticManager* >( c.data() )->sumProcessFuncLocal();
+}
+
+void KineticManager::reinitFunc( const Conn& c, ProcInfo info )
+{
+	static_cast< KineticManager* >( c.data() )->reinitFuncLocal( 
+					c.targetElement() );
+}
+void KineticManager::reinitFuncLocal( Element* e )
+{
+	static const Finfo* sumTotFinfo = 
+			Cinfo::find( "KineticManager" )->findFinfo( "sumTotal" );
+
+	A_ = B_ = total_ = 0.0;
+	n_ = nInit_;
+	if ( mode_ == 0 && sumTotFinfo->numIncoming( e ) > 0 )
+		mode_ = 1;
+	else if ( mode_ == 1 && sumTotFinfo->numIncoming( e ) == 0 )
+		mode_ = 0;
+	send1< double >( e, reacSlot, n_ );
+	send1< double >( e, nSlot, n_ );
+}
+
+void KineticManager::processFunc( const Conn& c, ProcInfo info )
+{
+	Element* e = c.targetElement();
+	static_cast< KineticManager* >( e->data() )->processFuncLocal( e, info );
+}
+void KineticManager::processFuncLocal( Element* e, ProcInfo info )
+{
+			if ( mode_ == 0 ) {
+				if ( n_ > EPSILON && B_ > EPSILON ) {
+					double C = exp( -B_ * info->dt_ / n_ );
+					n_ *= C + ( A_ / B_ ) * ( 1.0 - C );
+				} else {
+					n_ += ( A_ - B_ ) * info->dt_;
+				}
+				A_ = B_ = 0.0;
+			} else if ( mode_ == 1 ) {
+				n_ = total_;
+				total_ = 0.0;
+			} else if ( mode_ == 2 ) {
+				n_ = total_ * volumeScale_;
+				total_ = 0.0;
+			} else { 
+				n_ = nInit_;
+			}
+			send1< double >( e, reacSlot, n_ );
+			send1< double >( e, nSlot, n_ );
+}
+*/
+
+
+
+#ifdef DO_UNIT_TESTS
+/*
+#include "../element/Neutral.h"
+#include "Reaction.h"
+
+void testKineticManager()
+{
+	cout << "\nTesting KineticManager" << flush;
+
+	Element* n = Neutral::create( "Neutral", "n", Element::root() );
+	Element* m0 = Neutral::create( "KineticManager", "m0", n );
+	ASSERT( m0 != 0, "creating kineticManager" );
+	Element* m1 = Neutral::create( "KineticManager", "m1", n );
+	ASSERT( m1 != 0, "creating kineticManager" );
+	Element* r0 = Neutral::create( "Reaction", "r0", n );
+	ASSERT( r0 != 0, "creating reaction" );
+
+	bool ret;
+
+	ProcInfoBase p;
+	Conn cm0( m0, 0 );
+	Conn cm1( m1, 0 );
+	Conn cr0( r0, 0 );
+	p.dt_ = 0.001;
+	set< double >( m0, "concInit", 1.0 );
+	set< int >( m0, "mode", 0 );
+	set< double >( m1, "concInit", 0.0 );
+	set< int >( m1, "mode", 0 );
+	set< double >( r0, "kf", 0.1 );
+	set< double >( r0, "kb", 0.1 );
+	ret = m0->findFinfo( "reac" )->add( m0, r0, r0->findFinfo( "sub" ) );
+	ASSERT( ret, "adding msg 0" );
+	ret = m1->findFinfo( "reac" )->add( m1, r0, r0->findFinfo( "prd" ) );
+	ASSERT( ret, "adding msg 1" );
+
+	// First, test charging curve for a single compartment
+	// We want our charging curve to be a nice simple exponential
+	// n = 0.5 + 0.5 * exp( - t * 0.2 );
+	double delta = 0.0;
+	double n0 = 1.0;
+	double n1 = 0.0;
+	double y = 0.0;
+	double y0 = 0.0;
+	double y1 = 0.0;
+	double tau = 5.0;
+	double nMax = 0.5;
+	Reaction::reinitFunc( cr0, &p );
+	KineticManager::reinitFunc( cm0, &p );
+	KineticManager::reinitFunc( cm1, &p );
+	for ( p.currTime_ = 0.0; p.currTime_ < 20.0; p.currTime_ += p.dt_ ) 
+	{
+		n0 = KineticManager::getN( m0 );
+		n1 = KineticManager::getN( m1 );
+//		cout << p.currTime_ << "	" << n1 << endl;
+
+		y = nMax * exp( -p.currTime_ / tau );
+		y0 = 0.5 + y;
+		y1 = 0.5 - y;
+		delta += ( n0 - y0 ) * ( n0 - y0 );
+		delta += ( n1 - y1 ) * ( n1 - y1 );
+		Reaction::processFunc( cr0, &p );
+		KineticManager::processFunc( cm0, &p );
+		KineticManager::processFunc( cm1, &p );
+	}
+	ASSERT( delta < 5.0e-6, "Testing kineticManager and reacn" );
+
+	// Get rid of all the compartments.
+	set( n, "destroy" );
+}
+*/
+#endif
