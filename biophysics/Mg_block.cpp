60203495c2945877b6156c8bc078d6445a01bd5f
raameshd <raameshd@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2007-10-16 15:32:20 +0000

    I had forgotten to add files in the last commit. So I have added it. Also I removed some of the warnings and errors.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@211 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 176b0a21b99a73b913d513638f732ad5beb3e83f

---
+++ biophysics/Mg_block.cpp
@@ -0,0 +1,201 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2007 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+#include <math.h>
+#include "moose.h"
+#include "Mg_block.h"
+#include "../element/Neutral.h"
+// #include "DeletionMarkerFinfo.h"
+// #include "GlobalMarkerFinfo.h"
+
+const Cinfo* initMg_blockCinfo()
+{
+	static Finfo* processShared[] =
+	{
+		new DestFinfo( "process", Ftype1< ProcInfo >::global(),
+				RFCAST( &Mg_block::processFunc ) ),
+		new DestFinfo( "reinit", Ftype1< ProcInfo >::global(),
+				RFCAST( &Mg_block::reinitFunc ) ),
+	};
+	static Finfo* process =	new SharedFinfo( "process", processShared, 
+			sizeof( processShared ) / sizeof( Finfo* ) );
+	static Finfo* channelShared[] =
+	{
+		new SrcFinfo( "channel", Ftype2< double, double >::global() ),
+		new DestFinfo( "Vm", Ftype1< double >::global(), 
+				RFCAST( &Mg_block::channelFunc ) ),
+	};
+	static Finfo* Mg_blockFinfos[] =
+	{
+		new ValueFinfo( "KMg_A", ValueFtype1< double >::global(),
+			GFCAST( &Mg_block::getKMg_A ), 
+			RFCAST( &Mg_block::setKMg_A )
+		),
+		new ValueFinfo( "KMg_B", ValueFtype1< double >::global(),
+			GFCAST( &Mg_block::getKMg_B ), 
+			RFCAST( &Mg_block::setKMg_B )
+		),
+		new ValueFinfo( "CMg", ValueFtype1< double >::global(),
+			GFCAST( &Mg_block::getCMg ), 
+			RFCAST( &Mg_block::setCMg )
+		),
+		new ValueFinfo( "Ik", ValueFtype1< double >::global(),
+			GFCAST( &Mg_block::getIk ), 
+			RFCAST( &Mg_block::setIk )
+		),
+		new ValueFinfo( "Gk", ValueFtype1< double >::global(),
+			GFCAST( &Mg_block::getGk ), 
+			RFCAST( &Mg_block::setGk )
+		),
+		new ValueFinfo( "Ek", ValueFtype1< double >::global(),
+			GFCAST( &Mg_block::getEk ), 
+			RFCAST( &Mg_block::setEk )
+		),
+		new ValueFinfo( "Zk", ValueFtype1< double >::global(),
+			GFCAST( &Mg_block::getZk ), 
+			RFCAST( &Mg_block::setZk )
+		),
+		process,
+		new SharedFinfo( "process", processShared, 
+			sizeof( processShared ) / sizeof( Finfo* ) ),
+		new SharedFinfo( "channel", channelShared,
+			sizeof( channelShared ) / sizeof( Finfo* ) ),
+		new DestFinfo( "origChannel", Ftype2< double, double >::global(),
+				RFCAST( &Mg_block::origChannelFunc ) ),
+	};
+
+	static SchedInfo schedInfo[] = { { process, 0, 1 } };
+
+	static Cinfo Mg_blockCinfo(
+		"Mg_block",
+		"Upinder S. Bhalla, 2007, NCBS",
+		"Mg_block: Hodgkin-Huxley type voltage-gated Ion channel. Something\nlike the old tabchannel from GENESIS, but also presents\na similar interface as hhchan from GENESIS. ",
+		initNeutralCinfo(),
+		Mg_blockFinfos,
+		sizeof( Mg_blockFinfos )/sizeof(Finfo *),
+		ValueFtype1< Mg_block >::global(),
+		schedInfo, 1
+	);
+
+	return &Mg_blockCinfo;
+}
+
+static const Cinfo* Mg_blockCinfo = initMg_blockCinfo();
+
+static const unsigned int channelSlot =
+	initMg_blockCinfo()->getSlotIndex( "channel.channel" );
+
+
+///////////////////////////////////////////////////
+// Field function definitions
+///////////////////////////////////////////////////
+
+void Mg_block::setKMg_A( const Conn& c, double KMg_A )
+{
+	static_cast< Mg_block* >( c.data() )->KMg_A_ = KMg_A;
+}
+double Mg_block::getKMg_A( const Element* e )
+{
+	return static_cast< Mg_block* >( e->data() )->KMg_A_;
+}
+void Mg_block::setKMg_B( const Conn& c, double KMg_B )
+{
+	static_cast< Mg_block* >( c.data() )->KMg_B_ = KMg_B;
+}
+double Mg_block::getKMg_B( const Element* e )
+{
+	return static_cast< Mg_block* >( e->data() )->KMg_B_;
+}
+void Mg_block::setCMg( const Conn& c, double CMg )
+{
+	static_cast< Mg_block* >( c.data() )->CMg_ = CMg;
+}
+double Mg_block::getCMg( const Element* e )
+{
+	return static_cast< Mg_block* >( e->data() )->CMg_;
+}
+void Mg_block::setIk( const Conn& c, double Ik )
+{
+	static_cast< Mg_block* >( c.data() )->Ik_ = Ik;
+}
+double Mg_block::getIk( const Element* e )
+{
+	return static_cast< Mg_block* >( e->data() )->Ik_;
+}
+void Mg_block::setGk( const Conn& c, double Gk )
+{
+	static_cast< Mg_block* >( c.data() )->Gk_ = Gk;
+}
+double Mg_block::getGk( const Element* e )
+{
+	return static_cast< Mg_block* >( e->data() )->Gk_;
+}
+void Mg_block::setEk( const Conn& c, double Ek )
+{
+	static_cast< Mg_block* >( c.data() )->Ek_ = Ek;
+}
+double Mg_block::getEk( const Element* e )
+{
+	return static_cast< Mg_block* >( e->data() )->Ek_;
+}
+double Mg_block::getZk( const Element* e )
+{
+	return static_cast< Mg_block* >( e->data() )->Zk_;
+}
+void Mg_block::setZk( const Conn& c, double Zk )
+{
+	static_cast< Mg_block* >( c.data() )->Zk_ = Zk;
+}
+
+
+void Mg_block::processFunc( const Conn& c, ProcInfo p )
+{
+	Element* e = c.targetElement();
+	static_cast< Mg_block* >( e->data() )->innerProcessFunc( e, p );
+}
+
+void Mg_block::innerProcessFunc( Element* e, ProcInfo info )
+{
+	
+	double KMg = KMg_A_ * exp(Vm_/KMg_B_);
+	Gk_ = Gk_ * KMg / (KMg + CMg_);
+	send2< double, double >( e, channelSlot, Gk_, Ek_ );
+	Ik_ = Gk_ * (Ek_ - Vm_);
+}
+
+void Mg_block::reinitFunc( const Conn& c, ProcInfo p )
+{
+	Element* e = c.targetElement();
+	static_cast< Mg_block* >( e->data() )->innerReinitFunc( e, p );
+}
+
+void Mg_block::innerReinitFunc( Element* e, ProcInfo info )
+{
+	Zk_ = 0;
+	CMg_ = 0;
+	KMg_A_ = 0;
+	KMg_B_ = 0;
+}
+
+void Mg_block::channelFunc( const Conn& c, double Vm )
+{
+	static_cast< Mg_block* >( c.data() )->Vm_ = Vm;
+}
+
+void Mg_block::origChannelFunc( const Conn& c, double Gk, double Ek )
+{
+	Mg_block *e = static_cast< Mg_block* >( c.data() );
+	e->Gk_ = Gk;
+	e->Ek_ = Ek;
+}
+
+///////////////////////////////////////////////////
+// Unit tests
+///////////////////////////////////////////////////
+
---
+++ biophysics/Mg_block.h
@@ -0,0 +1,106 @@
+#ifndef _Mg_block_h
+#define _Mg_block_h
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment,
+** also known as GENESIS 3 base code.
+**           copyright (C) 2003-2005 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+*********************************************************************
+*/
+
+//typedef double ( *PFDD )( double, double );
+
+class Mg_block
+{
+	public:
+		Mg_block()
+			: Zk_( 0.0 ), KMg_A_( 0.0 ), KMg_B_( 0.0 ),
+			CMg_( 0.0 )
+		{
+			;
+		}
+
+		/////////////////////////////////////////////////////////////
+		// Value field access function definitions
+		/////////////////////////////////////////////////////////////
+
+		static void setKMg_A( const Conn& c, double Gbar );
+		static double getKMg_A( const Element* );
+		static void setKMg_B( const Conn& c, double Ek );
+		static double getKMg_B( const Element* );
+		static void setCMg( const Conn& c, double CMg );
+		static double getCMg( const Element* );
+		static void setIk( const Conn& c, double Ik );
+		static double getIk( const Element* );
+		static void setGk( const Conn& c, double Gk );
+		static double getGk( const Element* );
+		static void setEk( const Conn& c, double Ek );
+		static double getEk( const Element* );
+		static void setZk( const Conn& c, double Zk );
+		static double getZk( const Element* );
+		/////////////////////////////////////////////////////////////
+		// Dest function definitions
+		/////////////////////////////////////////////////////////////
+
+		/**
+		 * processFunc handles the update and calculations every
+		 * clock tick. It first sends the request for evaluation of
+		 * the gate variables to the respective gate objects and
+		 * recieves their response immediately through a return 
+		 * message. This is done so that many channel instances can
+		 * share the same gate lookup tables, but do so cleanly.
+		 * Such messages should never go to a remote node.
+		 * Then the function does its own little calculations to
+		 * send back to the parent compartment through regular 
+		 * messages.
+		 */
+		static void processFunc( const Conn& c, ProcInfo p );
+		void innerProcessFunc( Element* e, ProcInfo p );
+
+		/**
+		 * Reinitializes the values for the channel. This involves
+		 * computing the steady-state value for the channel gates
+		 * using the provided Vm from the parent compartment. It 
+		 * involves a similar cycle through the gates and then 
+		 * updates to the parent compartment as for the processFunc.
+		 */
+		static void reinitFunc( const Conn& c, ProcInfo p );
+		void innerReinitFunc( Element* e, ProcInfo p );
+
+		/**
+		 * Assign the local Vm_ to the incoming Vm from the compartment
+		 */
+		static void channelFunc( const Conn& c, double Vm );
+		static void origChannelFunc( const Conn& c, double Gk, double Ek );
+		
+		/**
+		 * Assign the local conc_ to the incoming conc from the
+		 * concentration calculations for the compartment. Typically
+		 * the message source will be a CaConc object, but there
+		 * are other options for computing the conc.
+		 */
+		static void concFunc( const Conn& c, double conc );
+
+
+	private:
+		/// charge
+		double Zk_;
+		/// 1/eta
+		double KMg_A_;
+		/// 1/gamma
+		double KMg_B_;
+		/// [Mg] in mM
+		double CMg_;
+		double Ik_;
+		double Gk_;
+		double Ek_;
+		double Vm_;
+};
+
+// Used by the solver
+//extern const Cinfo* initMg_blockCinfo();
+
+#endif // _Mg_block_h
