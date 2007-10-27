2c5fb207f27c2b194ac4ca4b3832efee45f1d0b5
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2007-10-27 17:03:26 +0000

    First steps toward setting up an interface to the Smoldyn single-particle 3-D Monte Carlo simulator by Steven Andrews and Dennis Bray. Implemented Particle class, derived from Molecule, to represent a population of individual molecules with 3-D positions Implemented skeleton of SmoldynHub.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@231 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 9e1c38e187e678da0139550dbc7b6d0b94bd438e [formerly 9e1c38e187e678da0139550dbc7b6d0b94bd438e [formerly 7039e7ef54b2298a4a831d281f78bc7f81f647fd]]
    Former-commit-id: 10f6957882f63b753d28c6d0a3d8c610505a5e9d
    Former-commit-id: 4787e63da53cda9823f4a72002ad5909d73ab4e4

---
+++ kinetics/SmoldynHub.cpp
@@ -0,0 +1,210 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment,
+** also known as GENESIS 3 base code.
+**           copyright (C) 2003-2007 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+
+#include "moose.h"
+#include "Smoldyn/source/smollib.h"
+#include "SmoldynHub.h"
+#include "ThisFinfo.h"
+#include "SolveFinfo.h"
+
+
+const Cinfo* initSmoldynHubCinfo()
+{
+	static Finfo* processShared[] =
+	{
+		new DestFinfo( "process", Ftype1< ProcInfo >::global(),
+			RFCAST( &SmoldynHub::processFunc ) ),
+		new DestFinfo( "reinit", Ftype1< ProcInfo >::global(),
+			RFCAST( &SmoldynHub::reinitFunc ) ),
+	};
+	static Finfo* process = new SharedFinfo( "process", processShared,
+		sizeof( processShared ) / sizeof( Finfo* ) );
+
+
+	static Finfo* smoldynHubFinfos[] =
+	{
+	///////////////////////////////////////////////////////
+	// Field definitions
+	///////////////////////////////////////////////////////
+		new ValueFinfo( "nSpecies", 
+			ValueFtype1< unsigned int >::global(),
+			GFCAST( &SmoldynHub::getNspecies ), 
+			&dummyFunc
+		),
+	///////////////////////////////////////////////////////
+	// MsgSrc definitions
+	///////////////////////////////////////////////////////
+	//	new SrcFinfo( "nSrc", Ftype1< double >::global() ),
+	///////////////////////////////////////////////////////
+	// MsgDest definitions
+	///////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////
+	// Synapse definitions
+	///////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////
+	// Shared definitions
+	///////////////////////////////////////////////////////
+		process,
+	};
+
+	// Schedule smoldynHubs for the slower clock, stage 0.
+	static SchedInfo schedInfo[] = { { process, 0, 0 } };
+	
+	static Cinfo smoldynHubCinfo(
+		"SmoldynHub",
+		"Upinder S. Bhalla, 2007, NCBS",
+		"SmoldynHub: Interface object between Smoldyn (by Steven Andrews) and MOOSE.",
+		initNeutralCinfo(),
+		smoldynHubFinfos,
+		sizeof( smoldynHubFinfos )/sizeof(Finfo *),
+		ValueFtype1< SmoldynHub >::global(),
+			schedInfo, 1
+	);
+
+	return &smoldynHubCinfo;
+}
+
+static const Cinfo* smoldynHubCinfo = initSmoldynHubCinfo();
+
+const Finfo* SmoldynHub::particleFinfo = 
+	initSmoldynHubCinfo()->findFinfo( "particleFinfo" );
+
+/*
+static const unsigned int reacSlot =
+	initSmoldynHubCinfo()->getSlotIndex( "reac.n" );
+static const unsigned int nSlot =
+	initSmoldynHubCinfo()->getSlotIndex( "nSrc" );
+*/
+
+///////////////////////////////////////////////////
+// Class function definitions
+///////////////////////////////////////////////////
+
+SmoldynHub::SmoldynHub()
+{
+		;
+}
+
+///////////////////////////////////////////////////
+// Hub utility function definitions
+///////////////////////////////////////////////////
+
+/**
+ * Looks up the solver from the zombie element e. Returns the solver
+ * element, or null on failure. 
+ * It needs the originating Finfo on the solver that connects to the zombie,
+ * as the srcFinfo.
+ * Also passes back the index of the zombie element on this set of
+ * messages. This is NOT the absolute Conn index.
+ */
+SmoldynHub* SmoldynHub::getHubFromZombie( 
+	const Element* e, const Finfo* srcFinfo, unsigned int& index )
+{
+	const SolveFinfo* f = dynamic_cast< const SolveFinfo* > (
+			       	e->getThisFinfo() );
+	if ( !f ) return 0;
+	const Conn& c = f->getSolvedConn( e );
+	unsigned int slot;
+       	srcFinfo->getSlotIndex( srcFinfo->name(), slot );
+	Element* hub = c.targetElement();
+	index = hub->connSrcRelativeIndex( c, slot );
+	return static_cast< SmoldynHub* >( hub->data() );
+}
+
+void SmoldynHub::setPos( unsigned int molIndex, double value, 
+			unsigned int i, unsigned int dim )
+{
+}
+
+double SmoldynHub::getPos( unsigned int molIndex, unsigned int i, 
+			unsigned int dim )
+{
+	return 0.0;
+}
+
+void SmoldynHub::setPosVector( unsigned int molIndex, 
+			const vector< double >& value, unsigned int dim )
+{
+}
+
+void SmoldynHub::getPosVector( unsigned int molIndex,
+			vector< double >& value, unsigned int dim )
+{
+}
+
+void SmoldynHub::setNinit( unsigned int molIndex, unsigned int value )
+{
+}
+
+unsigned int SmoldynHub::getNinit( unsigned int molIndex )
+{
+	return 0;
+}
+
+void SmoldynHub::setNmol( unsigned int molIndex, unsigned int value )
+{
+	;
+}
+
+unsigned int SmoldynHub::getNmol( unsigned int molIndex )
+{
+	return 0;
+}
+
+void SmoldynHub::setD( unsigned int molIndex, double value )
+{
+}
+
+double SmoldynHub::getD( unsigned int molIndex )
+{
+	return 0.0;
+}
+
+
+///////////////////////////////////////////////////
+// Field function definitions
+///////////////////////////////////////////////////
+
+unsigned int SmoldynHub::getNspecies( const Element* e )
+{
+	return static_cast< SmoldynHub* >( e->data() )->numSpecies();
+}
+
+unsigned int SmoldynHub::numSpecies() const
+{
+	return 0;
+}
+
+
+///////////////////////////////////////////////////
+// Dest function definitions
+///////////////////////////////////////////////////
+
+void SmoldynHub::reinitFunc( const Conn& c, ProcInfo info )
+{
+	static_cast< SmoldynHub* >( c.data() )->reinitFuncLocal( 
+					c.targetElement() );
+}
+
+void SmoldynHub::reinitFuncLocal( Element* e )
+{
+	// do stuff here.
+}
+
+void SmoldynHub::processFunc( const Conn& c, ProcInfo info )
+{
+	Element* e = c.targetElement();
+	static_cast< SmoldynHub* >( e->data() )->processFuncLocal( e, info );
+}
+void SmoldynHub::processFuncLocal( Element* e, ProcInfo info )
+{
+	// do stuff here
+}
