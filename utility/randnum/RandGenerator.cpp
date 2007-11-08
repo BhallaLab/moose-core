76ef85b28a810cb4e6c8be0b91b8b3a3b1601e12
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2007-11-08 05:03:37 +0000

    Created MOOSE interface for Poisson distributed RNG. The moose interfaces to various RNGs are now arranged in a hierarchy with RandGenerator as the base class.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@243 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 9eef0e2d2fdc10222597c9083f1c6642bfa9b23a

--- utility/randnum/RandGenerator.cpp
+++ utility/randnum/RandGenerator.cpp
@@ -7,17 +7,19 @@
  * Created:         2007-11-03 21:48:17
  ********************************************************************/
 /**********************************************************************
-** This program is part of 'MOOSE', the
-** Messaging Object Oriented Simulation Environment,
-** also known as GENESIS 3 base code.
-**           copyright (C) 2003-2005 Upinder S. Bhalla. and NCBS
-** It is made available under the terms of the
-** GNU General Public License version 2
-** See the file COPYING.LIB for the full notice.
-**********************************************************************/
+ ** This program is part of 'MOOSE', the
+ ** Messaging Object Oriented Simulation Environment,
+ ** also known as GENESIS 3 base code.
+ **           copyright (C) 2003-2005 Upinder S. Bhalla. and NCBS
+ ** It is made available under the terms of the
+ ** GNU General Public License version 2
+ ** See the file COPYING.LIB for the full notice.
+ **********************************************************************/
 
 #ifndef _RANDGENERATOR_CPP
 #define _RANDGENERATOR_CPP
+#include "RandGenerator.h"
+
 const Cinfo * initRandGeneratorCinfo()
 {
     static Finfo* processShared[] = 
@@ -29,8 +31,69 @@ const Cinfo * initRandGeneratorCinfo()
         };
     static Finfo* process = new SharedFinfo("process", processShared,
                                             sizeof(processShared)/sizeof(Finfo*));
+    static Finfo* randGeneratorFinfos[] =
+        {
+            process,
+            new SrcFinfo("output", Ftype1<double>::global()),
+        };
     
-    
+/*    
+    static SchedInfo schedInfo[] = 
+        {
+            {
+                process, 0, 0
+            },
+        };
+*/    
+    static Cinfo randGeneratorCinfo("RandGenerator",
+                                    "Subhasis Ray",
+                                    "Base class for random number generator.",
+                                    initNeutralCinfo(),
+                                    randGeneratorFinfos,
+                                    sizeof(randGeneratorFinfos)/sizeof(Finfo*),
+                                    ValueFtype1<RandGenerator>::global());
+    return &randGeneratorCinfo;
 }
+
+static const unsigned int outputSlot = initRandGeneratorCinfo()->getSlotIndex("output");
+
+RandGenerator::RandGenerator()
+{
+    rng_ = 0;    
+}
+
+RandGenerator::~RandGenerator()
+{
+    if (rng_)
+    {
+        delete rng_;
+    }    
+}
+
+double RandGenerator::getMean(const Element* e)
+{
+    return static_cast<RandGenerator*>(e->data())->rng_->getMean();
+}
+
+double RandGenerator::getVariance(const Element* e)
+{
+    return static_cast<RandGenerator*>(e->data())->rng_->getVariance();    
+}
+
+double RandGenerator::getSample(const Element* e)
+{
+    return static_cast<RandGenerator*>( e->data())->rng_->getNextSample();
+}
+
+void RandGenerator::processFunc( const Conn& c, ProcInfo info )
+{
+    send1<double>(c.targetElement(), outputSlot, getSample(c.targetElement()));    
+}
+
+void RandGenerator::reinitFunc(const Conn& c, ProcInfo info)
+{
+    cerr << "ERROR: RandGenerator::reinitFunc - this function should never be reached." << endl;
+}
+
 
 #endif
