1c3a14f6edc90afd247e55770de62210d5e3225f
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2007-11-09 12:35:06 +0000

    Added python interface for classes to sample random numbers from Binomial, Exponential, Gamma, Normal and Poisson distribution.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@248 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: dd3c004559f4888d21aa7443ccc08ef2cd111d19

---
+++ pymoose/GammaRng.cpp
@@ -0,0 +1,31 @@
+#ifndef _pymoose_GammaRng_cpp
+#define _pymoose_GammaRng_cpp
+#include "GammaRng.h"
+const std::string GammaRng::className = "GammaRng";
+GammaRng::GammaRng(Id id):RandGenerator(id){}
+GammaRng::GammaRng(std::string path):RandGenerator(className, path){}
+GammaRng::GammaRng(std::string name, Id parentId):RandGenerator(className, name, parentId){}
+GammaRng::GammaRng(std::string name, PyMooseBase* parent):RandGenerator(className, name, parent){}
+GammaRng::~GammaRng(){}
+const std::string& GammaRng::getType(){ return className; }
+double GammaRng::__get_alpha() const
+{
+    double alpha;
+    get < double > (id_(), "alpha",alpha);
+    return alpha;
+}
+void GammaRng::__set_alpha( double alpha )
+{
+    set < double > (id_(), "alpha", alpha);
+}
+double GammaRng::__get_theta() const
+{
+    double theta;
+    get < double > (id_(), "theta",theta);
+    return theta;
+}
+void GammaRng::__set_theta( double theta )
+{
+    set < double > (id_(), "theta", theta);
+}
+#endif
---
+++ pymoose/PoissonRng.h
@@ -0,0 +1,16 @@
+#ifndef _pymoose_PoissonRng_h
+#define _pymoose_PoissonRng_h
+#include "RandGenerator.h"
+class PoissonRng : public RandGenerator
+{    public:
+        static const std::string className;
+        PoissonRng(Id id);
+        PoissonRng(std::string path);
+        PoissonRng(std::string name, Id parentId);
+        PoissonRng(std::string name, PyMooseBase* parent);
+        ~PoissonRng();
+        const std::string& getType();
+        double __get_mean() const;
+        void __set_mean(double mean);
+};
+#endif
