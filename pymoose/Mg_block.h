f65c9a4f3fca70c9bdeb1c5d227d72f45b00a087
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2007-11-09 12:35:06 +0000

    Added python interface for classes to sample random numbers from Binomial, Exponential, Gamma, Normal and Poisson distribution.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@248 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ pymoose/Mg_block.h
@@ -0,0 +1,30 @@
+#ifndef _pymoose_Mg_block_h
+#define _pymoose_Mg_block_h
+#include "PyMooseBase.h"
+class Mg_block : public PyMooseBase
+{    public:
+        static const std::string className;
+        Mg_block(Id id);
+        Mg_block(std::string path);
+        Mg_block(std::string name, Id parentId);
+        Mg_block(std::string name, PyMooseBase* parent);
+        ~Mg_block();
+        const std::string& getType();
+        double __get_KMg_A() const;
+        void __set_KMg_A(double KMg_A);
+        double __get_KMg_B() const;
+        void __set_KMg_B(double KMg_B);
+        double __get_CMg() const;
+        void __set_CMg(double CMg);
+        double __get_Ik() const;
+        void __set_Ik(double Ik);
+        double __get_Gk() const;
+        void __set_Gk(double Gk);
+        double __get_Ek() const;
+        void __set_Ek(double Ek);
+        double __get_Zk() const;
+        void __set_Zk(double Zk);
+        double,double __get_origChannel() const;
+        void __set_origChannel(double,double origChannel);
+};
+#endif
---
+++ pymoose/RandGenerator.cpp
@@ -0,0 +1,51 @@
+#ifndef _pymoose_RandGenerator_cpp
+#define _pymoose_RandGenerator_cpp
+#include "RandGenerator.h"
+const std::string RandGenerator::className = "RandGenerator";
+RandGenerator::RandGenerator(Id id):PyMooseBase(id){}
+RandGenerator::RandGenerator(string className, std::string path):PyMooseBase(className, path){}
+RandGenerator::RandGenerator(string className, std::string name, Id parentId):PyMooseBase(className, name, parentId){}
+RandGenerator::RandGenerator(string className, std::string name, PyMooseBase* parent):PyMooseBase(className, name, parent){}
+//RandGenerator::~RandGenerator(){}
+const std::string& RandGenerator::getType(){ return className; }
+double RandGenerator::__get_sample() const
+{
+    double sample;
+    get < double > (id_(), "sample",sample);
+    return sample;
+}
+void RandGenerator::__set_sample( double sample )
+{
+    set < double > (id_(), "sample", sample);
+}
+double RandGenerator::__get_mean() const
+{
+    double mean;
+    get < double > (id_(), "mean",mean);
+    return mean;
+}
+void RandGenerator::__set_mean( double mean )
+{
+    set < double > (id_(), "mean", mean);
+}
+double RandGenerator::__get_variance() const
+{
+    double variance;
+    get < double > (id_(), "variance",variance);
+    return variance;
+}
+void RandGenerator::__set_variance( double variance )
+{
+    set < double > (id_(), "variance", variance);
+}
+double RandGenerator::__get_output() const
+{
+    double output;
+    get < double > (id_(), "output",output);
+    return output;
+}
+void RandGenerator::__set_output( double output )
+{
+    set < double > (id_(), "output", output);
+}
+#endif
