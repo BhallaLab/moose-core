bbf0d058230e546235ef26b662c9c67637d516e4
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2007-11-18 19:23:54 +0000

    Added first pass at geometry classes in order to define complex 3-D surfaces for reaction systems.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@264 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 35a7f14715065cb104a2db61f6a778c86e0bb173 [formerly aa5a50c892cbf59ef89d5a2b3b67820688b615df] [formerly aa5a50c892cbf59ef89d5a2b3b67820688b615df [formerly dc853f8d6c53beadaa8d7e81542d00d229222a2d]]
    Former-commit-id: fff41c2d292605da1cd40cf2932b88497207bb06
    Former-commit-id: c486db8e1069ec6cb2cb054585d058393e7899e6

---
+++ kinetics/Panel.cpp
@@ -0,0 +1,201 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment,
+** also known as GENESIS 3 base code.
+**           copyright (C) 2003-2007 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+
+#include "moose.h"
+#include "Panel.h"
+
+const Cinfo* initPanelCinfo()
+{
+	/*
+	static Finfo* reacShared[] =
+	{
+		new DestFinfo( "reac", Ftype2< double, double >::global(),
+			RFCAST( &Panel::reacFunc ) ),
+		new SrcFinfo( "n", Ftype1< double >::global() )
+	};
+	*/
+
+	static Finfo* panelFinfos[] =
+	{
+	///////////////////////////////////////////////////////
+	// Field definitions
+	///////////////////////////////////////////////////////
+		new LookupFinfo( "x", 
+			LookupFtype< double, unsigned int >::global(),
+			GFCAST( &Panel::getX ), 
+			RFCAST( &Panel::setX ) 
+		),
+		new LookupFinfo( "y", 
+			LookupFtype< double, unsigned int >::global(),
+			GFCAST( &Panel::getY ), 
+			RFCAST( &Panel::setY ) 
+		),
+		new LookupFinfo( "z", 
+			LookupFtype< double, unsigned int >::global(),
+			GFCAST( &Panel::getZ ), 
+			RFCAST( &Panel::setZ ) 
+		),
+
+		new ValueFinfo( "nPts", 
+			ValueFtype1< unsigned int >::global(),
+			GFCAST( &Panel::getNpts ), 
+			&dummyFunc
+		),
+		new ValueFinfo( "nDims", 
+			ValueFtype1< unsigned int >::global(),
+			GFCAST( &Panel::getNdims ), 
+			&dummyFunc
+		),
+		new ValueFinfo( "nNeighbors", 
+			ValueFtype1< unsigned int >::global(),
+			GFCAST( &Panel::getNneighbors ), 
+			&dummyFunc
+		),
+	///////////////////////////////////////////////////////
+	// MsgSrc definitions
+	///////////////////////////////////////////////////////
+		new SrcFinfo( "neighborSrc", Ftype0::global() ),
+	///////////////////////////////////////////////////////
+	// MsgDest definitions
+	///////////////////////////////////////////////////////
+		new DestFinfo( "neighbor", Ftype0::global(),
+			&dummyFunc ),
+	///////////////////////////////////////////////////////
+	// Shared definitions
+	///////////////////////////////////////////////////////
+	};
+
+	static Cinfo panelCinfo(
+		"Panel",
+		"Upinder S. Bhalla, 2007, NCBS",
+		"Panel: Base class for shapes. Also serves as interface to Smoldyn panel data struct.",
+		initNeutralCinfo(),
+		panelFinfos,
+		sizeof( panelFinfos )/sizeof(Finfo *),
+		ValueFtype1< Panel >::global()
+	);
+
+	return &panelCinfo;
+}
+
+static const Cinfo* panelCinfo = initPanelCinfo();
+static const Finfo* neighborSrcFinfo = initPanelCinfo()->findFinfo( "neighborSrc" );
+static const Finfo* neighborDestFinfo = initPanelCinfo()->findFinfo( "neighbor" );
+
+static const unsigned int panelSphere = 2;
+
+///////////////////////////////////////////////////
+// Class function definitions
+///////////////////////////////////////////////////
+
+Panel::Panel( unsigned int nDims )
+	: nDims_( nDims )
+{
+		;
+}
+
+///////////////////////////////////////////////////
+// Regular field functions 
+///////////////////////////////////////////////////
+
+unsigned int Panel::getNpts( const Element* e )
+{
+	return static_cast< Panel* >( e->data() )->localNpts();
+}
+
+// Default version
+unsigned int Panel::localNpts() const {
+	return 2;  // Centre coord, and then radius, with sign.
+}
+
+unsigned int Panel::getNdims( const Element* e )
+{
+	return static_cast< Panel* >( e->data() )->nDims_;
+}
+
+unsigned int Panel::getShapeId( const Element* e )
+{
+	return static_cast< Panel* >( e->data() )->localShapeId();
+}
+
+// Default version
+unsigned int Panel::localShapeId() const {
+	return panelSphere; 
+}
+
+unsigned int Panel::getNneighbors( const Element* e )
+{
+	return neighborSrcFinfo->numIncoming( e ) + 
+		neighborDestFinfo->numIncoming( e );
+}
+
+//////////////////////////////////////////////////////////
+// Special Field function definitions to do with coords.
+//////////////////////////////////////////////////////////
+
+// We don't have dynamic shapes yet, so this update to the solver is
+// not needed. Furthermore we don't want to tie this to Smoldyn.
+void Panel::setPos( const Conn& c, double value, 
+	unsigned int i, unsigned int dim )
+{
+	static_cast< Panel* >( c.data() )->localSetPos( value, i, dim );
+}
+
+void Panel::localSetPos( double value, unsigned int i, unsigned int dim )
+{
+	assert( i * nDims_ + dim < coords_.size() );
+	coords_[ i * nDims_ + dim ] = value;
+}
+
+void Panel::setX( const Conn& c, double value, const unsigned int& i )
+{
+	setPos( c, value, i, 0 );
+}
+
+void Panel::setY( const Conn& c, double value, const unsigned int& i )
+{
+	setPos( c, value, i, 1 );
+}
+
+void Panel::setZ( const Conn& c, double value, const unsigned int& i )
+{
+	setPos( c, value, i, 2 );
+}
+
+double Panel::getPos( const Element* e, unsigned int i, unsigned int dim)
+{
+	return static_cast< Panel* >( e->data() )->localGetPos( i, dim );
+}
+
+double Panel::localGetPos( unsigned int i, unsigned int dim )
+{
+	assert( i * nDims_ + dim < coords_.size() );
+	return coords_[ i * nDims_ + dim ];
+}
+
+double Panel::getX( const Element* e, const unsigned int& i )
+{
+	return getPos( e, i, 0 );
+}
+
+double Panel::getY( const Element* e, const unsigned int& i )
+{
+	return getPos( e, i, 1 );
+}
+
+double Panel::getZ( const Element* e, const unsigned int& i )
+{
+	return getPos( e, i, 2 );
+}
+
+///////////////////////////////////////////////////
+// Dest function definitions
+///////////////////////////////////////////////////
