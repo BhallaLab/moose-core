1a07430de2860abf7c9e1609ef52131c1505b179
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2007-11-20 14:44:04 +0000

    Added six 'panel' shapes to match those in Smoldyn, but to be used within MOOSE to set up spatial signaling models natively and for other solvers.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@265 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 3ccf806a20625e0dfd00aab4461a35cbc5157352 [formerly 3ccf806a20625e0dfd00aab4461a35cbc5157352 [formerly fc94d19949e8a09ff4856ee34213157620410783]]
    Former-commit-id: 857bfcac8249511ac53b206cfc4916d2bad75156
    Former-commit-id: 2c4d5c0c44bf5e5a8b223c8143178bb779ad2099

---
+++ kinetics/DiskPanel.cpp
@@ -0,0 +1,53 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment,
+** also known as GENESIS 3 base code.
+**           copyright (C) 2003-2007 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+
+#include "moose.h"
+#include "Panel.h"
+#include "DiskPanel.h"
+
+/**
+ * DiskPanel is derived from Panel, and almost everything in this class
+ * is handled by the base class and a few virtual functions.
+ */
+const Cinfo* initDiskPanelCinfo()
+{
+	static Cinfo diskPanelCinfo(
+		"DiskPanel",
+		"Upinder S. Bhalla, 2007, NCBS",
+		"DiskPanel: Disk panel shape for portion of compartmental surface.",
+		initPanelCinfo(),
+		0,
+		0,
+		ValueFtype1< DiskPanel >::global()
+	);
+
+	return &diskPanelCinfo;
+}
+
+static const Cinfo* diskPanelCinfo = initDiskPanelCinfo();
+
+///////////////////////////////////////////////////
+// Class function definitions
+///////////////////////////////////////////////////
+
+DiskPanel::DiskPanel( unsigned int nDims )
+	: Panel( nDims )
+{
+		;
+}
+
+void DiskPanel::localFiniteElementVertices( 
+	vector< double >& ret, double area ) const
+{
+	// Fill up 'ret' here.
+	;
+}
+
