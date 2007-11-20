eb5bc4b13155e54643ccea6f8ed2f578c774548f
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2007-11-20 14:44:04 +0000

    Added six 'panel' shapes to match those in Smoldyn, but to be used within MOOSE to set up spatial signaling models natively and for other solvers.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@265 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 30838fd041aab101fb0aed01231f051357e3e2a0 [formerly 30838fd041aab101fb0aed01231f051357e3e2a0 [formerly a67d88f0525a52b1cd2c7d7686411ddf1be4fbae]]
    Former-commit-id: 823af2f5580847ffe44975a842c8b67f2c8c6966
    Former-commit-id: 6caebf31ad3365218922943d3431fccce63d4994

---
+++ kinetics/SpherePanel.cpp
@@ -0,0 +1,53 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment,
+** also known as GENESIS 3 base code.
+**           copyright (C) 2003-2007 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+
+#include "moose.h"
+#include "Panel.h"
+#include "SpherePanel.h"
+
+/**
+ * SpherePanel is derived from Panel, and almost everything in this class
+ * is handled by the base class and a few virtual functions.
+ */
+const Cinfo* initSpherePanelCinfo()
+{
+	static Cinfo spherePanelCinfo(
+		"SpherePanel",
+		"Upinder S. Bhalla, 2007, NCBS",
+		"SpherePanel: Sphereangular panel shape for portion of compartmental surface.",
+		initPanelCinfo(),
+		0,
+		0,
+		ValueFtype1< SpherePanel >::global()
+	);
+
+	return &spherePanelCinfo;
+}
+
+static const Cinfo* spherePanelCinfo = initSpherePanelCinfo();
+
+///////////////////////////////////////////////////
+// Class function definitions
+///////////////////////////////////////////////////
+
+SpherePanel::SpherePanel( unsigned int nDims )
+	: Panel( nDims )
+{
+		;
+}
+
+void SpherePanel::localFiniteElementVertices( 
+	vector< double >& ret, double area ) const
+{
+	// Fill up 'ret' here.
+	;
+}
+
