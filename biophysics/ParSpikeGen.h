cebb474af36b728ceeb4dd336b8cfcf7f81affb5
mayuresh_crl <mayuresh_crl@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2007-11-26 09:24:36 +0000

    Added code for parallel versions of SpikeGen and SynChan
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@275 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 0b14777aeba5d4641c2fada56d9117b3e5d5b111 [formerly 903b60b8fff2f22a80d87d6e32bac8ca1b86cbd2] [formerly 903b60b8fff2f22a80d87d6e32bac8ca1b86cbd2 [formerly b3c918a95d5182f3feda5e7af9db6e74d3b4ebdc]]
    Former-commit-id: f062330090d471558d9bf576f183af2646ec3055
    Former-commit-id: 0e6155c504e10e0d4fb9d7e790809c25fce4ec51

---
+++ biophysics/ParSpikeGen.h
@@ -0,0 +1,18 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2007 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+#ifndef _ParSpikeGen_h
+#define _ParSpikeGen_h
+
+class ParSpikeGen : public SpikeGen
+{
+	public:
+		ParSpikeGen();
+};
+#endif // _ParSpikeGen_h
