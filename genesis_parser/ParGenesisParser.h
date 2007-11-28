4ac3b49d8130225b543c478c1ad497a4d48a7fed
mayuresh_crl <mayuresh_crl@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2007-11-28 12:42:39 +0000

    Added Doxygen comments
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@279 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: c92702fb9d8b8372d34f69d07e2f609750a99537 [formerly a2c3d53922b1c235291ba3002d17d63c3472cea8] [formerly a2c3d53922b1c235291ba3002d17d63c3472cea8 [formerly 30bde2f0c644fa390eb4099011d32dcba28f4d49]]
    Former-commit-id: 0200c98b2b9466b0e5f0015e1537a1bbd447409e
    Former-commit-id: c2c11dc5ba0bb805e5ce9b1e491ec32284655e7e

--- genesis_parser/ParGenesisParser.h
+++ genesis_parser/ParGenesisParser.h
@@ -1,15 +1,27 @@
 /**********************************************************************
 ** This program is part of 'MOOSE', the
-** Messaging Object Oriented Simulation Environment,
-** also known as GENESIS 3 base code.
-** Large parts of this individual file have been copied from files
-** in the genesis/src/ss directory. These are authored by 
-** Dave Bilitch, and copyright Caltech
-** under terms which permit free redistribution. Note that these
-** files are NOT GPL.
-** Additional modifications and conversion to C++ are
-**           copyright (C) 2004 Upinder S. Bhalla. and NCBS
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2007 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
 **********************************************************************/
+/**
+ * This class is a parallel version of Moose parser.
+ * This file is compiled only when the parallelizatin flag is enabled.
+ * This class derives from GenesisParserWrapper, the parser class for serial Moose. 
+ *
+ * 
+ * This class refers to the base class, GenesisParserWrapper, for all parser functionality. 
+ * Parallel moose parser requires overriding of some of the base class functionality. 
+ * Such functions are overridden in this class. Except the overridden functions the parallel parser
+ * refers to the base serial parser for all it's functionality.
+ *
+ * 
+ * Parallel moose parser parses the input script file only on the root process. Each parsed command 
+ * is then sent using MPI to each of the non-root processes. 
+ * 
+ */
 
 #ifndef _PARGENESIS_PARSER_H
 #define _PARGENESIS_PARSER_H
@@ -41,15 +53,48 @@ class ParGenesisParserWrapper:public GenesisParserWrapper
 public:
 	ParGenesisParserWrapper();
 	void MyFunc();
+	/**
+	 * This function overrides functions to be called for a moose command. A map is populated by the serial parser
+	 * which maintains the functions to be called for every moose command. This function overrides some of these 		 * functions with for parallel moose. 
+	 */
 	void loadBuiltinCommands();
 
+
+	/**
+	 * This function overrides the "GenesisParserWrapper::parserFunc" function for parallel moose. 
+	 * This function is called by both the root and non root processes. 
+	 * The script file is parsed and broadcast on the root process and commands are 
+	 * received and executed on the non-root processes.
+	 */
 	static void parseFunc( const Conn& c, string s );
+	
+	/**
+	 * This virtual function overrides the "myFlexLexer::ExecuteCommand" function for parallel moose.
+	 * When this function is called, parsed commands are sent to non-root processes on the root process
+	 * and the command is executed on the non-root processes. The function is virtual to ensure that the overridded
+	 * function gets called when invoked from the "myFlexLexer" class.
+	 */
 	virtual Result ExecuteCommand(int argc, char** argv);
+
+	/**
+	 * This function broadcasts the parsed command to all non-root processes
+	 */
 	int SendCommand(int argc);
+
+	/**
+	 * This object contains the parsed command to be sent to the non-root processes on MPI
+	 */
 	struct stCommand objCommand_; 
 
 private:
+	/**
+	 * The rank of the executing process.
+	 */
 	int	processrank_;
+
+	/**
+	 * The total count of processes.
+	 */
 	int	processcount_;
 };
 
