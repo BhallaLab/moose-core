28cdb2714485c8e154e21d051cc82d0e2d64e907
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2007-12-03 10:04:13 +0000

    Added python interface for BinSynchan. Added special templated class PyMooseIterable to handle LookupFinfo objects inside MOOSE classes.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@283 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ pymoose/BinSynchan.h
@@ -0,0 +1,84 @@
+/*******************************************************************
+ * File:            pymoose/BinSynchan.h
+ * Description:     SWIG wrapper for BinSynchan class. It has been
+ *                      created by modifying the generated class
+ *                      declaration.
+ * Author:          Subhasis Ray
+ * E-mail:          ray.subhasis@gmail.com
+ * Created:         2007-12-03 10:56:28
+ ********************************************************************/
+/**********************************************************************
+ ** This program is part of 'MOOSE', the
+ ** Messaging Object Oriented Simulation Environment,
+ ** also known as GENESIS 3 base code.
+ **           copyright (C) 2003-2005 Upinder S. Bhalla. and NCBS
+ ** It is made available under the terms of the
+ ** GNU General Public License version 2
+ ** See the file COPYING.LIB for the full notice.
+ **********************************************************************/
+
+#ifndef _pymoose_BinSynchan_h
+#define _pymoose_BinSynchan_h
+#include "PyMooseIterable.h"
+#include "PyMooseBase.h"
+    
+class BinSynchan : public PyMooseBase
+{
+  public:
+    static const std::string className;
+    BinSynchan(Id id);
+    BinSynchan(std::string path);
+    BinSynchan(std::string name, Id parentId);
+    BinSynchan(std::string name, PyMooseBase* parent);
+    ~BinSynchan();
+    const std::string& getType();
+    double __get_Gbar() const;
+    void __set_Gbar(double Gbar);
+    double __get_Ek() const;
+    void __set_Ek(double Ek);
+    double __get_tau1() const;
+    void __set_tau1(double tau1);
+    double __get_tau2() const;
+    void __set_tau2(double tau2);
+    bool __get_normalizeWeights() const;
+    void __set_normalizeWeights(bool normalizeWeights);
+    double __get_Gk() const;
+    void __set_Gk(double Gk);
+    double __get_Ik() const;
+    void __set_Ik(double Ik);
+    unsigned int __get_numSynapses() const;
+    void __set_numSynapses(unsigned int numSynapses);
+    
+
+    double __get_weight(unsigned int) const;
+    void __set_weight(unsigned int index, double weight);
+    InnerPyMooseIterable <BinSynchan, unsigned int, double>* weight;
+
+    double __get_delay(unsigned int index) const;
+    void __set_delay(unsigned int index, double delay);
+    InnerPyMooseIterable <BinSynchan, unsigned int, double >* delay;
+    
+
+    int __get_poolSize(unsigned int index) const;
+    void __set_poolSize(unsigned int index, int size);
+    InnerPyMooseIterable <BinSynchan, unsigned int, int>* poolSize;    
+
+    double __get_releaseP(unsigned int) const;
+    void __set_releaseP(unsigned int index, double releaseP);
+    InnerPyMooseIterable <BinSynchan, unsigned int, double >* releaseP;
+
+    double __get_releaseCount(unsigned int index) const;
+    void __set_releaseCount(unsigned int index, double releaseCount);
+    InnerPyMooseIterable <BinSynchan, unsigned int, double>* releaseCount;    
+    // double __get_IkSrc() const;
+    // void __set_IkSrc(double IkSrc);
+    // double,double __get_origChannel() const;
+    // void __set_origChannel(double, double origChannel);
+    double __get_synapse() const;
+    void __set_synapse(double synapse);
+    double __get_activation() const;
+    void __set_activation(double activation);
+    double __get_modulator() const;
+    void __set_modulator(double modulator);    
+};
+#endif
--- pymoose/pymoose.h
+++ pymoose/pymoose.h
@@ -1,21 +1,48 @@
 #ifndef _PYMOOSE_H
 #define _PYMOOSE_H
+#include "PyMooseIterable.h"
 #include "PyMooseContext.h"
+#include "PyMooseUtil.h"
 #include "PyMooseBase.h"
 #include "Neutral.h"
+
+#include "Class.h"
+
+#include "HSolve.h"
+#include "Cell.h"
 #include "Compartment.h"
-#include "HHChannel.h"
-#include "Interpol.h"
-#include "Table.h"
 #include "HHGate.h"
-#include "CaConc.h"
-#include "Nernst.h"
-#include "PyMooseUtil.h"
+#include "HHChannel.h"
 #include "SpikeGen.h"
 #include "SynChan.h"
+#include "BinSynchan.h"
+
+#include "Interpol.h"
+#include "Table.h"
 #include "TableIterator.h"
 #include "Tick.h"
 #include "ClockJob.h"
 
+#include "KineticHub.h"
+#include "Kintegrator.h"
+#include "MathFunc.h"
+//#include "Mg_block.h"
+#include "Neutral.h"
+
+#include "RandGenerator.h"
+#include "BinomialRng.h"
+#include "ExponentialRng.h"
+#include "GammaRng.h"
+#include "NormalRng.h"
+#include "PoissonRng.h"
+
+#include "Enzyme.h"
+#include "Reaction.h"
+#include "Stoich.h"
+#include "Molecule.h"
+#include "Nernst.h"
+#include "CaConc.h"
+
+#include "Tick.h"
 
 #endif
