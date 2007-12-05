2cf06fcc3ce68e6cfeecd39c390474cdf39aeb42
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2007-12-05 10:59:34 +0000

    Modularised python squid demo. Added Qt vesrion of the Squid Demo GUI.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@290 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 8138a2984606eca3beb46be98c28415b258ae51b

--- pymoose/examples/squid.py
+++ pymoose/examples/squid.py
@@ -1,4 +1,19 @@
-import moose
+#!/usr/bin/env python
+import sys
+import math
+# The PYTHONPATH should contain the location of moose.py and _moose.so
+# files.  Putting ".." with the assumption that moose.py and _moose.so
+# has been generated in ${MOOSE_SOURCE_DIRECTORY}/pymoose/ (as default
+# pymoose build does) and this file is located in
+# ${MOOSE_SOURCE_DIRECTORY}/pymoose/examples
+sys.path.append('..')
+try:
+    import moose
+except ImportError:
+    print "ERROR: Could not import moose. Please add the directory containing moose.py in your PYTHONPATH"
+    import sys
+    sys.exit(1)
+
 import math
 VMIN = -0.1
 VMAX = 0.05
@@ -28,161 +43,123 @@ def calc_Na_m_A( v ):
         v = v + 1e-6
     return 0.1e6 * (EREST + 0.025 -v)/(math.exp((EREST + 0.025 - v)/0.01) - 1.0)
 
-
-
 def calc_Na_m_B(v):
     return 4.0e3 * math.exp((EREST - v)/0.018)
-
-
 
 def calc_Na_h_A( v ):
     return  70.0 * math.exp(( EREST - v )/0.020 )
 
-
-
 def calc_Na_h_B( v ):
     return ( 1.0e3 / (math.exp ( ( 0.030 + EREST - v )/ 0.01 ) + 1.0 ))
 
-
-    
 def calc_K_n_A( v ):
     if math.fabs( 0.01 + EREST - v )  < 1.0e-6 :
         v = v + 1.0e-6
     return ( 1.0e4 * ( 0.01 + EREST - v ) )/(math.exp(( 0.01 + EREST - v )/0.01) - 1.0 )
 
-
-
 def calc_K_n_B( v ):
 	return 0.125e3 * math.exp((EREST - v ) / 0.08 )
 
-
-squid = moose.Compartment('squid')
-squid.Rm = RM
-squid.Ra = RA
-squid.Cm = CM
-squid.Em = VLEAK
-
-Na =  moose.HHChannel('Na', squid)
-Na.Ek = VNa
-Na.Gbar = GNa
-Na.Xpower = 3
-Na.Ypower = 1
-
-K = moose.HHChannel('K', squid)
-K.Ek = VK
-K.Gbar = GK
-K.Xpower = 4
-
-squid.connect('channel', Na, 'channel')
-squid.connect('channel', K, 'channel')
-
-Vm = moose.Table('Vm')
-Vm.stepmode = 3
-Vm.connect('inputRequest', squid, 'Vm')
-
-Na.xGate = moose.HHGate(moose.PyMooseBase.pathToId(Na.path()+'/xGate'))
-Na.xGate.A = moose.InterpolationTable(moose.PyMooseBase.pathToId(Na.xGate.path()+'/A'))
-Na.xGate.B = moose.InterpolationTable(moose.PyMooseBase.pathToId(Na.xGate.path()+'/B'))
-Na.yGate = moose.HHGate(moose.PyMooseBase.pathToId(Na.path()+'/yGate'))
-Na.yGate.A = moose.InterpolationTable(moose.PyMooseBase.pathToId(Na.yGate.path()+'/A'))
-Na.yGate.B = moose.InterpolationTable(moose.PyMooseBase.pathToId(Na.yGate.path()+'/B'))
-K.xGate = moose.HHGate(moose.PyMooseBase.pathToId(K.path()+'/xGate'))
-K.xGate.A = moose.InterpolationTable(moose.PyMooseBase.pathToId(K.xGate.path()+'/A'))
-K.xGate.B = moose.InterpolationTable(moose.PyMooseBase.pathToId(K.xGate.path()+'/B'))
-
-
-Na.xGate.A.xmin = VMIN
-Na.xGate.B.xmin = VMIN
-Na.yGate.A.xmin = VMIN
-Na.yGate.B.xmin = VMIN
-K.xGate.A.xmin = VMIN
-K.xGate.B.xmin = VMIN
-Na.xGate.A.xmax = VMAX
-Na.xGate.B.xmax = VMAX
-Na.yGate.A.xmax = VMAX
-Na.yGate.B.xmax = VMAX
-K.xGate.A.xmax = VMAX
-K.xGate.B.xmax = VMAX
-Na.xGate.A.xdivs = NDIVS
-Na.xGate.B.xdivs = NDIVS
-Na.yGate.A.xdivs = NDIVS
-Na.yGate.B.xdivs = NDIVS
-K.xGate.A.xdivs = NDIVS
-K.xGate.B.xdivs = NDIVS
-
-
-v = VMIN
-
-
-for i in range(NDIVS+1):
-	Na.xGate.A[i] = calc_Na_m_A ( v )
-        #print 'Na.xGate.A[',i,'] =',Na.xGate.A[i]
-	Na.xGate.B [i]  =  calc_Na_m_A (v)   +  calc_Na_m_B ( v   )
-        #print 'Na.xGate.B [',i,']  =',Na.xGate.B [i]
-	Na.yGate.A [i] = calc_Na_h_A  (v )
-        #print 'Na.yGate.A [',i,'] =',Na.yGate.A [i]
-	Na.yGate.B [i] =  calc_Na_h_A  (v)   +   calc_Na_h_B  (v   )
-        #print 'Na.yGate.B [',i,'] =',Na.yGate.B [i]
-	K.xGate.A [i] = calc_K_n_A  (v )
-        #print 'K.xGate.A [',i,'] =', K.xGate.A [i] 
-	K.xGate.B [i] =  calc_K_n_A ( v)   +  calc_K_n_B ( v )
-        #print 'K.xGate.B [',i,'] =', K.xGate.B [i]
-	v = v + dv
-
-
-# useclock /Vm,/squid,/squid/# 0
-# useclock /##[TYPE=Table] 1
-# // useclock /Vm 0
-
-# // Crazy hack, but the squid demo does it and we need to match.
-# setfield /squid initVm {EREST}
-# reset
-# setfield /squid inject 0
-# step 0.005 -t
-# setfield /squid inject {INJECT}
-# step 0.040 -t
-# setfield /squid inject 0
-# step 0.005 -t
-# setfield /Vm print "squid.plot"
+# Make the squid object global
 
 context = moose.PyMooseBase.getContext()
-context.setClock(0, SIMDT, 0)
-context.setClock(1, PLOTDT, 0)
-context.useClock(moose.PyMooseBase.pathToId('/sched/cj/t0'), '/Vm,/squid,/squid/#')
 
-# context.useClock(moose.PyMooseBase.pathToId('/sched/cj/t0'), '/Vm')
-# context.useClock(moose.PyMooseBase.pathToId('/sched/cj/t0'), '/squid')
-# context.useClock(moose.PyMooseBase.pathToId('/sched/cj/t0'), '/squid/#')
-# context.useClock(moose.PyMooseBase.pathToId('/sched/cj/t1'), '/##[TYPE=Table]')
+def setupModel():
+    """Set up the MOOSE model for squid demo"""
+    global context
 
-# setfield /squid initVm {EREST}
-# reset
-# setfield /squid inject 0
-# step 0.005 -t
-# setfield /squid inject {INJECT}
-# step 0.040 -t
-# setfield /squid inject 0
-# step 0.005 -t
-# setfield /Vm print "squid.plot"
+    if context.exists('squid'):
+        return moose.Compartment('squid')
+    squid = moose.Compartment('squid')
+    squid.Rm = RM
+    squid.Ra = RA
+    squid.Cm = CM
+    squid.Em = VLEAK
 
-squid.initVm = EREST
-context.reset()
-squid.inject = 0
-context.step(0.005)
-squid.inject = INJECT
-context.step(0.040)
-squid.inject = 0
-context.step(0.005)
-Vm.dumpFile("squid.plot")
-# Adding some eye-candy from matplotlib - also shows the power of being
-# able to combine any third-party python library with pymoose to do custom
-# visualization and other funky stuff.
-# Forget it (i.e. comment out) if you don't have matplotlib installed
-# and be happy with plotting the 'squid.plot' output file with some
-# other plotting software like xplot or gnuplot.
-from pylab import plot
-from pylab import array
-from pylab import show
-vm = array(Vm)
-plot(vm)
-show()
+    Na =  moose.HHChannel('Na', squid)
+    Na.Ek = VNa
+    Na.Gbar = GNa
+    Na.Xpower = 3
+    Na.Ypower = 1
+    
+    K = moose.HHChannel('K', squid)
+    K.Ek = VK
+    K.Gbar = GK
+    K.Xpower = 4
+
+    squid.connect('channel', Na, 'channel')
+    squid.connect('channel', K, 'channel')
+    
+    
+    Na_xA = moose.InterpolationTable('/squid/Na/xGate/A')
+    Na_xB = moose.InterpolationTable('/squid/Na/xGate/B')
+    Na_yA = moose.InterpolationTable('/squid/Na/yGate/A')
+    Na_yB = moose.InterpolationTable('/squid/Na/yGate/B')
+    K_xA = moose.InterpolationTable('/squid/K/xGate/A')
+    K_xB = moose.InterpolationTable('/squid/K/xGate/B')
+
+   
+    Na_xA.xmin = VMIN
+    Na_xA.xmax = VMAX
+    Na_xA.xdivs = NDIVS
+
+    Na_xB.xmin = VMIN
+    Na_xB.xmax = VMAX
+    Na_xB.xdivs = NDIVS
+
+    Na_yA.xmin = VMIN
+    Na_yA.xmax = VMAX
+    Na_yA.xdivs = NDIVS
+
+    Na_yB.xmin = VMIN
+    Na_yB.xmax = VMAX
+    Na_yB.xdivs = NDIVS
+
+    K_xA.xmin = VMIN
+    K_xA.xmax = VMAX
+    K_xA.xdivs = NDIVS
+
+    K_xB.xmin = VMIN
+    K_xB.xmax = VMAX
+    K_xB.xdivs = NDIVS
+    
+    v = VMIN
+
+    for i in range(NDIVS+1):
+	Na_xA[i] = calc_Na_m_A ( v )
+	Na_xB[i]  =  calc_Na_m_A (v)   +  calc_Na_m_B ( v   )
+	Na_yA[i] = calc_Na_h_A  (v )
+	Na_yB[i] =  calc_Na_h_A  (v)   +   calc_Na_h_B  (v   )
+	K_xA[i] = calc_K_n_A  (v )
+	K_xB[i] =  calc_K_n_A ( v)   +  calc_K_n_B ( v )
+	v = v + dv
+
+    context = moose.PyMooseBase.getContext()
+    context.setClock(0, SIMDT, 0)
+    context.setClock(1, PLOTDT, 0)
+
+
+    squid.initVm = EREST
+    return squid
+
+
+def runDemo():
+    """Run the simulation steps, setup the model if required"""
+    Vm = moose.Table('Vm')
+    Vm.stepmode = 3
+    squid = setupModel()
+    Vm.connect('inputRequest', squid, 'Vm')
+    context.useClock(moose.PyMooseBase.pathToId('/sched/cj/t0'), '/Vm,/squid,/squid/#')
+    context.reset()
+    squid.inject = 0
+    context.step(0.005)
+    squid.inject = INJECT
+    context.step(0.040)
+    squid.inject = 0
+    context.step(0.005)
+    Vm.dumpFile("squid.plot")
+    return Vm
+
+if __name__ == '__main__':
+    runDemo()
+    print 'The output is available in squid.plot'
