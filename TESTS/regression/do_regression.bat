75c4daf3bd6f7943f379a84c8268cfa94e615fb6
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2007-12-29 16:32:24 +0000

    Implemented the framework for doing regression tests automatically. Implemented a couple of initial tests: Squid and the oscillatory Kholodenko model.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@307 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 24e336a8514c89f592db7d92f40887b037478de3

---
+++ TESTS/regression/do_regression.bat
@@ -0,0 +1,17 @@
+#!/bin/tcsh
+
+set MOOSE = ../../moose
+
+# nearDiff is a function to see if two data files are within 
+# epsilon of each other. It assumes the files are in xplot format
+# If it succeeds, it prints out a period without a newline.
+# If it fails, it prints out the first argument.
+set NEARDIFF = neardiff
+
+/bin/rm -f test.plot
+$MOOSE moose_squid.g > /dev/null
+$NEARDIFF moose_squid.plot test.plot 1.0e-5
+
+/bin/rm -f test.plot
+$MOOSE moose_kholodenko.g > /dev/null
+$NEARDIFF moose_kholodenko.plot test.plot 1.0e-5
