fff77565d087e819ccdde6043b6bfa9ce65d74dd
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2008-01-24 10:58:51 +0000

    Removed duplicate definition of isEqual(double, double, double) from PyMooseUtil.cpp
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@390 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: f9eb47b130034500bc318efb7e9fec9a4d82bb71
    Former-commit-id: adb92d306fadda5fe68499f0efa29ca417903f96
    Former-commit-id: 3450572f325139f09eb10243108eedd0c1e8b042

--- pymoose/PyMooseUtil.cpp
+++ pymoose/PyMooseUtil.cpp
@@ -12,38 +12,6 @@
 #include <cassert>
 #include "PyMooseUtil.h"
 using namespace pymoose;
-bool isEqual(double val1, double val2, double epsilon)
-{
-    bool result = false;
-    double diff;
-    assert(epsilon > 0);
-    
-    if ((val1 == 0) && (val2 == 0))
-    {
-        return true;
-    }
-    
-    diff = (val1>val2) ? (val1-val2):(val2-val1);
-    if (val1 == 0)
-    {
-        if (val2 > 0)
-        {
-            result = (val2 < epsilon);
-        }
-        else 
-        {
-            result = (val2 > epsilon);
-        }
-    } else if ( val1 > 0 )
-    {
-        result = diff/val1 < epsilon;
-    }
-    else
-    {
-        result = (-diff/val1) < epsilon;
-    }
-    return result;
-}
 
     
     
