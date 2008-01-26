fc2532270c34af968786d04b277d02ec79fed28c
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2008-01-26 11:24:21 +0000

    Added channel blocks in gui.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@393 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: bae178b7a965f92ab66aeb35653d215d8f19aee0
    Former-commit-id: c002a29d825515da46f54f689bef2199a1213ee7
    Former-commit-id: ae72aec36ce1253f2cda77469ed503f718bb60f3

--- pymoose/examples/qtSquid.py
+++ pymoose/examples/qtSquid.py
@@ -13,35 +13,87 @@ import sys
 from PyQt4 import QtGui, QtCore
 from PyQt4 import Qwt5 as qwt
 import PyQt4.Qwt5.qplt as qplt
-
+import numpy
+import pdb
 import squid
 SIMDT = 1e-5
     
 class InputPanel(QtGui.QFrame):
     def __init__(self, *args):
         apply(QtGui.QFrame.__init__, (self,)+args)
+        self.keys = ["runtime", "prepulse", "pulsewidth", "amplitude", "updateT", "overlay", "blockNa", "blockK"]
         self.labels = {}
         self.labels["runtime"] = QtGui.QLabel("Run Time (s)")
         self.labels["prepulse"] = QtGui.QLabel("Pre-pulse time (s)")
         self.labels["pulsewidth"] = QtGui.QLabel("Pulse width (s)")
         self.labels["amplitude"] = QtGui.QLabel("Injection current (A)")
-        self.textInputs = {}
-        self.textInputs["runtime"] = QtGui.QLineEdit("0.050")
-        self.textInputs["prepulse"] = QtGui.QLineEdit("0.005")
-        self.textInputs["pulsewidth"] = QtGui.QLineEdit("0.040")
-        self.textInputs["amplitude"] = QtGui.QLineEdit("1.0e-7")
-
+        self.labels["overlay"] = QtGui.QLabel("Overlay plots")
+        self.labels["blockNa"] = QtGui.QLabel("Block Na Channels")
+        self.labels["blockK"] = QtGui.QLabel("Block K Channels")
+        self.labels["updateT"] = QtGui.QLabel("Graph update interval (s)")
+        self.inputs = {}
+        self.inputs["runtime"] = QtGui.QLineEdit("0.050")
+        self.inputs["prepulse"] = QtGui.QLineEdit("0.005")
+        self.inputs["pulsewidth"] = QtGui.QLineEdit("0.040")
+        self.inputs["amplitude"] = QtGui.QLineEdit("1.0e-7")
+        self.inputs["updateT"] = QtGui.QLineEdit("0.050")
+        self.inputs["overlay"] = QtGui.QCheckBox()
+        self.inputs["blockNa"] = QtGui.QCheckBox()
+        self.inputs["blockK"] = QtGui.QCheckBox()
         self.setLayout(QtGui.QGridLayout(self))
         i = 0
-        for key, value in self.labels.iteritems():
-            value.setFrameStyle(QtGui.QFrame.Panel ^ QtGui.QFrame.Raised)
-            self.layout().addWidget(value, i, 0)
-            self.layout().addWidget(self.textInputs[key] , i, 1)
+        for key in self.keys:
+            print key
+            
+            self.labels[key].setFrameStyle(QtGui.QFrame.Panel ^ QtGui.QFrame.Raised)
+            self.layout().addWidget(self.labels[key], i, 0)
+            self.layout().addWidget(self.inputs[key] , i, 1)
             i = i + 1
+class PlotData():
+    def __init__(self):
+        self.plotData = []
+        self.xEnd = 0.0
+
+    def append(self, x=0, y=0):
+        if (x == 0) or ( y == 0 ):
+            self.plotData.append([])
+        else:
+            self.plotData.append([x,y])
+        
+    def concatData(self, x, y):
+        if len(self.plotData) == 0  :
+            self.plotData.append([x,y])
+        elif len(self.plotData[-1]) != 2:
+            self.plotData[-1] = [x,y]
+        else:
+            self.plotData[-1][0] = numpy.concatenate((self.plotData[-1][0], x))
+            self.plotData[-1][1] = numpy.concatenate((self.plotData[-1][1], y))
+
+    def lastX(self):
+        if len(self.plotData) == 0 :
+            print "Error: data list is empty"
+        elif len(self.plotData[-1]) != 2:
+            print "Error: last data entry is empty"
+        else:
+            return self.plotData[-1][0]
+
+    def lastY(self):
+        if len(self.plotData) == 0 :
+            print "Error: data list is empty"
+        elif len(self.plotData[-1]) != 2:
+            print "Error: last data entry is empty"
+        else:
+            return self.plotData[-1][1]
+
+    def clear(self):
+        del self.plotData
+        self.plotData = []
+        self.append()
 
 class Dashboard(QtGui.QFrame):
     def __init__(self, *args):
         apply(QtGui.QFrame.__init__, (self,)+args)
+        self.colors = ( QtCore.Qt.white, QtCore.Qt.lightGray, QtCore.Qt.yellow, QtCore.Qt.cyan, QtCore.Qt.red, QtCore.Qt.green, QtCore.Qt.blue, QtCore.Qt.magenta, QtCore.Qt.gray )
         self.setLayout(QtGui.QHBoxLayout(self))
         # There is somthing to note here - qwt.QwtPlot is more
         # accessible, we can set the curve and the QwtPlotCurve we can
@@ -51,15 +103,32 @@ class Dashboard(QtGui.QFrame):
         # data without recreating the whole curve object.
         self.vmGraph = qwt.QwtPlot()  
         self.vmGraph.setCanvasBackground(QtGui.QColor(0, 0, 150))
-        self.curve = qwt.QwtPlotCurve("Vm")
-        self.curve.attach(self.vmGraph)        
-        self.curve.setPen(QtGui.QPen(QtGui.QColor(250, 250, 0)))
-        self.curve.setStyle(qwt.QwtPlotCurve.Lines)
-        
+        self.curves = []
+        self.addCurve("Vm")
         self.layout().addWidget(self.vmGraph)
         self.vmGraph.replot()
+        self.zoomer = qwt.QwtPlotZoomer(qwt.QwtPlot.xBottom,
+                                        qwt.QwtPlot.yLeft,
+                                        qwt.QwtPicker.DragSelection,
+                                        qwt.QwtPicker.AlwaysOn,
+                                        self.vmGraph.canvas())
+        self.zoomer.setRubberBandPen(QtGui.QPen(QtCore.Qt.white))
+        self.zoomer.setTrackerPen(QtGui.QPen(QtCore.Qt.cyan))
 
+    def addCurve(self, title):
+        self.curves.append(qwt.QwtPlotCurve(title))
+        self.curves[-1].attach(self.vmGraph)        
+        self.curves[-1].setPen(QtGui.QPen(self.colors[len(self.curves)%len(self.colors)]))
+        self.curves[-1].setStyle(qwt.QwtPlotCurve.Lines)
 
+    def clear(self):
+        print "Clearing dashborad"
+        del self.curves
+        self.vmGraph.clear()
+        self.curves = []
+        self.addCurve("Vm")
+        self.vmGraph.replot()
+        
 class SquidDemo(QtGui.QMainWindow):
     def __init__(self, *args):
         apply(QtGui.QMainWindow.__init__, (self,)+args)
@@ -78,11 +147,13 @@ class SquidDemo(QtGui.QMainWindow):
 
         self.dashboard = Dashboard()
         self.inputPanel = InputPanel()
-
+        self.connect(self.inputPanel.inputs["blockNa"], QtCore.SIGNAL("stateChanged(int)"), self.squid.toggleNaChannels)
+        self.connect(self.inputPanel.inputs["blockK"], QtCore.SIGNAL("stateChanged(int)"), self.squid.toggleKChannels)
         self.layout().setMenuBar(self.menu)
         self.mainLayout.addWidget(self.dashboard)
         self.mainLayout.addWidget(self.inputPanel)
         self.mainWidget.setLayout(self.mainLayout)
+        self.data = PlotData()
 
     def initActions(self):
         self.actions = {}
@@ -121,44 +192,50 @@ class SquidDemo(QtGui.QMainWindow):
     
     def slotRun(self):
         print 'Executing simulation'
-        totalTimeStr = self.inputPanel.textInputs["runtime"].text()
-        preTimeStr = self.inputPanel.textInputs["prepulse"].text()
-        pulseWidthStr = self.inputPanel.textInputs["pulsewidth"].text()
-        injectHighStr = self.inputPanel.textInputs["amplitude"].text()
+        totalTimeStr = self.inputPanel.inputs["runtime"].text()
+        preTimeStr = self.inputPanel.inputs["prepulse"].text()
+        pulseWidthStr = self.inputPanel.inputs["pulsewidth"].text()
+        updateIntervalStr = self.inputPanel.inputs["updateT"].text()
+        injectHighStr = self.inputPanel.inputs["amplitude"].text()
         injectBaseStr = "0"
         print "Read from text box", totalTimeStr, preTimeStr, pulseWidthStr, injectHighStr
         try:
-            totalTime, preTime, pulseWidth, injectBase, injectHigh =  map(float, [totalTimeStr, preTimeStr, pulseWidthStr, injectBaseStr, injectHighStr] )
+            totalTime, preTime, pulseWidth, updateInterval, injectBase, injectHigh =  map(float, [totalTimeStr, preTimeStr, pulseWidthStr, updateIntervalStr, injectBaseStr, injectHighStr] )
         except Exception, vError:
             print vError
-            totalTime, preTime, pulseWidth, injectBase, injectHigh = 0.050, 0.005, 0.040, 0, squid.INJECT
+            totalTime, preTime, pulseWidth, updateInterval, injectBase, injectHigh = 0.050, 0.005, 0.040, 0.050, 0, squid.INJECT
        
-        print totalTime, preTime, pulseWidth, injectBase, injectHigh
+        print totalTime, preTime, pulseWidth, updateInterval, injectBase, injectHigh
+        import time
+        t1 = time.clock()
         Vm = self.squid.doRun(totalTime, preTime, pulseWidth, injectBase, injectHigh)
-
-        t = 0.0
-        y = qplt.array(Vm)
-        x =  qplt.zeros(len(y))
-        for i in range(len(x)):
-            x[i] = t
-            t = t + SIMDT
-        self.dashboard.curve.setData(x,y)
-        self.dashboard.zoomer = qwt.QwtPlotZoomer(qwt.QwtPlot.xBottom,
-                                        qwt.QwtPlot.yLeft,
-                                        qwt.QwtPicker.DragSelection,
-                                        qwt.QwtPicker.AlwaysOn,
-                                        self.dashboard.vmGraph.canvas())
-        self.dashboard.zoomer.setRubberBandPen(QtGui.QPen(QtCore.Qt.white))
-        self.dashboard.zoomer.setTrackerPen(QtGui.QPen(QtCore.Qt.cyan))
-
+        t2 = time.clock()
+        print "Runtime = ", t2 - t1, "s"
+        t = self.data.xEnd
+        self.data.xEnd = t + totalTime
+        x = numpy.arange(t, self. data.xEnd, SIMDT)
+        y = numpy.array(Vm)
+        self.data.concatData(x,y)
+        self.dashboard.curves[-1].setData(self.data.lastX(), self.data.lastY())
+        self.dashboard.vmGraph.replot() 
         self.dashboard.zoomer.setZoomBase()
-        self.dashboard.vmGraph.replot()
         
     def slotReset(self):
         print 'Resetting the simulation'
-        self.dashboard.curve.setData([],[])
+        if self.inputPanel.inputs["overlay"].checkState() == QtCore.Qt.Checked:
+            print "Overlay is on"
+            self.data.append()
+            self.dashboard.curves.append(qwt.QwtPlotCurve("Vm"))
+            self.dashboard.addCurve("Vm")
+        else:
+            self.data.clear()
+            self.dashboard.clear()
+        
+        self.dashboard.curves[-1].setData([],[])
         self.dashboard.vmGraph.replot()
+       
         self.squid.getContext().reset()
+        self.data.xEnd = 0.0
         
 
 
