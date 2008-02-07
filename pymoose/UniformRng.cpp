cad617838e6165c4b0036ce36fe6a35d218278d4
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2008-02-07 10:08:22 +0000

    Updated PulseGen demo
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@410 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 1a374c94109339a0dfa4e3c5dfbe1fd84470e204
    Former-commit-id: e9da9f3cfea532cf1b077f1b61b8d5397dd19792
    Former-commit-id: d1dc5e87f0b7cfffd68b75e5a470056c36a2e927

---
+++ pymoose/UniformRng.cpp
@@ -0,0 +1,45 @@
+#ifndef _pymoose_UniformRng_cpp
+#define _pymoose_UniformRng_cpp
+#include "UniformRng.h"
+using namespace pymoose;
+
+const std::string UniformRng::className = "UniformRng";
+UniformRng::UniformRng(Id id):RandGenerator(id){}
+UniformRng::UniformRng(std::string path):RandGenerator(className, path){}
+UniformRng::UniformRng(std::string name, Id parentId):RandGenerator(className, name, parentId){}
+UniformRng::UniformRng(std::string name, PyMooseBase& parent):RandGenerator(className, name, parent){}
+UniformRng::~UniformRng(){}
+const std::string& UniformRng::getType(){ return className; }
+double UniformRng::__get_mean() const
+{
+    double mean;
+    get < double > (id_(), "mean",mean);
+    return mean;
+}
+double UniformRng::__get_variance() const
+{
+    double variance;
+    get < double > (id_(), "variance",variance);
+    return variance;
+}
+double UniformRng::__get_min() const
+{
+    double min;
+    get < double > (id_(), "min",min);
+    return min;
+}
+void UniformRng::__set_min( double min )
+{
+    set < double > (id_(), "min", min);
+}
+double UniformRng::__get_max() const
+{
+    double max;
+    get < double > (id_(), "max",max);
+    return max;
+}
+void UniformRng::__set_max( double max )
+{
+    set < double > (id_(), "max", max);
+}
+#endif
