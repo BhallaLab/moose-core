48ef342ec2cce2d417e4307c016a042d8e9230d3
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2008-05-30 04:22:08 +0000

    Putting in a new code file that was left out in the previous checkin for executing shell commands.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@501 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ genesis_parser/exec_fork.cpp
@@ -0,0 +1,81 @@
+////////////////////////////////////////////////////////////////////
+//
+// exec_fork.cpp: Based closely on system.c from GENESIS
+//
+////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <math.h>
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/wait.h>
+// #include "system_deps.h"
+
+extern int debug;
+
+void Beep(){
+    putchar('\007');
+}
+
+int ExecFork( int argc, char** argv)
+{
+#ifdef WINDOWS
+  return -1;
+#else
+int pid;
+int status;
+char	*newargv[4];
+char	string[1000];
+int	i;
+
+    /*
+    ** prepare the argument list for the fork
+    */
+    newargv[0] = "csh";
+    newargv[1] = "-c";
+    string[0] = '\0';
+    for(i=0;i<argc;i++){
+	strcat(string,argv[i]);
+	strcat(string," ");
+    }
+    newargv[2] = string;
+    newargv[3] = NULL;
+    /*
+    ** do the fork
+    */
+    pid = vfork();
+	// could be fork if vfork doesn't work
+    if(pid == -1){
+	printf("run: fork unsuccessful in ExecFork() for %s\n",string);
+	_exit(0);
+    } else 
+    if(pid ==0){
+	/*
+	** pid = 0 indicates that this is the child resulting
+	** from the fork so execute the program 
+	** which overlays the current process and therefore
+	** does not return
+	*/
+	execvp(newargv[0],newargv);
+	/*
+	** if the exec fails then exit the child
+	*/
+	printf("unable to execute '%s'\n",argv[0]);
+	_exit(0);
+    }
+#ifdef DEBUG
+    printf("waiting for child process %d\n",pid);
+#endif
+    /*
+    ** pid > 0 indicates successful child has been forked
+    ** so wait for it to complete execution and return the status
+    */
+    while(wait(&status) != pid);
+    // if(debug){
+	// printf("child process %d done. Status = %d\n", pid, status);
+    // }
+    return(status);
+#endif // WINDOWS
+}
