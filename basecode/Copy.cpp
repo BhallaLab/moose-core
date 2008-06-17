290883aeba77f87c838389219b77582a693b35f7
raameshd <raameshd@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2008-06-17 19:20:18 +0000

    Found the "only zeroth index resetting". It was because of the connection between the Tick and the array element was not of One2All type.
    An additional parameter, conTainerOption, has been added to schedule 
    function in the Cinfo. This parameter is the conTainer Type between the 
    Tick and the scheduled object. 
    
    
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@520 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: f6d3908f5096a87467f4add487f01c0ef175b9fb
    Former-commit-id: 4d7ddbae8cc2cfc97460fcdb2c952db3a93afb6c
    Former-commit-id: 8054e96b16bb5d8590257f19d9275b87bc310e26

--- basecode/Copy.cpp
+++ basecode/Copy.cpp
@@ -296,7 +296,7 @@ Element* SimpleElement::copy( Element* parent, const string& newName )
 		) ) {
 		for ( i = origDup.begin(); i != origDup.end(); i++ ) {
 			if ( i->first != i->second ) // a global
-				i->second->cinfo()->schedule( i->second );
+				i->second->cinfo()->schedule( i->second, ConnTainer::Default );
 		}
 	}
 
@@ -375,7 +375,7 @@ Element* SimpleElement::copyIntoArray( Id parent, const string& newName, int n )
 		) ) {
 		for ( i = origDup.begin(); i != origDup.end(); i++ ) {
 			if ( i->first != i->second ) // a global
-				i->second->cinfo()->schedule( i->second );
+				i->second->cinfo()->schedule( i->second, ConnTainer::One2All );
 		}
 	}
 	return child;
