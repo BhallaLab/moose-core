c086a97e36b4eb24d4492479ecec57e85060f542
raameshd <raameshd@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2008-07-10 13:44:46 +0000

    A new type of connection was added to address the connection between array elements and global elements. This particular connection involved connection of all the indices of the array element to a single global element. So this is All2One connection, inverse of One2All. An example where this would be used quite often is between channels and gates.
    The All2One classes have been created by taking inverse of One2All 
    classes. 
    
    
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@542 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- connections/All2OneConn.h
+++ connections/All2OneConn.h
@@ -8,8 +8,8 @@
 ** See the file COPYING.LIB for the full notice.
 **********************************************************************/
 
-#ifndef _ONE2ALL_CONN_H
-#define _ONE2ALL_CONN_H
+#ifndef _ALL2ONE_CONN_H
+#define _ALL2ONE_CONN_H
 
 
 /**
@@ -18,10 +18,10 @@
  * array object child, or the scheduler tick going to all compartments
  * on a cell.
  */
-class One2AllConnTainer: public ConnTainer
+class All2OneConnTainer: public ConnTainer
 {
 	public:
-		One2AllConnTainer( Eref e1, Eref e2, 
+		All2OneConnTainer( Eref e1, Eref e2, 
 			int msg1, int msg2,
 			unsigned int i1 = 0, unsigned int i2 = 0 );
 
@@ -35,14 +35,16 @@ class One2AllConnTainer: public ConnTainer
 		 * Returns the number of targets on this ConnTainer
 		 */
 		unsigned int size() const {
-			return e2numEntries_;
+			return 1;
 		}
 
 		/**
 		 * Returns the number of sources coming to the specified eIndex,
 		 */
 		unsigned int numSrc( unsigned int eIndex ) const {
-			return 1;
+			if ( eI2_ == eIndex );
+				return e1numEntries_;
+			return 0;
 		}
 
 		/**
@@ -50,17 +52,15 @@ class One2AllConnTainer: public ConnTainer
 		 * eIndex, on this ConnTainer.
 		 */
 		unsigned int numDest( unsigned int eIndex ) const {
-			if ( eI1_ == eIndex );
-				return e2numEntries_;
-			return 0;
+			return 1;
 		}
 
 		unsigned int eI1() const {
-			return eI1_;
+			return 0;
 		}
 
 		unsigned int eI2() const {
-			return 0;
+			return eI2_;
 		}
 
 		unsigned int i1() const {
@@ -81,11 +81,11 @@ class One2AllConnTainer: public ConnTainer
 		ConnTainer* copy( Element* e1, Element* e2, bool isArray ) const;
 
 		unsigned int option() const {
-			return One2All;
+			return All2One;
 		}
 
 		/**
-		 * Cannot add more messages to a One2All: already goes to all
+		 * Cannot add more messages to a All2One: already goes to all
 		 * possible targets.
 		 */
 		bool addToConnTainer( 
@@ -96,42 +96,42 @@ class One2AllConnTainer: public ConnTainer
 		}
 		
 	private:
-		unsigned int eI1_;
-		unsigned int e2numEntries_;
+		unsigned int eI2_;
+		unsigned int e1numEntries_;
 		unsigned int i1_;
 		unsigned int i2_;
 };
 
-class One2AllConn: public Conn
+class All2OneConn: public Conn
 {
 	public:
-		One2AllConn( const One2AllConnTainer* s, unsigned int index )
-			: s_( s ), index_( index ), size_( s->size() )
+		All2OneConn( const All2OneConnTainer* s, unsigned int index )
+			: s_( s ), index_( index )
 		{;}
 
-		~One2AllConn()
+		~All2OneConn()
 		{;}
 
 		Eref target() const {
-			return Eref( s_->One2AllConnTainer::e2(), index_ );
+			return Eref( s_->All2OneConnTainer::e2(), s_->All2OneConnTainer::eI2() );
 		}
 		unsigned int targetIndex() const {
-			return s_->One2AllConnTainer::i2();
+			return s_->All2OneConnTainer::i2();
 		}
 		int targetMsg() const {
-			return s_->One2AllConnTainer::msg2();
+			return s_->All2OneConnTainer::msg2();
 		}
 		Eref source() const {
-			return Eref( s_->One2AllConnTainer::e1(), s_->One2AllConnTainer::eI1() );
+			return Eref( s_->All2OneConnTainer::e1(), index_ );
 		}
 		unsigned int sourceIndex() const {
-			return s_->One2AllConnTainer::i1();
+			return s_->All2OneConnTainer::i1();
 		}
 		int sourceMsg() const {
-			return s_->One2AllConnTainer::msg1();
+			return s_->All2OneConnTainer::msg1();
 		}
 		void* data() const {
-			return s_->One2AllConnTainer::e2()->data( index_ );
+			return s_->All2OneConnTainer::e2()->data( s_->All2OneConnTainer::eI2() );
 		}
 
 		/**
@@ -139,13 +139,13 @@ class One2AllConn: public Conn
 		 * targets.
 		 */
 		void increment() {
-			++index_;
+			s_ = 0;
 		}
 		void nextElement() {
-			index_ = size_;
+			s_ = 0;
 		}
 		bool good() const {
-			return ( index_ < size_ );
+			return ( s_ != 0 );
 		}
 
 		/**
@@ -163,44 +163,44 @@ class One2AllConn: public Conn
 		}
 
 	private:
-		const One2AllConnTainer* s_;
+		const All2OneConnTainer* s_;
 		unsigned int index_;	 // Keeps track of e2 element index
-		unsigned int size_;	 	// Keeps track of e2 element size
+		
 };
 
-class ReverseOne2AllConn: public Conn
+class ReverseAll2OneConn: public Conn
 {
 	public:
-		ReverseOne2AllConn( const One2AllConnTainer* s, unsigned int index )
+		ReverseAll2OneConn( const All2OneConnTainer* s, unsigned int index )
 			: s_( s ), index_( index ) 
 		{;}
 
-		~ReverseOne2AllConn()
+		~ReverseAll2OneConn()
 		{;}
 
 		Eref target() const {
-			return Eref( s_->One2AllConnTainer::e1() , s_->One2AllConnTainer::eI1() );
+			return Eref( s_->All2OneConnTainer::e1() , index_ );
 		}
 		unsigned int targetEindex() const {
-			return s_->One2AllConnTainer::eI1();
+			return s_->All2OneConnTainer::eI1();
 		}
 		unsigned int targetIndex() const {
-			return s_->One2AllConnTainer::i1();
+			return s_->All2OneConnTainer::i1();
 		}
 		int targetMsg() const {
-			return s_->One2AllConnTainer::msg1();
+			return s_->All2OneConnTainer::msg1();
 		}
 		Eref source() const {
-			return Eref( s_->One2AllConnTainer::e2(), index_ );
+			return Eref( s_->All2OneConnTainer::e2(), s_->All2OneConnTainer::eI2() );
 		}
 		unsigned int sourceIndex() const {
-			return s_->One2AllConnTainer::i2();
+			return s_->All2OneConnTainer::i2();
 		}
 		int sourceMsg() const {
-			return s_->One2AllConnTainer::msg2();
+			return s_->All2OneConnTainer::msg2();
 		}
 		void* data() const {
-			return s_->e1()->data( s_->One2AllConnTainer::eI1() );
+			return s_->e1()->data( index_ );
 		}
 
 		/**
@@ -208,15 +208,15 @@ class ReverseOne2AllConn: public Conn
 		 * targets.
 		 */
 		void increment() {
-			s_ = 0;
+			++index_;
 		}
 
 		void nextElement() {
-			s_ = 0;
+			index_ = size_ ;
 		}
 
 		bool good() const {
-			return ( s_ != 0 );
+			return ( index_ < size_ );
 		}
 
 		/**
@@ -224,7 +224,7 @@ class ReverseOne2AllConn: public Conn
 		 * traverse back with the correct args.
 		 */
 		const Conn* flip() const {
-			return new One2AllConn( s_, index_ );
+			return new All2OneConn( s_, index_ );
 		}
 
 		const ConnTainer* connTainer() const {
@@ -236,8 +236,9 @@ class ReverseOne2AllConn: public Conn
 		}
 
 	private:
-		const One2AllConnTainer* s_;
+		const All2OneConnTainer* s_;
 		unsigned int index_; // Keeps track of the e2 element index
+		unsigned int size_;
 };
 
 // Some temporary typedefs while I think about implementations
@@ -247,4 +248,4 @@ typedef SimpleConnTainer Many2AllConnTainer;
 typedef SimpleConnTainer All2ManyConnTainer;
 typedef SimpleConnTainer All2AllMapConnTainer;
 
-#endif // _ONE2ALL_CONN_H
+#endif // _ALL2ONE_CONN_H
