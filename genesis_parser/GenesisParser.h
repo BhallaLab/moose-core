98f4df55c822741dfd9780443d7c39762b4b4679
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2008-08-05 06:03:39 +0000

    corrected errors introduced while comparing to svn.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@577 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- genesis_parser/GenesisParser.h
+++ genesis_parser/GenesisParser.h
@@ -55,7 +55,7 @@ typedef struct _pn
     struct _pn  *pn_right;
   } ParseNode;
 
-#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+
 typedef union _yyvals
   {
     int     iconst;
@@ -64,7 +64,7 @@ typedef union _yyvals
     ParseNode   *pn;
   } YYSTYPE;
 #define YYSTYPE_IS_DECLARED 1
-#endif
+
 
 typedef struct _symtabent
   {
