20b3125c58a2e607c4ad018913bb879a845af35f
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2008-09-01 11:09:46 +0000

    Fixed a major omission in PyMOOSE makefile. The kinetics objects were not passed to the linker. In revisions prior to this, attempt to create any kinetics object results in "Error: could not find any class of name <KinrticClassName>". Also, Cinfo.cpp has been changed to update code generation. From Shell.cpp one debug print has been removed for successful message add call.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@653 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: f589c4a5961206966f3ece0001daf1f5c7e373a9
    Former-commit-id: c3627be992e5462386b726eceb77925004966831
    Former-commit-id: ca5a4e79d1a8e36e7305d9a23491fa6eaa4e5b43

---
+++ pymoose/DiskPanel.h
@@ -0,0 +1,22 @@
+#ifndef _pymoose_DiskPanel_h
+#define _pymoose_DiskPanel_h
+#include "PyMooseBase.h"
+#include "Panel.h"
+
+namespace pymoose{
+    class DiskPanel : public Panel
+    {      public:
+        static const std::string className;
+        DiskPanel(Id id);
+        DiskPanel(std::string path);
+        DiskPanel(std::string name, Id parentId);
+        DiskPanel(std::string name, PyMooseBase& parent);
+        DiskPanel( const DiskPanel& src, std::string name, PyMooseBase& parent);
+        DiskPanel( const DiskPanel& src, std::string name, Id& parent);
+        DiskPanel( const DiskPanel& src, std::string path);
+        DiskPanel( const Id& src, std::string name, Id& parent);
+        ~DiskPanel();
+        const std::string& getType();
+    };
+}
+#endif
---
+++ pymoose/TriPanel.h
@@ -0,0 +1,21 @@
+#ifndef _pymoose_TriPanel_h
+#define _pymoose_TriPanel_h
+#include "PyMooseBase.h"
+#include "Panel.h"
+namespace pymoose{
+    class TriPanel : public Panel
+    {      public:
+        static const std::string className;
+        TriPanel(Id id);
+        TriPanel(std::string path);
+        TriPanel(std::string name, Id parentId);
+        TriPanel(std::string name, PyMooseBase& parent);
+        TriPanel( const TriPanel& src, std::string name, PyMooseBase& parent);
+        TriPanel( const TriPanel& src, std::string name, Id& parent);
+        TriPanel( const TriPanel& src, std::string path);
+        TriPanel( const Id& src, std::string name, Id& parent);
+        ~TriPanel();
+        const std::string& getType();
+    };
+}
+#endif
