28620f58cf0086c1bc6b7cf0ea6adb8770eef44f
hjorth <hjorth@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2008-09-26 18:22:16 +0000

    Added AscFile and TimeTable.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@768 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ builtins/AscFile.cpp
@@ -0,0 +1,241 @@
+#include <fstream>
+#include <math.h>
+#include "moose.h"
+#include "AscFile.h"
+
+const Cinfo* initAscFileCinfo()
+{
+  
+  static Finfo* processShared[] =
+    {
+      new DestFinfo( "process", Ftype1< ProcInfo >::global(),
+                     RFCAST( &AscFile::processFunc ) ),
+      new DestFinfo( "reinit", Ftype1< ProcInfo >::global(),
+                     RFCAST( &AscFile::reinitFunc ) ),
+    };
+      
+  static Finfo* process = 
+    new SharedFinfo( "process", processShared,
+                     sizeof( processShared ) / sizeof( Finfo* ) );
+
+
+  static Finfo* ascFileFinfos[] =
+    {
+      ///////////////////////////////////////////////////////
+      // Field definitions
+      ///////////////////////////////////////////////////////
+      new ValueFinfo( "fileName",
+                      ValueFtype1< string >::global(),
+                      GFCAST( &AscFile::getFileName ),
+                      RFCAST( &AscFile::setFileName )
+                      ),
+      new ValueFinfo( "appendFlag",
+                      ValueFtype1< int >::global(),
+                      GFCAST( &AscFile::getAppendFlag ),
+                      RFCAST( &AscFile::setAppendFlag )
+                      ),
+      ///////////////////////////////////////////////////////
+      // MsgSrc definitions
+      ///////////////////////////////////////////////////////
+      ///////////////////////////////////////////////////////
+      // MsgDest definitions
+      ///////////////////////////////////////////////////////
+      new DestFinfo( "save",
+                     Ftype1< double >::global(),
+                     RFCAST( &AscFile::input )
+                     ),
+      ///////////////////////////////////////////////////////
+      // Synapse definitions
+      ///////////////////////////////////////////////////////
+      ///////////////////////////////////////////////////////
+      // Shared definitions
+      ///////////////////////////////////////////////////////
+      process,
+    };
+
+  static SchedInfo schedInfo[] = { { process, 0, 0 } };
+
+  static Cinfo ascFileCinfo(
+                "AscFile",
+                "Johannes Hjorth, 2008, KTH, Stockholm",
+                "AscFile: Multi-column output to file.",
+                initNeutralCinfo(),
+                ascFileFinfos,
+                sizeof( ascFileFinfos )/sizeof(Finfo *),
+                ValueFtype1< AscFile >::global(),
+                schedInfo, 1
+                );
+
+  return &ascFileCinfo;
+
+}
+
+
+static const Cinfo* ascFileCinfo = initAscFileCinfo();
+
+
+AscFile::AscFile()
+        :
+        fileName_( "utdata.txt" ),
+        nCols_( 0 ),
+        appendFlag_( 0 )
+{
+  fileOut_ = new std::ofstream();
+}
+
+AscFile::~AscFile() {
+  cerr << "DESTROYING AscFile!!" << endl;
+}
+
+///////////////////////////////////////////////////
+
+
+
+void AscFile::processFunc( const Conn* c, ProcInfo p )
+{
+  // The data arriving by message belongs to the previous timestep.
+  static_cast< AscFile* >( c->data() )->processFuncLocal(p->currTime_ - p->dt_);
+}
+
+void AscFile::processFuncLocal(double time)
+{
+  // Write data to file
+
+  cerr << "processFuncLocal" << endl;
+
+  vector< double >::iterator i;
+
+  if(fileOut_->good()) {
+
+    if(time >= 0) {
+      // First column should contain the time
+      *fileOut_ << time << " ";
+
+      for( i = columnData_.begin(); i != columnData_.end(); i++) {
+        *fileOut_ << *i << " ";
+      }
+
+      *fileOut_ << endl;
+    }
+
+    // Empty the old data
+    // columnData_.clear();
+  }
+  else {
+    cout << "AscFile::processFuncLocal, unable to write to file" << endl;
+  }
+
+}
+
+void AscFile::reinitFunc( const Conn* c, ProcInfo info) 
+{
+  static_cast< AscFile* >( c->data() )->reinitFuncLocal(c);
+}
+
+void AscFile::reinitFuncLocal( const Conn* c ) 
+{
+  cerr << "AscFile::reinitFuncLocal called." << endl;
+
+
+  nCols_ = c->target().e->numTargets( "save" );
+  columnData_.resize(nCols_);
+  cout << "Saving " <<  nCols_ << " column(s) of data" << endl;
+
+
+
+  // Close the file if it is open
+  if(fileOut_->is_open()) {
+    fileOut_->close();
+  }
+
+  // Open file for writing
+
+  if(appendFlag_) {
+    fileOut_->open( fileName_.c_str(), ios_base::app );
+  } else {
+    fileOut_->open( fileName_.c_str(), ios_base::trunc );
+  }
+
+  if(!fileOut_->good()) {
+    cout << "AscFile::reinitFuncLocal: Unable to open " << fileName_ << endl;
+  }
+
+  //bup = fileOut_;
+  //bup2 = fileOut_;
+
+}
+
+
+
+///////////////////////////////////////////////////
+// Field function definitions
+///////////////////////////////////////////////////
+
+void AscFile::setFileName( const Conn* c, string s )
+{
+   static_cast< AscFile* >( c->data() )->fileName_ = s;
+}
+
+string AscFile::getFileName( Eref e )
+{
+  return static_cast< AscFile* >( e.data() )->fileName_;
+}
+
+void AscFile::setAppendFlag( const Conn* c, int f )
+{
+  static_cast< AscFile* >( c->data() )->appendFlag_ = f;
+}
+
+int AscFile::getAppendFlag( Eref e )
+{
+  return static_cast< AscFile* >( e.data() )->appendFlag_;
+}
+
+///////////////////////////////////////////////////
+// Dest function definitions
+///////////////////////////////////////////////////
+
+
+void AscFile::input( const Conn* c, double value )
+{
+  static_cast< AscFile* >( c->data() )->inputLocal( c->targetIndex(), value );
+}
+
+void AscFile::inputLocal( unsigned int columnId, double value )
+{
+  cout << "columnId = " << columnId << ", size = " << columnData_.size() << endl;
+
+  // assert(columnId < columnData_.size()); //  Temp. commented
+
+
+  // This is temporary check, since the reinit function fails
+  // to get the right number of incoming messages sometimes
+  if(columnId >= columnData_.size()) {
+    nCols_ = columnId + 1;
+    columnData_.resize(nCols_);
+  }
+
+  columnData_[columnId] = value;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
