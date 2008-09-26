c1ac75199920f97d1cc8be519c68672fca26de04
hjorth <hjorth@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2008-09-26 18:22:16 +0000

    Added AscFile and TimeTable.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@768 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 5d2d32d5cc581e1914507d2a65a360c91a403c98

---
+++ builtins/AscFile.h
@@ -0,0 +1,34 @@
+class AscFile
+{
+ public:
+  AscFile();
+  ~AscFile();
+
+  static void processFunc( const Conn* c, ProcInfo p );
+  void processFuncLocal(double time);
+
+  static void reinitFunc( const Conn* c, ProcInfo info );
+  void reinitFuncLocal( const Conn* c );
+
+  static void input( const Conn* c, double v );
+  void inputLocal( unsigned int columnId, double v );
+
+  static string getFileName( Eref e );
+  static void setFileName( const Conn* c, string s );
+
+  static int getAppendFlag( Eref e );
+  static void setAppendFlag( const Conn* c, int f );
+
+  void writeToFile();
+
+ private:
+
+  string fileName_;
+  vector<double> columnData_;
+  int nCols_;
+  int appendFlag_;
+
+  //std::ofstream *bup2; // If I pad here this one gets overwritten instead.
+  std::ofstream *fileOut_;
+  //std::ofstream *bup;
+};
