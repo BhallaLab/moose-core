8ee5da3f5cfbdb7f893a4763b249fcf58b8f9f38
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2008-10-04 11:12:38 +0000

    Deleted python bytecode file
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@800 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: eaf57dede702b5fd7ae403199a8a4a1e69cc5693

--- pymoose/PyMooseContext.h
+++ pymoose/PyMooseContext.h
@@ -114,9 +114,8 @@ namespace pymoose
         void setupTau( const Id& gateId, vector <double> parms );
         void tweakAlpha( const Id& gateId );
         void tweakTau( const Id& gateId);
-        /// This form deviates from GENESIS version: in GENESIS first arg is cellpath and second arg os filename. TODO - fix 
-        void readCell(string cellpath, string filename, double cm, double rm, double ra, double erestAct, double eleak);
-        void readCell(string cellpath, string filename, vector <double> params);        
+        void readCell(string filename, string cellpath, double cm, double rm, double ra, double erestAct, double eleak);
+        void readCell(string filename, string cellpath, vector <double> params);        
         void readCell(std::string fileName, std::string cellPath);    
         bool exists(const Id& id);
         bool exists(std::string path);
