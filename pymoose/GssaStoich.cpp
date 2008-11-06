8fda4c6e75f31edfe78e5eea3ac5eaf1a97d430e
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2008-11-06 05:44:05 +0000

    Updated PyMOOSE to include new classes in robots directory.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@915 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: bf41a4458f98c2b161392c9f3b2e284529e86165

---
+++ pymoose/GssaStoich.cpp
@@ -0,0 +1,36 @@
+#ifndef _pymoose_GssaStoich_cpp
+#define _pymoose_GssaStoich_cpp
+#include "GssaStoich.h"
+using namespace pymoose;
+const std::string GssaStoich::className = "GssaStoich";
+GssaStoich::GssaStoich(Id id):PyMooseBase(id){}
+GssaStoich::GssaStoich(std::string path):PyMooseBase(className, path){}
+GssaStoich::GssaStoich(std::string name, Id parentId):PyMooseBase(className, name, parentId){}
+GssaStoich::GssaStoich(std::string name, PyMooseBase& parent):PyMooseBase(className, name, parent){}
+GssaStoich::GssaStoich(const GssaStoich& src, std::string objectName, PyMooseBase& parent):PyMooseBase(src, objectName, parent){}
+GssaStoich::GssaStoich(const GssaStoich& src, std::string objectName, Id& parent):PyMooseBase(src, objectName, parent){}
+GssaStoich::GssaStoich(const GssaStoich& src, std::string path):PyMooseBase(src, path){}
+GssaStoich::GssaStoich(const Id& src, std::string name, Id& parent):PyMooseBase(src, name, parent){}
+GssaStoich::~GssaStoich(){}
+const std::string& GssaStoich::getType(){ return className; }
+string GssaStoich::__get_method() const
+{
+    string method;
+    get < string > (id_(), "method",method);
+    return method;
+}
+void GssaStoich::__set_method( string method )
+{
+    set < string > (id_(), "method", method);
+}
+string GssaStoich::__get_path() const
+{
+    string path;
+    get < string > (id_(), "path",path);
+    return path;
+}
+void GssaStoich::__set_path( string path )
+{
+    set < string > (id_(), "path", path);
+}
+#endif
