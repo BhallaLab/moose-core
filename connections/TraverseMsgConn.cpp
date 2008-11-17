7e2901d1c5febc2d71217cdc911521051f530cd6
sijigeorge <sijigeorge@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2008-11-17 07:06:33 +0000

    added a batfile to do regression test on parallel moose
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@943 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- connections/TraverseMsgConn.cpp
+++ connections/TraverseMsgConn.cpp
@@ -7,7 +7,6 @@
 ** GNU Lesser General Public License version 2.1
 ** See the file COPYING.LIB for the full notice.
 **********************************************************************/
-
 #include "header.h"
 #include "TraverseMsgConn.h"
 
