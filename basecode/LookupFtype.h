dc368b9653102ced6d944dee5e639b0dfa702701
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2008-12-01 23:18:38 +0000

    Added paging in MOOSE help function. Cleaned up debug prints from Ftypes. Made Helpless return the fixed string "Not documented." because the quotations clutter up the screen and make the available documentation difficult to see.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@978 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- basecode/LookupFtype.h
+++ basecode/LookupFtype.h
@@ -122,8 +122,6 @@ template < class T1, class T2 > class LookupFtype: public Ftype1< T1 >
     virtual std::string getTemplateParameters() const
     {
         static std::string s = Ftype::full_type(typeid(T1))+","+Ftype::full_type(typeid(T2));
-        cout << "In LookupFtype::getTemplateParameters - " << s << endl;
-        
         return s;
     }
 			/**
