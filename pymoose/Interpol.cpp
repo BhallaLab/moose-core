34bade7394a31eaaf79b0690e8b559eda67a740f
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2008-12-04 18:58:19 +0000

    Renamed pymoose::InterpolationTable to pymoose::Interpol for consistency with MOOSE class name.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@983 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: d076af0f464ea00ef33c24008957dcd36fad4a46

--- pymoose/Interpol.cpp
+++ pymoose/Interpol.cpp
@@ -2,7 +2,6 @@
 #define _pymoose_Interpol_cpp
 #include "pymoose/Interpol.h"
 #include "pymoose/TableIterator.h"
-#include "builtins/Interpol.h"
 
 #ifdef _USE_NUMPY // this was experimental and the relevant  functions should be either here or in moose.i in the xtend block. not both.
 #include <algorithm>
@@ -13,170 +12,171 @@
 using namespace pymoose;
 // We had to change the class name in order to avoid conflict with included moose class Interpol
 // But Interpol is the class name to be passed to base constructor
-const std::string InterpolationTable::className_ = "InterpolationTable";
-InterpolationTable::InterpolationTable(Id id):PyMooseBase(id){}
-InterpolationTable::InterpolationTable(std::string path):PyMooseBase("Interpol", path){}
-InterpolationTable::InterpolationTable(std::string name, Id parentId):PyMooseBase("Interpol", name, parentId){}
-InterpolationTable::InterpolationTable(
+const std::string Interpol::className_ = "Interpol";
+Interpol::Interpol(Id id):PyMooseBase(id){}
+Interpol::Interpol(std::string path):PyMooseBase("Interpol", path){}
+Interpol::Interpol(std::string name, Id parentId):PyMooseBase("Interpol", name, parentId){}
+Interpol::Interpol(
     std::string name,
     PyMooseBase& parent)
     :PyMooseBase("Interpol", name, parent){}
 
-InterpolationTable::InterpolationTable(
-    const InterpolationTable& src,
+Interpol::Interpol(
+    const Interpol& src,
     std::string objectName,
     PyMooseBase& parent)
     :PyMooseBase(src, objectName, parent){}
 
-InterpolationTable::InterpolationTable(
-    const InterpolationTable& src,
+Interpol::Interpol(
+    const Interpol& src,
     std::string objectName,
     Id& parent)
     :PyMooseBase(src, objectName, parent){}
 
-InterpolationTable::InterpolationTable(
-    const InterpolationTable& src,
+Interpol::Interpol(
+    const Interpol& src,
     std::string path)
     :PyMooseBase(src, path)
 {
 }
 
-InterpolationTable::InterpolationTable(
+Interpol::Interpol(
     const Id& src,
     string name,
     Id& parent)
     :PyMooseBase(src, name, parent)
 {
 }
-InterpolationTable::~InterpolationTable(){}
+Interpol::~Interpol(){}
 //Manually edited
 // These are for allowing Table access to constructors in PyMooseBase
-InterpolationTable::InterpolationTable(std::string typeName, std::string objectName, Id parentId):
+Interpol::Interpol(std::string typeName, std::string objectName, Id parentId):
     PyMooseBase(typeName, objectName, parentId)
 {
 }
    
-InterpolationTable::InterpolationTable(std::string typeName, std::string path):
+Interpol::Interpol(std::string typeName, std::string path):
     PyMooseBase(typeName, path)
 {
 }
 
-InterpolationTable::InterpolationTable(std::string typeName, std::string objectName, PyMooseBase& parent):
+Interpol::Interpol(std::string typeName, std::string objectName, PyMooseBase& parent):
     PyMooseBase(typeName, objectName, parent)
 {
 }
 
 
-const std::string& InterpolationTable::getType(){ return className_; }
-double InterpolationTable::__get_xmin() const
+const std::string& Interpol::getType(){ return className_; }
+double Interpol::__get_xmin() const
 {
     double xmin;
     get < double > (id_(), "xmin",xmin);
     return xmin;
 }
-void InterpolationTable::__set_xmin( double xmin )
+void Interpol::__set_xmin( double xmin )
 {
     set < double > (id_(), "xmin", xmin);
 }
-double InterpolationTable::__get_xmax() const
+double Interpol::__get_xmax() const
 {
     double xmax;
     get < double > (id_(), "xmax",xmax);
     return xmax;
 }
-void InterpolationTable::__set_xmax( double xmax )
+void Interpol::__set_xmax( double xmax )
 {
     set < double > (id_(), "xmax", xmax);
 }
-int InterpolationTable::__get_xdivs() const
+int Interpol::__get_xdivs() const
 {
     int xdivs;
     get < int > (id_(), "xdivs",xdivs);
     return xdivs;
 }
-void InterpolationTable::__set_xdivs( int xdivs )
+void Interpol::__set_xdivs( int xdivs )
 {
     set < int > (id_(), "xdivs", xdivs);
 }
-int InterpolationTable::__get_mode() const
+int Interpol::__get_mode() const
 {
     int mode;
     get < int > (id_(), "mode",mode);
     return mode;
 }
-void InterpolationTable::__set_mode( int mode )
+void Interpol::__set_mode( int mode )
 {
     set < int > (id_(), "mode", mode);
 }
-// int InterpolationTable::__get_calc_mode() const
+// int Interpol::__get_calc_mode() const
 // {
 //     int calc_mode;
 //     get < int > (id_(), "calc_mode",calc_mode);
 //     return calc_mode;
 // }
-// void InterpolationTable::__set_calc_mode( int calc_mode )
+// void Interpol::__set_calc_mode( int calc_mode )
 // {
 //     set < int > (id_(), "calc_mode", calc_mode);
 // }
-double InterpolationTable::__get_dx() const
+double Interpol::__get_dx() const
 {
     double dx;
-    get < double > (id_(), "dx",dx);
+    get < double > (id_(), "dx", dx);
     return dx;
 }
-void InterpolationTable::__set_dx( double dx )
+void Interpol::__set_dx( double dx )
 {
     set < double > (id_(), "dx", dx);
 }
-double InterpolationTable::__get_sy() const
+double Interpol::__get_sy() const
 {
     double sy;
     get < double > (id_(), "sy",sy);
     return sy;
 }
-void InterpolationTable::__set_sy( double sy )
+void Interpol::__set_sy( double sy )
 {
     set < double > (id_(), "sy", sy);
 }
-double InterpolationTable::__getitem__( unsigned int index) const
+double Interpol::__getitem__(const unsigned int index) const
 {
     double table;
-    table = static_cast<Interpol*>(id_()->data())->getTableValue(index);    
+    lookupGet <double, unsigned int>(id_.eref(), "table", table, index);
     return table;
 }
-void InterpolationTable::__setitem__(unsigned int index, double value )
+
+void Interpol::__setitem__(unsigned int index, double value )
 {
-    static_cast<Interpol*>(id_()->data())->setTableValue(value, index);
+    lookupSet<double, unsigned int>(id_.eref(), "table", value, index);
 }
 
-TableIterator* InterpolationTable::__iter__()
+TableIterator* Interpol::__iter__()
 {
     return new TableIterator(this);    
 }
-int InterpolationTable::__len__()
+int Interpol::__len__()
 {
     return __get_xdivs()+1;    
 }
 
-int InterpolationTable::__get_calcMode() const
+int Interpol::__get_calcMode() const
 {
     int calc_mode;
     get < int > (id_(), "calc_mode",calc_mode);
     return calc_mode;
 }
-void InterpolationTable::__set_calcMode( int calc_mode )
+void Interpol::__set_calcMode( int calc_mode )
 {
     set < int > (id_(), "calc_mode", calc_mode);
 }
 
-string InterpolationTable::dumpFile() const
+string Interpol::dumpFile() const
 {
     string print;
     get < string > (id_(), "print", print);
     return print;
 }
 
-void InterpolationTable::dumpFile( string fileName, bool append )
+void Interpol::dumpFile( string fileName, bool append )
 {
     if (append)
     {
@@ -188,13 +188,13 @@ void InterpolationTable::dumpFile( string fileName, bool append )
     }    
 }
 
-void InterpolationTable::tabFill(int xdivs, int mode)
+void Interpol::tabFill(int xdivs, int mode)
 {
     set <int, int> (id_(), "tabFill", xdivs, mode);    
 }
 
 #ifdef _USE_NUMPY// this was experimental and the relevant  functions should be either here or in moose.i in the xtend block. not both.
-PyObject* InterpolationTable::__array_struct__()
+PyObject* Interpol::__array_struct__()
 {
     PyArrayObject* result;
     int dimensions[1];
@@ -214,7 +214,7 @@ PyObject* InterpolationTable::__array_struct__()
    This function fills a table object using a Python sequence type object
 */
 
-void InterpolationTable::fillData(PyObject* args)
+void Interpol::fillData(PyObject* args)
 {
     PyObject* input_seq;
     PyArrayObject* array;
@@ -240,9 +240,9 @@ void InterpolationTable::fillData(PyObject* args)
 #include <cmath>
 using namespace std;
 
-void testInterpolationTable(void)
+void testPyMOOSEInterpol(void)
 {
-    InterpolationTable table("/interptbl");
+    Interpol table("/interptbl");
     double xmin = 0.0;
     double xmax = 10.0;
     int xdivs = 100;
