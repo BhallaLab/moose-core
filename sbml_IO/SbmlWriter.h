0fb51d33ac1b1d469134397ea62be059fd0189ed
sijigeorge <sijigeorge@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2009-02-04 11:45:40 +0000

    read and write sbml models
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@1004 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ sbml_IO/SbmlWriter.h
@@ -0,0 +1,30 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment,
+** also known as GENESIS 3 base code.
+**           copyright (C) 2003-2007 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+#ifndef _SBMLWRITER_H
+#define _SBMLWRITER_H
+#include <sbml/SBMLTypes.h>
+class SbmlWriter
+{
+	public:
+		SbmlWriter() {;}
+		~SbmlWriter() {;}
+		void write(string filename,Id location);
+		SBMLDocument* createModel(string filename);
+		bool writeModel(const SBMLDocument* sbmlDoc, const string& filename);
+	private:
+		static int targets(Eref object,	const string& msg,vector< Eref >& target,const string& type = "" );
+
+		static bool isType( Eref object, const string& type );
+};
+extern const Cinfo* initKinComptCinfo();
+extern const Cinfo* initMoleculeCinfo();
+extern const Cinfo* initReactionCinfo();
+#endif // _SBMLWRITER_H
