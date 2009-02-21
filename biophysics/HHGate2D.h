377962224c2eb5697b784162768bb1ba989e24f6
niraj_ <niraj_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2009-02-21 03:52:48 +0000

    Adding class HHChannel2D: analog of genesis tab2Dchannel
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@1026 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ biophysics/HHGate2D.h
@@ -0,0 +1,48 @@
+#ifndef _HHGate2D_h
+#define _HHGate2D_h
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment,
+** also known as GENESIS 3 base code.
+**           copyright (C) 2003-2005 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+class HHGate2D: public HHGate
+{
+	public:
+		HHGate2D()
+		{ ; }
+		
+		static double getAValue( Eref e, const vector< double >& v );
+		static double getBValue( Eref e, const vector< double >& v );
+		
+		static void gateFunc( const Conn* c, double v1 double v2 );
+		
+		static void postCreate( const Conn* c );
+		
+		static void setupAlpha( const Conn* c, vector< double > parms )
+		{ cerr << "Error: HHGate2D: setupAlpha not implemented.\n"; }
+		
+		static void setupTau( const Conn* c, vector< double > parms );
+		{ cerr << "Error: HHGate2D: setupTau not implemented.\n"; }
+		
+		static void tweakAlpha( const Conn* c );
+		{ cerr << "Error: HHGate2D: tweakAlpha not implemented.\n"; }
+		
+		static void tweakTau( const Conn* c );
+		{ cerr << "Error: HHGate2D: tweakTau not implemented.\n"; }
+		
+		static void setupGate( const Conn* c, vector< double > parms );
+		{ cerr << "Error: HHGate2D: setupGate not implemented.\n"; }
+		
+	private:
+		Interpol2D A_;
+		Interpol2D B_;
+};
+
+// Used by solver, readcell, etc.
+extern const Cinfo* initHHGate2DCinfo();
+
+#endif // _HHGate2D_h
