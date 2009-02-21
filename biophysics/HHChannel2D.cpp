25233d978371e5b944e57e5d7f320010e148fa76
niraj_ <niraj_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2009-02-21 08:41:44 +0000

    Preliminary fixes in 2D channel and gate classes.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@1028 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: a205bb8cf858981c52c9896560be6be5dda771fd
    Former-commit-id: d1a4405d099218985f9c63826ed333e59595795c
    Former-commit-id: 9f5abd30b6f853335ec3e87498a81e23f9374ab2

--- biophysics/HHChannel2D.cpp
+++ biophysics/HHChannel2D.cpp
@@ -97,9 +97,9 @@ const Cinfo* initHHChannel2DCinfo()
 	static Cinfo HHChannel2DCinfo(
 		doc,
 		sizeof( doc ) / sizeof( string ),
-		initNeutralCinfo(),
+		initHHChannelCinfo(),
 		HHChannel2DFinfos,
-		sizeof( HHChannel2DFinfos )/sizeof(Finfo *),
+		sizeof( HHChannel2DFinfos ) / sizeof(Finfo *),
 		ValueFtype1< HHChannel2D >::global()
 	);
 
@@ -172,7 +172,7 @@ void HHChannel2D::innerSetXindex( Eref e, string Xindex )
 	
 	int action = 1;
 	int dimension = ( Xdep1_ == -1 ) ? 1 : 2;
-	makeGate( e.e, e->findFinfo( "xGate" ), action, dimension );
+	makeGate( e.e, e->findFinfo( "xGate2D" ), action, dimension );
 }
 
 void HHChannel2D::innerSetYindex( Eref e, string Yindex )
@@ -188,7 +188,7 @@ void HHChannel2D::innerSetYindex( Eref e, string Yindex )
 	
 	int action = 1;
 	int dimension = ( Ydep1_ == -1 ) ? 1 : 2;
-	makeGate( e.e, e->findFinfo( "yGate" ), action, dimension );
+	makeGate( e.e, e->findFinfo( "yGate2D" ), action, dimension );
 }
 
 void HHChannel2D::innerSetZindex( Eref e, string Zindex )
@@ -204,7 +204,7 @@ void HHChannel2D::innerSetZindex( Eref e, string Zindex )
 	
 	int action = 1;
 	int dimension = ( Zdep1_ == -1 ) ? 1 : 2;
-	makeGate( e.e, e->findFinfo( "zGate" ), action, dimension );
+	makeGate( e.e, e->findFinfo( "zGate2D" ), action, dimension );
 }
 
 /**
@@ -219,7 +219,7 @@ void HHChannel2D::innerSetXpower( Eref e, double Xpower )
 	
 	int action = ( Xpower <= 0.0 ) ? 0 : 1;
 	int dimension = ( Xdep1_ == -1 ) ? 1 : 2;
-	makeGate( e.e, e->findFinfo( "xGate" ), action, dimension );
+	makeGate( e.e, e->findFinfo( "xGate2D" ), action, dimension );
 }
 
 void HHChannel2D::innerSetYpower( Eref e, double Ypower )
@@ -232,7 +232,7 @@ void HHChannel2D::innerSetYpower( Eref e, double Ypower )
 	
 	int action = ( Ypower <= 0.0 ) ? 0 : 1;
 	int dimension = ( Ydep1_ == -1 ) ? 1 : 2;
-	makeGate( e.e, e->findFinfo( "yGate" ), action, dimension );
+	makeGate( e.e, e->findFinfo( "yGate2D" ), action, dimension );
 }
 
 void HHChannel2D::innerSetZpower( Eref e, double Zpower )
@@ -245,7 +245,7 @@ void HHChannel2D::innerSetZpower( Eref e, double Zpower )
 	
 	int action = ( Zpower <= 0.0 ) ? 0 : 1;
 	int dimension = ( Zdep1_ == -1 ) ? 1 : 2;
-	makeGate( e.e, e->findFinfo( "zGate" ), action, dimension );
+	makeGate( e.e, e->findFinfo( "zGate2D" ), action, dimension );
 }
 
 int HHChannel2D::dependency( string index, unsigned int dim )
