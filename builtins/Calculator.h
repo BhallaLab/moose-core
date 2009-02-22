6c506f94b33f7f11a2d29c6ec2975cce48d60292
hjorth <hjorth@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2009-02-22 18:07:39 +0000

    Added Calculator.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@1043 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: d75a6072b1c0de9e5f2bf97219ebc3c1b6e2abf6
    Former-commit-id: 75950448e5aea362b49d67658bc8e5ca0268e89d
    Former-commit-id: aa53b44ce4fc48cf64a346aeca8a0d2bc2caa0d1

---
+++ builtins/Calculator.h
@@ -0,0 +1,38 @@
+#ifndef _CALCULATOR_H
+#define _CALCULATOR_H
+
+class Calculator {
+
+ public:
+  Calculator()
+    {
+      ;
+    }
+
+  static void setInitValue( const Conn* c, double v );
+  static double getInitValue( Eref );
+  static double getValue( Eref );
+
+  static void processFunc( const Conn* c, ProcInfo p );
+  static void reinitFunc( const Conn* c, ProcInfo p );
+
+  static void mulValue( const Conn* c, double factor );
+  static void divValue( const Conn* c, double divisor );
+  static void addValue( const Conn* c, double term );
+  static void subValue( const Conn* c, double term );
+
+ private:
+
+  void innerProcessFunc( Eref e, ProcInfo p );
+  void innerReinitFunc( Eref e, ProcInfo p );
+
+  double val_,initVal_;
+  
+
+};
+
+extern const Cinfo* initCalculatorCinfo();
+
+
+
+#endif
