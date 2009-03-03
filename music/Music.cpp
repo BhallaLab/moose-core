96df3103d6c4f6cbdb42b785851abc220e08de00
niraj_ <niraj_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2009-03-03 11:59:56 +0000

    Fixed a crash occurring at quit in the context of MUSIC.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@1062 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- music/Music.cpp
+++ music/Music.cpp
@@ -176,12 +176,11 @@ void Music::finalizeFunc( const Conn* c )
 
 void Music::innerFinalizeFunc( Eref e )
 {
-        std::cout << "Moose shutting down MUSIC communication" << std::endl;
-
-        // cerr << "Music time: " << runtime_->time() << endl;
-
-        runtime_->finalize();
-	delete runtime_;
+	// cerr << "Music time: " << runtime_->time() << endl;
+	if ( runtime_ ) {
+		runtime_->finalize();
+		delete runtime_;
+	}
 	runtime_ = 0;
 }
 
