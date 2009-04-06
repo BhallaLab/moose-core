6f892984c094ace8a1cace70aa96db9c75a7c767
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2009-04-06 23:38:57 +0000

    BROKEN PyMOOSE: an implementation of Izhikevich model added to biophysics. GHK and IntFire added to pymoose. But PyMOOSE cannot be imported because of undefined function declarations in Id.h (definitions of functions for scratchId has been remove, but the declarations are left in the header). Not sure if they are retained for some reason. But that breaks pymoose (and also ksolve I guess).
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@1082 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 0263a57b3f7e49d61307cd066182fd3fa6db81e6

---
+++ pymoose/IzhikevichNrn.cpp
@@ -0,0 +1,138 @@
+#ifndef _pymoose_IzhikevichNrn_cpp
+#define _pymoose_IzhikevichNrn_cpp
+#include "IzhikevichNrn.h"
+using namespace pymoose;
+const std::string IzhikevichNrn::className_ = "IzhikevichNrn";
+IzhikevichNrn::IzhikevichNrn(Id id):PyMooseBase(id){}
+IzhikevichNrn::IzhikevichNrn(std::string path):PyMooseBase(className_, path){}
+IzhikevichNrn::IzhikevichNrn(std::string name, Id parentId):PyMooseBase(className_, name, parentId){}
+IzhikevichNrn::IzhikevichNrn(std::string name, PyMooseBase& parent):PyMooseBase(className_, name, parent){}
+IzhikevichNrn::IzhikevichNrn(const IzhikevichNrn& src, std::string objectName, PyMooseBase& parent):PyMooseBase(src, objectName, parent){}
+IzhikevichNrn::IzhikevichNrn(const IzhikevichNrn& src, std::string objectName, Id& parent):PyMooseBase(src, objectName, parent){}
+IzhikevichNrn::IzhikevichNrn(const IzhikevichNrn& src, std::string path):PyMooseBase(src, path){}
+IzhikevichNrn::IzhikevichNrn(const Id& src, std::string name, Id& parent):PyMooseBase(src, name, parent){}
+IzhikevichNrn::~IzhikevichNrn(){}
+const std::string& IzhikevichNrn::getType(){ return className_; }
+double IzhikevichNrn::__get_Vmax() const
+{
+    double Vmax;
+    get < double > (id_(), "Vmax",Vmax);
+    return Vmax;
+}
+void IzhikevichNrn::__set_Vmax( double Vmax )
+{
+    set < double > (id_(), "Vmax", Vmax);
+}
+double IzhikevichNrn::__get_c() const
+{
+    double c;
+    get < double > (id_(), "c",c);
+    return c;
+}
+void IzhikevichNrn::__set_c( double c )
+{
+    set < double > (id_(), "c", c);
+}
+double IzhikevichNrn::__get_d() const
+{
+    double d;
+    get < double > (id_(), "d",d);
+    return d;
+}
+void IzhikevichNrn::__set_d( double d )
+{
+    set < double > (id_(), "d", d);
+}
+double IzhikevichNrn::__get_a() const
+{
+    double a;
+    get < double > (id_(), "a",a);
+    return a;
+}
+void IzhikevichNrn::__set_a( double a )
+{
+    set < double > (id_(), "a", a);
+}
+double IzhikevichNrn::__get_b() const
+{
+    double b;
+    get < double > (id_(), "b",b);
+    return b;
+}
+void IzhikevichNrn::__set_b( double b )
+{
+    set < double > (id_(), "b", b);
+}
+double IzhikevichNrn::__get_Vm() const
+{
+    double Vm;
+    get < double > (id_(), "Vm",Vm);
+    return Vm;
+}
+void IzhikevichNrn::__set_Vm( double Vm )
+{
+    set < double > (id_(), "Vm", Vm);
+}
+double IzhikevichNrn::__get_u() const
+{
+    double u;
+    get < double > (id_(), "u",u);
+    return u;
+}
+double IzhikevichNrn::__get_Im() const
+{
+    double Im;
+    get < double > (id_(), "Im",Im);
+    return Im;
+}
+double IzhikevichNrn::__get_initVm() const
+{
+    double initVm;
+    get < double > (id_(), "initVm",initVm);
+    return initVm;
+}
+void IzhikevichNrn::__set_initVm( double initVm )
+{
+    set < double > (id_(), "initVm", initVm);
+}
+double IzhikevichNrn::__get_initU() const
+{
+    double initU;
+    get < double > (id_(), "initU",initU);
+    return initU;
+}
+void IzhikevichNrn::__set_initU( double initU )
+{
+    set < double > (id_(), "initU", initU);
+}
+double IzhikevichNrn::__get_alpha() const
+{
+    double alpha;
+    get < double > (id_(), "alpha",alpha);
+    return alpha;
+}
+void IzhikevichNrn::__set_alpha( double alpha )
+{
+    set < double > (id_(), "alpha", alpha);
+}
+double IzhikevichNrn::__get_beta() const
+{
+    double beta;
+    get < double > (id_(), "beta",beta);
+    return beta;
+}
+void IzhikevichNrn::__set_beta( double beta )
+{
+    set < double > (id_(), "beta", beta);
+}
+double IzhikevichNrn::__get_gamma() const
+{
+    double gamma;
+    get < double > (id_(), "gamma",gamma);
+    return gamma;
+}
+void IzhikevichNrn::__set_gamma( double gamma )
+{
+    set < double > (id_(), "gamma", gamma);
+}
+#endif
