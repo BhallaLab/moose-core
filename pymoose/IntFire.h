a68273fc315d133064553a61581e67f5c3474b66
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2009-04-06 23:48:01 +0000

    Missed the IntFire in pymoose in last commit.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@1083 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: afe424b34e4ed92e5f4903858f3f0100af6324df
    Former-commit-id: 62d3d03d72c5081bbb6dc4173c69fb2f8462de51
    Former-commit-id: 9e131d4f9e8f794b22d3f19d79f5e98fbdf60ab4

---
+++ pymoose/IntFire.h
@@ -0,0 +1,39 @@
+#ifndef _pymoose_IntFire_h
+#define _pymoose_IntFire_h
+#include "PyMooseBase.h"
+namespace pymoose{
+    class IntFire : public PyMooseBase
+    {      public:
+        static const std::string className_;
+        IntFire(Id id);
+        IntFire(std::string path);
+        IntFire(std::string name, Id parentId);
+        IntFire(std::string name, PyMooseBase& parent);
+        IntFire( const IntFire& src, std::string name, PyMooseBase& parent);
+        IntFire( const IntFire& src, std::string name, Id& parent);
+        IntFire( const IntFire& src, std::string path);
+        IntFire( const Id& src, std::string name, Id& parent);
+        ~IntFire();
+        const std::string& getType();
+            double __get_Vt() const;
+            void __set_Vt(double Vt);
+            double __get_Vr() const;
+            void __set_Vr(double Vr);
+            double __get_Rm() const;
+            void __set_Rm(double Rm);
+            double __get_Cm() const;
+            void __set_Cm(double Cm);
+            double __get_Vm() const;
+            void __set_Vm(double Vm);
+            double __get_tau() const;
+            double __get_Em() const;
+            void __set_Em(double Em);
+            double __get_refractT() const;
+            void __set_refractT(double refractT);
+            double __get_initVm() const;
+            void __set_initVm(double initVm);
+            double __get_inject() const;
+            void __set_inject(double inject);
+    };
+}
+#endif
