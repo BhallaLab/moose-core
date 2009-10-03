03f97cf2f7513fe0fc23e99ea56f951d7f842b06
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2009-10-03 16:53:10 +0000

    Updated support for HHChannel2D - HSolve still does not support it.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@1363 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- utility/Property.cpp
+++ utility/Property.cpp
@@ -153,7 +153,6 @@ void Property::initialize(string fileName, int format)
     {
         readProperties(fileName, format);
     }
-    cout << Property::getProperty(Property::SIMPATH) << endl;
     initialized_ = true;
 }
 
