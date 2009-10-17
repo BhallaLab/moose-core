f06ea8787d7a81a15afc7ddf40035ca44e663a41
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2009-10-17 17:25:40 +0000

    Updated the scripts with file comments, some utility functions. Tested with the latest NEURON version of Traub et al, 2005 model.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@1383 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- DEMOS/pymoose/traub2005/py/suppyrFRB.py
+++ DEMOS/pymoose/traub2005/py/suppyrFRB.py
@@ -6,9 +6,9 @@
 # Maintainer: 
 # Created: Mon Sep 21 01:45:00 2009 (+0530)
 # Version: 
-# Last-Updated: Sat Oct 17 10:17:38 2009 (+0530)
+# Last-Updated: Sat Oct 17 22:28:30 2009 (+0530)
 #           By: subhasis ray
-#     Update #: 114
+#     Update #: 119
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -123,13 +123,7 @@ class SupPyrFRB(TraubCell):
         mycell = SupPyrFRB(SupPyrFRB.prototype, sim.model.path + "/SupPyrFRB")
         print 'Created cell:', mycell.path
         vm_table = mycell.comp[mycell.presyn].insertRecorder('Vm_suppyrFRB', 'Vm', sim.data)
-        ca_conc_path = mycell.soma.path + '/CaPool'
-        ca_table = None
-        if config.context.exists(ca_conc_path):
-            ca_conc = moose.CaConc(ca_conc_path)
-            ca_table = moose.Table('Ca_suppyrFRB', sim.data)
-            ca_table.stepMode = 3
-            ca_conc.connect('Ca', ca_table, 'inputRequest')
+        ca_table = mycell.soma.insertCaRecorder('CaPool', sim.data)
         pulsegen = mycell.soma.insertPulseGen('pulsegen', sim.model, firstLevel=3e-10, firstDelay=50e-3, firstWidth=50e-3)
 
 
@@ -142,31 +136,17 @@ class SupPyrFRB(TraubCell):
         delta = t2 - t1
         print 'simulation time: ', delta.seconds + 1e-6 * delta.microseconds
         sim.dump_data('data')
-        mycell.dump_cell('suppyrFRB.txt')
         mus_vm = pylab.array(vm_table) * 1e3
         mus_t = linspace(0, sim.simtime * 1e3, len(mus_vm))
-        ca_array = pylab.array(ca_table)
+        mus_ca = pylab.array(ca_table)
+        nrn_vm = trbutil.read_nrn_data('Vm_suppyrFRB.plot', 'test_suppyrFRB.hoc')
+        nrn_ca = trbutil.read_nrn_data('Ca_suppyrFRB.plot', 'test_suppyrFRB.hoc')
+        if len(nrn_vm) > 0:
+            nrn_t = nrn_vm[:, 0]
+            nrn_vm = nrn_vm[:, 1]
+            nrn_ca = nrn_ca[:,1]
 
-        if config.neuron_bin:
-            call([config.neuron_bin, 'test_suppyrFRB.hoc'], cwd='../nrn')
-
-        nrn_vm = trbutil.read_nrn_data('Vm_suppyrFRB.plot')
-        nrn_t = nrn_vm[:, 0]
-        nrn_vm = nrn_vm[:, 1]
-        nrn_ca = trbutil.read_nrn_data('Ca_suppyrFRB.plot')
-        nrn_ca = nrn_ca[:,1]
-        
-        pylab.subplot(211)
-        pylab.plot(nrn_t, nrn_vm, 'y-', label='NEURON')
-        pylab.plot(mus_t, mus_vm, 'g-.', label='MOOSE')
-        pylab.title('Vm in presynaptic compartment of %s' % cls.__name__)
-        pylab.legend()
-        pylab.subplot(212)
-        pylab.plot(nrn_t, nrn_ca, 'r-', label='NEURON')
-        pylab.plot(mus_t, ca_array, 'b-.', label='MOOSE')
-        pylab.title('[Ca2+] in soma of %s' % cls.__name__)
-        pylab.legend()
-        pylab.show()
+        trbutil.do_plot(cls.__name__, mus_t, mus_ca, mus_vm, nrn_t, nrn_ca, nrn_vm)
         
         
 # test main --
