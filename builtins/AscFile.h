2ce20a6e391635e36dd84dd12e9c8b44c69871e6
niraj_ <niraj_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2009-10-23 12:59:00 +0000

    Demo for PulseGen and AscFile objects. Also some cleanup in timetable demo and other places.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@1400 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- builtins/AscFile.h
+++ builtins/AscFile.h
@@ -4,31 +4,62 @@ class AscFile
   AscFile();
   ~AscFile();
 
+  ///////////////////////////////////////////////////
+  // Dest functions
+  ///////////////////////////////////////////////////
   static void processFunc( const Conn* c, ProcInfo p );
   void processFuncLocal(Eref e, double time);
 
   static void reinitFunc( const Conn* c, ProcInfo info );
-  void reinitFuncLocal( const Conn* c );
+  void reinitFuncLocal( Eref e );
 
   static void input( const Conn* c, double v );
   void inputLocal( unsigned int columnId, double v );
 
+ /**
+  * \todo At present this call must be made explicitly at the end of a simulation
+  * to flush the file. Currently elements do not get destroyed at exit, which is
+  * why the destructor also never gets called.
+  */
+  static void closeFunc( const Conn* c );
+  void closeFuncLocal( );
+  
+  ///////////////////////////////////////////////////
+  // Field access functions
+  ///////////////////////////////////////////////////
   static string getFileName( Eref e );
   static void setFileName( const Conn* c, string s );
 
-  static int getAppendFlag( Eref e );
-  static void setAppendFlag( const Conn* c, int f );
+  static int getAppend( Eref e );
+  static void setAppend( const Conn* c, int f );
 
-  void writeToFile();
+  static int getTime( Eref e );
+  static void setTime( const Conn* c, int time );
+
+  static int getHeader( Eref e );
+  static void setHeader( const Conn* c, int header );
+
+  static string getDelimiter( Eref e );
+  static void setDelimiter( const Conn* c, string delimiter );
+
+  static string getComment( Eref e );
+  static void setComment( const Conn* c, string comment );
 
  private:
-
-  string fileName_;
+  string filename_;
+  int append_;
+  int time_;
+  int header_;
+  string delimiter_;
+  string comment_;
+  
   vector<double> columnData_;
-  int nCols_;
-  int appendFlag_;
-
-  //std::ofstream *bup2; // If I pad here this one gets overwritten instead.
-  std::ofstream *fileOut_;
-  //std::ofstream *bup;
+  /*
+   * If fileOut_ is used as an object, instead of a pointer, then this file
+   * does not compile. This is due to a call to the following function during
+   * Cinfo initialization:
+   *    ValueFtype1< AscFile >::global()
+   * Not really sure why, but it has something to do with a copy constructor.
+   */
+  std::ofstream* fileOut_;
 };
