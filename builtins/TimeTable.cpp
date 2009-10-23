654a622a002bd2e702378bf4c115ef9293b75bb1
niraj_ <niraj_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2009-10-23 03:46:32 +0000

    Cleaned up and fixed TimeTable ; added demo.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@1398 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 72739b1ba15ef4a5e28d6c29c10afdbcf5c5ecf9

--- builtins/TimeTable.cpp
+++ builtins/TimeTable.cpp
@@ -31,11 +31,6 @@ const Cinfo* initTimeTableCinfo()
       ///////////////////////////////////////////////////////
       // Field definitions
       ///////////////////////////////////////////////////////
-      new ValueFinfo( "maxTime",
-                      ValueFtype1< double >::global(),
-                      GFCAST( &TimeTable::getMaxTime ),
-                      RFCAST( &TimeTable::setMaxTime )
-                      ),
       new ValueFinfo( "tableVector", 
                       ValueFtype1< vector< double > >::global(),
                       GFCAST( &TimeTable::getTableVector ),
@@ -50,8 +45,6 @@ const Cinfo* initTimeTableCinfo()
                        GFCAST( &TimeTable::getTable ),
                        RFCAST( &TimeTable::setTable )
                        ),
-
-      
       new DestFinfo( "load", Ftype2< string, unsigned int >::global(),
                      RFCAST( &TimeTable::load ),
 					 "Load contents from file.load filename skiplines"
@@ -71,10 +64,6 @@ const Cinfo* initTimeTableCinfo()
       ///////////////////////////////////////////////////////
 
       ///////////////////////////////////////////////////////
-      // Synapse definitions
-      ///////////////////////////////////////////////////////
-
-      ///////////////////////////////////////////////////////
       // Shared definitions
       ///////////////////////////////////////////////////////
       process,
@@ -91,8 +80,8 @@ const Cinfo* initTimeTableCinfo()
 	};
   static Cinfo timeTableCinfo(
                             doc,
-		            sizeof( doc ) / sizeof( string ),
-			    initNeutralCinfo(),
+                            sizeof( doc ) / sizeof( string ),
+                            initNeutralCinfo(),
                             timeTableFinfos,
                             sizeof( timeTableFinfos )/sizeof(Finfo *),
                             ValueFtype1< TimeTable >::global(),
@@ -114,58 +103,38 @@ static const Slot eventSlot =  initTimeTableCinfo()->getSlot( "event" );
 
 TimeTable::TimeTable()
   :
-  maxTime_( 0.0 ),
   state_( 0 ),
   curPos_( 0 ),
   method_( 4 )
-{
-  ;
-  // This is just for testing purposes, REMOVE!
-  //innerLoad("testtider.txt",1);
+{ ; }
 
-}
-
-TimeTable::~TimeTable() {
-  cerr << "DESTRUCTOR CALLED FOR TimeTable" << cerr;
-
-}
+TimeTable::~TimeTable()
+{ ; }
 
 ///////////////////////////////////////////////////
 // Field function definitions
 ///////////////////////////////////////////////////
-
-void TimeTable::setMaxTime( const Conn* c, double time )
-{
-  static_cast< TimeTable* >( c->data() )->maxTime_ = time;
-}
-
-double TimeTable::getMaxTime( Eref e )
-{
-  return static_cast< TimeTable* >( e.data() )->maxTime_;
-}
 
 void TimeTable::setTableVector( const Conn* c, vector< double > table )
 {
   static_cast< TimeTable* >( c->data() )->localSetTableVector( table );
 }
 
-
 vector< double > TimeTable::getTableVector( Eref e )
 {
         return static_cast< TimeTable* >( e.data() )->timeTable_;
 }
-
 
 unsigned int TimeTable::getTableSize( Eref e )
 {
   return static_cast< TimeTable* >( e.data() )->timeTable_.size();
 }
 
-
 void TimeTable::setTable(const Conn* c, double val, const unsigned int& i )
 {
         static_cast< TimeTable* >( c->data() )->setTableValue( val, i );
 }
+
 double TimeTable::getTable( Eref e, const unsigned int& i )
 {
         return static_cast< TimeTable* >( e.data() )->getTableValue( i );
@@ -186,17 +155,13 @@ void TimeTable::innerLoad( const string& fName, unsigned int skipLines )
     return;
   }
 
-  cout << "TimeTable: Reading from " << fName << endl;
-
   if( !fin.good()) {
     cout << "Error: TimeTable::innerload: Unable to open file" 
          << fName << endl;
   }
   
-  for(unsigned int i = 0; (i < skipLines) & fin.good() ; i++) {
+  for(unsigned int i = 0; (i < skipLines) & fin.good() ; i++)
     getline( fin, line );
-    cout << "Skipping header line (" << i+1 << "):" << line << endl;
-  }
   
   timeTable_.clear();
   
@@ -205,19 +170,14 @@ void TimeTable::innerLoad( const string& fName, unsigned int skipLines )
     timeTable_.push_back(dataPoint);
 
     if(dataPoint < dataPointOld) {
-      cout << "TimeTable: Warning: Spike times are not in increasing order."
+      cout << "TimeTable: Warning: Spike times in file " << fName
+           << " are not in increasing order."
            << endl;
     }
 
     dataPointOld = dataPoint;
   }
 }
-
-
-
-///////////////////////////////////////////////////
-// Dest function definitions
-///////////////////////////////////////////////////
 
 void TimeTable::localSetTableVector( const vector< double >& timeTable )
 {
@@ -240,8 +200,6 @@ double TimeTable::getState() {
   return state_;
 }
 
-
-
 void TimeTable::reinitFunc( const Conn* c, ProcInfo info )
 {
   static_cast< TimeTable* >( c->data() )->reinitFuncLocal( );
@@ -251,15 +209,12 @@ void TimeTable::reinitFuncLocal( )
 {
   curPos_ = 0;
   state_ = 0;
-
 }
-
 
 void TimeTable::processFunc( const Conn* c, ProcInfo info )
 {
   static_cast< TimeTable* >( c->data() )->processFuncLocal( c->target(), info );
 }
-
 
 void TimeTable::processFuncLocal( Eref e, ProcInfo info )
 {
@@ -273,14 +228,11 @@ void TimeTable::processFuncLocal( Eref e, ProcInfo info )
 
   state_ = 0;
 
-  if(curPos_ < timeTable_.size() 
-     && info->currTime_ >= timeTable_[curPos_]) {
-
-    state_ = 1;
-    curPos_++;
-
+  if ( curPos_ < timeTable_.size() &&
+       info->currTime_ >= timeTable_[curPos_] ) {
     send1< double >( e, eventSlot, timeTable_[curPos_]);
-
+    curPos_++;
+    state_ = 1;
   }
 
   send1< double >( e, stateSlot, state_ );
