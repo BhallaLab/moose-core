b643fe9a89b8784867dd2dc1c3f6774d8d69360f
karanvasudeva <karanvasudeva@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2010-01-07 14:37:19 +0000

    pymoose: GLcell seems to work correctly. See the testcase pymoose/tests/gl/testGLcell.py.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@1530 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 763a21b970a1a18c680b71f6d6f91db0a4698592
    Former-commit-id: 2bd2bc1b6ab5b699b7a08b60e2b48b8bd800631b
    Former-commit-id: 0ce533c38017a292ed12efe32838f3bdc36daeb6

--- pymoose/tests/gl/testGLcell.py
+++ pymoose/tests/gl/testGLcell.py
@@ -7,29 +7,25 @@ RUNTIME = 0.05
 
 context = PyMooseBase.getContext()
 container = Neutral("/test")
-#proto_file_name = "../../DEMOS/gbar/myelin2.p"
-proto_file_name = "../regression/cell.p"
+proto_file_name = "../../../DEMOS/gbar/myelin2.p"
 context.readCell(proto_file_name, "/test/axon")
 
 gl0 = GLcell("GLcell", container)
-gl0.vizpath = '/text/axon'
+gl0.vizpath = '/test/axon'
 gl0.port = '9999'
 gl0.host = 'localhost'
-#gl0.attribute = 'Vm'
-#gl0.threshold = 0.0015
-#gl0.sync = 'on'
-#gl0.vscale = 10
+gl0.attribute = 'Vm'
+gl0.threshold = 0.0015
+gl0.sync = 'on'
+gl0.vscale = 10
 gl0.bgcolor = '050050050'
-#gl0.highvalue = 0.05
-#gl0.lowvalue = -0.1
-
-# TODO fix the segfault - use a different proto
-# TODO use GLDT for gl0
+gl0.highvalue = 0.05
+gl0.lowvalue = -0.1
 
 context.setClock(0, SIMDT, 0)
 context.setClock(1, PLOTDT, 0)
 context.setClock(4, GLDT, 0)
-
+context.useClock(4, "/#[TYPE=GLcell]")
 
 context.reset()
 context.step(RUNTIME)
