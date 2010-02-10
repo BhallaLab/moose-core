f1e76bccd8cdf827dacdfe6174ce3213f05ac79d
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2010-02-10 08:15:30 +0000

    Added menu item for neuroML: nothing to plot. neuroML_reader seems to always load the model under /root - which should be corrected to allow arbitrary container.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@1596 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 2a093b5ca0aab8f0d74d67db232b8ef5ecddfdba
    Former-commit-id: 620e7e6c66a57bfe9e99d06b48a49ca08862db5a
    Former-commit-id: 3054d177ea59ec755d4a4a2f71b74c862a8a2329

---
+++ pymoose/gui/qt/test.py
@@ -0,0 +1,61 @@
+# test.py --- 
+# 
+# Filename: test.py
+# Description: 
+# Author: subhasis ray
+# Maintainer: 
+# Created: Wed Jan 27 11:33:37 2010 (+0530)
+# Version: 
+# Last-Updated: Wed Jan 27 16:42:01 2010 (+0530)
+#           By: subhasis ray
+#     Update #: 12
+# URL: 
+# Keywords: 
+# Compatibility: 
+# 
+# 
+
+# Commentary: 
+# 
+# 
+# 
+# 
+
+# Change log:
+# 
+# 
+# 
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+# Floor, Boston, MA 02110-1301, USA.
+# 
+# 
+
+# Code:
+
+import sys
+from PyQt4 import QtCore, QtGui, Qsci
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    shell = Qsci.QsciScintilla()
+    shell.setLexer(Qsci.QsciLexerPython(shell))
+    
+    shell.show()
+    app.exec_()
+
+
+# 
+# test.py ends here
