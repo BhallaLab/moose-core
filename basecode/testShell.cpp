3c08936804a409bda564dc6d8d7379afb472fb68
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2010-03-09 07:11:14 +0000

    Minor compatibility fix in testShell.cpp
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/Msg@1625 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- basecode/testShell.cpp
+++ basecode/testShell.cpp
@@ -12,7 +12,8 @@
 
 void testCreate()
 {
-	Eref ser(Id()(), 0 );
+	
+	Eref ser = Id().eref();
 	// Need to get the id back so that I can delete it later.
 	bool ret = SetGet4< string, Id, Id, string >::set( ser, "create", "Neutral", Id(), Id(2) , "testCreate" );
 	assert( ret );
