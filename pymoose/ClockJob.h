ae7f60092dcfe89dc42c6993a1c4e8e7be885513
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2010-04-22 09:32:21 +0000

    Updated ClockJob to have an autoschedule field. Cinfo updated to skip autoscheduling if this is set to 0.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@1732 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 6b1e55348c0922c5a84cbda2f6f27e4f3345ae66

--- pymoose/ClockJob.h
+++ pymoose/ClockJob.h
@@ -21,18 +21,20 @@ namespace pymoose
         double __get_runTime() const;
         void __set_runTime(double runTime);
         double __get_currentTime() const;
-        void __set_currentTime(double currentTime);
+        // void __set_currentTime(double currentTime);
         int __get_nsteps() const;
         void __set_nsteps(int nsteps);
         int __get_currentStep() const;
-        void __set_currentStep(int currentStep);
-        double __get_start() const;
-        void __set_start(double start);
-        int __get_step() const;
-        void __set_step(int step);
-        void resched();
-        void reinit();
-        void stop();
+        int __get_autoschedule() const;
+        void __set_autoschedule(int value);
+        // void __set_currentStep(int currentStep);
+        // double __get_start() const;
+        // void __set_start(double start);
+        // int __get_step() const;
+        // void __set_step(int step);
+        // void resched();
+        // void reinit();
+        // void stop();
         static vector <double> & getClocks(); // check with Upi - if this
         // clock is the set of
         // individual clocks or
