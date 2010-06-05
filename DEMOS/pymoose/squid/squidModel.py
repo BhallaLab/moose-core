f0756fbeb6ea17dbc6dd805c1307d64a1fac97e1
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2010-06-05 09:46:24 +0000

    Fixed PIDController. Some changes towards implementing voltage clamp in Squid demo. Minor spelling correction in RC.cpp
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@1836 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: f3c9afd7b5f2fed1621a6f36c5ce089f32b97366
    Former-commit-id: 280b85a50740fb007c0c10a6e94003a841c365f6
    Former-commit-id: 085bb2d2c4861e2a91ef00f7ca7e48199b9ec7bb

--- DEMOS/pymoose/squid/squidModel.py
+++ DEMOS/pymoose/squid/squidModel.py
@@ -20,7 +20,7 @@ except ImportError:
 from squid import *
 
 import numpy
-
+import pylab
 
 EPSILON = numpy.finfo(float).eps
 class SquidModel(moose.Neutral):
@@ -57,14 +57,8 @@ class SquidModel(moose.Neutral):
         self._hParamTable = moose.Table("hParamTable", self._data)
         self._hParamTable.stepMode = 3
         self._hParamTable.connect("inputRequest", self._squidAxon.Na(), "Y")
-        
-        # Set up the pulsegenrator and table to pull the pulse data
-        self._pulseGen = moose.PulseGen("pulseGen", self._model)
-        self._setupPulseGen()
-        self._pulseGen.connect("outputSrc", self._squidAxon, "injectMsg")
-        self._iInjectTable = moose.Table("iInjectTable", self._data)
-        self._iInjectTable.stepMode = 3
-        self._iInjectTable.connect("inputRequest", self._pulseGen, "output")
+        self._setupElectronics()
+        self._iInjectTable = None
         # Assign clock ticks
         self.getContext().useClock(0, self._model.path+"/##")
         self._squidAxon.useClock(0)
@@ -74,10 +68,53 @@ class SquidModel(moose.Neutral):
         self.getContext().useClock(2,self._data.path+"/#")
         self._runTime = 0.050
 
+
+    def _setupElectronics(self):
+        self._pulseGen = moose.PulseGen("pulseGen", self._model)
+        self._lowpass = moose.RC("lowpass", self._model)
+        self._vClamp = moose.DiffAmp("Vclamp", self._model)
+        self._iClamp = moose.DiffAmp("IClamp", self._model)
+        self._PID = moose.PIDController("PID", self._model)
+        self._pulseGen.firstLevel = 25e-6
+        self._pulseGen.firstWidth = 50e-3
+        self._pulseGen.firstDelay = 2e-3
+        self._pulseGen.secondDelay = 1e6
+        self._pulseGen.trigMode = 1
+        self._pulseGen.trigTime = 0.0
+
+        self._lowpass.R = 1e-3
+        self._lowpass.C = 3e-3
+        self._iClamp.gain = 0.0
+        self._vClamp.gain = 0.0
+        self._PID.gain = 0.5e-6
+        self._PID.tauI = self.simDt()
+        self._PID.tauD = self.simDt()/4.0
+        self._PID.saturation = 1e10
+        # Connect current clamp circuitry
+        self._pulseGen.connect("outputSrc", self._iClamp, "plusDest")
+        self._iClamp.connect("outputSrc", self._squidAxon, "injectMsg")
+        # Connect voltage clamp circuitry
+        self._pulseGen.connect("outputSrc", self._lowpass, "injectMsg")
+        self._lowpass.connect("outputSrc", self._vClamp, "plusDest")
+        self._vClamp.connect("outputSrc", self._PID, "commandDest")
+        self._squidAxon.connect("VmSrc", self._PID, "sensedDest")
+        self._PID.connect("outputSrc", self._squidAxon, "injectMsg")
+        # Tables for recording current and voltage clamp current.
+        # injections We have to maintain two tables as MOOSE does not
+        # report the correct Compartment injection current.
+        self._iClampInjectTable = moose.Table('IClampInject')
+        self._iClampInjectTable.stepMode = 3
+        self._iClampInjectTable.connect("inputRequest", self._iClamp, "output")
+        self._vClampInjectTable = moose.Table('VClampInject')
+        self._vClampInjectTable.stepMode = 3
+        self._vClampInjectTable.connect("inputRequest", self._PID, "output")
+        
+
     def _setupPulseGen(self, paramDict=None):
         self._pulseGen.trigMode = 0
 
-        if paramDict == None: # Default values
+        if paramDict is None: # Default values
+            print '$$$ paramDict is None'
             self._pulseGen.firstLevel = 0.1e-6
             self._pulseGen.firstDelay = 0.005
             self._pulseGen.firstWidth = 0.040
@@ -85,6 +122,8 @@ class SquidModel(moose.Neutral):
             self._pulseGen.secondDelay = 1e8
             self._pulseGen.secondWidth = 0.0
         else:
+            print '$$$ paramDict is :' , paramDict
+
             self._pulseGen.firstLevel = paramDict["firstLevel"]
             self._pulseGen.firstWidth = paramDict["firstWidth"]
             self._pulseGen.firstDelay = paramDict["firstDelay"]
@@ -94,10 +133,8 @@ class SquidModel(moose.Neutral):
             self._pulseGen.baseLevel = paramDict["baseLevel"]
             if paramDict["singlePulse"]:
                 self._pulseGen.trigMode = 1
-            #                 self._pulseGen.secondDelay = 1e8 # Set a large delay to prevent a second pulse within the simulation time
-            else:
-                self._pulseGen.trigMode = 0
-
+                
+                
             print "firstLevel:", self._pulseGen.firstLevel, \
                 "firstWidth:", self._pulseGen.firstWidth, \
                 "firstDelay:", self._pulseGen.firstDelay, \
@@ -105,6 +142,7 @@ class SquidModel(moose.Neutral):
                 "secondWidth:",self._pulseGen.secondWidth, \
                 "secondDelay:", self._pulseGen.secondDelay, \
                 "baseLevel:", self._pulseGen.baseLevel
+
 
     def _setupClocks(self, simDt=None, plotDt=None):
         if simDt == None:
@@ -168,20 +206,10 @@ class SquidModel(moose.Neutral):
         return self._runTime
 
 
-    def doResetForIClamp(self, paramDict=None):
-        """Do setup for current clamp and do a reset.
-        paramDict should contain the following keys:
-        simDt - simulation time step (+ve float)
-        plotDt - plotting time step (+ve float < simDt)
-        firstLevel - amplitude of injection current in first pulse
-        firstWidth - time for which first current injection is applied
-        firstDelay - time from start of simulation when the first pulse should be applied
-        secondLevel - amplitude of second current injection
-        secondDelay - time from the beginning of fisrt pulse when second pulse is to be applied
-        secondWidth - duration of second injection current
-        runTime - total simulation time - must be greater than the total duration of the current injection
+    def doResetForVClamp(self, paramDict=None):
+        """Do setup for voltage clamp and do a reset. 
         """
-        if not paramDict == None:
+        if paramDict is not None:
             simDt = paramDict["simDt"]
             plotDt = paramDict["plotDt"]
             if plotDt < 0.0:
@@ -196,16 +224,81 @@ class SquidModel(moose.Neutral):
             self._squidAxon.blockNaChannel(paramDict["blockNa"])
             self._squidAxon.blockKChannel(paramDict["blockK"])
             self._squidAxon.setIonPotential(paramDict["temperature"], paramDict["naConc"], paramDict["kConc"])
-#             self._runTime = paramDict["runTime"]
+        self._lowpass.R = 1.0
+        self._lowpass.C = 0.03e-4
+        self._vClamp.gain = 1.0
+        self._iClamp.gain = 0.0
+        self._PID.gain = 0.3e-8
+        self._PID.tauI = 200.0 #self.simDt() * 2
+        self._PID.tauD = 1e-2/8 #self.simDt()
+        print 'PID tauD', self._PID.tauD
+        print paramDict
         self.getContext().reset()
-        if (not paramDict == None) and (paramDict["singlePulse"]): # This is an ugly hack borrowed from GENESIS squid demo
-            self._pulseGen.trigTime = 0.0 
+        if paramDict is not None and paramDict["singlePulse"]:
+            self._pulseGen.trigMode = 1
+            self._pulseGen.trigTime = 0.0
+        self._iInjectTable = self._vClampInjectTable
+
+    def doResetForIClamp(self, paramDict=None):
+        """Do setup for current clamp and do a reset.
+        paramDict should contain the following keys:
+
+        simDt - simulation time step (+ve float)
+
+        plotDt - plotting time step (+ve float < simDt)
+
+        firstLevel - amplitude of injection current in first pulse
+
+        firstWidth - time for which first current injection is applied
+
+        firstDelay - time from start of simulation when the first pulse should be applied
+
+        secondLevel - amplitude of second current injection
+
+        secondDelay - time from the beginning of fisrt pulse when second pulse is to be applied
+
+        secondWidth - duration of second injection current
+
+        runTime - total simulation time - must be greater than the total duration of the current injection
+        """
+        print '$$$ reset for IClamp'
+        if paramDict is not None:
+            print '$$ paramdict is:', paramDict
+            simDt = paramDict["simDt"]
+            plotDt = paramDict["plotDt"]
+            if plotDt < 0.0:
+                print "Plot time step must be positive"
+            if simDt < 0.0:
+                print "Simulatipon time step must be postive"
+            if fabs(self._clockTick2.dt - plotDt) > EPSILON:
+                self.setPlotDt(plotDt)
+            if fabs(self._clockTick0.dt - simDt) > EPSILON:
+                self.setSimDt(simDt)
+            self._setupPulseGen(paramDict)
+            self._squidAxon.blockNaChannel(paramDict["blockNa"])
+            self._squidAxon.blockKChannel(paramDict["blockK"])
+            self._squidAxon.setIonPotential(paramDict["temperature"], paramDict["naConc"], paramDict["kConc"])
+        else:
+            self._setupClocks()
+            self._setupPulseGen(paramDict)
+
+        self._iClamp.gain = 1.0
+        self._vClamp.gain = 0.0
+        self._PID.gain = 0.0
+        
+        self.getContext().reset()
+        if paramDict is not None and paramDict["singlePulse"]:
+            self._pulseGen.trigMode = 1
+            self._pulseGen.trigTime = 0.0
+
+        self._iInjectTable = self._iClampInjectTable
         # !SquidModel.doResetForIClamp
 
     def doRun(self, runTime=None):
         if not runTime == None:
             self._runTime = runTime
         self.getContext().step(self._runTime)
+
         
     def dumpPlotData(self):
         self._vmTable.dumpFile("squidModelVm.plot")
