24b29eb1932c0011e4f767a5493f08b267f91d66
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2010-06-15 05:20:35 +0000

    First pass at MMenz implementation. Compiles.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/Msg@1869 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ kinetics/MMenz.cpp
@@ -0,0 +1,181 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2010 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+#include "header.h"
+#include "MMenz.h"
+
+#define EPSILON 1e-15
+
+static SrcFinfo2< double, double > toSub( 
+		"toSub", 
+		"Sends out increment of molecules on product each timestep"
+	);
+
+static SrcFinfo2< double, double > toPrd( 
+		"toPrd", 
+		"Sends out increment of molecules on product each timestep"
+	);
+
+static DestFinfo sub( "subDest",
+		"Handles # of molecules of substrate",
+		new OpFunc1< MMenz, double >( &MMenz::sub ) );
+
+static DestFinfo enzDest( "enz",
+		"Handles # of molecules of MMenzyme",
+		new OpFunc1< MMenz, double >( &MMenz::enz ) );
+
+static DestFinfo prd( "prdDest",
+		"Handles # of molecules of product. Dummy.",
+		new OpFunc1< MMenz, double >( &MMenz::prd ) );
+	
+static Finfo* subShared[] = {
+	&toSub, &sub
+};
+
+static Finfo* prdShared[] = {
+	&toPrd, &prd
+};
+
+const Cinfo* MMenz::initCinfo()
+{
+		//////////////////////////////////////////////////////////////
+		// Field Definitions
+		//////////////////////////////////////////////////////////////
+		static ValueFinfo< MMenz, double > Km(
+			"Km",
+			"Michaelis-Menten constant",
+			&MMenz::setKm,
+			&MMenz::getKm
+		);
+
+		static ValueFinfo< MMenz, double > kcat(
+			"kcat",
+			"Forward rate constant for enzyme",
+			&MMenz::setKcat,
+			&MMenz::getKcat
+		);
+
+		//////////////////////////////////////////////////////////////
+		// MsgDest Definitions
+		//////////////////////////////////////////////////////////////
+		static DestFinfo process( "process",
+			"Handles process call",
+			new EpFunc1< MMenz, ProcPtr >( &MMenz::eprocess ) );
+
+		static DestFinfo group( "group",
+			"Handle for group msgs. Doesn't do anything",
+			new OpFuncDummy() );
+
+		//////////////////////////////////////////////////////////////
+		// Shared Msg Definitions
+		//////////////////////////////////////////////////////////////
+		static SharedFinfo sub( "sub",
+			"Connects to substrate molecule",
+			subShared, sizeof( subShared ) / sizeof( const Finfo* )
+		);
+		static SharedFinfo prd( "prd",
+			"Connects to product molecule",
+			prdShared, sizeof( prdShared ) / sizeof( const Finfo* )
+		);
+
+	static Finfo* mmEnzFinfos[] = {
+		&Km,	// Value
+		&kcat,	// Value
+		&process,			// DestFinfo
+		&enzDest,				// DestFinfo
+		&sub,				// SharedFinfo
+		&prd,				// SharedFinfo
+	};
+
+	static Cinfo mmEnzCinfo (
+		"MMenz",
+		Neutral::initCinfo(),
+		mmEnzFinfos,
+		sizeof( mmEnzFinfos ) / sizeof ( Finfo* ),
+		new Dinfo< MMenz >()
+	);
+
+	return &mmEnzCinfo;
+}
+
+ static const Cinfo* mmEnzCinfo = MMenz::initCinfo();
+
+//////////////////////////////////////////////////////////////
+// MMenz internal functions
+//////////////////////////////////////////////////////////////
+
+
+MMenz::MMenz( )
+	: Km_( 5 ), kcat_( 0.1 )
+{
+	;
+}
+
+//////////////////////////////////////////////////////////////
+// MsgDest Definitions
+//////////////////////////////////////////////////////////////
+
+void MMenz::sub( double n )
+{
+	sub_ *= n;
+}
+
+void MMenz::prd( double n ) // dummy
+{
+	;
+}
+
+void MMenz::enz( double n )
+{
+	enz_ = n;
+}
+
+void MMenz::eprocess( Eref e, const Qinfo* q, ProcInfo* p )
+{
+	process( p, e );
+}
+
+void MMenz::process( const ProcInfo* p, const Eref& e )
+{
+	double rate = enz_ * sub_ / ( Km_ + sub_ );
+	toSub.send( e, p, 0, rate );
+	toPrd.send( e, p, rate, 0 );
+	
+	sub_ = 1.0;
+	enz_ = kcat_;
+}
+
+void MMenz::reinit( const Eref& e, const Qinfo*q, ProcInfo* p )
+{
+	;
+}
+
+//////////////////////////////////////////////////////////////
+// Field Definitions
+//////////////////////////////////////////////////////////////
+
+void MMenz::setKm( double v )
+{
+	Km_ = v;
+}
+
+double MMenz::getKm() const
+{
+	return Km_;
+}
+
+void MMenz::setKcat( double v )
+{
+	kcat_ = v;
+}
+
+double MMenz::getKcat() const
+{
+	return kcat_;
+}
---
+++ kinetics/MMenz.h
@@ -0,0 +1,51 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2010 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+#ifndef _MM_ENZ_H
+#define _MM_ENZ_H
+
+/**
+ * This class represents the Michaelis-Menten type enzyme, obeying the
+ * equation
+ * V = kcat.[Etot].[S]/( Km + [S] )
+ */
+class MMenz: public Data
+{
+	public: 
+		MMenz();
+
+		//////////////////////////////////////////////////////////////////
+		// Field assignment stuff
+		//////////////////////////////////////////////////////////////////
+
+		void setKm( double v );
+		double getKm() const;
+		void setKcat( double v );
+		double getKcat() const;
+
+		//////////////////////////////////////////////////////////////////
+		// Dest funcs
+		//////////////////////////////////////////////////////////////////
+
+		void process( const ProcInfo* p, const Eref& e );
+		void eprocess( Eref e, const Qinfo* q, ProcInfo* p );
+		void reinit( const Eref& e, const Qinfo*q, ProcInfo* p );
+		void sub( double n );
+		void enz( double n );
+		void prd( double n );
+
+		static const Cinfo* initCinfo();
+	private:
+		double Km_;
+		double kcat_;
+		double sub_;
+		double enz_;
+};
+
+#endif // MM_ENZ_H
