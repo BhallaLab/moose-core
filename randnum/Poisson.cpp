a5e37417c63d9885bb8a15347d058fea6be9da69
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2010-06-15 08:27:34 +0000

    Updated moose.py. Updated Izhikevich model to use all SI unit system. The gui-demo is yet to be updated.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@1872 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: a46a67b34ed132e1f171fbbb2b38e0161a02fec2
    Former-commit-id: b1ded1a57ea370321a8c11768b22409e5ece57ff
    Former-commit-id: 8f77e94c734dee679b6d1b9420f0d2d2c6399db4

--- randnum/Poisson.cpp
+++ randnum/Poisson.cpp
@@ -9,7 +9,7 @@
 ** This program is part of 'MOOSE', the
 ** Messaging Object Oriented Simulation Environment,
 ** also known as GENESIS 3 base code.
-**           copyright (C) 2003-2005 Upinder S. Bhalla. and NCBS
+**           copyright (C) 2003-2010 Upinder S. Bhalla. and NCBS
 ** It is made available under the terms of the
 ** GNU General Public License version 2
 ** See the file COPYING.LIB for the full notice.
