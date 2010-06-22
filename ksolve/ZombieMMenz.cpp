662cade2c70aa3d9e6260552a5ac414a2901f945
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2010-06-22 10:06:11 +0000

    Implemented ZombieMMenz.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/Msg@1910 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ ksolve/ZombieMMenz.cpp
@@ -0,0 +1,171 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2010 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+#include "header.h"
+#include "RateTerm.h"
+#include "SparseMatrix.h"
+#include "KinSparseMatrix.h"
+#include "Stoich.h"
+#include "ElementValueFinfo.h"
+#include "ZombieMMenz.h"
+
+static SrcFinfo2< double, double > toSub( 
+		"toSub", 
+		"Sends out increment of molecules on product each timestep"
+	);
+
+static SrcFinfo2< double, double > toPrd( 
+		"toPrd", 
+		"Sends out increment of molecules on product each timestep"
+	);
+
+static DestFinfo sub( "subDest",
+		"Handles # of molecules of substrate",
+		new OpFunc1< ZombieMMenz, double >( &ZombieMMenz::dummy ) );
+
+static DestFinfo enzDest( "enz",
+		"Handles # of molecules of ZombieMMenzyme",
+		new OpFunc1< ZombieMMenz, double >( &ZombieMMenz::dummy ) );
+
+static DestFinfo prd( "prdDest",
+		"Handles # of molecules of product. Dummy.",
+		new OpFunc1< ZombieMMenz, double >( &ZombieMMenz::dummy ) );
+	
+static Finfo* subShared[] = {
+	&toSub, &sub
+};
+
+static Finfo* prdShared[] = {
+	&toPrd, &prd
+};
+
+const Cinfo* ZombieMMenz::initCinfo()
+{
+		//////////////////////////////////////////////////////////////
+		// Field Definitions
+		//////////////////////////////////////////////////////////////
+		static ElementValueFinfo< ZombieMMenz, double > Km(
+			"Km",
+			"Michaelis-Menten constant",
+			&ZombieMMenz::setKm,
+			&ZombieMMenz::getKm
+		);
+
+		static ElementValueFinfo< ZombieMMenz, double > kcat(
+			"kcat",
+			"Forward rate constant for enzyme",
+			&ZombieMMenz::setKcat,
+			&ZombieMMenz::getKcat
+		);
+
+		//////////////////////////////////////////////////////////////
+		// MsgDest Definitions
+		//////////////////////////////////////////////////////////////
+		static DestFinfo process( "process",
+			"Handles process call",
+			new EpFunc1< ZombieMMenz, ProcPtr >( &ZombieMMenz::eprocess ) );
+
+		static DestFinfo group( "group",
+			"Handle for group msgs. Doesn't do anything",
+			new OpFuncDummy() );
+
+		//////////////////////////////////////////////////////////////
+		// Shared Msg Definitions
+		//////////////////////////////////////////////////////////////
+		static SharedFinfo sub( "sub",
+			"Connects to substrate molecule",
+			subShared, sizeof( subShared ) / sizeof( const Finfo* )
+		);
+		static SharedFinfo prd( "prd",
+			"Connects to product molecule",
+			prdShared, sizeof( prdShared ) / sizeof( const Finfo* )
+		);
+
+	static Finfo* mmEnzFinfos[] = {
+		&Km,	// Value
+		&kcat,	// Value
+		&process,			// DestFinfo
+		&enzDest,				// DestFinfo
+		&sub,				// SharedFinfo
+		&prd,				// SharedFinfo
+	};
+
+	static Cinfo zombieMMenzCinfo (
+		"ZombieMMenz",
+		Neutral::initCinfo(),
+		mmEnzFinfos,
+		sizeof( mmEnzFinfos ) / sizeof ( Finfo* ),
+		new Dinfo< ZombieMMenz >()
+	);
+
+	return &zombieMMenzCinfo;
+}
+
+ static const Cinfo* zombieMMenzCinfo = ZombieMMenz::initCinfo();
+
+//////////////////////////////////////////////////////////////
+// ZombieMMenz internal functions
+//////////////////////////////////////////////////////////////
+
+
+ZombieMMenz::ZombieMMenz( )
+{;}
+
+//////////////////////////////////////////////////////////////
+// MsgDest Definitions
+//////////////////////////////////////////////////////////////
+
+void ZombieMMenz::dummy( double n )
+{;}
+
+void ZombieMMenz::eprocess( Eref e, const Qinfo* q, ProcInfo* p )
+{;}
+
+void ZombieMMenz::process( const ProcInfo* p, const Eref& e )
+{;}
+
+void ZombieMMenz::reinit( const Eref& e, const Qinfo*q, ProcInfo* p )
+{;}
+
+//////////////////////////////////////////////////////////////
+// Field Definitions
+//////////////////////////////////////////////////////////////
+
+void ZombieMMenz::setKm( Eref e, const Qinfo* q, double v )
+{
+	rates_[ convertId( e.id() ) ]->setR1( v ); // First rate is Km
+}
+
+double ZombieMMenz::getKm( Eref e, const Qinfo* q ) const
+{
+	return rates_[ convertId( e.id() ) ]->getR1(); // First rate is Km
+}
+
+void ZombieMMenz::setKcat( Eref e, const Qinfo* q, double v )
+{
+	rates_[ convertId( e.id() ) ]->setR2( v ); // Second rate is kcat
+}
+
+double ZombieMMenz::getKcat( Eref e, const Qinfo* q ) const
+{
+	return rates_[ convertId( e.id() ) ]->getR2(); // Second rate is kcat
+}
+
+//////////////////////////////////////////////////////////////
+// Utility function
+//////////////////////////////////////////////////////////////
+
+unsigned int  ZombieMMenz::convertId( Id id ) const
+{
+	unsigned int i = id.value() - objMapStart_;
+	assert( i < objMap_.size() );
+	i = objMap_[i];
+	assert( i < rates_.size() );
+	return i;
+}
---
+++ ksolve/ZombieMMenz.h
@@ -0,0 +1,50 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2010 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+#ifndef _ZOMBIE_MM_ENZ_H
+#define _ZOMBIE_MM_ENZ_H
+
+/**
+ * This class represents the Michaelis-Menten type enzyme, obeying the
+ * equation
+ * V = kcat.[Etot].[S]/( Km + [S] )
+ */
+class ZombieMMenz: public Stoich
+{
+	public: 
+		ZombieMMenz();
+
+		//////////////////////////////////////////////////////////////////
+		// Field assignment stuff
+		//////////////////////////////////////////////////////////////////
+
+		void setKm( Eref e, const Qinfo* q, double v );
+		double getKm( Eref e, const Qinfo* q ) const;
+		void setKcat( Eref e, const Qinfo* q, double v );
+		double getKcat( Eref e, const Qinfo* q ) const;
+
+		//////////////////////////////////////////////////////////////////
+		// Dest funcs
+		//////////////////////////////////////////////////////////////////
+
+		void process( const ProcInfo* p, const Eref& e );
+		void eprocess( Eref e, const Qinfo* q, ProcInfo* p );
+		void reinit( const Eref& e, const Qinfo*q, ProcInfo* p );
+		void dummy( double n );
+
+		//////////////////////////////////////////////////////////////////
+		// Utility  funcs
+		//////////////////////////////////////////////////////////////////
+		unsigned int convertId ( Id id ) const;
+
+		static const Cinfo* initCinfo();
+	private:
+};
+
+#endif // _ZOMBIE_MM_ENZ_H
