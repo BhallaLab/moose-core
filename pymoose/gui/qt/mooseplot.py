1d7e7a052c9c34213250c7c3fcaaf65ffc70e785
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2010-07-05 22:08:15 +0000

    Added a new version of mooseplot to new moosegui.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@1971 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ pymoose/gui/qt/mooseplot.py
@@ -0,0 +1,184 @@
+# mooseplots.py --- 
+# 
+# Filename: mooseplots.py
+# Description: 
+# Author: Subhasis Ray
+# Maintainer: 
+# Created: Mon Jul  5 21:35:09 2010 (+0530)
+# Version: 
+# Last-Updated: Tue Jul  6 03:39:23 2010 (+0530)
+#           By: Subhasis Ray
+#     Update #: 200
+# URL: 
+# Keywords: 
+# Compatibility: 
+# 
+# 
+
+# Commentary: 
+# 
+# Class to handle plotting in MOOSE GUI
+# 
+# 
+
+# Change log:
+# 
+# 
+# 
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+# Floor, Boston, MA 02110-1301, USA.
+# 
+# 
+
+# Code:
+
+
+from PyQt4 import QtGui
+from PyQt4.Qt import Qt
+import PyQt4.Qwt5 as Qwt
+from PyQt4.Qwt5.anynumpy import *
+
+import moose
+
+class MoosePlot(Qwt.QwtPlot):
+    """Handler for plots in MOOSE gui"""
+    plot_index = 0
+    colors = [ Qt.red, 
+               Qt.blue, 
+               Qt.darkYellow, 
+               Qt.green, 
+               Qt.magenta, 
+               Qt.darkCyan,
+               Qt.black, 
+               Qt.cyan, 
+               Qt.darkRed, 
+               Qt.darkGreen, 
+               Qt.yellow, 
+               Qt.darkMagenta,  
+               Qt.gray,   
+               Qt.darkBlue, 
+               Qt.lightGray ]
+    
+    def __init__(self, *args):
+        Qwt.QwtPlot.__init__(self, *args)
+        self.plotNo = MoosePlot.plot_index
+        self.steptime = 1e-3
+        self.simtime = 100e-3
+        MoosePlot.plot_index = MoosePlot.plot_index + 1
+        self.curveIndex = 0
+        self.setCanvasBackground(Qt.white)
+        self.alignScales()
+        self.xmin = 100.1
+        self.curveTableMap = {} # curve -> moose table
+        self.tableCurveMap = {} # moose table -> curve
+        self.setTitle('Plot %d' % (self.plotNo))
+        mY = Qwt.QwtPlotMarker()
+        mY.setLabelAlignment(Qt.AlignRight | Qt.AlignTop)
+        mY.setLineStyle(Qwt.QwtPlotMarker.HLine)
+        mY.setYValue(0.0)
+        mY.attach(self)
+        ###########################################
+        # This is a place holder curve
+        ###########################################
+        dummyCurve = Qwt.QwtPlotCurve('Dummy curve')
+        xx = arange(0.0, 100.1, 0.5)
+        yy = sin(xx * pi / 27.18282)
+        dummyCurve.setData(xx, yy)
+        dummyCurve.setPen(MoosePlot.colors[0])
+        dummyCurve.attach(self)
+        ###########################################
+        self.setAxisTitle(Qwt.QwtPlot.xBottom, 'Time (ms)')
+        self.setAxisTitle(Qwt.QwtPlot.yLeft, 'Values')
+
+        # self.timer = self.startTimer(500)
+
+    def alignScales(self):
+        self.canvas().setFrameStyle(QtGui.QFrame.Box | QtGui.QFrame.Plain)
+        self.canvas().setLineWidth(1)
+        for ii in range(Qwt.QwtPlot.axisCnt):
+            scaleWidget = self.axisWidget(ii)
+            if scaleWidget:
+                scaleWidget.setMargin(0)
+            scaleDraw = self.axisScaleDraw(ii)
+            if scaleDraw:
+                scaleDraw.enableComponent(Qwt.QwtAbstractScaleDraw.Backbone, False)
+
+    def timerEvent(self, e):
+        # print 'Timer event'
+        current_time = moose.PyMooseBase.getContext().getCurrentTime()
+        if current_time + self.steptime < self.simtime:
+            moose.PyMooseBase.getContext().step(self.steptime)
+        elif current_time < self.simtime:
+            moose.PyMooseBase.getContext().step(self.simtime - current_time)
+        else: 
+            self.killTimer(self.timer)
+        current_time = moose.PyMooseBase.getContext().getCurrentTime()
+        if current_time > self.xmin * 1e-3:
+            self.xmin = current_time * 1e3
+        for curve, table in self.curveTableMap.items():
+            if len(table) == 0:
+                print 'Table is 0 length'
+                continue
+            else:
+                print 'Table is %d length' % (len(table))
+            xx = linspace(0.0, current_time * 1e3, len(table))
+            yy = array(table.table)
+            curve.setData(xx, yy)
+        self.replot()
+
+    def addTable(self, table):
+        curve = Qwt.QwtPlotCurve(table.name)
+        curve.setPen(MoosePlot.colors[self.curveIndex])
+        self.curveIndex = self.curveIndex + 1
+        self.curveTableMap[curve] = table
+        self.tableCurveMap[table] = curve
+        current_time = moose.PyMooseBase.getContext().getCurrentTime() * 1e3
+        if current_time > self.xmin:
+            self.xmin = current_time
+        if len(table) > 0:
+            yy = array(table.table)
+            xx = linspace(0.0, current_time, len(yy))
+            curve.setData(xx, yy)
+        curve.attach(self)
+
+import sys
+if __name__ == '__main__':
+    app = QtGui.QApplication([])
+    testComp = moose.Compartment('c')
+    testTable = moose.Table('t')
+    testTable.stepMode = 3
+    testTable.connect('inputRequest', testComp, 'Vm')
+    testPulse = moose.PulseGen('p')
+    testPulse.firstDelay = 50e-3
+    testPulse.firstWidth = 40e-3
+    testPulse.firstLevel = 1e-9
+    testPulse.connect('outputSrc', testComp, 'injectMsg')
+    context = moose.PyMooseBase.getContext()
+    simdt = 1e-4/4
+    context.setClock(0, simdt)
+    context.setClock(1, simdt)
+    context.setClock(2, simdt)
+    stop = 1000 # stop every 1000 steos
+    simtime = 500e-3
+    context.reset()
+    plot = MoosePlot()
+    plot.addTable(testTable)
+    plot.show()
+        
+    sys.exit(app.exec_())
+
+# 
+# mooseplots.py ends here
