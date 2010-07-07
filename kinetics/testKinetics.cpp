a1dcb2afd4393c3fb1ecdc04c71fe5353c1caa30
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2010-07-07 17:08:57 +0000

    Brought in the MathFunc class written by Raamesh into the current tree. Converted it to current form, set up unit tests.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/Msg@1988 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- kinetics/testKinetics.cpp
+++ kinetics/testKinetics.cpp
@@ -9,6 +9,7 @@
 
 #include "header.h"
 #include "ReadKkit.h"
+#include "MathFunc.h"
 
 void testReadKkit()
 {
@@ -26,10 +27,106 @@ void testReadKkit()
 	cout << "." << flush;
 }
 
+bool isClose( double x, double y, double tol )
+{
+	return ( fabs( x - y ) < tol * 1e-8 );
+}
+
+void testMathFunc()
+{
+	Shell* shell = reinterpret_cast< Shell* >( Id().eref().data() );
+	vector< unsigned int > dims( 1, 1 );
+	Id nid = shell->doCreate( "Neutral", Id(), "n", dims );
+	Id mid = shell->doCreate( "MathFunc", nid, "m", dims );
+	MathFunc* math = reinterpret_cast< MathFunc* >( mid.eref().data() );
+	double tolerance = 1.0;
+
+	// Element* n = nid();
+	Element* m = mid();
+	assert( m != 0 );
+	ProcInfo p;
+	double d = 0;
+	Field< string >::set( mid.eref(), "function", "f(x, y, z) = x + y*z" );
+	SetGet1< double >::set( mid.eref(), "arg1", 1.0 );
+	SetGet1< double >::set( mid.eref(), "arg2", 2.0 );
+	SetGet1< double >::set( mid.eref(), "arg3", 3.0 );
+
+	// SetGet1< ProcPtr >::set( mid.eref(), "process", &p );
+
+	math->processFunc( mid.eref(), &p );
+
+	d = Field< double >::get( mid.eref(), "result" );
+
+	assert ( isClose( d, 7.0, tolerance ) );
+	
+	/*mml function*/
+	
+	Field< string >::set( mid.eref(), "mathML", 
+		"<eq/> <apply><ci type=\"function\"> f </ci> <ci> x </ci> <ci> y </ci></apply><apply><plus/>  <ci>x</ci>  <apply><times/><ci>y</ci>  <cn>57</cn> </apply></apply> " );
+	SetGet1< double >::set( mid.eref(), "arg1", 1.0 );
+	SetGet1< double >::set( mid.eref(), "arg2", 2.0 );
+
+	// SetGet1< ProcPtr >::set( mid.eref(), "process", &p );
+	math->processFunc( mid.eref(), &p );
+	d = Field< double >::get( mid.eref(), "result" );
+	assert ( isClose( d, 115.0, tolerance ) );
+	
+	/*A formula from Tyson paper*/
+	Field< string >::set( mid.eref(), "function", 
+		"f(PP1T, CycE, CycA, CycB) = PP1T/(1 + 0.02*(2.33*(CycE+ CycA) + 1.2E1*CycB))");
+	SetGet1< double >::set( mid.eref(), "arg1", 1);
+	SetGet1< double >::set( mid.eref(), "arg2", 2);
+	SetGet1< double >::set( mid.eref(), "arg3", 3);
+	SetGet1< double >::set( mid.eref(), "arg4", 4);
+
+	// SetGet1< ProcPtr >::set( mid.eref(), "process", &p );
+	math->processFunc( mid.eref(), &p );
+	// MathFunc::processFunc( &c, &p);
+
+	d = Field< double >::get( mid.eref(), "result" );
+	assert (
+		isClose( d, 1/(1+0.02*(2.33*(2 + 3)+1.2E1*4)), tolerance )
+	);
+	
+	Field< string >::set( mid.eref(), "function", 
+		"f(ERG, DRG) = 0.5*(0.1*ERG + ((0.2*(DRG/0.3)^2)/(1 + (DRG/0.3)^2)))");
+	SetGet1< double >::set( mid.eref(), "arg1", 1);
+	SetGet1< double >::set( mid.eref(), "arg2", 2);
+	// MathFunc::processFunc( &c, &p);
+	math->processFunc( mid.eref(), &p );
+	// SetGet1< ProcPtr >::set( mid.eref(), "process", &p );
+
+	d = Field< double >::get( mid.eref(), "result" );
+	assert (
+		isClose( d, 0.5*(0.1*1 + (0.2*(2/0.3)*(2/0.3))/(1 + (2/0.3)*(2/0.3))), tolerance )
+	);
+	
+	/*Another formula form Tyson paper*/
+	/*f(ERG, DRG) = 0.5(0.1*ERG + (0.2*(DRG/0.3)^2)/(1 + (DRG/0.3)^2))*/
+	Field< string >::set( mid.eref(), "mathML", 
+		"<eq/> <apply> <ci type=\"function\"> f </ci> <ci> ERG </ci> <ci> DRG </ci> </apply> <apply><times/> <cn>0.5</cn> <apply><plus/> <apply><times/> <cn>0.1</cn> <ci>ERG<ci> </apply> <apply><divide/> <apply><times/> <cn>0.2</cn> <apply><power/> <apply><divide/> <ci>DRG</ci> <cn>0.3</cn> </apply> <cn>2</cn> </apply> </apply> <apply><plus/> <cn>1</cn> <apply><power/> <apply><divide/> <ci>DRG</ci> <cn>0.3</cn> </apply> <cn>2</cn> </apply> </apply> </apply> </apply> </apply>" );
+	SetGet1< double >::set( mid.eref(), "arg1", 1);
+	SetGet1< double >::set( mid.eref(), "arg2", 2);
+	// MathFunc::processFunc( &c, &p);
+// 	SetGet1< ProcPtr >::set( mid.eref(), "process", &p );
+	math->processFunc( mid.eref(), &p );
+
+	d = Field< double >::get( mid.eref(), "result" );
+	//cout << d << endl;
+	//cout << 0.5*(0.1*1 + (0.2*(2/0.3)*(2/0.3))/(1 + (2/0.3)*(2/0.3))) << endl;
+	assert (
+		isClose( d, 0.5*(0.1*1 + (0.2*(2/0.3)*(2/0.3))/(1 + (2/0.3)*(2/0.3))), tolerance )
+	);
+	
+	nid.destroy();
+	cout << "." << flush;
+}
+
 
 void testKinetics()
 {
 	testReadKkit();
+	testMathFunc();
 }
 
 void testMpiKinetics( )
