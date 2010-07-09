d25f8b96a50a631c5bda2f6333014e574b27f7df
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2010-07-09 10:29:55 +0000

    Added connection-setup dialog.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@2001 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: a07746da9f793502dfb1802bc00bf2b1d470efdc

--- pymoose/gui/qt/moosetree.py
+++ pymoose/gui/qt/moosetree.py
@@ -6,9 +6,9 @@
 # Maintainer: 
 # Created: Tue Jun 23 18:54:14 2009 (+0530)
 # Version: 
-# Last-Updated: Thu Jul  8 14:57:05 2010 (+0530)
+# Last-Updated: Fri Jul  9 15:43:42 2010 (+0530)
 #           By: Subhasis Ray
-#     Update #: 147
+#     Update #: 149
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -45,11 +45,12 @@
 
 # Code:
 
-import moose
 import sys
 from PyQt4 import QtCore, QtGui
-sys.path.append('/home/subha/src/moose/pymoose')
 
+import moose
+
+import config
 
 class MooseTreeItem(QtGui.QTreeWidgetItem):
     def __init__(self, *args):
@@ -70,7 +71,6 @@ class MooseTreeItem(QtGui.QTreeWidgetItem):
 	return self.mooseObj_
 
     def updateSlot(self, text):
-        config.LOGGER.debug('updateSlot -- ', self.mooseObj_.name)
 	self.setText(0, QtCore.QString(self.mooseObj_.name))
 
 class MooseTreeWidget(QtGui.QTreeWidget):
