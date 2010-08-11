ab5837f25c428d6b684a5e3997ca53891400eef0
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2010-08-11 19:42:08 +0000

    Added two functions version() and revision() to return the version no and subversion revision no in pymoose.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@2116 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 9bd3404610377baec277b39d85f0c3b81420d1fa
    Former-commit-id: a9326163cec7b47f5fe6b36053c492194bd840e1
    Former-commit-id: 84d2b607f8dd9aadaa2ea4a353c94362a000a7da

--- pymoose/PyMooseContext.h
+++ pymoose/PyMooseContext.h
@@ -25,8 +25,9 @@
 
 namespace pymoose
 {
-const float version = 3.0;
-const char *revision;
+const float version();
+const string revision();
+
 enum FieldType { FTYPE_ALL, FTYPE_VALUE, FTYPE_LOOKUP, FTYPE_SOURCE, FTYPE_DEST, FTYPE_SHARED, FTYPE_SOLVE, FTYPE_THIS, FTYPE_GLOBAL, FTYPE_DEL };
 enum {OUTGOING, INCOMING, INOUT};
     class PyMooseContext
