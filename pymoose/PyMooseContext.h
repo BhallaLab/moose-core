70db16ec8437142c8ccc73912f79162c0e9bd2bc
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2010-08-11 18:56:53 +0000

    Added support for subversion revision no. PyMoose compilation broken.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@2115 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- pymoose/PyMooseContext.h
+++ pymoose/PyMooseContext.h
@@ -25,7 +25,8 @@
 
 namespace pymoose
 {
-
+const float version = 3.0;
+const char *revision;
 enum FieldType { FTYPE_ALL, FTYPE_VALUE, FTYPE_LOOKUP, FTYPE_SOURCE, FTYPE_DEST, FTYPE_SHARED, FTYPE_SOLVE, FTYPE_THIS, FTYPE_GLOBAL, FTYPE_DEL };
 enum {OUTGOING, INCOMING, INOUT};
     class PyMooseContext
