1a35ca89e8d0edb3fabbecaa535292a9bf86905b
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2010-11-04 09:49:48 +0000

    Added header.h
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/Msg@2313 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ testProcLoop/header.h
@@ -0,0 +1,23 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2010 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <pthread.h>
+#include "FuncBarrier.h"
+#include "ProcInfo.h"
+#include "Tracker.h"
+
+extern void addToOutQ( const ProcInfo* p, const Tracker* t );
+extern void* eventLoop( void* info );
+extern void* mpiEventLoop( void* info );
+extern void allocQs();
+extern void swapQ();
+extern void swapMpiQ();
