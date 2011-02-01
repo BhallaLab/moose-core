0c2036e825cf67c05c00ccf5a8f38db69d8ce6ee
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2011-02-01 16:40:41 +0000

    Forgot to add ReduceBase files to managed set for Subversion. Now done.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/Msg@2439 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ basecode/ReduceBase.cpp
@@ -0,0 +1,48 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2011 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+#include "header.h"
+#include "ReduceBase.h"
+
+ReduceBase::ReduceBase()
+{;}
+
+ReduceBase::~ReduceBase()
+{;}
+/////////////////////////////////////////////////////////////////////////
+
+// The function is set up by a suitable SetGet templated wrapper.
+ReduceStats::ReduceStats( double (*func)( const Eref& e ) )
+	: 
+		sum_( 0.0 ),
+		sumsq_( 0.0 ),
+		count_( 0 ),
+		func_( func )
+{;}
+
+ReduceStats::~ReduceStats()
+{;}
+
+void ReduceStats::primaryReduce( const Eref& e )
+{
+	double x = (func_)( e );
+	sum_ += x;
+	sumsq_ += x * x;
+	count_++;
+}
+
+// Must not use other::func_
+void ReduceStats::secondaryReduce( const ReduceBase* other )
+{
+	const ReduceStats* r = dynamic_cast< const ReduceStats* >( other );
+	assert( r );
+	sum_ += r->sum_;
+	sumsq_ += r->sumsq_;
+	count_ += r->count_;
+}
---
+++ basecode/ReduceBase.h
@@ -0,0 +1,54 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2011 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+#ifndef REDUCE_BASE_H
+#define REDUCE_BASE_H
+
+class ReduceBase
+{
+	public:
+		ReduceBase();
+		virtual ~ReduceBase();
+
+		/**
+		 * Reduces the original data type, typically a double. To do this
+		 * in a general way it just looks up the object in this
+		 * interface function.
+		 */
+		virtual void primaryReduce( const Eref& e ) = 0;
+
+		/**
+		 * Reduces contents of other (identical) Reduce subclasses.
+		 */
+		virtual void secondaryReduce( const ReduceBase* other ) = 0;
+	private:
+};
+
+// Here we try it without a type dependence
+// The whole class is suitable to be passed around, but the func_
+// must be ignored.
+class ReduceStats: public ReduceBase
+{
+	public:
+		// The function is set up by a suitable SetGet templated wrapper.
+		ReduceStats( double (*func)( const Eref& e ) );
+		~ReduceStats();
+
+		void primaryReduce( const Eref& e );
+		
+		// Must not use other::func_
+		void secondaryReduce( const ReduceBase* other );
+
+	private:
+		double sum_;
+		double sumsq_;
+		unsigned int count_;
+		double (*func_)( const Eref& e );
+};
+#endif // REDUCE_BASE_H
