629d723816427851ce6a68a2ab35a7cb16bb8d7e
ccluri <ccluri@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2011-03-31 08:53:32 +0000

    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@2592 d1858a74-f715-0410-99c9-c66fe9cce3d6
    Former-commit-id: 3dcb3296ae35291b0e4d62f48a90dfe182c3ff43
    Former-commit-id: 48b50116bf08125877ea65a311b7dacf95ca79c7
    Former-commit-id: b5785e49bfbf2e03289ed0bd68b8ec9e17c2f700

--- GuiOG/updatepaintGL.py
+++ GuiOG/updatepaintGL.py
@@ -8,33 +8,93 @@ from OpenGL.GLUT import *
 from oglfunc.objects import *
 from oglfunc.group import *
 
+import moose
+mc=moose.context
 
 class updatepaintGL(PyGLWidget):
+	
     def paintGL(self):
         PyGLWidget.paintGL(self)
 	self.render()
 
-    def repaintGL(self,coords):	#update the cell diagram with coordinates
-	self.l_coords = coords
-	for i in range(0,len(self.l_coords),1):	
-	    newline=cLine(self,self.l_coords[i])
-	    self.sceneObjects.append(newline)	 
-
+    def repaintGL(self):	#update the canvas with coordinates
+	mode =1
+	
+    def setSelectionMode(self,mode):	
+	self.selectionMode = mode	
+	
     def render(self):
+	if self.lights:
+		glMatrixMode(GL_MODELVIEW)
+		glEnable(GL_LIGHTING)
+		glEnable(GL_LIGHT0)
+		glEnable(GL_COLOR_MATERIAL)
+		
+		light0_pos = 200.0, 200.0, 300.0, 0
+		diffuse0 = 1.0, 1.0, 1.0, 1.0
+		specular0 = 1.0, 1.0, 1.0, 1.0
+		ambient0 = 0, 0, 0, 1
+
+		glMatrixMode(GL_MODELVIEW)
+		glLightfv(GL_LIGHT0, GL_POSITION, light0_pos)
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse0)
+		glLightfv(GL_LIGHT0, GL_SPECULAR, specular0)
+		glLightfv(GL_LIGHT0, GL_AMBIENT, ambient0)
+	
+	
+	self.renderAxis()	#draws 3 axes at origin
+	
+	
 	for obj in self.sceneObjects:
 	    obj.render()
+
 	self.selectedObjects.render()		
 
+	
+    def drawNewCell(self, cellName, style = 2, cellCentre=[0.0,0.0,0.0], cellAngle=[0.0,0.0,0.0,0.0]):		#cellName = moosepath
+    	if style ==1:
+    		self.lights = 0
+    	else: 
+    		self.lights = 1
+    		
+	an=moose.Neutral(cellName)
+	all_ch=an.childList 					#all children
+	ch = self.get_childrenOfField(all_ch,'Compartment')	#compartments only
+	l_coords = []
+	for i in range(0,len(ch),1):
+    	    	x=float(mc.getField(ch[i],'x'))*(1e+04)
+    	    	y=float(mc.getField(ch[i],'y'))*(1e+04)
+    	    	z=float(mc.getField(ch[i],'z'))*(1e+04)
+    	    	x0=float(mc.getField(ch[i],'x0'))*(1e+04)
+    	    	y0=float(mc.getField(ch[i],'y0'))*(1e+04)
+	   	z0=float(mc.getField(ch[i],'z0'))*(1e+04)
+	   	d=float(mc.getField(ch[i],'diameter'))*(1e+04)
+    	    	l_coords.append((x0,y0,z0,x,y,z,d,ch[i].path()))
 
-    #def seltool(self):
-#	self.toolsel = 1	
+	if (self.selectionMode):		#cells are pickable
+		newCell = cellStruct(self,l_coords,cellName,style)
+		newCell._centralPos = cellCentre
+		newCell.rotation = cellAngle
+		self.sceneObjectNames.append(cellName)
+		self.sceneObjects.append(newCell)	
+	else:					#comapartments are pickable
+		for i in range(0,len(l_coords),1):
+			if (moose.Compartment(ch[i]).name=='soma'):
+				compartmentLine = somaSphere(self,l_coords[i],cellName)
+			else:
+				if style==1:
+					compartmentLine=cLine(self,l_coords[i],cellName)
+				else: 	#style==2
+					compartmentLine=cCylinder(self,l_coords[i],cellName)
+			compartmentLine._centralPos = cellCentre
+			compartmentLine.rotation = cellAngle
+			self.sceneObjectNames.append(l_coords[i][7])
+	    		self.sceneObjects.append(compartmentLine)	
 
-#    def mousePressEvent(self, _event):
-#	if (self.toolsel==1):
-	   # if (_event.button()==1):
-	   # 	self.selectiontool(_event.pos())
-		#self.current_point_2D_ = _event.pos()            	
-		#self.current_point_ok_, self.current_point_3D_ = self.map_to_sphere(self.current_point_2D_)
-	    	#self.selectiontool(self.current_point_3D_)		
-
+    def get_childrenOfField(self,all_ch,field):	#'all_ch' is a tuple of moose.id, 'field' is the field to sort with; returns a tuple with valid moose.id's
+        ch=[]
+        for i in range(0,len(all_ch)):	
+	    if(mc.className(all_ch[i])==field):
+	        ch.append(all_ch[i])
+        return tuple(ch)  
 	
