b4bc788046d9e5915a6ef0b6bece6dc548508558
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2011-04-03 15:04:07 +0000

    Implemented geometry classes in new 'geom' subdirectory.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/threadMsg@2613 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ geom/testGeom.cpp
@@ -0,0 +1,19 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2011 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+#include "header.h"
+
+#include "ReduceBase.h"
+#include "ReduceMax.h"
+#include "../shell/Shell.h"
+
+void testGeom()
+{
+	;
+}
