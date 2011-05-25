e7f9d7ced161fc75d4cc999896ef86c52f7936ef
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2011-05-25 07:17:47 +0000

    Implemented SynChanBase, yet to test or use.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/threadMsg@2695 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- biophysics/ChanBase.h
+++ biophysics/ChanBase.h
@@ -4,7 +4,7 @@
 ** This program is part of 'MOOSE', the
 ** Messaging Object Oriented Simulation Environment,
 ** also known as GENESIS 3 base code.
-**           copyright (C) 2003-2005 Upinder S. Bhalla. and NCBS
+**           copyright (C) 2003-2011 Upinder S. Bhalla. and NCBS
 ** It is made available under the terms of the
 ** GNU Lesser General Public License version 2.1
 ** See the file COPYING.LIB for the full notice.
