11369d8ceb1babfc9bcd44842a6026df2090fde3
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2011-06-21 03:33:32 +0000

    Added first-pass unit tests for CubeMesh. OK.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/threadMsg@2784 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: f5de011aeadf3d59614c8efaf4d24ab7010b6d6a

--- mesh/testMesh.cpp
+++ mesh/testMesh.cpp
@@ -12,6 +12,7 @@
 #include "MeshEntry.h"
 #include "ChemMesh.h"
 #include "CylMesh.h"
+#include "CubeMesh.h"
 
 /**
  * Low-level tests for the CylMesh object: No MOOSE calls involved.
@@ -166,11 +167,112 @@ void testMidLevelCylMesh()
 	assert( neighbors[0] = 1 );
 	assert( neighbors[1] = 3 );
 
+	s->doDelete( cylId );
+
 	cout << "." << flush;
 }
+
+/**
+ * Low-level tests for the CubeMesh object: No MOOSE calls involved.
+ */
+void testCubeMesh()
+{
+	CubeMesh cm;
+	assert( cm.getMeshType( 0 ) == CUBOID );
+	assert( cm.getMeshDimensions( 0 ) == 3 );
+	assert( cm.getDimensions() == 3 );
+
+	vector< double > coords( 9 );
+	coords[0] = 0; // X0
+	coords[1] = 0; // Y0
+	coords[2] = 0; // Z0
+
+	coords[3] = 2; // X1
+	coords[4] = 4; // Y1
+	coords[5] = 8; // Z1
+
+	coords[6] = 1; // DX
+	coords[7] = 1; // DY
+	coords[8] = 1; // DZ
+
+	cm.setCoords( coords );
+
+	assert( doubleEq( cm.getX0(), 0 ) );
+	assert( doubleEq( cm.getY0(), 0 ) );
+	assert( doubleEq( cm.getZ0(), 0 ) );
+
+	assert( doubleEq( cm.getX1(), 2 ) );
+	assert( doubleEq( cm.getY1(), 4 ) );
+	assert( doubleEq( cm.getZ1(), 8 ) );
+
+	assert( doubleEq( cm.getDx(), 1 ) );
+	assert( doubleEq( cm.getDy(), 1 ) );
+	assert( doubleEq( cm.getDz(), 1 ) );
+
+	cm.setX0( 1 );
+	cm.setY0( 2 );
+	cm.setZ0( 4 );
+
+	cm.setX1( 5 );
+	cm.setY1( 6 );
+	cm.setZ1( 8 );
+
+	vector< double > temp = cm.getCoords();
+	assert( temp.size() == 9 );
+	assert( doubleEq( temp[0], 1 ) );
+	assert( doubleEq( temp[1], 2 ) );
+	assert( doubleEq( temp[2], 4 ) );
+	assert( doubleEq( temp[3], 5 ) );
+	assert( doubleEq( temp[4], 6 ) );
+	assert( doubleEq( temp[5], 8 ) );
+	assert( doubleEq( temp[6], 1 ) );
+	assert( doubleEq( temp[7], 1 ) );
+	assert( doubleEq( temp[8], 1 ) );
+
+	vector< unsigned int > m2s;
+	vector< unsigned int > s2m( 64, ~0);
+	for ( unsigned int i = 0; i < 4; ++i ) {
+		for ( unsigned int j = 0; j < 4; ++j ) {
+			for ( unsigned int k = 0; k < 4; ++k ) {
+				unsigned int index = ( i * 4 + j ) * 4 + k;
+				if ( i*i + j * j + k * k < 15 ) { 
+					// include shell around 1 octant of sphere
+					s2m[index] = m2s.size();
+					m2s.push_back( index );
+				} else {
+					s2m[index] = ~0;
+				}
+			}
+		}
+	}
+	unsigned int numEntries = m2s.size();
+	cm.setMeshToSpace( m2s );
+	cm.setSpaceToMesh( s2m );
+
+	assert( cm.getNumEntries() == numEntries );
+	assert( cm.getMeshEntrySize( 0 ) == 1 );
+
+	coords = cm.getCoordinates( 0 );
+	assert( doubleEq( coords[0], 1 ) );
+	assert( doubleEq( coords[1], 2 ) );
+	assert( doubleEq( coords[2], 4 ) );
+	assert( doubleEq( coords[3], 2 ) );
+	assert( doubleEq( coords[4], 3 ) );
+	assert( doubleEq( coords[5], 5 ) );
+
+	vector< unsigned int > neighbors = cm.getNeighbors( 0 );
+	assert( neighbors.size() == 3 );
+	assert( neighbors[0] = 1 );
+	assert( neighbors[1] = s2m[ 4 ] );
+	assert( neighbors[2] = s2m[ 16 ] );
+
+	cout << "." << flush;
+}
+
 
 void testMesh()
 {
 	testCylMesh();
 	testMidLevelCylMesh();
+	testCubeMesh();
 }
