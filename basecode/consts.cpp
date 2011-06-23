aa78862f2c63cd44bb02cc45d1c511699d4b3345
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2011-06-23 11:26:47 +0000

    oops, forgot to add the consts.cpp file.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/threadMsg@2798 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: b379170c9b498feb6a4cd0cb008400933460968c

---
+++ basecode/consts.cpp
@@ -0,0 +1,15 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2010 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+#include "header.h"
+
+const double PI = 3.141592653589793;
+
+// There is experimental error in the last sig fig here.
+const double NA = 6.0221415e23; 
