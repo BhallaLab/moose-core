849c87582331ef7d4c85b6ca427f2d0b0cbb89c4
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2011-06-26 02:20:53 +0000

    Fixed STPNMDAChan. Some corrections/cleanup in STPSynChan and SynChan.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@2807 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: b3b5fbc24941d01fa44a58f8d3431d506ef67911

--- biophysics/STPNMDAChan.h
+++ biophysics/STPNMDAChan.h
@@ -1,53 +1,58 @@
-/*******************************************************************
- * File:            STPNMDAChan.h
- * Description:      
- * Author:          Subhasis Ray
- * E-mail:          
- * Created:         2011-06-15 14:45:05 (+0530)
- ********************************************************************/
-/**********************************************************************
-** This program is part of 'MOOSE', the
-** Messaging Object Oriented Simulation Environment,
-** also known as GENESIS 3 base code.
-**           copyright (C) 2003-2005 Upinder S. Bhalla. and NCBS
-** It is made available under the terms of the
-** GNU General Public License version 2
-** See the file COPYING.LIB for the full notice.
-**********************************************************************/
+// STPNMDAChan.h --- 
+// 
+// Filename: STPNMDAChan.h
+// Description: 
+// Author: Subhasis Ray
+// Maintainer: 
+// Copyright (C) 2010 Subhasis Ray, all rights reserved.
+// Created: Sat Jun 25 15:06:52 2011 (+0530)
+// Version: 
+// Last-Updated: Sat Jun 25 15:46:32 2011 (+0530)
+//           By: Subhasis Ray
+//     Update #: 28
+// URL: 
+// Keywords: 
+// Compatibility: 
+// 
+// 
+
+// Commentary: 
+// 
+// 
+// 
+// 
+
+// Change log:
+// 
+// 2011-06-25 15:07:17 (+0530) Started from scratch after the old code
+// turned out to be too much to debug.
+// 
+
+// Code:
 
 #ifndef _STPNMDACHAN_H
 #define _STPNMDACHAN_H
 
+#include "moose.h"
+#include "header.h"
+
+#include "STPSynChan.h"
 
 class STPNMDAChan: public STPSynChan
-{    
+{
   public:
-    // Functions duplicated from SynChan
     STPNMDAChan();
-    virtual ~STPNMDAChan(){};
-    ///////////////////////////////////////////////////
-    // Dest function definitions
-    ///////////////////////////////////////////////////
-
-
-    ///////////////////////////////////////
-    // Functions specific to STPNMDAChan
-    ///////////////////////////////////////
-    static void setTransitionParam(const Conn* c, double value, const unsigned int& index);
-    static double getTransitionParam(Eref e, const unsigned int& index);
-    static double getUnblocked(Eref e);
-    static double getSaturation(Eref e);
-    static void setSaturation(const Conn * conn, double value);
-    static void setMgConc(const Conn* conn, double conc);
+    virtual ~STPNMDAChan();    
+    static void setMgConc(const Conn * conn, double value);
     static double getMgConc(Eref e);
-
-///////////////////////////////////////////////////
-// Protected fields.
-///////////////////////////////////////////////////
-
+    static void setTransitionParam(const Conn * conn, double value, const unsigned int & index);
+    static double getTransitionParam(Eref e, const unsigned int& index);
+    static void setSaturation(const Conn * conn, double value);
+    static double getSaturation(Eref e);
+    static double getUnblocked(Eref e);
   protected:
-    virtual void innerSynapseFunc( const Conn* c, double time );
-    virtual void innerProcessFunc( Eref e, ProcInfo p );
+    virtual void innerSynapseFunc(const Conn * conn, double time);
+    virtual void innerProcessFunc(Eref e, ProcInfo p);
     virtual void innerReinitFunc( Eref e,  ProcInfo p );
     void innerSetTransitionParam(Eref e, double value, const unsigned int index);
     double innerGetTransitionParam(Eref e, unsigned int index);
@@ -56,11 +61,19 @@ class STPNMDAChan: public STPSynChan
     void innerSetSaturation(double value);
     void innerSetMgConc(double value);
     double innerGetMgConc();
-
-    double saturation_, unblocked_, Mg_, A_, B1_, B2_, decayFactor_;
+    /////////////////////////////////////
+    // Fields
+    /////////////////////////////////////
+    double saturation_, unblocked_, MgConc_, A_, B1_, B2_, decayFactor_;
     vector<double> c_;
     priority_queue<SynInfo> oldEvents_;
-    
 };
 
+extern const Cinfo * initSTPSynChanCinfo();
+
+
 #endif
+
+
+// 
+// STPNMDAChan.h ends here
