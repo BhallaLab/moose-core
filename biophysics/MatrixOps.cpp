31082e9f842f7630de62bcc9b356a348b40ab1ea
darthshak <darthshak@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2011-06-27 06:54:39 +0000

    Second round of optimizations to the matrix inverse function. Earlier implementation took about 4.5 us/call on average to execute, which has now been brought down to 2.78 us/call on average. Reduction was made possible by obviating the need for (expensive) matrix multiplications for merely permuting the columns of the final inverse.
    Added more robust singularity checks. The inverse method
    now uses a very small number as a pivot when singularity is detected and
    proceeds to give an answer, instead of terminating as before. 
    
    
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/threadMsg@2809 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- biophysics/MatrixOps.cpp
+++ biophysics/MatrixOps.cpp
@@ -12,7 +12,13 @@
 #include <iostream>
 #include "MatrixOps.h" 
 
-#define VERSION_2 1
+#define FIRST 1
+#define SECOND 2
+
+//Idea taken from the implementation of the DGETRF method in LAPACK. When 
+//the pivot is zero, we divide by a small number instead of simply throwing up
+//an error and not returning a result.
+#define EPSILON 1e-15
 
 using std::cerr;
 
@@ -33,14 +39,11 @@ Matrix* matMatMul( Matrix* A, Matrix* B )
 	return C;
 }
 
-void matMatMul( Matrix* A, Matrix* B, unsigned int dummy )
+void matMatMul( Matrix* A, Matrix* B, unsigned int resIndex )
 {
 	unsigned int n = A->size();
 	Matrix *C = matAlloc( n );
-//	Matrix *Bt;
-	dummy = 0;			//To keep the compiler happy.
-
-//	Bt = matTrans(B);
+//	dummy = 0;			//To keep the compiler happy.
 
 	for( unsigned int i = 0; i < n; ++i )
 	{
@@ -51,13 +54,32 @@ void matMatMul( Matrix* A, Matrix* B, unsigned int dummy )
 		}
 	}
 
-	for( unsigned int i = 0; i < n; ++i )
-	{
-		for( unsigned int j = 0; j < n; ++j )
-			(*A)[i][j] = (*C)[i][j];
-	}
+	if ( resIndex == FIRST )
+		*A = *C;
+	else if ( resIndex == SECOND )
+		*B = *C;
+}
 
-	delete C;
+void matPermMul( Matrix* A, vector< unsigned int >* swaps )
+{
+	unsigned int i,j,n,index;
+	double temp;
+	n = A->size();
+
+	while (!swaps->empty())
+	{
+		index = swaps->back();
+		swaps->pop_back();
+		i = index % 10;
+		j = (index / 10 ) % 10;
+
+		for( unsigned int l = 0; l < n; ++l ) 
+		{
+			temp = (*A)[l][i];
+			(*A)[l][i] = (*A)[l][j];
+			(*A)[l][j] = temp;
+		}
+	}
 }
 
 Matrix* matMatAdd( Matrix& A, Matrix& B ) 
@@ -173,20 +195,11 @@ Matrix* matTrans( Matrix* A )
 	return At;
 }
 
-/*void vecVecScalAdd( Matrix::iterator& xItr, Matrix::iterator& yItr, 
-										double alpha, double beta ) 
-{
-	unsigned int n = (*xItr).size();	
-
-	for( unsigned int i = 0; i < n; ++i )
-		(*xItr)[i] = alpha * x[i] + beta * y[i];
-}*/
-
 double doPartialPivot( Matrix* A, unsigned int row, unsigned int col,
 											 vector< unsigned int >* swaps )
 {
-	unsigned int n = A->size(), pivotRow = 0;
-	double pivot = 0.0;		
+	unsigned int n = A->size(), pivotRow = row;
+	double pivot = (*A)[row][col];		
 	
 	for( unsigned i = row; i < n; ++i )
 	{
@@ -198,7 +211,7 @@ double doPartialPivot( Matrix* A, unsigned int row, unsigned int col,
 	}
 
 	//If pivot is non-zero, do the row swap.
-	if ( pivot != 0 && pivotRow != row )
+	if ( !doubleEq(pivot,0.0) && pivotRow != row )
 	{
 		Matrix::iterator pivotRowItr = (A->begin() + pivotRow); 
 		Matrix::iterator currRowItr = (A->begin() + row); 
@@ -210,57 +223,53 @@ double doPartialPivot( Matrix* A, unsigned int row, unsigned int col,
 		swaps->push_back( 10 * pivotRow + row ); 
 		return pivot;
 	}
-	else if ( pivot > 0 && pivotRow == row )	
+	else if ( !doubleEq(pivot, 0.0) && pivotRow == row )	
 		return (*A)[row][col];			//Choice of pivot is unchanged.
 	else
 		return 0;										//Matrix is singular!
 }
 
-void matInv( Matrix* A, Matrix* P, vector< unsigned int >* swaps )
+void matInv( Matrix* A, vector< unsigned int >* swaps, Matrix* invA )
 {
-	Matrix *U, *L, *invL, *invU, *invA;		
+	Matrix *L, *invL;		
 	unsigned int n = A->size(), i, j, diagPos;
 	double pivot, rowMultiplier1, rowMultiplier2;
 
 	L = matAlloc( n );   
-	U = matAlloc( n );   
+	invL = matAlloc( n );
+	*invA = *A;
+
 	//The upper triangular portion is stored and inverted in invA
 
 	//Creating a copy of the input matrix, as well as initializing the 
 	//lower triangular matrix L.
 	for (i = 0; i < n; ++i)
-	{
 		(*L)[i][i] = 1;
-		for (j = 0; j < n; ++j)
-			(*U)[i][j] = (*A)[i][j];
-	}
-	invL = L;
 
-	////////////////
-	//Reduction of A to upper triangular form U.
-	//////////////
+	////////////////////////////////////
+	//Calculation of LU decomposition.
+	///////////////////////////////////
+	for ( unsigned int k = 0; k < n; ++k )
+		pivot = doPartialPivot( invA, k, k, swaps );
+
 	diagPos = 0;
 	pivot = 0;
 	i = 1;
 	j = 0;
-	
-	//Pivoting for the first column.
-	pivot = doPartialPivot( U, 0, 0, swaps );
-
 	while( diagPos < n - 1 )
 	{
-		//Direct computation of the terms of the inverse of the lower
-		//triangular matrix L.
-		(*L)[i][j] = (*U)[i][j] / (*U)[diagPos][j];
 
 		//Carrying out the row operations in this fashion seemed like a way of 
 		//reducing round-off error, at the cost of one extra floating point
 		//operation.  
-		rowMultiplier1 = (*U)[diagPos][j];
-		rowMultiplier2 = (*U)[i][j];
+		rowMultiplier1 = (*invA)[diagPos][j];		//Pivot element.
+		rowMultiplier2 = (*invA)[i][j];
+
 		for( unsigned int k = j; k < n; ++k )
-			(*U)[i][k] = ( (*U)[i][k] * rowMultiplier1 - 
-									   (*U)[diagPos][k] *rowMultiplier2 ) / rowMultiplier1;
+			(*invA)[i][k] = ( (*invA)[i][k] * rowMultiplier1 - 
+									   (*invA)[diagPos][k] *rowMultiplier2 ) / rowMultiplier1;
+
+		(*L)[i][j] = rowMultiplier2 / rowMultiplier1;
 
 		if ( i != n - 1 ) 
 			++i;
@@ -268,72 +277,76 @@ void matInv( Matrix* A, Matrix* P, vector< unsigned int >* swaps )
 		{
 			++j;
 			++diagPos;
-
-			if ( diagPos < n - 1)
+			//In case of zero pivot, divide by a very small number whose value we
+			//know.
+			if ( doubleEq( (*invA)[diagPos][diagPos], 0.0 ) )
 			{
-				pivot = doPartialPivot( U, diagPos, diagPos, swaps );
-				if ( doubleEq( pivot, 0.0 ) )
-				{
-					cerr << "Matrix is singular!\n";
-					return 0;
-				}
+				cerr << "Warning : Singularity detected. Proceeding with computation"
+								"anyway.\n";
+				(*invA)[diagPos][diagPos] = EPSILON; 
 			}
 
 			i = diagPos + 1;	
 		}
 	}
-	//End of computation of L and U.
+
+/*	if ( doubleEq( (*invA)[n-1][n-1], 0.0 ) ) 
+		cerr << "Warning : Matrix is singular!\n";*/
+
+	//End of computation of L and U (which is stored in invA).
 	////////////////////////////
 
 	////////////////////////////
-	//Obtaining the inverse of U and L, which is obtained by solving the 
+	//Obtaining the inverse of invA and L, which is obtained by solving the 
 	//simple systems Ux = I and Lx= I.
 	///////////////////////////
 
-	invU = U;
 	double sum = 0;
-	int k,l,m;
-	
+
 	//We serially solve for the equations Ux = e_n, Ux=e_{n-1} ..., Ux = e1. 
 	//where, e_k is the k'th elementary basis vector.
-	for( k = n - 1; k >= 0; --k )
+	for( int k = n - 1; k >= 0; --k )
 	{
-		for ( l = k; l >= 0; --l )
+		for ( int l = k; l >= 0; --l )
 		{
-			m = k;
 			sum = 0;
-			while ( m > l && l != k)
+
+			if ( l != k )
 			{
-				sum += (*U)[l][m] * (*invU)[m][k];
-				--m;
+				for ( int m = k; m > l; --m )
+					sum += (*invA)[l][m] * (*invA)[m][k];
 			}
 
 			if ( l == k )
-				(*invU)[l][k] = (1 - sum)/(*U)[l][l]; 
+				(*invA)[l][k] = (1 - sum)/(*invA)[l][l]; 
 			else
-				(*invU)[l][k] = -sum/(*U)[l][l];
+				(*invA)[l][k] = -sum/(*invA)[l][l];
 		}
 	}
 
 	//Similarly as above, we find the inverse of the lower triangular matrix by
-	//back-substitution.
+	//forward-substitution.
 
-	invL = L;
-	for( k = 0; k <= n - 1; ++k )
+	*invL = *L;
+	double invTemp;
+
+	//Always true when the diagonal elements are 1.
+	for( unsigned int k = 0; k < n - 1; ++k )
+		(*invL)[k+1][k] = -(*invL)[k+1][k];
+
+	for( unsigned int k = 0; k <= n - 1; ++k )
 	{
-		for( l = k + 1; l <= n - 1; ++l )  
+		for( unsigned int l = k + 2; l <= n - 1; ++l )  
 		{
-			m = 0;
-			sum = 0;
-			while( m < l ) 
-			{
-				sum += (*L)[l][m] * (*invL)[m][k];
-				++m;
-			}
+			invTemp = 0;
+			for ( unsigned int m = k + 1; m <= n - 1; ++m )
+				invTemp -= (*invL)[m][k] * (*L)[l][m];
 
-			(*invL)[l][k] = -sum;
+			(*invL)[l][k] = invTemp;
 		}
 	}
+
+
 	//End of computation of invL and invU. Note that they have been computed in
 	//place, which means the original copies of L and U are now gone.
 	/////////////////////////////
@@ -343,35 +356,9 @@ void matInv( Matrix* A, Matrix* P, vector< unsigned int >* swaps )
 	//P is calculated only if there was any pivoting carried out.
 	////////////////////////////
 
-	matMatMul( invU, invL, VERSION_2 );	
+	matMatMul( invA, invL, FIRST );	
 	if ( !swaps->empty() )
-	{
-		//Permutation matrix needs to be reset before anything further
-		//can be done.
-		for( unsigned int k = 0; k < n; ++k )
-		{
-			(*P)[k][k] = 1;
-			for( unsigned int l = 0; l < n; ++l )
-			{
-				if ( l != k )
-					(*P)[l][k] = 0;
-			}
-		}
-
-		unsigned int index;
-		while ( !swaps->empty() )
-		{
-			index = swaps->back();
-			i = index % 10;
-			j = ( index / 10 ) % 10;
-
-			Matrix::iterator row1Itr = (P->begin() + i); 
-			Matrix::iterator row2Itr = (P->begin() + j); 
-			swap( *row1Itr, *row2Itr );
-			matMatMul( invU, P, VERSION_2 );
-			swaps->pop_back();
-		}
-	}
+		matPermMul( invA, swaps );
 
 	////////////////////////
 	//At this stage, L^(-1) has already been calculated. U^(-1) is just -U, and
@@ -382,12 +369,9 @@ void matInv( Matrix* A, Matrix* P, vector< unsigned int >* swaps )
 	//inverse is just U^(-1) * L^(-1).
 	////////////////////////
 
-	invA = invU;
-
 	delete invL;
-	//Cannot delete U or invU as invA points to them!
+	delete L;
 
-	return invA;
 }
 
 Matrix* matAlloc( unsigned int n )
