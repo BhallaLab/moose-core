f84517073b48789b78e68018fe39d21a31765c54
ccluri <ccluri@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2011-08-03 04:30:15 +0000

    1) adding current clamp and voltage clamp electrodes. 2)a dialog for the electrodes. 3) changed the axis, to the right corner of the gl canvas.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@2864 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- pymoose/gui/qt/PyGLWidget.py
+++ pymoose/gui/qt/PyGLWidget.py
@@ -91,7 +91,7 @@ class PyGLWidget(QtOpenGL.QGLWidget):
         self.viewport_matrix_   = []
         self.projection_matrix_ = []
         self.near_   = 1.0		#0.1
-        self.far_    = 500.0
+        self.far_    = 200.0
         self.fovy_   = 4.0
         self.radius_ = 10.0
         self.last_point_2D_ = QtCore.QPoint()
@@ -99,8 +99,14 @@ class PyGLWidget(QtOpenGL.QGLWidget):
         self.last_point_3D_ = [1.0, 0.0, 0.0]
         self.isInRotation_  = False
 	
-	#additions by chaitanya
 	
+
+        #additions by chaitanya
+	#additions by chaitanya
+	self.xpan = 0.0
+        self.ypan = 0.0
+        self.zpan = 0.0
+
 	self.lights = 1			#lights	
 	self.ctrlPressed = False 	#default no control pressed
 	self.selectedObjects =Group(self)		#each line is a scene object.
@@ -259,9 +265,11 @@ class PyGLWidget(QtOpenGL.QGLWidget):
     def wheelEvent(self, _event):
         # Use the mouse wheel to zoom in/out
         d =  float(_event.delta()) / 200.0 * self.radius_
-	self.translate([0.0, 0.0, d])
-        self.updateGL()
-        _event.accept()
+        if ((self.zpan+d) <= (self.near_+3) and (self.zpan+d) >= -1*(self.far_-10)):
+            self.translate([0.0, 0.0, d])
+            self.zpan = self.zpan + d
+            self.updateGL()
+            _event.accept()
 	#print self.z_d
 
     def mousePressEvent(self, _event):
@@ -315,6 +323,9 @@ class PyGLWidget(QtOpenGL.QGLWidget):
             n      = 0.01 * self.radius_
             up     = math.tan(fovy / 2.0 * math.pi / 180.0) * n
             right  = aspect * up
+
+            self.xpan += 2.0 * dx / w * right / n * z
+            self.ypan += -2.0 * dy / h * up / n * z
 
             self.translate( [2.0 * dx / w * right / n * z,
                              -2.0 * dy / h * up / n * z,
@@ -371,22 +382,30 @@ class PyGLWidget(QtOpenGL.QGLWidget):
 	    self.ctrlPressed = True	
 	elif (ev.key() == QtCore.Qt.Key_Up):
 		self.translate([0.0, 0.25, 0.0])
+                self.ypan += 0.25
 		self.updateGL()
 	elif (ev.key() == QtCore.Qt.Key_Down):
 		self.translate([0.0, -0.25, 0.0])
+                self.ypan += -0.25
 		self.updateGL()
 	elif (ev.key() == QtCore.Qt.Key_Left):
 		self.translate([-0.25, 0.0, 0.0])
+                self.xpan += -0.25
 		self.updateGL()
 	elif (ev.key() == QtCore.Qt.Key_Right):
 		self.translate([0.25, 0.0, 0.0])
+                self.xpan += 0.25
 		self.updateGL()
 	elif (ev.key() == QtCore.Qt.Key_Plus)or(ev.key() == QtCore.Qt.Key_PageUp)or(ev.key()==QtCore.Qt.Key_Period):
-		self.translate([0.0, 0.0, 0.75])
+            if ((self.zpan+0.75) <= self.near_+3):
+                self.translate([0.0, 0.0, 0.75])
+                self.zpan += 0.75
 		self.updateGL()
 	elif (ev.key() == QtCore.Qt.Key_Minus)or(ev.key() == QtCore.Qt.Key_PageDown)or(ev.key()==QtCore.Qt.Key_Comma):
-		self.translate([0.0, 0.0, -0.75])
-		self.updateGL()
+             if ((self.zpan-0.75) >= -1*(self.far_-10)):
+        	self.translate([0.0, 0.0, -0.75])
+                self.zpan += -0.75
+                self.updateGL()
 	elif (ev.key() == QtCore.Qt.Key_A):
 		self.rotate([1.0, 0.0, 0.0],2.0)
 		self.updateGL()
@@ -516,23 +535,43 @@ class PyGLWidget(QtOpenGL.QGLWidget):
 	"""
 	Creates the XYZ axis in the 0 coordinate, just for reference.
 	"""
-	# XYZ axis
-	glLineWidth(1)
+        glViewport(0,0,self.width()/8,self.height()/8)
+        self.translate([-1*self.xpan,-1*self.ypan,-1*self.zpan])
+        glMatrixMode(GL_MODELVIEW)
+
+        # XYZ axis
+	glLineWidth(2)
 	glDisable(GL_LIGHTING)
 	glBegin(GL_LINES)
 	glColor(1, 0, 0)	#Xaxis, Red color
 	glVertex3f(0, 0, 0)
-	glVertex3f(1, 0, 0)
+	glVertex3f(0.15, 0, 0)
 	glColor(0, 1, 0)	#Yaxis, Green color
 	glVertex3f(0, 0, 0)
-	glVertex3f(0, 1, 0)
+	glVertex3f(0, 0.15, 0)
 	glColor(0, 0, 1)	#Zaxis, Blue color
 	glVertex3f(0, 0, 0)
-	glVertex3f(0, 0, 1)
+	glVertex3f(0, 0, 0.15)
 	glEnd()
-	glEnable(GL_LIGHTING)
+ 
 	glLineWidth(1)	
-    
+
+        glutInit()
+        glColor(1,0,0)
+        glRasterPos3f(0.16, 0.0, 0.0)
+        glutBitmapCharacter(GLUT_BITMAP_8_BY_13,88)#ascii x
+        glColor(0,1,0)
+        glRasterPos3f(0.0, 0.16, 0.0)
+        glutBitmapCharacter(GLUT_BITMAP_8_BY_13,89)#ascii y
+        glColor(0,0,1)
+        glRasterPos3f(0.0, 0.0, 0.16)
+        glutBitmapCharacter(GLUT_BITMAP_8_BY_13,90)#ascii z
+
+        glEnable(GL_LIGHTING)
+
+        self.translate([self.xpan,self.ypan,self.zpan])
+        glViewport(0,0,self.width(),self.height())
+
 
 #===============================================================================
 #
