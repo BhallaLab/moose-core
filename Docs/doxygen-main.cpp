a6514f55c14797513d8bedba117d8a291ba8595d
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2011-08-16 18:10:52 +0000

    Added some documentation about threading, MPI, and process loop.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/newQ@2911 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 3a0039f6032739927718e4f3e13ce96536b4bfb4

--- Docs/doxygen-main.cpp
+++ Docs/doxygen-main.cpp
@@ -10,6 +10,28 @@ neuronal networks, and to still larger systems. it is backwards-compatible
 with GENESIS, and forward compatible with Python and XML-based model 
 definition standards like SBML and NeuroML. 
 
+MOOSE uses Python as its primary scripting language. For backward 
+compatibility we have a GENESIS scripting module, but this is deprecated.
+MOOSE uses Qt/OpenGL for its graphical interface. The entire GUI is
+written in Python, and the MOOSE numerical code is written in C++.
+
+MOOSE is currently in beta 1.3. One more beta, 1.4 is planned, which
+will have a first-order functional GUI. Following this
+we'll go into release-candidate mode with RC0.9 and so on.
+
+\section support_sec Hardware and availability
+MOOSE runs on everything from laptops to large clusters. It supports
+multiple-core machines through threading, and cluster architectures using
+MPI, the Message Passing Interface. MOOSE is compiled for Linux,
+MacOS, and to the extent that we can get it to compile, on Windows.
+
+MOOSE is free software.
+MOOSE makes extensive use of external libraries. The main MOOSE code itself
+is LGPL, meaning it is easy to reuse with attribution but will remain
+free. However, the common release of MOOSE uses the GNU scientific library
+(GSL) which is under the GPL. For such releases, MOOSE should be treated
+as also being under the GPL.
+
 ================================================================================
 
 Some notes about adding general documentation in Doxygen. These notes can be
