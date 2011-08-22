0f43d39f30ebbbda60193cbf618add1faef4cf4a
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2011-08-22 07:44:16 +0000

    Fixed compilation error due to multiline documentation string.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/newQ@2936 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- pymoose/moosemodule.cpp
+++ pymoose/moosemodule.cpp
@@ -7,9 +7,9 @@
 // Copyright (C) 2010 Subhasis Ray, all rights reserved.
 // Created: Thu Mar 10 11:26:00 2011 (+0530)
 // Version: 
-// Last-Updated: Mon Aug 22 11:52:58 2011 (+0530)
+// Last-Updated: Mon Aug 22 13:11:10 2011 (+0530)
 //           By: Subhasis Ray
-//     Update #: 4045
+//     Update #: 4046
 // URL: 
 // Keywords: 
 // Compatibility: 
@@ -178,11 +178,11 @@ extern "C" {
         {"stop", (PyCFunction)_pymoose_stop, METH_VARARGS, "Stop simulation"},
         {"isRunning", (PyCFunction)_pymoose_isRunning, METH_VARARGS, "True if the simulation is currently running."},
         {"exists", (PyCFunction)_pymoose_exists, METH_VARARGS, "True if there is an object with specified path."},
-        {"loadModel", (PyCFunction)_pymoose_loadModel, METH_VARARGS, "Load model from a file to a specified path.\n
-Parameters:
-\tstr filename -- model description file.\n
-\tstr modelpath -- moose path for the top level element of the model model to be created.\n
-\tstr solverclass -- (optional) solver type to be used for simulating the model.\n"},
+        {"loadModel", (PyCFunction)_pymoose_loadModel, METH_VARARGS, "Load model from a file to a specified path.\n"
+"Parameters:"
+"\tstr filename -- model description file.\n"
+"\tstr modelpath -- moose path for the top level element of the model model to be created.\n"
+"\tstr solverclass -- (optional) solver type to be used for simulating the model.\n"},
         {"connect", (PyCFunction)_pymoose_connect, METH_VARARGS, "Create a message between srcField on src element to destField on target element."},        
         {"getCwe", (PyCFunction)_pymoose_getCwe, METH_VARARGS, "Get the current working element."},
         {"setCwe", (PyCFunction)_pymoose_setCwe, METH_VARARGS, "Set the current working element."},
