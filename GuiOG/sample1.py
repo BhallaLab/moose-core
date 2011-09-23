33459e5ac4509902d69abf8bf0b93b81bccc2d3e
ccluri <ccluri@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2011-09-23 11:35:57 +0000

    inclusion of a player to unpickle saved files
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@2991 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 782c024c9462515408daba549e7f9069f286735a

--- GuiOG/sample1.py
+++ GuiOG/sample1.py
@@ -1,13 +1,19 @@
 import sys
+import pickle
+import numpy
 from PyQt4 import QtGui,QtCore
 from PyQt4.Qt import Qt
-import pickle
-from header import newGLWindow
+import h5py
+#from header import newGLWindow
+from updatepaintGL import newGLWindow
 from oglfunc.sobjects import *
 
 def updateColor(w,q,t):
+#    p = numpy.digitize(q[t],w.stepVals)
+    
     for i in range(len(w.vizObjects)):
         w.vizObjects[i].r,w.vizObjects[i].g,w.vizObjects[i].b = w.colorMap[q[t][i]-1]
+        w.vizObjects[i].radius = indRadius[r[t][i]-1]
     w.updateGL()
 
 def getValue(value):
@@ -16,7 +22,7 @@ def getValue(value):
 def playMovie():
     t = newWin.slider.value()
     t += 1        
-    if t < len(q)-1:
+    if t < len(q):
         newWin.slider.setValue(t)
     else:
         stopTimer()
@@ -36,7 +42,7 @@ def playMovie():
 #         #w.rotate([1,0,0],-50)
 
 def startTimer():
-    newWin.ctimer.start(200)
+    newWin.ctimer.start(50)
 
 def stopTimer():
     newWin.ctimer.stop()
@@ -47,8 +53,18 @@ newWin.show()
 newWin.setWindowState(Qt.WindowMaximized)
 
 f = open(sys.argv[1],'r')
-q = pickle.load(f)
+p = pickle.load(f)
 f.close()
+
+q = []
+dummy=[]
+for i in range(3060):
+    dummy = []
+    for j in range(3060):
+        dummy.append(p[i+(j*10001)])
+    q.append(dummy)
+
+indRadius = numpy.arange(0.05,0.20,0.005)
 
 #timer
 newWin.ctimer = QtCore.QTimer()
@@ -83,12 +99,51 @@ newWin.verticalLayout.addWidget(newWin.slider)
 newWin.setWindowState(Qt.WindowMaximized)
 
 w =  newWin.mgl
-cnf = q[len(q)-1]
-for i in range(len(cnf)):
-    a = locals()[cnf[i][0]](w,cnf[i][1],cnf[i][2])
-    a.setCellParentProps(cnf[i][3],cnf[i][4],1,0,0)
-    w.vizObjects.append(a)
+i = 0
+for yAxis in range(60):
+    for xAxis in range(60):
+        if i < 3060:
+            comp = somaDisk(parent=w,cellName=str(i),l_coords=[0,0,0,0,0,0,0,str(i)])
+            comp.radius = 0.20
+            comp._centralPos=[xAxis*0.5,yAxis*0.5,0.0]
+            #w.sceneObjects.append(comp)
+            #w.sceneObjectNames.append(str(i))
+            w.vizObjects.append(comp)
+            #w.vizObjectNames.append(str(i))
+            i += 1
+
 w.setColorMap()
+w.setColorMap_2()
 w.updateGL()
-         
+
+print 'done drawing'
+
+r=[0]*len(q)
+for i in range(len(q)):
+    q[i] = numpy.digitize(q[i],w.stepVals)
+    r[i] = numpy.digitize(q[i],w.stepVals_2)
+
+# f = h5py.File(sys.argv[1],'r')
+# group = f['/Vm']
+# vm = None
+
+# for cellName in group.keys():
+#     tmp = numpy.zeros(shape=group[cellName].shape, dtype='float64')
+#     tmp[:] = group[cellName][:]
+#     if vm is None:
+#         vm = tmp
+#     else:
+#         vm = numpy.hstack((vm, tmp))
+# print 'reading done'
+
+# f = open('vm.pkl','w')
+# pickle.dump(vm,f)
+# f.close()
+
+# for i in range(len(vm)):
+#         q.append(numpy.digitize(vm[i],w.stepVals))
+
+# print len(q)
+# print 'digitizing'         
+
 sys.exit(app.exec_())
