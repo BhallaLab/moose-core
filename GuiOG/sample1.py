dd928011a3620228a83c3fa93bad73e359388fc7
ccluri <ccluri@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2011-09-27 06:09:06 +0000

    some changes in way h5py files are read. visualization of subhasis's data
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@2996 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- GuiOG/sample1.py
+++ GuiOG/sample1.py
@@ -4,13 +4,10 @@ import numpy
 from PyQt4 import QtGui,QtCore
 from PyQt4.Qt import Qt
 import h5py
-#from header import newGLWindow
-from updatepaintGL import newGLWindow
+from header import newGLWindow
 from oglfunc.sobjects import *
 
 def updateColor(w,q,t):
-#    p = numpy.digitize(q[t],w.stepVals)
-    
     for i in range(len(w.vizObjects)):
         w.vizObjects[i].r,w.vizObjects[i].g,w.vizObjects[i].b = w.colorMap[q[t][i]-1]
         w.vizObjects[i].radius = indRadius[r[t][i]-1]
@@ -52,19 +49,28 @@ newWin = newGLWindow()
 newWin.show()
 newWin.setWindowState(Qt.WindowMaximized)
 
-f = open(sys.argv[1],'r')
-p = pickle.load(f)
-f.close()
+vm = []
+name_comp =[]
+
+def read_data(filename):
+    f = h5py.File(filename, 'r')
+    vmnode = f['/Vm']
+    for key, value in vmnode.items():
+        vm.append(numpy.zeros(len(value)))
+        vm[-1][:] = value[:]
+        name_comp.append(key)
+#    print 'done reading from file'
+
+read_data(sys.argv[1]) #give h5 file here
 
 q = []
-dummy=[]
-for i in range(3060):
+for i in range(10001):
     dummy = []
     for j in range(3060):
-        dummy.append(p[i+(j*10001)])
+            dummy.append(vm[j][i])
     q.append(dummy)
+#print 'done reshaping'
 
-indRadius = numpy.arange(0.05,0.20,0.005)
 
 #timer
 newWin.ctimer = QtCore.QTimer()
@@ -109,19 +115,22 @@ for yAxis in range(60):
             #w.sceneObjects.append(comp)
             #w.sceneObjectNames.append(str(i))
             w.vizObjects.append(comp)
-            #w.vizObjectNames.append(str(i))
+            w.vizObjectNames.append(name_comp[i])
             i += 1
 
 w.setColorMap()
 w.setColorMap_2()
 w.updateGL()
-
 print 'done drawing'
 
+#colormaps
+indRadius = numpy.arange(0.05,0.20,0.005)
 r=[0]*len(q)
 for i in range(len(q)):
     q[i] = numpy.digitize(q[i],w.stepVals)
     r[i] = numpy.digitize(q[i],w.stepVals_2)
+#print 'done assigning colormaps'
+
 
 # f = h5py.File(sys.argv[1],'r')
 # group = f['/Vm']
