ecd2f6b0e0c1c4d3c05100deaf0686b718fded5e
ccluri <ccluri@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2011-11-04 11:29:30 +0000

    additing the feature of saving the visualization as a movie, .avi works with mencoder minor changes for min pan offset. player.py for custom visualization of MAPK_glu, Ca data.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@3154 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: b25c056a705f734a68e16ed33ef50fc55a57838a
    Former-commit-id: 77226cce4b2b69b328d3e2058e683433ba6ce72a
    Former-commit-id: e1d355289ebec5e4ef273acf7da2726a89199558

--- GuiOG/sample.py
+++ GuiOG/sample.py
@@ -5,13 +5,48 @@ import pickle
 from header import newGLWindow
 from oglfunc.sobjects import *
 
+playTimeStep = 200
+
 def updateColor(w,q,t):
     for i in range(len(w.vizObjects)):
         w.vizObjects[i].r,w.vizObjects[i].g,w.vizObjects[i].b = w.colorMap[q[t][i]-1]
     w.updateGL()
 
+def saveAsMovie(w,startFrame,stopFrame):
+    for framenum in range(startFrame,stopFrame+1):
+        newWin.slider.setValue(framenum)
+        pic = w.grabFrameBuffer()
+        pic.save('movie/sim_'+str(framenum)+'.png','PNG')
+    
+    tempPath = os.getcwd()
+    os.chdir('movie')
+    f = open('filelist.txt','w')
+    filelist = ["sim_"+str(i)+".png" for i in range(startFrame,stopFrame+1)]
+    f.write('\n'.join(filelist))
+    f.close()
+
+    ## mpeg4 compression
+    os.system('mencoder mf://@filelist.txt -mf w=1280:h=800:fps=10:type=png '\
+                  ' -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy -o simulation.avi')
+
+    os.chdir(tempPath)
+
+def saveFrames():
+    w.inputDialog = QtGui.QInputDialog()
+    text,pressOK =  w.inputDialog.getText(w.inputDialog,'Save Frames(Movie)','First,Last:')
+    if pressOK:
+        frameNumbers = str(text).split(',')
+        if len(frameNumbers) == 2 :
+            if (int(frameNumbers[0]) < int(frameNumbers[1])):
+                saveAsMovie(w,int(frameNumbers[0]),int(frameNumbers[1]))
+            else:
+                print 'starting frame > ending frame, encountered a causality Error'
+        else:
+            print 'wrong inputs'
+
 def getValue(value):
     updateColor(w,q,value)
+    statusBar.showMessage(str('Showing '+ str(value)+ '/' +str(len(q)-2)))
     
 def playMovie():
     t = newWin.slider.value()
@@ -36,10 +71,11 @@ def playMovie():
 #         #w.rotate([1,0,0],-50)
 
 def startTimer():
-    newWin.ctimer.start(200)
+    newWin.ctimer.start(playTimeStep)
 
 def stopTimer():
     newWin.ctimer.stop()
+
 
 app = QtGui.QApplication(sys.argv) 
 newWin = newGLWindow()
@@ -71,11 +107,22 @@ newWin.pauseButton.setIcon(QtGui.QIcon('pause.png'))
 newWin.pauseButton.setGeometry(30,0,30,30)
 newWin.connect(newWin.pauseButton, QtCore.SIGNAL('clicked()'),stopTimer)
 
+#saveButton
+newWin.saveButton = QtGui.QToolButton(newWin.toolbar)
+newWin.saveButton.setIcon(QtGui.QIcon('save.png'))
+newWin.saveButton.setGeometry(60,0,30,30)
+newWin.connect(newWin.saveButton, QtCore.SIGNAL('clicked()'),saveFrames)
+
 #slider
 newWin.slider = QtGui.QSlider(QtCore.Qt.Horizontal,newWin.centralwidget)
 newWin.slider.setRange(0,len(q)-2)
 newWin.slider.setTickPosition(0)
 newWin.connect(newWin.slider, QtCore.SIGNAL('valueChanged(int)'), getValue)
+
+#statusbar
+statusBar = QtGui.QStatusBar(newWin)
+newWin.setStatusBar(statusBar)
+statusBar.showMessage(str('Loaded ' + sys.argv[1]+ ', has '+ str(len(q)-2)+ ' time instances'))
 
 #default setting
 #newWin.verticalLayout.addWidget(newWin.toolbar)
