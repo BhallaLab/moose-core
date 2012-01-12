3c4af160d69b473fe57d2a3c63fa6331bcc71ed1
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-01-12 16:18:24 +0000

    Added skeleton directory and files for SimManager.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/dh_branch@3249 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ manager/testSimManager.cpp
@@ -0,0 +1,27 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2012 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+void testBuildFromBareKineticTree()
+{
+}
+
+void testBuildFromKkitTree()
+{
+}
+
+void testMakeStandardElements()
+{
+}
+
+void testSimManager()
+{
+	testBuildFromBareKineticTree();
+	testBuildFromKkitTree();
+	testMakeStandardElements();
+}
