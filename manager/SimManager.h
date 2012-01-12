7d864ae1991596ee891b6fea41dc0709716e71f3
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-01-12 16:18:24 +0000

    Added skeleton directory and files for SimManager.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/dh_branch@3249 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 58c83a86a9f80cc8bd62186448f3d6f0482f930c
    Former-commit-id: 4c374a4480e06138e7a1e5b8d3d1f3fc8024b12d

---
+++ manager/SimManager.h
@@ -0,0 +1,49 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2012 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+class SimManager
+{
+	public:
+		enum TreeType { CHEM_ONLY, KKIT, CHEM_SPACE, CHEM_SPACE_MULTISOLVER, SIGNEUR };
+
+		SimManager();
+		~SimManager();
+
+		// Field Definitions
+		void setAutoPlot( bool v );
+		bool getAutoPlot() const;
+
+		void setSyncTime( double v );
+		double getSyncTime() const;
+
+		/// Destination function
+		void build( const Eref& e, const Qinfo* q, string method );
+
+
+		// Utility functions
+		TreeType findTreeType( const Eref& e );
+		void buildFromBareKineticTree( const string& method );
+		void buildFromKkitTree( const string& method );
+		void makeStandardElements( const Eref& e, const Qinfo*q, string meshClass );
+
+		static const Cinfo* initCinfo();
+	private:
+		/// syncTime is the interval between synchronizing various solvers.
+		double syncTime_;
+
+		/** 
+		 * When autoPlot is true, the SimManager builds plots automatically
+		 * for fields that it thinks are of interest
+		 */
+		bool autoPlot_;
+
+		Id baseId_;
+
+		double plotdt_;
+};
