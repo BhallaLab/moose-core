340802aab24e0d0246515ce4b5b2775d079ad9f4
ccluri <ccluri@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-01-20 09:57:47 +0000

    added additional missing files.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@3262 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ GuiOG/Moogli/layout/graphPlotter.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'graphPlotter.ui'
+#
+# Created: Wed Jan 11 00:19:36 2012
+#      by: PyQt4 UI code generator 4.8.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.verticalLayout = QtGui.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.plotCanvas = MplWidget(self.centralwidget)
+        self.plotCanvas.setObjectName(_fromUtf8("plotCanvas"))
+        self.verticalLayout.addWidget(self.plotCanvas)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "Plots", None, QtGui.QApplication.UnicodeUTF8))
+
+from mplwidget import MplWidget
