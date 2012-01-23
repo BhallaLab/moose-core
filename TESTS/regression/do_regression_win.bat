f4c53351c58cdd30c76f804879d9ed84cbd628a7
sijig <sijig@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-01-23 11:32:30 +0000

    merged the rev 3265 from beta 1.4->trunk.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@3266 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: a3f32c9c41dd350b510f361a541d6de0c875b1bb

--- TESTS/regression/do_regression_win.bat
+++ TESTS/regression/do_regression_win.bat
@@ -1,10 +1,12 @@
 @echo off
 
-set MOOSE=..\..\moose_vcpp2005\Debug\moose
+set MOOSE=moose
 
 REM nearDiff is a function to see if two data files are within epsilon of each other. It assumes the files are in xplot format.If it succeeds, it prints out a period without a newline.If it fails, it prints out the first argument and indicates where it failed.
 
 set NEARDIFF=.\neardiff
+
+echo Beginning tests...
 
 IF EXIST regression.out  ERASE regression.out
 
@@ -18,12 +20,6 @@ IF EXIST test.plot ERASE test.plot
 %MOOSE% moose_sbml_reader.g >> regression.out 2>&1
 %NEARDIFF% acc88_copasi.plot test.plot 3.0e-2 -f
 ECHO sbml_Read
-
-IF EXIST test.plot ERASE test.plot
-
-%MOOSE% moose_sbml_read_write.g >> regression.out 2>&1
-%NEARDIFF% moose.plot test.plot 1.0e-16 
-ECHO sbml_Read_Write
 
 IF EXIST test.plot ERASE test.plot
 
