1cd1c9c022c8e4419c098531dfad66d751479fb2
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-06-02 05:54:58 +0000

    Added test for anomalous rectifier (AR) channel.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/dh_branch@3525 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 5a15c973d704c6115e2be7fee9421ca5ca9aada0

---
+++ Demos/traub_2005/py/test_archan.py
@@ -0,0 +1,58 @@
+# test_archan.py --- 
+# 
+# Filename: test_archan.py
+# Description: 
+# Author: 
+# Maintainer: 
+# Created: Sat Jun  2 11:05:22 2012 (+0530)
+# Version: 
+# Last-Updated: Sat Jun  2 11:06:52 2012 (+0530)
+#           By: subha
+#     Update #: 5
+# URL: 
+# Keywords: 
+# Compatibility: 
+# 
+# 
+
+# Commentary: 
+# 
+# 
+# 
+# 
+
+# Change log:
+# 
+# 
+# 
+
+# Code:
+
+
+import numpy as np
+from testutils import *
+from archan import *
+
+
+class TestAR(ChannelTestBase):
+    channelname = 'AR'
+    params = run_single_channel(channelname, 1e-9, simtime)
+    vm = np.array(params['Vm'].vec)
+    gk = np.array(params['Gk'].vec)
+    tseries = np.arange(0, len(vm), 1.0) * simdt
+    
+    def testAR_Vm_Neuron(self):
+        data = np.c_[self.tseries, self.vm]
+        err = compare_channel_data(data, self.channelname, 'Vm', 'neuron', x_range=(simtime/10.0, simtime))
+        self.assertLess(err, 0.01)
+
+    def testAR_Gk_Neuron(self):
+        data = np.c_[self.tseries, self.gk]
+        err = compare_channel_data(data, self.channelname, 'Gk', 'neuron', x_range=(simtime/10.0, simtime), plot=True)
+        self.assertLess(err, 0.01)
+
+if __name__ == '__main__':
+    unittest.main()
+
+# 
+# test_archan.py ends here
