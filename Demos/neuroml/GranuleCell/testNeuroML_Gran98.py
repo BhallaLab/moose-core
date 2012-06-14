81db6d65d38d5345f32ed9a6ac6e87094a7fca96
aditya-g <aditya-g@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-06-14 13:51:54 +0000

    added a script to be run in its directory as on node 0, numNodes = 1, numCores = 2, numComputeThreads = 2 ............................................................................................................................... Not in regression test directory, so skipping them Info: Time to define moose classes:0.02 Info: Time to initialize module:18.88 Loading neuroml file ...  Generated.net.xml Using default temperature of 32.0 degrees Celsius. creating populations ... loading Gran loading cell : Granule_98 into /library . Soma Setting  CM  for  /library/Granule_98/Soma_0  value  0.01 Setting  RA  for  /library/Granule_98/Soma_0  value  1.0 Setting  initVm  for  /library/Granule_98/Soma_0  value  -0.065 Loading mechanism  Gran_KDr_98 loading channel : Gran_KDr_98 into /library . Setting  Gbar  for  /library/Granule_98/Soma_0  value  88.9691 Loading mechanism  GranPassiveCond Setting  Em  for  /library/Granule_98/Soma_0  value  -0.065 Setting  RM  for  /library/Granule_98/Soma_0  value  3.0300000303 Loading mechanism  Gran_H_98 loading channel : Gran_H_98 into /library . Setting  Gbar  for  /library/Granule_98/Soma_0  value  0.3090506 Loading mechanism  Gran_CaHVA_98 loading channel : Gran_CaHVA_98 into /library . Setting  Gbar  for  /library/Granule_98/Soma_0  value  9.084216 Loading mechanism  Gran_KCa_98 loading channel : Gran_KCa_98 into /library . Setting  Gbar  for  /library/Granule_98/Soma_0  value  179.811 Loading mechanism  Gran_KA_98 loading channel : Gran_KA_98 into /library . Setting  Gbar  for  /library/Granule_98/Soma_0  value  11.4567 Loading mechanism  Gran_NaF_98 loading channel : Gran_NaF_98 into /library . Setting  Gbar  for  /library/Granule_98/Soma_0  value  557.227 Loading mechanism  Gran_CaPool_98 loading Ca pool : Gran_CaPool_98 into /library . Setting  None  for  /library/Granule_98/Soma_0  value  default Connected IkOut of /library/Granule_98/Soma_0/Gran_CaHVA_98 to current of /library/Granule_98/Soma_0/Gran_CaPool_98 Finished loading into library, cell:  Granule_98 creating connections ... creating inputs in /elec ... Warning: no Elements found on path /elec/##[TYPE=Compartment] Warning: no Elements found on path /elec/##[TYPE=Compartment] Warning: no Elements found on path /elec/##[TYPE=HHChannel] Warning: no Elements found on path /elec/##[TYPE=HHChannel2D] Warning: no Elements found on path /elec/##[TYPE=CaConc] Warning: no Elements found on path /elec/##[TYPE=Table] Warning: no Elements found on path /cells/##[TYPE=PulseGen] Warning: no Elements found on path /cells/##[TYPE=DiffAmp] Warning: no Elements found on path /cells/##[TYPE=HHChannel2D] that loads this neuroml-based single compartment granule cell model, injects current as per the <inputs> tags in the neuroml L3 file, and plots the soma Vm using matplotlib/numpy (pylab). Note numpy is anyway required for the new parallel pymoose.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/dh_branch@3550 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ Demos/neuroml/GranuleCell/testNeuroML_Gran98.py
@@ -0,0 +1,33 @@
+## Aditya Gilra, NCBS, Bangalore, 2012
+
+"""
+Inside the .../Demos/GranuleCell/ directory supplied with MOOSE, run
+python testNeuroML_Gran98.py
+(other channels and morph xml files are already present in this same directory).
+The soma name below is hard coded for gran98, else any other file can be used by modifying this script.
+"""
+
+import moose
+from moose.utils import *
+from moose.neuroml.NeuroML import NeuroML
+
+from pylab import *
+
+def loadGran98NeuroML_L123(filename):
+    neuromlR = NeuroML()
+    populationDict, projectionDict = \
+        neuromlR.readNeuroMLFromFile(filename)
+    soma_path = populationDict['Gran'][1][0].path+'/Soma_0'
+    somaVm = setupTable('somaVm',moose.Compartment(soma_path),'VmOut')
+    resetSim(['/elec','/cells'],50e-6,50e-6) # from moose.utils
+    moose.start(1.0)
+    plot(somaVm.vec)
+    show()
+
+if __name__ == "__main__":
+    if len(sys.argv)<2:
+        filename = "Generated.net.xml"
+    else:
+        filename = sys.argv[1]
+    loadGran98NeuroML_L123(filename)
+    
