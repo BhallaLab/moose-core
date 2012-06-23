93932ef2290628676c4554ce1e0239959d4b32b8
ccluri <ccluri@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-06-23 11:39:48 +0000

    added weights to synapses and pulsegen to input
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/dh_branch@3593 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: b30b7beaa30bb9314096a7c36c18717ba5f5fe45
    Former-commit-id: 5fc7ba00c12a278f131b28ba5b7337f6926cddf9

--- Demos/hopfield/hopfield.py
+++ Demos/hopfield/hopfield.py
@@ -10,15 +10,11 @@ import moose
 import csv
 import numpy as np
 
-memoryFile = "memory.csv"
-inputFile = "input.csv"
-
 def readMemory(memoryFile):
     f = open(memoryFile,'r')
     testLine = f.readline()
     dialect = csv.Sniffer().sniff(testLine) #to get the format of the csv
     f.close()
-
     f = open(memoryFile, 'r')
     reader = csv.reader(f,dialect)
     memory = []
@@ -39,34 +35,53 @@ def updateWeights(memory,weightMatrix):
         weightMatrix.append(tempList)
     return weightMatrix
 
-def createNetwork(synWeights):
+def createNetwork(synWeights,inputGiven):
     numberOfCells = len(synWeights[0])
     cells = []
+
+    pg = moose.PulseGen('/inPulGen')
+    pg.count = 1
+    pg.level[0] = 2.0
+    pg.width[0] = 0.1
+    pg.delay[0] = 5e-02 #every 50ms
+
     for i in range(numberOfCells):
         cell = moose.IntFire('/cell_'+str(i+1)) #non programmer friendly numbering
         cell.setField('tau',10.0)
         cell.setField('Vm', -0.07)
         #cell.setField('refractoryPeriod', 0.1)
         #cell.setField('thresh', 0.0)
+        cell.synapse.num = numberOfCells+1 
+        #number of synapses - for user friendly numbering - ignore synapse[0]
+        cell.synapse[i+1].weight = 1
+        cell.synapse[i+1].delay = 1e-3
 
-        inSyn = moose.element('/cell_'+str(i+1)+'/synapse')
         inSpkGen = moose.SpikeGen('/cell_'+str(i+1)+'/inSpkGen')
-        moose.connect(inSpkGen, 'event', inSyn ,'addSpike') 
+        moose.connect(inSpkGen, 'event', cell.synapse[i+1] ,'addSpike') #self connection is the input 
+
+        if inputGiven[i] == 1:
+            moose.connect(pg, 'outputOut', moose.element('/cell_'+str(i+1)+'/inSpkGen'), 'Vm')
 
         cells.append(cell)
 
     for currentCell in range(numberOfCells): #currCell 
         for connectCell in range(numberOfCells): #connectToCell
-            if currentCell != connectCell:
-                #how to give synaptic input weight? synWeights[currentCell][connectCell]
-                connSyn = moose.element('/cell_'+str(connectCell+1)+'/synapse')
+            if currentCell != connectCell: #no self connections
+                connSyn = moose.element('/cell_'+str(connectCell+1)).synapse[connectCell+1]
+                connSyn.weight = synWeights[currentCell][connectCell]
+                connSyn.delay = 2e-3
                 moose.connect(moose.element('/cell_'+str(currentCell+1)), 'spike', connSyn, 'addSpike')
 
     return cells
 
-memory = readMemory(memoryFile)
-
+memoryFile1 = "memory1.csv"
+memory = readMemory(memoryFile1)
 synWeights = updateWeights(memory,[])
 
-cells = createNetwork(synWeights)
+memoryFile2 = "memory2.csv"
+memory2 = readMemory(memoryFile2)
+synWeights = updateWeights(memory2,synWeights)
+
+inputFile = "input.csv"
+cells = createNetwork(synWeights,readMemory(inputFile))
 
