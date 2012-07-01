6c05cbd5d66df7ec3a9e316f9d7f4d861aebb506
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-07-01 09:09:37 +0000

    Added snippet to show start and stop functions. Does not really work in singlethreaded code.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/dh_branch@3619 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 02a376daf815367c5cada512deb9efa438f6a44b

---
+++ Demos/snippets/startstop.py
@@ -0,0 +1,98 @@
+# startstop.py --- 
+# 
+# Filename: startstop.py
+# Description: 
+# Author: 
+# Maintainer: 
+# Created: Sun Jul  1 13:42:28 2012 (+0530)
+# Version: 
+# Last-Updated: Sun Jul  1 14:41:34 2012 (+0530)
+#           By: subha
+#     Update #: 62
+# URL: 
+# Keywords: 
+# Compatibility: 
+# 
+# 
+
+# Commentary: 
+# 
+# 
+# 
+# 
+
+# Change log:
+# 
+# 
+# 
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+# Floor, Boston, MA 02110-1301, USA.
+# 
+# 
+
+# Code:
+
+import time
+import pylab
+
+import sys
+sys.path.append('../../python')
+import moose
+
+def setup_model():
+    model_container = moose.Neutral('/model')    
+    pulse = moose.PulseGen('/model/pulse')
+    pulse.level[0] = 1.0
+    pulse.delay[0] = 0.5
+    pulse.width[0] = 0.5
+    table = moose.Table('%s/tab' % (pulse.path))
+    moose.connect(table, 'requestData', pulse, 'get_output')
+    moose.setClock(0, 0.1)
+    moose.setClock(1, 0.1)
+    moose.setClock(2, 0.1)
+    moose.useClock(0, '%s,%s' % (pulse.path, table.path), 'process')
+    return table
+            
+def startstop_demo():
+    """Starts a simulation for some steps, pauses midway with the
+    current data, runs till end."""
+    table = setup_model()
+    moose.reinit()
+    clock = moose.element('/clock')
+    dt = clock.tick[0].dt
+    print dt
+    steps = 100
+    simtime = dt * steps
+    print 'Total simulation time', simtime
+    t = simtime/2.0
+    print 'Running simulation for', t, 'seconds'
+    moose.start(t)
+    # moose.stop() # Somehow this does not work in single threaded Python script
+    pylab.plot(pylab.linspace(0, clock.currentTime, len(table.vec)), table.vec)    
+    pylab.show()
+    print 'Simulator time:', clock.currentTime
+    # How much of the total simulation time is left?
+    time_left_to_run = simtime - clock.currentTime
+    moose.start(time_left_to_run)
+    print 'Simulator time at end of simulation', clock.currentTime
+    pylab.plot(pylab.linspace(0, clock.currentTime, len(table.vec)), table.vec)    
+    pylab.show()
+
+if __name__ == '__main__':
+    startstop_demo()
+
+# 
+# startstop.py ends here
