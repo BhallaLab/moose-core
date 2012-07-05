d7241c9ccf036443bb15051b1e320d3f388d0a8e
ccluri <ccluri@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-07-05 14:01:42 +0000

    test code for leakyiaf
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/dh_branch@3658 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- Demos/hopfield/test.py
+++ Demos/hopfield/test.py
@@ -8,34 +8,44 @@ pg = moose.PulseGen('/elec/inPulGen')
 pgTable = moose.Table('/elec/inPulGen/pgTable')
 moose.connect(pgTable, 'requestData', pg, 'get_output')
 
-pg.count = 1
-pg.level[0] = 3
-pg.width[0] = 10e-03
-pg.delay[0] = 5e-02 #every 50ms
+pg.firstDelay = 10e-3
+pg.firstWidth = 2e-03
+pg.firstLevel = 3
+pg.secondDelay = 1.0
 
 cellPath = '/cell'
-cell = moose.IntFire(cellPath) #non programmer friendly numbering
-cell.setField('tau',10e-3)
-cell.setField('Vm', -0.07)
+cell = moose.LeakyIaF(cellPath)
+cell.setField('Cm',1e-6)
+cell.setField('Rm',1e4)
+cell.setField('Em',-0.07)
+cell.setField('initVm',-0.05)
+cell.setField('Vreset',-0.07)
+cell.setField('Vthreshold',0.0)
+cell.setField('refractoryPeriod',0.01)
+
+# cell = moose.IntFire(cellPath) #non programmer friendly numbering
+# cell.setField('tau',10e-3)
+# cell.setField('Vm', -0.07)
 #cell.setField('refractoryPeriod', 0.1)
 #cell.setField('thresh', 0.0)
-cell.synapse.num = 1
-cell.synapse[0].weight = 4
-cell.synapse[0].delay = 1e-3
+#cell.synapse.num = 1
+#cell.synapse[0].weight = 4
+#cell.synapse[0].delay = 1e-3
 
 VmVal = moose.Table(cellPath+'/Vm_cell')
 print 'table>cell:',moose.connect(VmVal, 'requestData', cell, 'get_Vm')
 
-inSpkGen = moose.SpikeGen(cellPath+'/inSpkGen')
-inSpkGen.setField('threshold', 2.0)
-inSpkGen.setField('edgeTriggered', True)
+# inSpkGen = moose.SpikeGen(cellPath+'/inSpkGen')
+# inSpkGen.setField('threshold', 2.0)
+# inSpkGen.setField('edgeTriggered', True)
 
-if inputGiven == 1:
-    print 'pulse>spike:', moose.connect(pg, 'outputOut', moose.element(cellPath+'/inSpkGen'), 'Vm')
-    inTable = moose.Table(cellPath+'/inSpkGen/inTable')
-    print 'table>spike:',moose.connect(inTable, 'requestData', inSpkGen, 'get_hasFired')
+# if inputGiven == 1:
+#     print 'pulse>spike:', moose.connect(pg, 'outputOut', moose.element(cellPath+'/inSpkGen'), 'Vm')
+#     inTable = moose.Table(cellPath+'/inSpkGen/inTable')
+#     print 'table>spike:',moose.connect(inTable, 'requestData', inSpkGen, 'get_hasFired')
 
-print 'spike>cell:', moose.connect(inSpkGen, 'event', cell.synapse[0] ,'addSpike')
+#print 'spike>cell:', moose.connect(inSpkGen, 'event', cell.synapse[0] ,'addSpike')
+print 'spike>cell:', moose.connect(pg, 'outputOut', cell ,'injectDest')
 
 moose.setClock(0, 1e-4)
 moose.useClock(0, '/cell,/cell/##,/elec/##','process')
@@ -45,7 +55,7 @@ moose.start(0.2)
 subplot(311)
 plot(pgTable.vec[1:])
 subplot(312)
-plot(inTable.vec[1:])
+#plot(inTable.vec[1:])
 subplot(313)
 plot(VmVal.vec[1:])
 show()
