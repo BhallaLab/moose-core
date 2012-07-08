7347306d02d993b1ed0c360361f61c08c9b3a69c
aditya-g <aditya-g@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-07-08 13:50:55 +0000

    MainenEtAl_PyramidalCell added in neuroml demos. Minor modifications to other demos, CA1 demo has current injection from 0s so that one doesn't have to wait for settling time.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/dh_branch@3696 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- Demos/neuroml/CA1PyramidalCell/CA1_hsolve.py
+++ Demos/neuroml/CA1PyramidalCell/CA1_hsolve.py
@@ -1,41 +1,41 @@
 ## Aditya Gilra, NCBS, Bangalore, 2012
 
 """
-Inside the .../Demos/GranuleCell/ directory supplied with MOOSE, run
-python testNeuroML_Gran98.py
+Inside the .../Demos/CA1PyramidalCell/ directory supplied with MOOSE, run
+python testNeuroML_CA1.py
 (other channels and morph xml files are already present in this same directory).
-The soma name below is hard coded for gran98, else any other file can be used by modifying this script.
+The soma name below is hard coded for CA1, else any other file can be used by modifying this script.
 """
 
 import moose
 from moose.utils import *
+
 from moose.neuroml.NeuroML import NeuroML
 
 from pylab import *
 
 simdt = 10e-6 # s
 plotdt = 10e-6 # s
-runtime = 0.7 # s
+runtime = 0.2 # s
 cells_path = '/cells' # neuromlR.readNeuroMLFromFile creates cells in '/cells'
 
 def loadGran98NeuroML_L123(filename):
     neuromlR = NeuroML()
     populationDict, projectionDict = \
         neuromlR.readNeuroMLFromFile(filename)
-    soma_path = populationDict['Gran'][1][0].path+'/Soma_0'
+    soma_path = populationDict['CA1group'][1][0].path+'/Seg0_soma_0_0'
     somaVm = setupTable('somaVm',moose.Compartment(soma_path),'Vm')
-    somaCa = setupTable('somaCa',moose.CaConc(soma_path+'/Gran_CaPool_98'),'Ca')
-    somaIKCa = setupTable('somaIKCa',moose.HHChannel(soma_path+'/Gran_KCa_98'),'Gk')
-    ## Am not able to plot KDr gating variable X when running under hsolve
+    #somaCa = setupTable('somaCa',moose.CaConc(soma_path+'/Gran_CaPool_98'),'Ca')
+    #somaIKCa = setupTable('somaIKCa',moose.HHChannel(soma_path+'/Gran_KCa_98'),'Gk')
     #KDrX = setupTable('ChanX',moose.HHChannel(soma_path+'/Gran_KDr_98'),'X')
-
+    soma = moose.Compartment(soma_path)
+    
     h = moose.HSolve( cells_path+'/solve' )
     h.dt = simdt
     h.path = cells_path
 
     print "Reinit MOOSE ... "
-    resetSim(['/elec',cells_path], simdt, plotdt, hsolve_path=cells_path+'/solve')
-
+    resetSim(['/elec','/cells'],simdt,plotdt,hsolve_path=cells_path+'/solve') # from moose.utils
     print "Running ... "
     moose.start(runtime)
     tvec = arange(0.0,runtime,simdt)
@@ -43,23 +43,12 @@ def loadGran98NeuroML_L123(filename):
     title('Soma Vm')
     xlabel('time (s)')
     ylabel('Voltage (V)')
-    figure()
-    plot(tvec,somaCa.vec[1:])
-    title('Soma Ca')
-    xlabel('time (s)')
-    ylabel('Ca conc (mol/m^3)')
-    figure()
-    plot(tvec,somaIKCa.vec[1:])
-    title('soma KCa current')
-    xlabel('time (s)')
-    ylabel('KCa current (A)')
     print "Showing plots ..."
     show()
 
-filename = "GranuleCell.net.xml"
 if __name__ == "__main__":
     if len(sys.argv)<2:
-        filename = "GranuleCell.net.xml"
+        filename = "CA1.net.xml"
     else:
         filename = sys.argv[1]
 loadGran98NeuroML_L123(filename)
