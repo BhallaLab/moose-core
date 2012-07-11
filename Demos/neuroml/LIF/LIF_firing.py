b45a9617d47ff02a791451574d4302cfa17245b2
aditya-g <aditya-g@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-07-11 09:43:19 +0000

    this LIF/LIF_firing.py uses leaky integrate and fire now
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/dh_branch@3734 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 981a0e0ea5b781d26066cb3d8bdd1eefa675c473

--- Demos/neuroml/LIF/LIF_firing.py
+++ Demos/neuroml/LIF/LIF_firing.py
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
 ## all SI units
 ########################################################################################
-## Plot the firing rate vs current injection curve for a leaky integrate and fire neuron
+## Plot the membrane potential for a leaky integrate and fire neuron with current injection
 ## Author: Aditya Gilra
 ## Creation Date: 2012-06-08
 ## Modification Date: 2012-06-08
 ########################################################################################
 
+import os
+os.environ['NUMPTHREADS'] = '1'
+import sys
+sys.path.append('../../../python')
+
 ## simulation parameters
 SIMDT = 5e-5 # seconds
 PLOTDT = 5e-5 # seconds
 RUNTIME = 1.0 # seconds
-injectI = 200e-12 # Amperes
+injectI = 100e-12 # Amperes
 
 ## moose imports
 import moose
-mcontext = moose.context
 from moose.neuroml import *
 from moose.utils import * # has setupTable(), resetSim() etc
 import math
@@ -24,11 +28,11 @@ import math
 Em = -65e-3 # V
 ## if length l and diameter d of cylinder are equal, pi*d*l = 4*pi*r^2 where r=d/2
 ## i.e. surface area of cylinder is same as surface area of sphere with dia d
-length = 50e-6 # 100 microns
-diameter = 50e-6 # 100 microns
+length = 100e-6 # 100 microns
+diameter = 100e-6 # 100 microns
 ## specific resistances and capacitance
-RM = 1 # Ohm.m^2
-CM = 0.01 # F/m^2 # standard value is 1 microF/cm^2
+RM = 10000000 # Ohm.m^2
+CM = 0.01e3 # F/m^2 # standard value is 1 microF/cm^2
 RA = 1 # Ohm.m
 ## actual resistances and capacitance: derived values
 surfaceArea = math.pi*diameter*length # pi from math module
@@ -37,50 +41,41 @@ Rm = RM/surfaceArea
 Cm = CM*surfaceArea
 Ra = RA*length/crossSectionArea
 ## firing properties
-Vthreshold = Em + 10e-3 # Volts # above resting
-refractT = 5e-3 # seconds
-refractV = Em-5e-3 # Volts # goes below Em after spike
+Vthreshold = 10e-3 # Volts # above resting
+refractoryPeriod = 2e-3 # seconds
+Vreset = Em-5e-3 # Volts #goes below Em after spike
 
 ## import numpy and matplotlib in matlab style commands
 from pylab import *
 
 ## create the integrate and fire neuron
-IF1 = moose.IntFire('/cells/IF1')
+cells = moose.Neutral('/cells')
+#IF1 = moose.IntFire('/cells/IF1')
+IF1 = moose.LeakyIaF('/cells/IF1')
+IF1.inject = injectI
 IF1.Rm = Rm
 IF1.Cm = Cm
 IF1.Em = Em
-IF1.initVm = Em
-IF1.Vt = Vthreshold # firing threshold potential
-IF1.refractT = refractT # min refractory time before next spike
-IF1.Vr = refractV # voltage after spike, typicaly below resting
-
+IF1.initVm = Vreset
+IF1.Vthreshold = Vthreshold # firing threshold potential
+IF1.refractoryPeriod = refractoryPeriod # min refractory time before next spike
+IF1.Vreset = Vreset # voltage after spike, typicaly below resting
 ## from moose_utils.py sets up a table to record Vm from IF1
 ## table is named vmTableIF1 under path of IF1
-IF1.vmTable = setupTable("vmTableIF1",IF1,'Vm')
-IF1.vmTable.stepMode = TAB_SPIKE
-IF1.vmTable.stepSize = Vthreshold-1e-3
-
+IF1vmTable = setupTable("vmTableIF1",IF1,'Vm')
 
 ## reset and run the simulation
-print "Resetting MOOSE."
+print "Reinit MOOSE."
 ## from moose_utils.py sets clocks and resets
-resetSim(mcontext, SIMDT, PLOTDT)
+resetSim(['/cells'], SIMDT, PLOTDT)
+moose.showfield(IF1)
+print Rm, RM*injectI, Vthreshold
+print Rm*Cm*log(Rm*injectI/(Rm*injectI-Vthreshold))
+print "Running now..."
+moose.start(RUNTIME)
 
-## Loop through different current injections
-spikesTillNow = 0
-freqList = []
-currentvec = arange(0.0, injectI, injectI/30.0)
-for currenti in currentvec:
-    IF1.inject = currenti
-    print "Running for injected current =",currenti
-    mcontext.step(RUNTIME)
-    spikesList = array(IF1.vmTable)
-    spikesList = spikesList[where(spikesList>0.0)[0]]
-    spikesNow = len(spikesList) - spikesTillNow
-    freqList.append( spikesNow/float(RUNTIME) )
-    spikesTillNow = len(spikesList)
-
-## plot the F vs I curve of the neuron
+## plot the membrane potential of the neuron
+timevec = arange(0.0,RUNTIME+PLOTDT/2.0,PLOTDT)
 figure(facecolor='w')
-plot(currentvec, freqList)
+plot(timevec, IF1vmTable.vec)
 show()
