34c3390630b68d517feb45c0f28e160f1440264b
aditya-g <aditya-g@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-07-12 13:11:31 +0000

    All demos working. Just need to do some code cleanup and replace electrical pulsegen-s by compartment injects, so that it is easier to change in the GUI.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/dh_branch@3754 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 6cbe8208c3da19bf48e0ebc23a1e0a269c3884c8
    Former-commit-id: 76e02653a6a5d729fb86786683452a05ac5f3b84

--- Demos/neuroml/LIF/LIFxml_firing.py
+++ Demos/neuroml/LIF/LIFxml_firing.py
@@ -16,7 +16,7 @@ sys.path.append('../../../python')
 SIMDT = 5e-5 # seconds
 PLOTDT = 5e-5 # seconds
 RUNTIME = 1.0 # seconds
-injectI = 100e-12 # Amperes
+injectI = 10e-12 # Amperes
 
 ## moose imports
 import moose
@@ -24,46 +24,16 @@ from moose.neuroml import *
 from moose.utils import * # has setupTable(), resetSim() etc
 import math
 
-## compartment constants
-Em = -65e-3 # V
-## if length l and diameter d of cylinder are equal, pi*d*l = 4*pi*r^2 where r=d/2
-## i.e. surface area of cylinder is same as surface area of sphere with dia d
-length = 100e-6 # 100 microns
-diameter = 100e-6 # 100 microns
-## specific resistances and capacitance
-RM = 100 # Ohm.m^2
-CM = 0.01 # F/m^2 # standard value is 1 microF/cm^2
-RA = 1 # Ohm.m
-## actual resistances and capacitance: derived values
-surfaceArea = math.pi*diameter*length # pi from math module
-crossSectionArea = math.pi*(diameter/2.0)**2
-Rm = RM/surfaceArea
-Cm = CM*surfaceArea
-Ra = RA*length/crossSectionArea
-## firing properties
-Vthreshold = Em+5e-3 # Volts # above resting
-refractoryPeriod = 2e-3 # seconds
-Vreset = Em-2e-3 # Volts #goes below Em after spike
-
 ## import numpy and matplotlib in matlab style commands
 from pylab import *
 
 def create_LIF():
-	## create the integrate and fire neuron
-	cells = moose.Neutral('/cells')
-	#IF1 = moose.IntFire('/cells/IF1')
-	IF1 = moose.LeakyIaF('/cells/IF1')
-	IF1.inject = injectI
-	IF1.Rm = Rm
-	IF1.Cm = Cm
-	IF1.Em = Em
-	IF1.initVm = Vreset
-	IF1.Vthreshold = Vthreshold # firing threshold potential
-	IF1.refractoryPeriod = refractoryPeriod # min refractory time before next spike
-	IF1.Vreset = Vreset # voltage after spike, typicaly below resting
-	## from moose_utils.py sets up a table to record Vm from IF1
-	## table is named vmTableIF1 under path of IF1
-	return IF1
+    neuromlR = NeuroML()
+    neuromlR.readNeuroMLFromFile('cells_channels/LIF.morph.xml')
+    libcell = moose.Neuron('/library/LIF')
+    LIFCellid = moose.copy(libcell,moose.Neutral('/cells'),'IF1')
+    LIFCell = moose.LeakyIaF(LIFCellid)
+    return LIFCell
 
 def run_LIF():
 	## reset and run the simulation
@@ -75,17 +45,20 @@ def run_LIF():
 
 if __name__ == '__main__':
     IF1 = create_LIF()
+    IF1.inject = injectI
     IF1vmTable = setupTable("vmTableIF1",IF1,'Vm')
 
+    ## edge-detect the spikes using spike-gen (table does not have edge detect)
+    spikeGen = moose.SpikeGen(IF1.path+'/spikeGen')
+    spikeGen.threshold = IF1.Vthreshold
+    moose.connect(IF1,'VmOut',spikeGen,'Vm')
     ## save spikes in table
     table_path = moose.Neutral(IF1.path+'/data').path
-    IF1spikesTable = moose.Table(table_path+'/spikesTable')
-    IF1spikesTable.threshold = Vthreshold-1e-3
-    #moose.connect(IF1,'VmOut',IF1spikesTable,'spike') # This is not edge triggered :(
-    moose.connect(IF1,'spike',IF1spikesTable,'input')
+    IF1SpikesTable = moose.Table(table_path+'/spikesTable')
+    moose.connect(spikeGen,'event',IF1SpikesTable,'input')
 
     run_LIF()
-    print IF1spikesTable.vec
+    print "Spiketimes :",IF1SpikesTable.vec
     ## plot the membrane potential of the neuron
     timevec = arange(0.0,RUNTIME+PLOTDT/2.0,PLOTDT)
     figure(facecolor='w')
