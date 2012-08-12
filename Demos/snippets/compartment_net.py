91039b5312b265054e5f74333515c6f6eff7ca3b
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-08-12 10:04:29 +0000

    network snippet sort of working. the neurons are spontaneously spiking and the effect of synaptic conductance is not very clear.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@3844 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- Demos/snippets/compartment_net.py
+++ Demos/snippets/compartment_net.py
@@ -6,9 +6,9 @@
 # Maintainer: 
 # Created: Sat Aug 11 14:30:21 2012 (+0530)
 # Version: 
-# Last-Updated: Sun Aug 12 01:06:14 2012 (+0530)
-#           By: Subhasis Ray
-#     Update #: 294
+# Last-Updated: Sun Aug 12 15:36:02 2012 (+0530)
+#           By: subha
+#     Update #: 517
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -50,6 +50,7 @@ import sys
 sys.path.append('../../python')
 import os
 os.environ['NUMPTHREADS'] = '1'
+from pylab import *
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -112,24 +113,6 @@ def create_na_proto():
     yGate = moose.element(na.path + '/gateY')
     yGate.setupAlpha(Na_h_params + 
                       [VDIVS, VMIN, VMAX])
-    varray = np.linspace(VMIN, VMAX, VDIVS+1)
-    ax = np.array([xGate.A[v] for v in varray])
-    bx = np.array([xGate.B[v] for v in varray])
-    plt.subplot(2,2,1)
-    plt.plot(ax/bx, label='m_inf')
-    plt.legend()
-    plt.subplot(2,2,2)
-    plt.plot(1/bx, label='tau_m')
-    plt.legend()
-    ax = np.array([yGate.A[v] for v in varray])
-    bx = np.array([yGate.B[v] for v in varray])
-    plt.subplot(2,2,3)
-    plt.plot(ax/bx, label='h_inf')
-    plt.legend()
-    plt.subplot(2,2,4)
-    plt.plot(1/bx, label='tau_h')
-    plt.legend()
-    plt.show()
     return na
 
 def create_k_proto():
@@ -139,48 +122,117 @@ def create_k_proto():
     xGate = moose.element(k.path + '/gateX')    
     xGate.setupAlpha(K_n_params +
                       [VDIVS, VMIN, VMAX])
-    varray = np.linspace(VMIN, VMAX, VDIVS+1)
-    ax = np.array([xGate.A[v] for v in varray])
-    bx = np.array([xGate.B[v] for v in varray])
-    plt.subplot(2,1,1)
-    plt.plot(ax/bx, label='n_inf')
-    plt.legend()
-    plt.subplot(2,1,2)
-    plt.plot(1/bx, label='tau_n')
-    plt.legend()
-    plt.show()
     return k
 
-def create_network(container, size):
+def gate_params(channel):
+    xGate = None
+    x_inf = []
+    tau_x = []
+    yGate = None
+    y_inf = []
+    tau_y = []
+    varray = []
+    print '>>', channel.path, channel.Xpower, channel.Ypower
+    if channel.Xpower > 0:
+        xGate = moose.element(channel.path + '/gateX')
+        vmin = xGate.min
+        vmax = xGate.max
+        vdivs = xGate.divs
+        varray = linspace(vmin, vmax, vdivs+1)
+        print channel.path, vmin, vmax, vdivs
+        ax = array([xGate.A[v] for v in varray])
+        bx = array([xGate.B[v] for v in varray])
+        x_inf = ax/bx
+        tau_x = 1/bx
+    if channel.Ypower > 0:
+        yGate = moose.element(channel.path + '/gateY')
+        vmin = yGate.min
+        vmay = yGate.max
+        vdivs = yGate.divs
+        varray = linspace(vmin, vmax, vdivs+1)
+        ay = array([yGate.A[v] for v in varray])
+        by = array([yGate.B[v] for v in varray])
+        y_inf = ay/by
+        tau_y = 1/by
+    return {'x_inf': x_inf,
+            'tau_x': tau_x,
+            'y_inf': y_inf,
+            'tau_y': tau_y,
+            'v_array': varray}
+
+def plot_gate_params(chan):
+    params = gate_params(moose.HHChannel(chan))    
+    subplot(2,1,1)    
+    plot(params['v_array'], na_params['x_inf'], label='m_inf')
+    if len(params['y_inf']) == len(params['v_array']):
+        plot(params['v_array'], na_params['y_inf'], label='h_inf')
+    legend()
+    subplot(212)
+    plot(params['v_array'], params['tau_x'], label='tau_m')
+    if len(params['y_inf']) == len(params['v_array']):
+        plot(params['v_array'], params['tau_y'], label='tau_h')
+    legend()
+    show()
+    
+def create_population(container, size):
+    """Create a population of `size` single compartmental neurons with
+    Na and K channels. Also create SpikeGen objects and SynChan
+    objects connected to these which can act as plug points for
+    setting up synapses later."""
     path = container.path
     comps = moose.Id(path+'/soma', size, 'Compartment')    
-    comps.Em = [EREST_ACT+10.613e-3] * size
-    comps.initVm = [-65e-3] * size
-    comps.Cm = [1e-12] * size
-    comps.Rm = [1e9] * size
-    comps.Ra = [1e5] * size
-    nachan = moose.copy(create_na_proto(), comps[0])
-    moose.connect(nachan, 'channel', comps, 'channel')
-    # moose.connect(nachan, 'channel', comps, 'channel', 'OneToOne')
-    kchan = moose.copy(create_k_proto(), comps[0])
-    moose.connect(kchan, 'channel', comps, 'channel')
-    # moose.connect(kchan, 'channel', comps, 'channel', 'OneToOne')
+    Em = EREST_ACT+10.613e-3
+    comps.Em = np.random.normal(Em, np.abs(Em) * 0.1, size)
+    comps.initVm = np.random.normal(EREST_ACT, np.abs(EREST_ACT) * 0.1, size)
+    comps.Cm = [7.85e-9] * size
+    comps.Rm = [4.2e5] * size
+    comps.Ra = [190.98] * size
+    nachan = moose.copy(create_na_proto(), container, 'na', size)
+    nachan.Gbar = [0.942e-3] * size
+    nachan.Ek = [115e-3+EREST_ACT] * size
+    moose.connect(nachan, 'channel', comps, 'channel', 'OneToOne')
+    kchan = moose.copy(create_k_proto(), container, 'k', size)
+    kchan.Gbar = [0.2836e-4] * size
+    kchan.Ek = [-12e-3+EREST_ACT] * size
+    moose.connect(kchan, 'channel', comps, 'channel', 'OneToOne')
     synchan = moose.Id(path + '/synchan', size, 'SynChan')
-    synchan.Gbar = [1e-9] * size
-    for s in synchan: moose.SynChan(s).synapse.num = size
+    synchan.Gbar = [1e-8] * size
+    synchan.tau1 = [2e-3] * size
+    synchan.tau2 = [2e-3] * size
     # Question: What is this going to do? Connect comps[ii] to comps[ii]/synchan? 
     # if we had synchan under each  compartment created like: synchan = moose.SynChan(comps.path + '/synchan')
+
     m = moose.connect(comps, 'channel', synchan, 'channel', 'OneToOne')
+
     ## Or would this have been the correct approach?
     # for c in comps: moose.connect(c, 'channel', moose.SynChan(c.path+'/synchan'), 'channel', 'Single')
     spikegen = moose.Id(path + '/spikegen', size, 'SpikeGen')
     spikegen.threshold = [0.0] * size
     m = moose.connect(comps, 'VmOut', spikegen, 'Vm', 'OneToOne')
-    # m = moose.connect(spikegen, 'event', moose.ObjId(synchan.path + '/synapse'),  'addSpike', 'Sparse')
-    # m.setRandomConnectivity(0.2, 0)
     return {'compartment': comps,
             'spikegen': spikegen,
             'synchan': synchan}
+
+def make_synapses(spikegen, synchan, connprob=1.0, delay=0.1e-3):
+    """Create synapses from spikegen array to synchan array.
+
+    connprob: connection probability.
+
+    delay: mean delay of synaptic transmission. Individual delays are
+    normally distributed with sd=0.1*mean.  
+    """
+    for ii in synchan: 
+        s = moose.SynChan(ii)
+        scount = len(spikegen)
+        s.synapse.num = scount
+        delay_list = np.random.normal(delay, delay*0.1, scount)
+        for jj in range(scount): s.synapse[jj].delay = delay_list[jj]
+    m = moose.connect(spikegen, 'event', moose.ObjId(synchan.path + '/synapse'),  'addSpike', 'Sparse')
+    # The sparse message maintains an adjacency matrix. In the special
+    # case of synapses on synchan objects, entry a[i][j] = k means
+    # that source object no i (say spikegen[i] connects to synapse
+    # no. k on the j-th synchan object.
+    moose.SparseMsg(m).setRandomConnectivity(connprob, 1)
             
             
 
@@ -188,17 +240,23 @@ if __name__ == '__main__':
     simtime = 0.1
     simdt = 0.25e-5
     plotdt = 0.25e-3
-    size = 1
-    net = create_network(moose.Neutral('network'), size)
+    size = 2
+    net = moose.Neutral('network')
+    pop_a = create_population(moose.Neutral('/network/pop_A'), size)
+    pop_b = create_population(moose.Neutral('/network/pop_B'), size)
+    make_synapses(pop_a['spikegen'], pop_b['synchan'])
     pulse = moose.PulseGen('pulse')
-    pulse.firstLevel = 1e-12
-    pulse.firstDelay = 0.05
+    pulse.firstLevel = 1e-9
+    pulse.firstDelay = 0.05e10 # disable the pulsegen
     pulse.firstWidth = 1e9
-    moose.connect(pulse, 'outputOut', net['compartment'][0], 'injectMsg')
+    moose.connect(pulse, 'outputOut', pop_a['compartment'][0], 'injectMsg')
     data = moose.Neutral('/data')
-    vmtables = moose.Table('/data/Vm', size)
-    # moose.connect(tables, 'requestData', net['compartment'], 'get_Vm', 'OneToOne')    
-    moose.connect(vmtables, 'requestData', net['compartment'], 'get_Vm')
+    vm_a = moose.Table('/data/Vm_A', size)
+    moose.connect(vm_a, 'requestData', pop_a['compartment'], 'get_Vm', 'OneToOne')
+    vm_b = moose.Table('/data/Vm_B', size)
+    moose.connect(vm_b, 'requestData', pop_b['compartment'], 'get_Vm', 'OneToOne')
+    gksyn_b = moose.Table('/data/Gk_syn_b', size)
+    moose.connect(gksyn_b, 'requestData', pop_b['synchan'], 'get_Gk', 'OneToOne')
     pulsetable = moose.Table('/data/pulse')
     pulsetable.connect('requestData', pulse, 'get_output')
     moose.setClock(0, simdt)
@@ -206,17 +264,25 @@ if __name__ == '__main__':
     moose.setClock(2, simdt)
     moose.setClock(3, simdt)
     moose.setClock(4, plotdt)
-    moose.useClock(0, '/network/#[TYPE=Compartment]', 'init')
-    moose.useClock(1, '/network/#[TYPE=Compartment]', 'process')
-    moose.useClock(2, '/network/##[TYPE=SynChan],/network/##[TYPE=HHChannel]', 'process')
-    moose.useClock(3, '/network/#[TYPE=SpikeGen],/#[TYPE=PulseGen]', 'process')    
+    moose.useClock(0, '/network/##[TYPE=Compartment]', 'init')
+    moose.useClock(1, '/network/##[TYPE=Compartment]', 'process')
+    moose.useClock(2, '/network/##[TYPE=SynChan]', 'process')
+    moose.useClock(2, '/network/##[TYPE=HHChannel]', 'process')
+    moose.useClock(3, '/network/##[TYPE=SpikeGen],/#[TYPE=PulseGen]', 'process')    
     moose.useClock(4, '/data/#[TYPE=Table]', 'process')
     moose.reinit()
     moose.start(simtime)
-    for oid in vmtables.id_:
-        print oid.path, len(oid.vec)
+    plt.subplot(221)
+    for oid in vm_a.id_:
         plt.plot(oid.vec, label=oid.path)
-    plt.plot(pulsetable.vec, label='Pulse')
+    plt.legend()
+    plt.subplot(223)
+    for oid in vm_b.id_:
+        plt.plot(oid.vec, label=oid.path)
+    plt.legend()
+    plt.subplot(224)
+    for ii in gksyn_b.id_:
+        plt.plot(ii.vec, label=ii.path)
     plt.legend()
     plt.show()
     
