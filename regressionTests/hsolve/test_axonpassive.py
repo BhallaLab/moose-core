7536b412bdb982317a0b1abe2a77ae2cf2ecff4c
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-08-12 12:42:48 +0000

    Updated code to test HSOLVE with passive axon. Passes the test.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@3846 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: be4301e6aeaa391fa97678a6853e60fc8adddd2f

--- regressionTests/hsolve/test_axonpassive.py
+++ regressionTests/hsolve/test_axonpassive.py
@@ -6,9 +6,9 @@
 # Maintainer: 
 # Created: Wed Jul 11 00:03:30 2012 (+0530)
 # Version: 
-# Last-Updated: Wed Jul 11 09:57:12 2012 (+0530)
+# Last-Updated: Sun Aug 12 18:13:49 2012 (+0530)
 #           By: subha
-#     Update #: 61
+#     Update #: 102
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -49,18 +49,21 @@ def load_axon():
     for x in model[0].children:
         print x.path, x.class_
     pulsegen = moose.PulseGen('/pulsegen')
-    pulsegen.delay[0] = simlength * 0.2
-    pulsegen.width[0] = simlength
+    pulsegen.delay[0] = simdt * 200 # The Axon.g in oldmoose flips the current every 20 pulses
+    pulsegen.width[0] = simdt * 200
     pulsegen.level[0] = inject
     moose.connect(pulsegen, 'outputOut', moose.element('/axon/soma'), 'injectMsg')
     data = moose.Neutral('/data')
     tab = moose.Table('%s/Vm100' % (data.path))
     moose.connect(tab, 'requestData', moose.ObjId('/axon/c100'), 'get_Vm')
+    pulsetab = moose.Table('/data/inject')
+    moose.connect(pulsetab, 'requestData', pulsegen, 'get_output')
     solver = moose.HSolve('/hsolve')
     solver.dt = simdt
     solver.target = model.path
     return {'model': model,
             'Vm': tab,
+            'inject': pulsetab,
             'soma': moose.element('/axon/soma'),
             'pulse': pulsegen,
             'solver': solver}
@@ -70,15 +73,25 @@ def run(params):
     moose.setClock(1, simdt)
     moose.setClock(2, simdt)
     moose.setClock(3, simdt)
+    moose.setClock(4, iodt)
     moose.useClock(0, params['solver'].path, 'process')
     moose.useClock(1, params['model'].path + '/##[ISA=Compartment]', 'init')
     moose.useClock(2, params['model'].path + '/##[ISA=Compartment]', 'process')
-    moose.useClock(3, params['Vm'].path, 'process')
+    moose.useClock(3, params['pulse'].path, 'process')
+    moose.useClock(4, '/data/#[TYPE=Table]', 'process')
     moose.reinit()
     moose.start(simlength)
-    np.savetxt('moose_axon_passive_Vm.dat', params['Vm'].vec)
-    plt.plot(np.linspace(0, simlength, len(params['Vm'].vec)), params['Vm'].vec)
+    np.savetxt('axon100.dat', params['Vm'].vec)
+    try:
+        oldmoose_vm100 = np.loadtxt('axon100.oldmoose.dat.gz')
+        plt.plot(np.linspace(0, simlength, len(oldmoose_vm100)), oldmoose_vm100, ':', label='oldmoose')
+    except IOError:
+        print 'Could not open data file for old moose.'
+    plt.plot(np.linspace(0, simlength, len(params['Vm'].vec)), params['Vm'].vec, '--', label='newmoose')
+    plt.plot(np.linspace(0, simlength, len(params['inject'].vec)), params['inject'].vec * 1e6)
+    plt.legend()
     plt.show()
+    
     
 if __name__ == '__main__':
     params = load_axon()
