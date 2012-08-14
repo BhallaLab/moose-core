079893703151fa9153ee6da6cb0ab6cd21f336f7
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-08-14 15:35:04 +0000

    Added a base class for Cylinder data in the mesh directory. Used it for the NeuroNode which will be part of the NeuroMesh.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@3850 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 2fb676e1d5e63b46f0791b733f2f362c84f052c1
    Former-commit-id: 68185dd4ca8385ac5c54ba83dd5c078e8cfde437

---
+++ mesh/NeuroNode.cpp
@@ -0,0 +1,68 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2012 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+#include "header.h"
+#include "CylBase.h"
+#include "NeuroNode.h"
+
+/**
+ * Helper class for the NeuroMesh. Defines the geometry of the branching
+ * neuron.
+ */
+
+NeuroNode::NeuroNode( const CylBase& cb, 
+	unsigned int parent, unsigned int startFid, Id elecCompt,
+	bool isDummyNode, bool isSphere, bool isStartNode )
+		:
+				CylBase( cb ), 
+				parent_( parent ),
+				startFid_( startFid ),
+				elecCompt_( elecCompt ),
+				isDummyNode_( isDummyNode ),
+				isSphere_( isSphere ),
+				isStartNode_( isStartNode )
+{;}
+
+NeuroNode::NeuroNode()
+		:
+				parent_( 0 ),
+				startFid_( 0 ),
+				elecCompt_( Id() ),
+				isDummyNode_( false ),
+				isSphere_( false ),
+				isStartNode_( false )
+{;}
+
+
+unsigned int NeuroNode::parent() const
+{
+		return parent_;
+}
+
+unsigned int NeuroNode::startFid() const
+{
+		return startFid_;
+}
+
+Id NeuroNode::elecCompt() const
+{
+		return elecCompt_;
+}
+bool NeuroNode::isDummyNode() const
+{
+		return isDummyNode_;
+}
+bool NeuroNode::isSphere() const
+{
+		return isSphere_;
+}
+bool NeuroNode::isStartNode() const
+{
+		return isStartNode_;
+}
---
+++ mesh/NeuroNode.h
@@ -0,0 +1,85 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2012 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+#ifndef _NEURO_NODE_H
+#define _NEURO_NODE_H
+
+/**
+ * Helper class for the NeuroMesh. Defines the geometry of the branching
+ * neuron.
+ */
+
+class NeuroNode: public CylBase
+{
+	public:
+		NeuroNode( const CylBase& cb, 
+			unsigned int parent, unsigned int startFid_, Id elecCompt,
+			bool isDummyNode, bool isSphere, bool isStartNode );
+		NeuroNode();
+
+		unsigned int parent() const;
+		unsigned int startFid() const;
+		Id elecCompt() const;
+		bool isDummyNode() const;
+		bool isSphere() const;
+		bool isStartNode() const;
+
+	private:
+		/**
+		 * Index of parent NeuroNode, typically a diffusive compartment. 
+		 * In the special case where the junction to the parent electrical 
+		 * compartment is NOT at the end of the parent compartment, this
+		 * refers instead to a dummy NeuroNode which has the coordinates.
+		 *
+		 * One of the nodes, typically the soma, will have no parent.
+		 */
+		unsigned int parent_; 
+
+		/**
+		 * Index of starting MeshEntry handled by this NeuroNode. Assumes
+		 * a block of contiguous fids are handled by each NeuroNode.
+		 */
+		unsigned int startFid_;
+
+		/// Id of electrical compartment in which this diffusive compt lives
+		Id elecCompt_; 
+
+		/**
+		 * True when this is a dummy node to represent the coordinates
+		 * of the start end of a compartment. For example, the start coords
+		 * of a compartment 
+		 * sitting on a spherical soma, or the start coords of a spine neck 
+		 * along a longer dendritic compartment.
+		 * In all other cases the start coordinates are just those of the
+		 * end of the parent compartment.
+		 *
+		 * When the isDummyNode is true, the elecCompt represents the Id of
+		 * the compartment whose start it is.
+		 */
+		bool isDummyNode_; 
+
+		/**
+		 * Special case for soma, perhaps for spine heads.
+		 * When true, xyz are centre, and dia is dia.
+		 */
+		bool isSphere_;
+
+		/**
+		 * Special case for starting node of tree, typically soma. 
+		 * There should
+		 * be just one StartNode. Its parent is undefined.
+		 */
+		bool isStartNode_;
+
+	// For spines we will also need a double to indicate position along
+	// parent dendrite. 
+	//
+};
+
+#endif	// _NEURO_NODE_H
