81dfedfa39acffc7b54d4bc86d162707f669ad0c
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-09-20 02:34:27 +0000

    Added two example programs for running kinetic models. These also are good tests for the kinetic solvers and volume scaling.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@3960 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 5a26e820231db3ebfa04b3ec1f3f8768ad17d2d3
    Former-commit-id: a92ff3e7d111dd766eda336f7e52524ee97d45a7

---
+++ Demos/snippets/switchKineticSolvers.py
@@ -0,0 +1,36 @@
+# This example illustrates the transition from deterministic to 
+# stochastic in different methods and different simulation volumes.
+# See [[./loadKineticModel.py]] for the basic loading process.
+# This example first loads and runs the model using the gsl solver. Then it
+# switches to the Gillespie gssa method and runs it again at different
+# volumes.
+# The script dumps the output into an xplot file called kineticSolvers.plot.
+
+import moose
+def main():
+		# Load in the model and set up to use the gsl solver.
+		modelId = moose.loadModel( '../Genesis_files/reaction.g', 'model', 'gsl' )
+
+		moose.start( 100.0 ) # Run the model for 100 seconds.
+
+		# Iterate through all plots, dump their contents to data.plot.
+		for x in moose.wildcardFind( '/model/graphs/conc#/#' ):
+			moose.element( x[0] ).xplot( 'kineticSolvers.plot', x[0].name + "gsl" )
+
+		# switch methods
+		modelId[0].method = 'gssa'
+		for vol in ( 1e-20, 1e-19, 1e-18, 1e-17 ):
+			moose.element( '/model/kinetics' ).size = vol
+			moose.reinit() # Start over from time zero
+			moose.start( 100.0 ) # Run the model for 100 seconds.
+
+			# Dump plots again, with a slightly modified name.
+			for x in moose.wildcardFind( '/model/graphs/conc#/#' ):
+				name = 'gssa_' + x[0].name + '_' + str( vol )
+				moose.element( x[0] ).xplot( 'kineticSolvers.plot', name )
+
+		quit()
+
+# Run the 'main' if this script is executed standalone.
+if __name__ == '__main__':
+	main()
