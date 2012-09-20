c796d96477576a1de42780cf6ccebb3f83e0c3e2
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-09-20 02:34:27 +0000

    Added two example programs for running kinetic models. These also are good tests for the kinetic solvers and volume scaling.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@3960 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ Demos/snippets/loadKineticModel.py
@@ -0,0 +1,22 @@
+# This example illustrates loading and running a kinetic model defined
+# in kkit format. We use the gsl solver here. The model already defines
+# a couple of plots and sets the runtime to 100 seconds. The
+# script dumps the output into an xplot file called data.plot.
+
+import moose
+def main():
+		# This command loads the file into the path '/model', and tells
+		# the system to use the gsl solver.
+		modelId = moose.loadModel( '../Genesis_files/reaction.g', 'model', 'gsl' )
+
+		moose.start( 100.0 ) # Run the model for 100 seconds.
+
+		# Iterate through all plots, dump their contents to data.plot.
+		for x in moose.wildcardFind( '/model/graphs/conc#/#' ):
+				moose.element( x[0] ).xplot( 'data.plot', x[0].name )
+
+		quit()
+
+# Run the 'main' if this script is executed standalone.
+if __name__ == '__main__':
+	main()
