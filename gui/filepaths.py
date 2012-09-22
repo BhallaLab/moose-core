d7289c9fff2782522901463d2b5d6427d4691248
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-09-22 10:53:45 +0000

    Removed filepaths.py from gui. All contents are now part of config.py and should be accessed via config.settings.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@3987 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- gui/filepaths.py
+++
@@ -1,5 +0,0 @@
-import os
-
-PATH_MAIN = os.path.abspath(__file__).rpartition('filepaths.py')[0]
-PATH_KKIT_COLORMAPS = os.path.join(PATH_MAIN,'colormaps','KKIT')
-PATH_Neural_COLORMAPS = os.path.join(PATH_MAIN,'colormaps','Neural')
