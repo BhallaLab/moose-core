bceea81d48efbbdccccd822af02aa8cf6b40140f
hrani <hrani@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-09-26 06:27:59 +0000

    removed setplaceholder for lineEdit_4:QLineEdit,lineEdit_2:QLineEdit,popFilterSelectLineEdit:QLineEdit currently we are not using these and anything below Qt<6  will not work
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@4037 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: fb64b96a00c0b77f1dfb3f2e4a2715f57f25b953
    Former-commit-id: bd8b46d194604e0b0373d7a4106b23984c0d3d5b

--- gui/newgui.py
+++ gui/newgui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'newgui.ui'
 #
-# Created: Tue Sep 25 22:44:10 2012
+# Created: Wed Sep 26 12:02:13 2012
 #      by: PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -773,8 +773,6 @@ class Ui_MainWindow(object):
         self.propEditorSelParentPushButton.setText(QtGui.QApplication.translate("MainWindow", "Select Parent", None, QtGui.QApplication.UnicodeUTF8))
         self.label_19.setText(QtGui.QApplication.translate("MainWindow", "Properties", None, QtGui.QApplication.UnicodeUTF8))
         self.moosePopulationEditDock.setWindowTitle(QtGui.QApplication.translate("MainWindow", "Population Editor", None, QtGui.QApplication.UnicodeUTF8))
-        if QtCore.PYQT_VERSION >= 6:
-            self.popFilterSelectLineEdit.setPlaceholderText(QtGui.QApplication.translate("MainWindow", "filter", None, QtGui.QApplication.UnicodeUTF8))
         self.popSelectParentPushButton.setText(QtGui.QApplication.translate("MainWindow", "SelectParent", None, QtGui.QApplication.UnicodeUTF8))
         self.label.setText(QtGui.QApplication.translate("MainWindow", "Common Properties", None, QtGui.QApplication.UnicodeUTF8))
         self.label_2.setText(QtGui.QApplication.translate("MainWindow", "Distribution: ", None, QtGui.QApplication.UnicodeUTF8))
@@ -786,14 +784,10 @@ class Ui_MainWindow(object):
         self.libMakeCopiesButton.setText(QtGui.QApplication.translate("MainWindow", "MakeCopies", None, QtGui.QApplication.UnicodeUTF8))
         self.mooseConnectDock.setWindowTitle(QtGui.QApplication.translate("MainWindow", "Connect Elements", None, QtGui.QApplication.UnicodeUTF8))
         self.label_4.setText(QtGui.QApplication.translate("MainWindow", "Sources (Current Selection):", None, QtGui.QApplication.UnicodeUTF8))
-        if QtCore.PYQT_VERSION >= 6:
-            self.lineEdit_2.setPlaceholderText(QtGui.QApplication.translate("MainWindow", "filter", None, QtGui.QApplication.UnicodeUTF8))
         self.label_3.setText(QtGui.QApplication.translate("MainWindow", "Source field", None, QtGui.QApplication.UnicodeUTF8))
         self.label_6.setText(QtGui.QApplication.translate("MainWindow", "Connect via", None, QtGui.QApplication.UnicodeUTF8))
         self.comboBox.setItemText(0, QtGui.QApplication.translate("MainWindow", "one2one", None, QtGui.QApplication.UnicodeUTF8))
         self.label_7.setText(QtGui.QApplication.translate("MainWindow", "Target", None, QtGui.QApplication.UnicodeUTF8))
-        if QtCore.PYQT_VERSION >= 6:
-            self.lineEdit_4.setPlaceholderText(QtGui.QApplication.translate("MainWindow", "filter", None, QtGui.QApplication.UnicodeUTF8))
         self.label_5.setText(QtGui.QApplication.translate("MainWindow", "Target field", None, QtGui.QApplication.UnicodeUTF8))
         self.pushButton.setText(QtGui.QApplication.translate("MainWindow", "Connect", None, QtGui.QApplication.UnicodeUTF8))
         self.plotConfigDockWidget.setStatusTip(QtGui.QApplication.translate("MainWindow", "Plot Window. From \'Property Editor\'   Select \'plot Field\', \'plot Window\' to add a plot and \'show\' for displaying plots", None, QtGui.QApplication.UnicodeUTF8))
@@ -919,4 +913,4 @@ class Ui_MainWindow(object):
         self.actionSave_Model.setShortcut(QtGui.QApplication.translate("MainWindow", "Ctrl+S", None, QtGui.QApplication.UnicodeUTF8))
         self.actionMerge_Models.setText(QtGui.QApplication.translate("MainWindow", "Merge Models", None, QtGui.QApplication.UnicodeUTF8))
 
-from objectedit2 import ObjectEditView
+from objectedit import ObjectEditView
