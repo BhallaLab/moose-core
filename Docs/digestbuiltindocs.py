95a9a29d69a38aa43e594cd4736b24d6da350890
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-10-04 11:21:28 +0000

    Updated the script to extract documentation. Added the generated documentation.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@4112 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: c44f25ae6ce2bca7d290f67be3e74508e0d94a3f
    Former-commit-id: 2e8ca458767dc87a746537503c5baec1391610b6

--- Docs/digestbuiltindocs.py
+++ Docs/digestbuiltindocs.py
@@ -6,9 +6,9 @@
 # Maintainer: 
 # Created: Thu Oct  4 11:33:31 2012 (+0530)
 # Version: 
-# Last-Updated: Thu Oct  4 15:15:34 2012 (+0530)
+# Last-Updated: Thu Oct  4 16:53:43 2012 (+0530)
 #           By: subha
-#     Update #: 144
+#     Update #: 234
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -48,6 +48,9 @@
 import sys
 import cStringIO
 import re
+import inspect
+from datetime import datetime
+
 sys.path.append('../python')
 import moose
 
@@ -64,7 +67,7 @@ config = {'heading1': '* ',
 	  'nlist': 1}
 
 
-def extract_ftype_doc(cinfo, finfotype, docio, indent):
+def extract_ftype_doc(cinfo, finfotype, docio, indent, lmarker):
     """Extract field documentation for all fields of type `finfotype`
     in class `cinfo` into `docio`.
 
@@ -88,11 +91,11 @@ def extract_ftype_doc(cinfo, finfotype, docio, indent):
 					 fid.name.startswith('set_')):
 	    continue
 	dtype = type_mangling_regex.sub('', fid.type)
-	docio.write('\n%s%s%s/%s/ ::\n' % (indent, config['ulist2'], fid.name, dtype))
+	docio.write('\n%s%s%s:\t/%s/ ::\n' % (indent, lmarker, fid.name, dtype))
 	doc = fid.docs.replace('\n', '\n%s' % (indent2))
 	docio.write(indent2+doc)
 
-def extract_class_doc(name, docio, indent):
+def extract_class_doc(name, docio, indent, lmarker):
     """Extract documentation for Cinfo object at path
     
     Parameters
@@ -104,22 +107,41 @@ def extract_class_doc(name, docio, indent):
 	output object to write the documentation into.
     """
     cinfo = moose.Cinfo('/classes/%s' % (name))
-    docs = cinfo.docs.replace('\n', '\n'+indent)
-    if docs.strip():
+    docs = cinfo.docs.strip()
+    if docs:	
+	docs = docs.replace('\n', '\n'+indent)
 	docio.write('\n%s%s' % (indent, docs))
     for finfotype, readablename in moose.finfotypes:
-        docio.write('\n\n%s%s*%s*' % (indent, config['ulist1'], readablename.capitalize()))
-	extract_ftype_doc(cinfo, finfotype, docio, indent + ' ' * len(config['ulist1']))
+        docio.write('\n\n%s%s*%s*' % (indent, lmarker, readablename.capitalize()))
+	extract_ftype_doc(cinfo, finfotype, docio, indent + ' ' * len(lmarker), lmarker)
 
-def extract_all_class_doc(docio):
+def extract_all_class_doc(docio, hmarker, lmarker):
     for cinfo in moose.element('/classes').children:
-	docio.write('\n\n%s%s' % (config['heading1'], cinfo[0].name))
-	extract_class_doc(cinfo.name, docio, ' ' * len(config['heading1']))
+	docio.write('\n\n%s%s' % (hmarker, cinfo[0].name))
+	extract_class_doc(cinfo.name, docio, ' ' * len(hmarker), lmarker)
+
+def extract_all_func_doc(docio, hmarker):
+    for fname, fdef in inspect.getmembers(moose, inspect.isbuiltin) + inspect.getmembers(moose, inspect.isfunction):
+	docio.write('\n\n%s%s\n' % (hmarker, fname))
+	doc = inspect.getdoc(fdef)
+	doc = doc.replace('\n', '\n'+' '*len(hmarker))
+	docio.write(' '*len(hmarker)+doc)
 
 if __name__ == '__main__':
-    docio = cStringIO.StringIO()
-    extract_all_class_doc(docio)
-    print docio.getvalue()
+    if len(sys.argv)  > 1:
+	docio = open(sys.argv[1], 'w')
+    else:
+	docio = cStringIO.StringIO()
+    docio.write(config['heading1'] + 'MOOSE Classes\n')
+    ts = datetime.now()
+    extract_all_class_doc(docio, config['heading2'], config['ulist1'])
+    docio.write('\n\n'+config['heading1'] + 'MOOSE Functions\n')
+    extract_all_func_doc(docio, config['heading2'])
+    docio.write('/automatically extracted on %s/\n' % (ts.isoformat()))
+    if isinstance(docio, cStringIO.OutputType):
+	print docio.getvalue()
+    else:
+	docio.close()
 
 # 
 # digestbuiltindocs.py ends here
