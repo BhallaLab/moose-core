02f3a4b9061602ad2a7f739ebf9c4d2a0ca641f1
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-10-15 10:48:43 +0000

    Cleaned up test code for single cells in traub 2005 model.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@4131 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 2a9b0935a911c72e8fbf6804a7b759eafc175b09
    Former-commit-id: 0257f89bf719efbf1252baa595cd04a97e860fbf

--- Demos/traub_2005/py/test_tcr.py
+++ Demos/traub_2005/py/test_tcr.py
@@ -6,9 +6,9 @@
 # Maintainer: 
 # Created: Mon Jul 16 16:12:55 2012 (+0530)
 # Version: 
-# Last-Updated: Tue Sep  4 09:55:40 2012 (+0530)
+# Last-Updated: Mon Oct 15 16:18:01 2012 (+0530)
 #           By: subha
-#     Update #: 424
+#     Update #: 465
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -42,91 +42,16 @@ import uuid
 import numpy as np
 import pylab
 import moose
-from testutils import compare_cell_dump, setupClocks, assignClocks
+import testutils
+from testutils import compare_cell_dump, setup_clocks, assign_clocks, step_run
+from cell_test_util import setup_current_step_model, SingleCellCurrentStepTest
 import cells
 from moose import utils
 
-INITCLOCK = 0
-ELECCLOCK = 1
-CHANCLOCK = 2
-POOLCLOCK = 3
-LOOKUPCLOCK = 6
-STIMCLOCK = 7
-PLOTCLOCK = 8
-
-SIMDT = 5e-6
-PLOTDT = 0.25e-3
-SIMTIME = 1.0
-
+simdt = 5e-6
+plotdt = 0.25e-3
+simtime = 1.0
     
-def setupCurrentStepModel(testId, celltype, pulsearray, simdt, plotdt, solver='euler'):
-    """Setup a single cell simulation.
-
-    testId: integer - identifying the model
-
-    celltype: str - cell type
-    
-    pulsearray: nx3 array - with row[i] = (delay[i], width[i], level[i]) of current injection.
-
-    simdt: float - simulation time step
-
-    plotdt: float - sampling interval for plotting 
-
-    solver: str - numerical method to use, can be `hsolve` or `ee`
-    """
-    modelContainer = moose.Neutral('/test%d' % (testId))
-    dataContainer = moose.Neutral('/data%d' % (testId))
-    cell_list = []
-    for ii in range(1):
-        cell = cells.TCR('%s/TCR_%d' % (modelContainer.path, ii)) 
-        cell_list.append(cell)
-    pulsegen = moose.PulseGen('%s/pulse' % (modelContainer.path))
-    pulsegen.count = len(pulsearray)
-    for ii in range(len(pulsearray)):
-        pulsegen.delay[ii] = pulsearray[ii][0]
-        pulsegen.width[ii] = pulsearray[ii][1]
-        pulsegen.level[ii] = pulsearray[ii][2]
-    for ii, cell in enumerate(cell_list):
-        moose.connect(pulsegen, 'outputOut', cell.soma, 'injectMsg')
-        presynVm = moose.Table('%s/presynVm_%d' % (dataContainer.path, ii))
-        somaVm =  moose.Table('%s/somaVm_%d' % (dataContainer.path, ii))
-        moose.connect(presynVm, 'requestData', cell.presynaptic, 'get_Vm')
-        moose.connect(somaVm, 'requestData', cell.soma, 'get_Vm')
-    pulseTable = moose.Table('%s/pulse' % (dataContainer.path))
-    moose.connect(pulseTable, 'requestData', pulsegen, 'get_output')
-    setupClocks(simdt, plotdt)
-    moose.useClock(STIMCLOCK, modelContainer.path+'/##[TYPE=PulseGen]', 'process')
-    moose.useClock(PLOTCLOCK, dataContainer.path+'/##[TYPE=Table]', 'process')
-    if solver == 'hsolve':
-        for ii in range(len(cell_list)):
-            hsolve = moose.HSolve(modelContainer.path + '/solve_%d' % (ii))
-            print 'Created', hsolve.path
-            hsolve.dt = simdt
-            hsolve.target = cell_list[ii].path
-        moose.useClock(INITCLOCK, modelContainer.path+'/##[TYPE=HSolve]', 'process')
-    else:
-        moose.useClock(INITCLOCK, modelContainer.path+'/##[TYPE=Compartment]', 'init')
-        moose.useClock(ELECCLOCK, modelContainer.path+'/##[TYPE=Compartment]', 'process')
-        moose.useClock(CHANCLOCK, modelContainer.path+'/##[TYPE=HHChannel]', 'process')
-        moose.useClock(POOLCLOCK, modelContainer.path+'/##[TYPE=CaConc]', 'process')
-    return {'cell': cell,
-            'stimulus': pulsegen,
-            'presynapticVm': presynVm,
-            'somaVm': somaVm,
-            'stimTable': pulseTable,
-            'model': modelContainer,
-            'data': dataContainer
-            }
-    
-def runsim(simtime, steptime=0.1):
-    clock = moose.Clock('/clock')
-    print 'Starting simulation for', simtime
-    while clock.currentTime < simtime - steptime:
-        moose.start(steptime)
-        print 'Simulated till', clock.currentTime, 's'
-    remaining = simtime % steptime
-    moose.start(remaining)
-    print 'Finished simulation'
 
 # pulsearray = [[1.0, 100e-3, 1e-9],
 #               [0.5, 100e-3, 0.3e-9],
@@ -138,50 +63,56 @@ pulsearray = [[100e-3, 100e-3, 1e-9],
               [1e9, 0, 0]]
 
 
-class TestTCR(unittest.TestCase):
+class TestTCR(SingleCellCurrentStepTest):
     def setUp(self):
-        self.testId = uuid.uuid4().int
+        SingleCellCurrentStepTest.setUp(self)
         self.solver = 'hsolve'
-        params = setupCurrentStepModel(self.testId, 'TCR', pulsearray, SIMDT, PLOTDT, solver=self.solver)
-        cell = params['cell']
-        moose.le('/library')
-        kahp_slower = moose.element(cell.soma.path + '/KAHP_SLOWER')
-        capool = moose.element(cell.soma.path + '/CaPool')
-        # self.dump_file = 'data/TCR.csv'
-        # params['cell'].dump_cell(self.dump_file)
+        params = setup_current_step_model(
+            self.model_container, 
+            self.data_container, 
+            'TCR', 
+            pulsearray, 
+            testutils.SIMDT, 
+            testutils.PLOTDT,
+            solver=self.solver)
+        self.cell = params['cell']
+        testutils.setup_clocks(testutils.SIMDT, testutils.PLOTDT)
+        testutils.assign_clocks(self.model_container, 
+                                self.data_container, 
+                                solver=self.solver)
         moose.reinit()
         start = datetime.now()
-        runsim(SIMTIME)
+        step_run(simtime, 0.1)
         end = datetime.now()
         delta = end - start
-        print 'Simulation time:', delta.seconds + delta.microseconds * 1e-6            
-        tseries = np.linspace(0, SIMTIME, len(params['somaVm'].vec))
-        for table_id in params['data'].children:
+        print 'Simulation time:', delta.seconds \
+            + delta.microseconds * 1e-6            
+        tseries = np.linspace(0, simtime, len(params['somaVm'].vec))
+        for table_id in self.data_container.children:
+            data = np.vstack((tseries, table_id[0].vec))
             np.savetxt('data/TCR_%s_%s.dat' % (table_id[0].name, self.solver), 
-                       np.transpose(np.vstack((tseries, table_id[0].vec))))
-        # np.savetxt('data/TCR_soma_Vm_%s.dat' % (self.solver), np.transpose(np.vstack((tseries, params['somaVm'].vec))))
-        # np.savetxt('data/TCR_presynaptic_Vm_%s.dat' % (self.solver), np.transpose(np.vstack((tseries, params['presynapticVm'].vec))))
-        # pylab.subplot(211)
-        # pylab.title('Soma Vm')
-        # pylab.plot(tseries*1e3, params['somaVm'].vec * 1e3, label='Vm (mV) - moose')
-        # pylab.plot(tseries*1e3, params['stimTable'].vec * 1e9, label='Stimulus (nA)')
-        # try:
-        #     nrn_data = np.loadtxt('../nrn/data/TCR_soma_Vm.dat')
-        #     pylab.plot(nrn_data[:,0], nrn_data[:,1], label='Vm (mV) - neuron')
-        # except IOError:
-        #     print 'No neuron data found.'
-        # pylab.legend()
-        # pylab.subplot(212)
-        # pylab.title('Presynaptic Vm')
-        # pylab.plot(tseries*1e3, params['presynapticVm'].vec * 1e3, label='Vm (mV) - moose')
-        # pylab.plot(tseries*1e3, params['stimTable'].vec * 1e9, label='Stimulus (nA)')
-        # try:
-        #     nrn_data = np.loadtxt('../nrn/data/TCR_presynaptic_Vm.dat')
-        #     pylab.plot(nrn_data[:,0], nrn_data[:,1], label='Vm (mV) - neuron')
-        # except IOError:
-        #     print 'No neuron data found.'
-        # pylab.legend()
-        # pylab.show()
+                       np.transpose(data))
+        pylab.subplot(211)
+        pylab.title('Soma Vm')
+        pylab.plot(tseries*1e3, params['somaVm'].vec * 1e3, label='Vm (mV) - moose')
+        pylab.plot(tseries*1e3, params['injectionCurrent'].vec * 1e9, label='Stimulus (nA)')
+        try:
+            nrn_data = np.loadtxt('../nrn/data/TCR_soma_Vm.dat')
+            pylab.plot(nrn_data[:,0], nrn_data[:,1], label='Vm (mV) - neuron')
+        except IOError:
+            print 'No neuron data found.'
+        pylab.legend()
+        pylab.subplot(212)
+        pylab.title('Presynaptic Vm')
+        pylab.plot(tseries*1e3, params['presynapticVm'].vec * 1e3, label='Vm (mV) - moose')
+        pylab.plot(tseries*1e3, params['injectionCurrent'].vec * 1e9, label='Stimulus (nA)')
+        try:
+            nrn_data = np.loadtxt('../nrn/data/TCR_presynaptic_Vm.dat')
+            pylab.plot(nrn_data[:,0], nrn_data[:,1], label='Vm (mV) - neuron')
+        except IOError:
+            print 'No neuron data found.'
+        pylab.legend()
+        pylab.show()
 
     def testChannelDensities(self):
         pass
