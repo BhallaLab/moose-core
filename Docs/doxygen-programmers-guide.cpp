1e8ba8ea9d343d3caf5fc693f395c7b1ef7252fa
niraj_ <niraj_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-10-27 17:18:53 +0000

    Cleaning up hand-written Doxygen documentation.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@4205 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: d9b69ae39688def3790ce50abce80dc446bc4e38

--- Docs/doxygen-programmers-guide.cpp
+++ Docs/doxygen-programmers-guide.cpp
@@ -1,12 +1,6 @@
 /**
 \page ProgrammersGuide Programmer's Guide
-Main or introductory text for this page.
-
-\section UniqueSectionLabel Section Title Here
-Section text inserted here.
-
-\subsection UniqueSubsectionLabel Subsection Title Here
-Subsection text inserted here.
+Documentation for programmers.
 
 \section PG_ProcessLoop	Process Loop
 The MOOSE main process loop coordinates script commands, multiple threads
@@ -280,13 +274,4 @@ to the arguments used by the object function. Here are some of the key features:
 	<li> FieldOpFunc:
 </ul>
 
-*/
-
-/**
-\page ProgrammersGuide Programmer's Guide
-
-\section YetAnotherSection Yet Another Section
-All content on a given "page" need not be in the same comment block, or even in
-the same file. This comment block can be moved to another file, and its content
-will still end up on the ProgrammersGuide page.
 */
