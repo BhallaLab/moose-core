41445f89cead7f4b6d7196d2a6b357c79ce1cb3c
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-10-28 07:47:04 +0000

    Moved the FuncTerm stuff to /kinetics, reorganized into individual files, made a FuncBase class and derived SumFunc off it. Lots of related cleanup.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@4207 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: f121fd777259ce6eaabb6bbc549c517d6e975e81
    Former-commit-id: c1d893a7290d2553450572514a12d0c0e921d6fc

---
+++ kinetics/FuncTerm.h
@@ -0,0 +1,35 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment,
+** also known as GENESIS 3 base code.
+**           copyright (C) 2003-2010 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+#ifndef _FUNC_TERM_H
+#define _FUNC_TERM_H
+
+class FuncTerm
+{
+	public:
+		FuncTerm() {;}
+		virtual ~FuncTerm()
+		{;}
+		/**
+		 * This computes the value. The time t is an argument needed by
+		 * some peculiar functions.
+		 */
+		virtual double operator() ( const double* S, double t ) const = 0;
+
+		/**
+		 * This function finds the reactant indices in the vector
+		 * S. It returns the number of indices found, which are the
+		 * entries in molIndex.
+		 */
+		virtual unsigned int  getReactants( 
+			vector< unsigned int >& molIndex ) const = 0;
+		virtual const string& function() const = 0;
+};
+
+#endif // _FUNC_TERM_H
