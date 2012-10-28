99312b69f10ef5ba59d7405762de170b742b8e8a
niraj_ <niraj_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-10-28 10:35:54 +0000

    More updates in Doxygen documentation.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@4208 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 75ed915d971ba3de893c181ea1887249a74201ce

--- Docs/doxygen-main.cpp
+++ Docs/doxygen-main.cpp
@@ -15,10 +15,6 @@ compatibility we have a GENESIS scripting module, but this is deprecated.
 MOOSE uses Qt/OpenGL for its graphical interface. The entire GUI is
 written in Python, and the MOOSE numerical code is written in C++.
 
-MOOSE is currently in beta 1.3. One more beta, 1.4 is planned, which
-will have a first-order functional GUI. Following this
-we'll go into release-candidate mode with RC0.9 and so on.
-
 \section support_sec Hardware and availability
 MOOSE runs on everything from laptops to large clusters. It supports
 multiple-core machines through threading, and cluster architectures using
@@ -31,23 +27,6 @@ is LGPL, meaning it is easy to reuse with attribution but will remain
 free. However, the common release of MOOSE uses the GNU scientific library
 (GSL) which is under the GPL. For such releases, MOOSE should be treated
 as also being under the GPL.
-
-================================================================================
-
-Some notes about adding general documentation in Doxygen. These notes can be
-removed later.
-
-General documentation in Doxygen can be organized into "pages". A page can have
-sections, subsections, and perhaps more components. Every page, section and
-subsection has a label (a single-word id), a title, and body text. If the label
-is unique across the entire project documentation, cross-page hyper-linked
-references can be added as shown below.
-
-Doxygen can collect all the content for a given page from multiple source files.
-This is demonstrated in Docs/doxygen-programmers-guide.cpp. However, I am not
-sure how will Doxygen order the sections on the page.
-
-================================================================================
 
 Apart from the auto-generated documentation for the source-code itself, here are
 some higher-level hand-written documents:
