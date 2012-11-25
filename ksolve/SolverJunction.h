0dba7a98c753251eac727e6b4622036ad445059b
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-11-25 18:17:54 +0000

    Implemented code for setting up meshing stuff in SolverJunctions.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@4294 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- ksolve/SolverJunction.h
+++ ksolve/SolverJunction.h
@@ -10,6 +10,75 @@
 #ifndef _SOLVER_JUNCTION_H
 #define _SOLVER_JUNCTION_H
 
+/**
+ * The SolverJunction is a one-to-one connection point between solvers
+ * which need to exchange information about molecular interchange. There
+ * is precisely one SolverJunction between any two solvers.
+ * The SolverJunction handles molecular flow through reactions, diffusion,
+ * and motors.
+ * The SolverJunction supports molecular flow between arbitrary mesh
+ * geometries, but itself does not know about any of these details.
+ * An overview of the process follows.
+ *
+ * 1. At each interfacing mesh entry, we have a set of reactions and
+ * 	diffusion processes. These are common between all interfacing entries
+ * 	at the junction.
+ * 		1.1 The total amount of information sent from compartment A is
+ *			nM * ( nR + nD )
+ *		where nM = number of meshEntries at the junction on the A side
+ *		nR = number of reaction terms going across the junction.
+ *		nD = number of diffusing molecules.
+ *		1.2 This information is sent in a single vector of doubles of length
+ *			nM * ( nR + nD )
+ *		1.3 The implicit indexing of the vector is like 
+ *				vec[meshEntry][Reac,then Diff term]
+ *			with the meshEntry varying slower.
+ *		1.4 Both nM and nR may differ going the other way across the 
+ *			junction. nD must be identical.
+ *
+ * 2. The SolverJunction handles data structures needed for Sending
+ * 	data as well as for Receiving it.
+ *		2.1 It sends locally computed reaction and diffusion rates. To do
+ *			this it must map to local Stoich::rateTerm data structures.
+ *		2.2 It receives externally computed reaction and diffusion rates.
+ *			To do this it must map the arriving data to local mesh and
+ *			pool indices.
+ *
+ * 3. The nR reaction rates are all computed in a portion of the regular
+ * 		StoichCore::rates_ vector, which contains RateTerms. These
+ * 		are updated in StoichCore::updateJunctionRates. The indices of
+ * 		RateTerms that are updated are stored in SolverJunction::reacTerms_
+ * 		This is passed to StoichCore by GslStoich::updateJunction 
+ * 		or equivalent.
+ *
+ * 4. The nD diffusion rates are all computed in 
+ * 		GslStoich::updateJunctionDiffusion or equivalent. The poolIndices
+ * 		of diffusing molecules are stored in SolverJunction::diffTerms_
+ * 		This is also passed by the GslStoich::updateJunction
+ *
+ * 5. Each of the above calculations in 3 and 4 are computed for all nM
+ * 		meshEntries on the junction on the local compartment. The list 
+ * 		of relevant meshEntries is stored in SolverJunction::meshIndex_
+ *
+ * 6. The combined vector of nM * ( nR + nD ) doubles is sent to the other
+ * 	solver. These are all rates, or fluxes. They need to be mapped onto
+ * 	the meshIndices and poolIndices of the target solver. Any given mesh
+ * 	on the source may map onto one or more on target. Any given rate term
+ * 	on the vector may map onto one or more on target.
+ * 	With these mapped fluxes, but not with this vector, we can do numerical 
+ * 	integration using the method of choice (for now Forward Euler).
+ *
+ * 7. The received vector is mapped onto Pools by SolverJunction as follows:
+ * 	The targetMols_ vector::first = vecIndex % (nR+nD)
+ * 	The targetMols_ vector::second = poolIndex 
+ * 		This happens in SolverJunction.cpp::incrementTargets.
+ * 	
+ * 8. The received vector is mapped onto meshEntries (voxels) as follows:
+ * 	The targetMeshIndices_ vector::first = vecIndex / (nR + nD )
+ * 	The targetMeshIndices_ vector::second = meshIndex
+ * 		This happens in SolverJunction.cpp::incrementTargets.
+ *
+ */
 class SolverJunction
 {
 	public:
@@ -103,12 +172,19 @@ class SolverJunction
 		 * A simple one-to-one map won't work, because a
 		 * given reac may have multiple targets, and a given meshIndex
 		 * may map onto multiple (or fractional) target meshIndices.
-		 * So each of the vectors below has first the index = 
-		 * (mesh * (total num of reac&diffterms) + reac&diff index )
-		 * used in the transmitted vector, 
-		 * and then the index (meshIndex or poolIndex) of the target.
 		 */
+		/**
+ 		 * The received vector maps onto Pools by SolverJunction as follows:
+ 		 * 	The targetMols_ vector::first = vecIndex % (nR+nD)
+ 		 * 	The targetMols_ vector::second = poolIndex 
+ 		 */
 		vector< pair< unsigned int, unsigned int > > targetMols_;
+
+ 		/** 
+		 * The received vector maps onto meshEntries (voxels) as follows:
+ 		 * 	The targetMeshIndices_ vector::first = vecIndex / (nR + nD )
+ 		 * 	The targetMeshIndices_ vector::second = meshIndex
+		 */
 		vector< pair< unsigned int, unsigned int > > targetMeshIndices_;
 };
 
