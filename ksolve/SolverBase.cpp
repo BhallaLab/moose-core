16a4378e56a983e6343fd48220a709186905da71
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2013-01-28 04:30:59 +0000

    Had forgotten to add a number of files. Also a valgrind fix. This clears unit and regression tests.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@4384 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 01a2d3cab4360940d37241c10261d46458292131
    Former-commit-id: db831f994bc50e89f370c9b219afa0cfab9e064f

---
+++ ksolve/SolverBase.cpp
@@ -0,0 +1,281 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2010 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+#include "header.h"
+#include "../mesh/VoxelJunction.h"
+#include "SolverJunction.h"
+#include "SolverBase.h"
+#include "../shell/Shell.h"
+
+const Cinfo* SolverBase::initCinfo()
+{
+	static DestFinfo addJunction( "addJunction",
+			"Add a junction between the current solver and the one whose"
+			" Id is passed in.",
+			new EpFunc1< SolverBase, Id >( &SolverBase::addJunction )
+	);
+
+	static DestFinfo dropJunction( "dropJunction",
+			"Drops a junction between the current solver and the one whose"
+			" Id is passed in. Ignores if no junction.",
+			new EpFunc1< SolverBase, Id >( &SolverBase::dropJunction )
+	);
+
+	static FieldElementFinfo< SolverBase, SolverJunction > junction(
+		"junction",
+		"Handles how solvers communicate with each other in case of "
+		"diffusion, motors, or reaction.",
+		SolverJunction::initCinfo(),
+		&SolverBase::getJunction,
+		&SolverBase::setNumJunctions,
+		&SolverBase::getNumJunctions,
+		16
+	);
+
+	static Finfo* solverBaseFinfos[] = {
+		&addJunction,	// DestFinfo
+		&dropJunction,	// DestFinfo
+		&junction		// FieldElement
+	};
+
+	static string doc[] = 
+	{
+			"Name", "SolverBase",
+			"Author", "Upinder S. Bhalla, 2012, NCBS",
+			"Description", "Pure virtual base class for handling "
+			"solvers, with special emphasis toward reaction-diffusion "
+			"systems spanning multiple solvers and processors. "
+			"GslStoich is derived from this."
+			"The main role of theis class is to set up the Junctions "
+			"between solvers."
+	};
+
+	static Cinfo solverBaseCinfo(
+		"SolverBase",
+		Neutral::initCinfo(),
+		solverBaseFinfos,
+		sizeof( solverBaseFinfos ) / sizeof( Finfo* ),
+		new ZeroSizeDinfo< int >(),
+		doc, sizeof( doc ) / sizeof( string )
+	);
+	
+	return &solverBaseCinfo;
+}
+
+static const Cinfo* solverBaseCinfo = SolverBase::initCinfo();
+
+//////////////////////////////////////////////////////////////
+// Class definitions
+//////////////////////////////////////////////////////////////
+
+SolverBase::SolverBase()
+{;}
+
+SolverBase::~SolverBase()
+{;}
+
+//////////////////////////////////////////////////////////////////////////
+// Access functions for junctions
+//////////////////////////////////////////////////////////////////////////
+SolverJunction* SolverBase::getJunction( unsigned int i )
+{
+	static SolverJunction dummy;
+	if ( i < junctions_.size() )
+		return &junctions_[i];
+
+	cout << "Warning: SolverBase::getJunction: Index: " << i << 
+			" is out of range: " << junctions_.size() << endl;
+
+	return &dummy;
+}
+
+void SolverBase::setNumJunctions( unsigned int v )
+{
+	cout << "Warning: SolverBase::setNumJunctions: Direct assignment "
+		"of number not permitted, use addJunction instead.";
+}	
+
+unsigned int SolverBase::getNumJunctions() const
+{
+	return junctions_.size();
+}
+
+void SolverBase::handleJunctionPoolDelta( unsigned int fieldIndex,
+		vector< double > v )
+{
+	vHandleJunctionPoolDelta( fieldIndex, v );
+}
+
+void SolverBase::handleJunctionPoolNum( unsigned int fieldIndex,
+		vector< double > v )
+{
+	vHandleJunctionPoolNum( fieldIndex, v );
+}
+
+bool validateJunction( Id me, Id other )
+{
+	// Check that types match
+	if ( !other.element()->cinfo()->isA( "SolverBase" ) ) {
+		cout << "Warning: SolverBase::validateJunction: Other Id '" <<
+				other.path() << " is not a StoichPool\n";
+		return false;
+	}
+	// Check if junction already exists.
+	Id myJunction( me.value() + 1);
+	Id otherJunction( other.value() + 1);
+	vector< Id > ret;
+	myJunction.element()->getNeighbours( ret, junctionPoolDeltaFinfo() );
+	if ( find( ret.begin(), ret.end(), otherJunction ) != ret.end() ) {
+		cout << "Warning: SolverBase::validateJunction: junction " <<
+		" already present from " << 
+		me.path() << " to " << other.path() << endl;
+		return false;
+	}
+	return true;
+}
+
+void SolverBase::findDiffusionTerms( 
+				const SolverBase* otherSP,
+				vector< unsigned int >& selfTerms,
+				vector< unsigned int >& otherTerms
+	) const
+{
+	selfTerms.resize( 0 );
+	otherTerms.resize( 0 );
+	map< string, unsigned int > selfDiffTerms;
+	this->vBuildDiffTerms( selfDiffTerms );
+	map< string, unsigned int > otherDiffTerms;
+	otherSP->vBuildDiffTerms( otherDiffTerms );
+	map< string, unsigned int >::iterator j;
+	for ( map< string, unsigned int >::iterator i = selfDiffTerms.begin();
+					i != selfDiffTerms.end(); ++i )
+	{
+		j = otherDiffTerms.find( i->first );
+		if ( j != otherDiffTerms.end() ) {
+			selfTerms.push_back( i->second );
+			otherTerms.push_back( j->second );
+		}
+	}
+}
+
+void SolverBase::innerConnectJunctions( 
+			Id me, Id other, SolverBase* otherSP )
+{
+	Shell* shell = reinterpret_cast< Shell* >( Id().eref().data() );
+	Id myJunction( me.value() + 1);
+	Id otherJunction( other.value() + 1);
+	// Set up message
+	ObjId myOid( myJunction, getNumJunctions() );
+	ObjId otherOid( otherJunction, otherSP->getNumJunctions() );
+	MsgId mid = shell->doAddMsg( "single", myOid, "masterJunction", 
+					otherOid, "followerJunction" );
+	assert( mid != Msg::bad );
+
+	// Make junction entries
+	junctions_.resize( junctions_.size() + 1 );
+	Id myCompt = getCompt( me );
+	Id otherCompt = getCompt( other );
+	junctions_.back().setCompartments( myCompt, otherCompt );
+	otherSP->junctions_.resize( otherSP->junctions_.size() + 1 );
+	otherSP->junctions_.back().setCompartments( otherCompt, myCompt );
+}
+
+// This is called only on the master solver, the one which does the
+// diffusion calculations. To do so, this solver expands its own pool
+// matrices S and Sinit to include the abutting voxels.
+// The follower solver sends messages to put pool #s into the abutting
+// voxels, and gets back changes in these pool #s. Does not do the
+// diffusion calculations and does not expand.
+void SolverBase::addJunction( const Eref& e, const Qinfo* q, Id other )
+{
+	if ( !validateJunction( e.id(), other ) ) 
+			return;
+
+	SolverBase* otherSP = 
+			reinterpret_cast< SolverBase* >( other.eref().data() );
+	
+	// Set up message
+	innerConnectJunctions( e.id(), other, otherSP );
+
+	// Work out reaction terms.
+	vector< Id > pools;
+	this->findPoolsOnOther( other, pools );
+	otherSP->setLocalCrossReactingPools( pools );
+
+	pools.clear();
+	otherSP->findPoolsOnOther( e.id(), pools );
+	this->setLocalCrossReactingPools( pools );
+
+	// Work out diffusion terms
+	// Here the vectors are just the PoolIndices (as used within the Stoichs
+	// to identify pools) for diffusing molecules. These map one-to-one
+	// with each other and with data transfer terms.
+	vector< unsigned int > selfDiffPoolIndex;
+	vector< unsigned int > otherDiffPoolIndex;
+	findDiffusionTerms( otherSP, selfDiffPoolIndex, otherDiffPoolIndex );
+	junctions_.back().setDiffTerms( selfDiffPoolIndex );
+	otherSP->junctions_.back().setDiffTerms( otherDiffPoolIndex );
+
+	// Work out matching meshEntries. Virtual func. Voxelized solvers 
+	// refer to their ChemMesh. Non-voxelized ones always have a single
+	// meshEntry, and hold a dummy ChemMesh to do this calculation.
+	// In these vectors the first entry of the pair is the index in the 
+	// arriving // vector of rates modulo # of reacs, and the second is 
+	// the target meshIndex.
+	vector< unsigned int > selfMeshIndex; 
+	vector< unsigned int > otherMeshIndex; 
+	vector< VoxelJunction > selfMeshMap; 
+	vector< VoxelJunction > otherMeshMap; 
+	this->matchMeshEntries( otherSP, selfMeshIndex, selfMeshMap,
+					otherMeshIndex, otherMeshMap );
+	junctions_.back().setMeshMap( selfMeshMap );
+	junctions_.back().setSendPools( selfMeshIndex, selfDiffPoolIndex );
+	// Here we have to expand the S matrix to include these points.
+	// This function also sets the abutRecvIndex vector.
+	this->expandSforDiffusion( 
+					otherMeshIndex, selfDiffPoolIndex, junctions_.back() );
+
+	otherSP->junctions_.back().setMeshMap( otherMeshMap );
+	otherSP->junctions_.back().setSendPools( otherMeshIndex, otherDiffPoolIndex );
+	// The junction on the otherSP does not expand the S matrix.
+	
+	
+	// Here we tell the affected derived classes to update anything that
+	// depends on the Junctions, such as the GslStoich::ode_ array.
+	this->updateJunctionInterface( e );
+	otherSP->updateJunctionInterface( e );
+}
+
+
+void SolverBase::dropJunction( const Eref& e, const Qinfo* q, Id other )
+{
+	vDropJunction( e, q, other );
+}
+
+// Utility function: return the compartment in which the specified
+// object is located.
+// Simply traverses the tree toward the root till it finds a
+// compartment. Pools use a special msg, but this works for reacs too.
+Id getCompt( Id id )
+{
+	const Element* e = id.element();
+	if ( e->cinfo()->isA( "PoolBase" ) ) {
+		vector< Id > neighbours;
+		if ( e->getNeighbours( neighbours, e->cinfo()->findFinfo( "requestSize" ) ) == 1 ) {
+			Id pa = Neutral::parent( neighbours[0].eref() ).id;
+			if ( pa.element()->cinfo()->isA( "ChemMesh" ) )
+				return pa;
+		}
+	}
+	Id pa = Neutral::parent( id.eref() ).id;
+	if ( pa == Id() )
+		return pa;
+	else if ( pa.element()->cinfo()->isA( "ChemMesh" ) )
+		return pa;
+	return getCompt( pa );
+}
---
+++ ksolve/VoxelPools.h
@@ -0,0 +1,96 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2010 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+#ifndef _VOXEL_POOLS_H
+#define _VOXEL_POOLS_H
+
+class VoxelPools
+{
+	public: 
+		VoxelPools();
+		virtual ~VoxelPools();
+
+		//////////////////////////////////////////////////////////////////
+		// Field assignment stuff
+		//////////////////////////////////////////////////////////////////
+		/// Allocate # of pools
+		void resizeArrays( unsigned int totNumPools );
+
+		/**
+		 * Returns # of pools
+		 */
+		unsigned int size() const;
+
+		/**
+		 * Returns the array of doubles of current mol #s at the specified
+		 * mesh index
+		 */
+		const double* S() const;
+
+		/**
+		 * Returns the array of doubles of current mol #s at the specified
+		 * mesh index. Dangerous, allows one to modify the values.
+		 */
+		double* varS();
+
+		/**
+		 * Returns the array of doubles of initial mol #s at the specified
+		 * mesh index
+		 */
+		const double* Sinit() const;
+
+		/**
+		 * Returns the array of doubles of initial mol #s at the specified
+		 * mesh index, as a writable array.
+		 */
+		double* varSinit();
+
+		/**
+		 * Assigns S = Sinit.
+		 */
+		void reinit();
+
+		unsigned int getSolver() const;
+		void setSolver( unsigned int s );
+
+	private:
+		/**
+		 * 
+		 * S_ is the array of molecules. Stored as n, number of molecules
+		 * per mesh entry. 
+		 * The poolIndex specifies which molecular species pool to use.
+		 *
+		 * The first numVarPools_ in the poolIndex are state variables and
+		 * are integrated using the ODE solver. 
+		 * The last numEfflux_ molecules within numVarPools are those that
+		 * go out to another solver. They are also integrated by the ODE
+		 * solver, so that at the end of dt each has exactly as many
+		 * molecules as diffused away.
+		 * The next numBufPools_ are fixed but can be changed by the script.
+		 * The next numFuncPools_ are computed using arbitrary functions of
+		 * any of the molecule levels, and the time.
+		 * The functions evaluate _before_ the ODE. 
+		 * The functions should not cascade as there is no guarantee of
+		 * execution order.
+		 */
+		vector< double > S_;
+
+		/**
+		 * Sinit_ specifies initial conditions at t = 0. Whenever the reac
+		 * system is rebuilt or reinited, all S_ values become set to Sinit.
+		 * Also used for buffered molecules as the fixed values of these
+		 * molecules.
+		 */
+		vector< double > Sinit_;
+
+		/// Identifies solver to use for this set of pools. 
+		unsigned int solver_;
+};
+
+#endif	// _VOXEL_POOLS_H
