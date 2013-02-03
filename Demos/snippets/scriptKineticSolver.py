004639b14933098ef4bb5c7f34b0088364c9b2b3
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2013-02-03 17:37:29 +0000

    Added snippet to show how to make and run a kinetic solver. Almost identical to the existing scriptKineticModel.py. In doing so uncovered a numerical inaccuracy in the original scriptKineticModel.py, so reduced its timestep. Also uncovered and fixed a bug in GslStoichZombies.cpp, to do with handling changed pool concs.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@4395 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 7fbfcb921b976e69760b17a775835ba97ee4a40f
    Former-commit-id: cdf614f716c733b49dcf349319e310c8a3f92820

--- Demos/snippets/scriptKineticSolver.py
+++ Demos/snippets/scriptKineticSolver.py
@@ -7,10 +7,10 @@
 ## See the file COPYING.LIB for the full notice.
 #########################################################################
 
-# This example illustrates how to define a kinetic model using the
-# scripting interface. Normally one uses standard model formats like
-# SBML or kkit to concisely define kinetic models, but in some cases one
-# would like to modify the model through the script.
+# This example illustrates how to set up a kinetic solver and kinetic model
+# using the scripting interface. Normally this would be done using the
+# Shell::doLoadModel command, and normally would be coordinated by the
+# SimManager as the base of the entire model.
 # This example creates a bistable model having two enzymes and a reaction.
 # One of the enzymes is autocatalytic.
 # The model is set up to run using Exponential Euler integration.
@@ -74,7 +74,7 @@ def makeModel():
 		moose.connect( outputB, 'requestData', b, 'get_conc' );
 
 		# Schedule the whole lot
-		moose.setClock( 4, 0.001 ) # for the computational objects
+		moose.setClock( 4, 0.01 ) # for the computational objects
 		moose.setClock( 8, 1.0 ) # for the plots
 		# The wildcard uses # for single level, and ## for recursive.
 		moose.useClock( 4, '/model/compartment/##', 'process' )
@@ -83,6 +83,13 @@ def makeModel():
 
 def main():
 		makeModel()
+		solver = moose.GslStoich( '/model/compartment/solver' )
+		solver.path = "/model/compartment/##"
+		solver.method = "rk5"
+		mesh = moose.element( "/model/compartment/mesh" )
+		moose.connect( mesh, "remesh", solver, "remesh" )
+		moose.setClock( 5, 1.0 ) # clock for the solver
+		moose.useClock( 5, '/model/compartment/solver', 'process' )
 
 		moose.reinit()
 		moose.start( 100.0 ) # Run the model for 100 seconds.
@@ -102,7 +109,7 @@ def main():
 
 		# Iterate through all plots, dump their contents to data.plot.
 		for x in moose.wildcardFind( '/model/graphs/conc#' ):
-				moose.element( x[0] ).xplot( 'scriptKineticModel.plot', x[0].name )
+				moose.element( x[0] ).xplot( 'scriptKineticSolver.plot', x[0].name )
 
 		quit()
 
