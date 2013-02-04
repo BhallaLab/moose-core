c7ea2ec7059d4a54249ea2ce96b1189bf0915d8c
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2013-02-04 13:43:51 +0000

    Voltage clamp circuit implemented along with a demo and documentation.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@4398 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 1e31895a736bacd2ea5dacc9c5b209376184b590

--- Demos/snippets/vclamp.py
+++ Demos/snippets/vclamp.py
@@ -6,9 +6,9 @@
 # Maintainer: 
 # Created: Sat Feb  2 19:16:54 2013 (+0530)
 # Version: 
-# Last-Updated: Sat Feb  2 19:31:08 2013 (+0530)
+# Last-Updated: Mon Feb  4 19:14:45 2013 (+0530)
 #           By: subha
-#     Update #: 16
+#     Update #: 172
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -46,21 +46,80 @@
 # Code:
 
 import sys
-sys.path.append('../../python')
-
+sys.path.append('/home/subha/src/moose/python')
 import moose
+sys.path.append('../squid')
+from squid import SquidAxon
+from pylab import *
 
-def vclamp_demo():
+def vclamp_demo(simtime=1000.0, dt=1e-2):
+    # It is good practice to modularize test elements inside a
+    # container
     container = moose.Neutral('/vClampDemo')
     clamp = moose.VClamp('/vClampDemo/vclamp')
-    comp = moose.Compartment('/vClampDemo/compt')
+    # Setup command voltage time course
+    command = moose.PulseGen('/vClampDemo/command')
+    command.delay[0] = 200.0
+    command.width[0] = 100.0
+    command.level[0] = -10.0
+    command.delay[1] = 500.0
+    command.level[1] = 55.0
+    command.width[1] = 100.0
+    moose.connect(command, 'outputOut', clamp, 'set_holdingPotential')
+    # Create a compartment with properties of a squid giant axon
+    comp = SquidAxon('/vClampDemo/axon')
+    # Connect the Voltage Clamp to the compartemnt
     moose.connect(clamp, 'currentOut', comp, 'injectMsg')
     moose.connect(comp, 'VmOut', clamp, 'voltageIn')
-    moose.setClock(0, 0.01)
-    moose.useClock(0, comp.path, 'process')
-    moose.useClock(0, clamp.path, 'process')    
+    clamp.gain = comp.Cm/dt # This is a decent gain value
+    # # This is for checking a current clamp
+    # stim = moose.PulseGen('/vClampDemo/stim')
+    # stim.delay[0] = 1e9
+    # stim.width[0] = 100.0
+    # stim.level[0] = 0.1
+    # stim.delay[1] = 500.0
+    # stim.level[1] = 0.2
+    # stim.width[1] = 1e9
+    # moose.connect(stim, 'outputOut', comp, 'injectMsg')
+    # setup stimulus recroding
+    # stimtab = moose.Table('/vClampDemo/stimtab')
+    # moose.connect(stimtab, 'requestData', stim, 'get_output')
+    # Set up Vm recording
+    vmtab = moose.Table('/vClampDemo/vclamp_Vm')
+    moose.connect(vmtab, 'requestData', comp, 'get_Vm')
+    # setup command potential recording
+    commandtab = moose.Table('/vClampDemo/vclamp_command')
+    moose.connect(commandtab, 'requestData', clamp, 'get_holdingPotential')
+    # setup current recording
+    Imtab = moose.Table('/vClampDemo/vclamp_Im')
+    moose.connect(Imtab, 'requestData', clamp, 'get_current')
+    # Scheduling
+    moose.setClock(0, dt)
+    moose.setClock(1, dt)    
+    moose.setClock(2, dt)
+    moose.setClock(3, dt)
+    moose.useClock(0, '%s/##[TYPE=Compartment]' % (container.path), 'init')
+    moose.useClock(0, '%s/##[TYPE=PulseGen]' % (container.path), 'process')
+    moose.useClock(1, '%s/##[TYPE=Compartment]' % (container.path), 'process')
+    moose.useClock(2, '%s/##[TYPE=HHChannel]' % (container.path), 'process')
+    moose.useClock(2, '%s/##[TYPE=VClamp]' % (container.path), 'process')
+    moose.useClock(3, '%s/##[TYPE=Table]' % (container.path), 'process')
     moose.reinit()
-    moose.start(1.0)
+    moose.start(simtime)
+    print 'Finished simulation for %g seconds' % (simtime)
+    tseries = linspace(0, simtime, len(vmtab.vec))
+    subplot(211)
+    title('Membrane potential and clamp voltage')
+    plot(tseries, vmtab.vec, label='Vm (mV)')
+    plot(tseries, commandtab.vec, label='Command (mV)')
+    legend()
+    # print len(commandtab.vec)
+    subplot(212)
+    title('Current through clamp circuit')
+    # plot(tseries, stimtab.vec, label='stimulus (uA)')
+    plot(tseries, Imtab.vec, label='Im (uA)')
+    legend()
+    show()
 
 if __name__ == '__main__':
     vclamp_demo()
