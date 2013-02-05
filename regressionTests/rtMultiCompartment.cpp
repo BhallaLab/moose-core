5db26979decb587b3b48f9bd05cc8abfac8e8c76
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2013-02-05 16:55:27 +0000

    Further details added to MultiCompartment test.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@4401 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 93760b802b659e6d0d2135ec095756c572babb51
    Former-commit-id: bc7241e61046f1508be2251ea58f419018580c7d

--- regressionTests/rtMultiCompartment.cpp
+++ regressionTests/rtMultiCompartment.cpp
@@ -73,13 +73,76 @@ void rtTestMultiCompartmentReaction()
 	assert( gs->ode()[0].stoich_->getNumProxyPools() == 0 );
 	assert( gs->ode()[0].stoich_->getNumRates() == 2 );
 
+	// R3 will use this.
 	assert( gs->ode()[5].compartmentSignature_.size() == 2 );
 	assert( gs->ode()[5].compartmentSignature_[0] == B );
 	assert( gs->ode()[5].compartmentSignature_[1] == C );
 	assert( gs->ode()[5].stoich_->getNumVarPools() == 2 );
 	assert( gs->ode()[5].stoich_->getNumProxyPools() == 2 );
 	assert( gs->ode()[5].stoich_->getNumRates() == 3 );
+
+	// R4 will use this.
+	assert( gs->ode()[2].compartmentSignature_.size() == 1 );
+	assert( gs->ode()[2].compartmentSignature_[0] == D );
+	assert( gs->ode()[2].stoich_->getNumVarPools() == 2 );
+	assert( gs->ode()[2].stoich_->getNumProxyPools() == 1 );
+	assert( gs->ode()[2].stoich_->getNumRates() == 3 );
 	/// Should set up and check diffusion stuff.
+	////////////////////////////////////////////////////////////////
+	Id gsB( "/model/compartment_1/stoich" );
+	assert( gsB != Id() );
+	gs = reinterpret_cast< GslStoich* >( gsB.eref().data() );
+	assert( gs->pools().size() == 1 ); // No diffusion
+	assert( gs->pools()[0].size() == 5 );
+	assert( gs->ode().size() == 2 ); // combos: x C
+	assert( gs->coreStoich()->getNumVarPools() == 4 ); // M1, M3, M6, cplx
+	assert( gs->coreStoich()->getNumProxyPools() == 1 ); // M4 on C
+	assert( gs->coreStoich()->getNumRates() == 3 ); // R6, R7, (R5 on C)
+	assert( gs->coreStoich()->getNumCoreRates() == 2 ); // R6, R7
+	assert( gs->ode()[0].compartmentSignature_.size() == 0 );
+	assert( gs->ode()[0].stoich_->getNumVarPools() == 4 );
+	assert( gs->ode()[0].stoich_->getNumProxyPools() == 0 );
+	assert( gs->ode()[0].stoich_->getNumRates() == 2 );
+
+	// R5 uses this.
+	assert( gs->ode()[1].compartmentSignature_.size() == 1 );
+	assert( gs->ode()[1].compartmentSignature_[0] == C );
+	assert( gs->ode()[1].stoich_->getNumVarPools() == 4 );
+	assert( gs->ode()[1].stoich_->getNumProxyPools() == 1 );
+	assert( gs->ode()[1].stoich_->getNumRates() == 3 );
+
+	////////////////////////////////////////////////////////////////
+	Id gsC( "/model/compartment_3/stoich" );
+	assert( gsC != Id() );
+	gs = reinterpret_cast< GslStoich* >( gsC.eref().data() );
+	assert( gs->pools().size() == 1 ); // No diffusion
+	assert( gs->pools()[0].size() == 2 );
+	assert( gs->ode().size() == 1 ); // No combos, just core reacs.
+	assert( gs->coreStoich()->getNumVarPools() == 2 ); // M1, M3, M6, cplx
+	assert( gs->coreStoich()->getNumProxyPools() == 0 ); // M4 on C
+	assert( gs->coreStoich()->getNumRates() == 1 ); // R8
+	assert( gs->coreStoich()->getNumCoreRates() == 1 ); // R8
+	assert( gs->ode()[0].compartmentSignature_.size() == 0 );
+	assert( gs->ode()[0].stoich_->getNumVarPools() == 2 );
+	assert( gs->ode()[0].stoich_->getNumProxyPools() == 0 );
+	assert( gs->ode()[0].stoich_->getNumRates() == 1 );
+
+
+	////////////////////////////////////////////////////////////////
+	Id gsD( "/model/compartment_2/stoich" );
+	assert( gsD != Id() );
+	gs = reinterpret_cast< GslStoich* >( gsD.eref().data() );
+	assert( gs->pools().size() == 1 ); // No diffusion
+	assert( gs->pools()[0].size() == 2 );
+	assert( gs->ode().size() == 1 ); // No combos, just core reacs.
+	assert( gs->coreStoich()->getNumVarPools() == 2 ); // M1, M3, M6, cplx
+	assert( gs->coreStoich()->getNumProxyPools() == 0 ); // M4 on C
+	assert( gs->coreStoich()->getNumRates() == 1 ); // R8
+	assert( gs->coreStoich()->getNumCoreRates() == 1 ); // R8
+	assert( gs->ode()[0].compartmentSignature_.size() == 0 );
+	assert( gs->ode()[0].stoich_->getNumVarPools() == 2 );
+	assert( gs->ode()[0].stoich_->getNumProxyPools() == 0 );
+	assert( gs->ode()[0].stoich_->getNumRates() == 1 );
 
 
 	shell->doDelete( model );
