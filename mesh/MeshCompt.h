7b2832d26d24a77fad595b518d7cfbe26cb866c8
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2013-03-13 11:56:58 +0000

    Added skeleton for MeshCompt, this will now be base class for other things like CubeMesh using Meshes and sparse matrices for stencils.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@4459 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ mesh/MeshCompt.h
@@ -0,0 +1,73 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2011 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+#ifndef _MESH_COMPT_H
+#define _MESH_COMPT_H
+
+/**
+ * The MeshCompt represents the base class for all compartments which
+ * are set up as a meshes. These include CubeMesh, CylMesh and NeuroMesh.
+ * The MeshCompt handles the stencil that is the core of all the meshes.
+ * The stencil specifies which compartments diffuse into each other, and by
+ * how much.
+ */
+class MeshCompt: public ChemCompt
+{
+	public: 
+		MeshCompt();
+		~MeshCompt();
+
+		//////////////////////////////////////////////////////////////////
+		/**
+		 * Utility function to return volume of any voxel including those
+		 * diffusively coupled and aubtting the present volume.
+		 */
+		double extendedMeshEntrySize( unsigned int fid ) const;
+
+		/// Inherited virtual function to clear the vector of MeshEntrySize
+		void clearExtendedMeshEntrySize();
+
+		/// virtual func implemented here.
+		void innerResetStencil();
+
+		/// Derived function to return SparseMatrix-style row info for
+		/// specified mesh entry. 
+		unsigned int getStencil( unsigned int meshIndex,
+				const double** entry, const unsigned int** colIndex ) const;
+
+		vector< unsigned int > getNeighbors( unsigned int fid ) const;
+
+		void addRow( unsigned int index, const vector< double >& entry,
+			const vector< unsigned int >& colIndex );
+
+		void setStencilSize( unsigned int numRows, unsigned int numCols );
+
+		//////////////////////////////////////////////////////////////////
+
+		/// Add boundary voxels to stencil for cross-solver junctions
+		void extendStencil(
+				const ChemCompt* other, const vector< VoxelJunction >& vj );
+
+	private:
+
+		/// Handles the core stencil for own vol
+		SparseMatrix< double > coreStencil_; 
+
+		/// Handles stencil for core + abutting voxels
+		SparseMatrix< double > m_; 
+
+		/**
+		 * vector of meshEntrySizes for abutting surfaces, needed to compute
+		 * diffusion rates across junctions.
+		 * Indexed from zero.
+		 */
+		vector< double > extendedMeshEntrySize_;
+};
+
+#endif	// _MESH_COMPT_H
