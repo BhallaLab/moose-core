803f1e724580a7e8e856128ba4454798cb09a975
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2013-06-04 10:40:21 +0000

    gui: Added a widget to do wildcard search.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@4583 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: f2ec9bc3ca11e151d0b5f95f2242583df46b0f87
    Former-commit-id: dae823e11f78f4c6fd8811aaa0132ee0cf7d3861

---
+++ gui/msearch.py
@@ -0,0 +1,194 @@
+# msearch.py --- 
+# 
+# Filename: msearch.py
+# Description: 
+# Author: 
+# Maintainer: 
+# Created: Tue Jun  4 15:10:16 2013 (+0530)
+# Version: 
+# Last-Updated: Tue Jun  4 16:10:17 2013 (+0530)
+#           By: subha
+#     Update #: 135
+# URL: 
+# Keywords: 
+# Compatibility: 
+# 
+# 
+
+# Commentary: 
+# 
+# 
+# 
+# 
+
+# Change log:
+# 
+# 
+# 
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+# Floor, Boston, MA 02110-1301, USA.
+# 
+# 
+
+# Code:
+"""
+Widget giving access to wildcardFind function in MOOSE
+
+"""
+import sys
+
+from PyQt4 import QtGui, QtCore
+from PyQt4.Qt import Qt
+
+import moose
+
+class SearchWidget(QtGui.QWidget):
+    def __init__(self, *args):
+        QtGui.QWidget.__init__(self, *args)
+        layout = QtGui.QVBoxLayout()
+        self.modeCombo = QtGui.QComboBox()
+        self.modeCombo.addItem('Basic', QtCore.QVariant(QtCore.QString('basic')))
+        self.modeCombo.addItem('Advanced', QtCore.QVariant(QtCore.QString('advanced')))
+        self.modeCombo.setCurrentIndex(0)
+        self.setLayout(layout)
+        layout.addWidget(self.modeCombo)
+        layout.addWidget(self.getBasicPanel())
+        layout.addWidget(self.getAdvancedPanel())
+        self.connect(self.modeCombo, QtCore.SIGNAL('currentIndexChanged(const QString &)'), self.switchModeSlot)
+        self.switchModeSlot('basic')
+        self.searchButton = QtGui.QPushButton('Search')
+        self.searchButton.clicked.connect(self.searchSlot)
+        layout.addWidget(self.searchButton)
+        self.__results = []
+
+    def switchModeSlot(self, text):
+        basic = (str(text).lower() == 'basic')
+        self.getBasicPanel().setVisible(basic)
+        self.getAdvancedPanel().setVisible(not basic)
+
+    def getBasicPanel(self):
+        """Create a widget for basic level usage."""
+        if not hasattr(self, '_basicPanel'):
+            self._basicPanel = QtGui.QWidget()
+            self.searchRootEdit = QtGui.QLineEdit()
+            self.searchRootEdit.returnPressed.connect(self.searchSlot)
+            self.searchRootLabel = QtGui.QLabel('In')
+            self.typeEdit = QtGui.QLineEdit()
+            self.typeEdit.returnPressed.connect(self.searchSlot)            
+            self.typeLabel = QtGui.QLabel('Type')
+            self.fieldEdit = QtGui.QLineEdit()
+            self.fieldEdit.returnPressed.connect(self.searchSlot)            
+            self.fieldLabel = QtGui.QLabel('Field')
+            self.comparisonCombo = QtGui.QComboBox()
+            self.comparisonCombo.addItem('=', QtCore.QVariant(QtCore.QString('=')))
+            self.comparisonCombo.addItem('<', QtCore.QVariant(QtCore.QString('<')))
+            self.comparisonCombo.addItem('>', QtCore.QVariant(QtCore.QString('>')))
+            self.comparisonCombo.addItem('<=', QtCore.QVariant(QtCore.QString('<=')))
+            self.comparisonCombo.addItem('>=', QtCore.QVariant(QtCore.QString('>=')))
+            self.comparisonCombo.addItem('!=', QtCore.QVariant(QtCore.QString('!=')))
+            self.valueEdit = QtGui.QLineEdit()
+            self.valueEdit.returnPressed.connect(self.searchSlot)            
+            self.recurseButton = QtGui.QRadioButton('Search inside children recursively')
+            self.recurseButton.setChecked(True)
+            layout = QtGui.QGridLayout()
+            layout.setVerticalSpacing(2)
+            self._basicPanel.setLayout(layout)
+            layout.addWidget(self.searchRootLabel, 0, 0, 1, 2)
+            layout.addWidget(self.searchRootEdit, 1, 0, 1, 2)
+            layout.setColumnStretch(0, 2)
+            layout.addWidget(self.typeLabel, 0, 1, 1, 1)
+            layout.addWidget(self.typeEdit, 1, 1, 1, 1)
+            layout.addWidget(self.fieldLabel, 0, 3, 1, 1)
+            layout.addWidget(self.fieldEdit, 1, 3, 1, 1)
+            layout.addWidget(self.comparisonCombo, 1, 4, 1, 1)
+            layout.addWidget(self.valueEdit, 1, 5, 1, 2)
+            layout.addWidget(self.recurseButton, 2, 0, 1, 1)
+            layout.addItem(QtGui.QSpacerItem(1,1), 3, 0, 1, 7)
+            layout.setRowStretch(3, 10)
+        return self._basicPanel
+
+    def getAdvancedPanel(self):
+        """Create a panel for advanced users who can enter the search string
+        directly."""
+        if not hasattr(self, '_advancedPanel'):
+            self._advancedPanel = QtGui.QWidget(self)
+            self._searchEdit = QtGui.QLineEdit()
+            self._searchLabel = QtGui.QLabel('Search string:')
+            layout = QtGui.QHBoxLayout()
+            self._advancedPanel.setLayout(layout)
+            layout.addWidget(self._searchLabel)
+            layout.addWidget(self._searchEdit)
+        return self._advancedPanel
+    
+    def searchSlot(self):
+        """Do the search and store the paths of the id's found. The results
+        can be obtained later with getResults()"""
+        search_str = ''
+        if str(self.modeCombo.currentText()).lower() == 'basic':
+            root = str(self.searchRootEdit.text()).strip()
+            if len(root) == 0:
+                root = '/'
+            search_str += root
+            if not search_str.endswith('/'):
+                search_str += '/'
+            if self.recurseButton.isChecked():
+                search_str += '##'
+            else:
+                search_str += '#'
+            type_ = str(self.typeEdit.text()).strip()
+            if len(type_) > 0:
+                search_str += '[TYPE=%s]' % (type_)
+            field = str(self.fieldEdit.text()).strip()
+            if len(field) > 0:
+                search_str += '[FIELD(%s)'
+                operator = str(self.comparisonCombo.currentText())
+                value = str(self.valueEdit.text())
+                search_str += '%s%s]' % (operator, value)
+        else:
+            search_str = str(self._searchEdit.text()).strip()
+        ids = moose.wildcardFind(search_str)
+        self.__results = [el.path for el in ids]
+        # print 'Search string:', search_str, type(search_str)
+        # print 'Search result'
+        # for ii in self.__results:
+        #     print ii
+
+    def getResults(self):
+        return list(self.__results)
+
+def main():
+    """Test main"""
+    model = moose.Neutral('/model')
+    moose.loadModel('../Demos/Genesis_files/Kholodenko.g', '/model/Kholodenko')
+    # tab = moose.element('/model/Kholodenko/graphs/conc1/MAPK_PP.Co')
+    # print tab
+    # for t in tab.children:
+    #     print t
+    app = QtGui.QApplication(sys.argv)
+    mainwin = QtGui.QMainWindow()
+    mainwin.setWindowTitle('Model tree test')
+    wildcardWidget = SearchWidget()
+    mainwin.setCentralWidget(wildcardWidget)
+    mainwin.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
+
+
+# 
+# msearch.py ends here
