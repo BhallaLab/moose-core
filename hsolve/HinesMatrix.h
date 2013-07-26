46fb5e8ad29ca5a1e377ee9cbd4bbb2d39c8f9a7
praveenv253 <praveenv253@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2013-07-26 09:02:37 +0000

    Add a lot of comments to HSolve headers, describing various class members.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@4705 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 99bcbe356dc533f63849bd55ed5a1b22b83d62a4

--- hsolve/HinesMatrix.h
+++ hsolve/HinesMatrix.h
@@ -33,13 +33,15 @@ struct JunctionStruct
 		return ( index < other.index );
 	}
 	
-	unsigned int index;
-	unsigned int rank;
+	unsigned int index;		///< Hines index of the compartment.
+	unsigned int rank;		///< Number of elements "remaining" in this
+							///< compartment's group: i.e., number of children
+							///< with a larger Hines index, +1 for the parent.
 };
 
 struct TreeNodeStruct
 {
-	vector< unsigned int > children;
+	vector< unsigned int > children;	///< Hines indices of child compts
 	double Ra;
 	double Rm;
 	double Cm;
@@ -66,25 +68,59 @@ protected:
 	double                    dt_;
 	
 	vector< JunctionStruct >  junction_;
-	vector< double >          HS_;
-	vector< double >          HJ_;
+	vector< double >          HS_;			/**< Hines, series.
+		* Flattened array containing the tridiagonal of the approximately
+		* tridiagonal Hines matrix, stacked against the column vector "b" that
+		* appears on the RHS of the equation that we're trying to solve: Ax=b.
+		*/
+	vector< double >          HJ_;			/**< Hines, junctions.
+		* Flattened array containing the off-diagonal elements of the Hines
+		* matrix */
 	vector< double >          HJCopy_;
-	vector< double >          VMid_;
+	vector< double >          VMid_;		///< Compartment voltage at the
+											///< middle of a time step.
 	vector< vdIterator >      operand_;
 	vector< vdIterator >      backOperand_;
-	int                       stage_;
+	int                       stage_;		///< Which stage the simulation has
+											///< reached. Used in getA.
 	
 private:
 	void clear();
 	void makeJunctions();
-	void makeMatrix();
-	void makeOperands();
+		/**< This function creates junction structs to be stored in junction_.
+		 *   It does so by first looking through all compartments and finding
+		 *   those which have more than zero or one child. The zero-one child
+		 *   compts are left alone. coupled_ is populated with the rest of the
+		 *   compartments. Each element of coupled_ is sorted (they are all
+		 *   unsigned ints) and coupled_ itself is sorted by the first element
+		 *   in each element (groupCompare does this comparison in
+		 *   HinesMatrix.cpp).
+		 *   Note: the children themselves are unsigned ints that store the
+		 *   Hines index of the corresponding child compartment.
+		 *   So essentially, at each branch, a JunctionStruct is created for 
+		 *   each child, which contains the hines index of that child and its
+		 *   rank, which is group-size() - childIndex - 1. */
+	void makeMatrix();		/**< Populates HS_ and HJ_.
+		 *   All of the electrical circuit analysis goes into this one single
+		 *   function (and updateMatrix, of course). */
+	void makeOperands();	///< Makes operands in order to make forward
+							///< elimination easier.
 	
-	const vector< TreeNodeStruct >          *tree_;
-	vector< double >                         Ga_;
-	vector< vector< unsigned int > >         coupled_;
-	map< unsigned int, vdIterator >          operandBase_;
-	map< unsigned int, unsigned int >        groupNumber_;
+	const vector< TreeNodeStruct >     *tree_;		///< Stores compt info for
+													///< setup.
+	vector< double >                   Ga_;
+	vector< vector< unsigned int > >   coupled_;
+		/**< Contains a list of all children of a given compt. Also contains
+		 *   the parent itself. i.e., for each compartment that has more than
+		 *   one child, coupled_ stores a vector containing the children of the
+		 *   compartment and the compartment itself.
+		 *   coupled_ is therefore a vector of such vectors. */
+	map< unsigned int, vdIterator >    operandBase_;
+		/**< Contains iterators into HJ_ demarcating where a child's neighbours
+		 *   begin. Used for iterating through HJ_ along with junction_. */
+	map< unsigned int, unsigned int >  groupNumber_;
+		/**< Tells you the index of a compartment's group within coupled_, 
+		 *   given the compartment's Hines index. */
 };
 
 #endif // _HINES_MATRIX_H
