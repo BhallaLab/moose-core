82404c39ce076ec14cabf5e72a4c4c13ebf31865
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2013-09-06 09:19:08 +0000

    Deleted sbml models added accidently.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@4749 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 1e7bb37d754a500514ca33ef2bb67b22c52b9bcf

--- python/moose/mutliscale/parser/parser.py
+++ python/moose/mutliscale/parser/parser.py
@@ -1,57 +1,68 @@
 # This file contains functions to parse neuroML and sbml.
 
 import debug.debug as debug 
-from IPython import embed
 import os
 
 try:
-  from lxml import etree
-  debug.printDebug("DEBUG", "running with lxml.etree")
+    from lxml import etree
+    debug.printDebug("DEBUG", "running with lxml.etree")
 except ImportError:
-  try:
-    # Python 2.5
-    import xml.etree.cElementTree as etree
-    debug.printDebug("DEBUG", "running with cElementTree")
-  except ImportError:
     try:
-      # Python 2.5
-      import xml.etree.ElementTree as etree
-      debug.printDebug("DEBUG", "running with ElementTree")
-    except ImportError:
-      try:
-        # normal cElementTree install
-        import cElementTree as etree
+        # Python 2.5
+        import xml.etree.cElementTree as etree
         debug.printDebug("DEBUG", "running with cElementTree")
-      except ImportError:
+    except ImportError:
+    try:
+        # Python 2.5
+        import xml.etree.ElementTree as etree
+        debug.printDebug("DEBUG", "running with ElementTree")
+    except ImportError:
         try:
-          # normal ElementTree install
-          import elementtree.ElementTree as etree
-          debug.printDebug("DEBUG", "running with ElementTree")
-        except ImportError:
-          debug.prefix("FATAL", "Failed to import ElementTree")
-          os._exit(1)
+          # normal cElementTree install
+          import cElementTree as etree
+          debug.printDebug("DEBUG", "running with cElementTree")
+      except ImportError:
+          try:
+              # normal ElementTree install
+              import elementtree.ElementTree as etree
+              debug.printDebug("DEBUG", "running with ElementTree")
+          except ImportError:
+              debug.prefix("FATAL", "Failed to import ElementTree")
+              os._exit(1)
 
 
 def parseModels(models) :
-  """
-  Parse given xml models. We can get either one or two models; one described in
-  neuroML and the other in sbml. 
 
-  """
-  if models.nml :
-    # Get the schema 
-    schemaFile = "./xml/NeuroML_v2beta1.xsd"
-    with open(schemaFile, "r") as f :
-      nmlSchemaTxt = etree.XML(f.read())
+    """
+    Parse given xml models. We can get either one or two models; one described in
+    neuroML and the other in sbml. 
 
-    nmlSchema = etree.XMLSchema(nmlSchemaTxt)
-    nmlParser = etree.XMLParser(schema = nmlSchema)
+    return a list of elementTree of given models.
 
-    with open(models.nml, "r") as nmlFile :
-      nmlText = nmlFile.read()
-    rootElementOfNml = etree.fromstring(nmlText, nmlParser)
+    """
+    elemDict = dict()
+    if models.nml :
+        # Get the schema 
+        schemaFile = "./xml/NeuroML_v2beta1.xsd"
+        with open(schemaFile, "r") as f :
+            nmlSchemaTxt = etree.XML(f.read())
 
-  if models.sbml :
-    sbmlXML = etree.parse(models.sbml)
+        nmlSchema = etree.XMLSchema(nmlSchemaTxt)
+        nmlParser = etree.XMLParser(schema = nmlSchema)
 
+        with open(models.nml, "r") as nmlFile :
+            nmlText = nmlFile.read()
+        try :
+            rootElementOfNml = etree.fromstring(nmlText, nmlParser)
+        except :
+            debug.printDebug("WARN" 
+              , "Validation with schema {0} failed. CONTINUING...".format(schemaFile))
+            rootElementOfNml = etree.parse(models.nml)
+            elemDict['nml'] = rootElementOfNml
+
+    if models.sbml :
+        # TODO : parse it in the same way as we are parsing neuroML
+        rootElementOfSbml = etree.parse(models.sbml)
+        elemDict['sbml'] = rootElementOfSbml
+    return elemDict
 
