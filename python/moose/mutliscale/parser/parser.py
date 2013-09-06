2cf77ce09a05d6c5791ab1296be38f516722df5d
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2013-09-06 09:21:14 +0000

    Fixed indentation.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@4750 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 56c1c317ebcf474e210c6a7fb3da91f420a020aa

--- python/moose/mutliscale/parser/parser.py
+++ python/moose/mutliscale/parser/parser.py
@@ -12,23 +12,23 @@ except ImportError:
         import xml.etree.cElementTree as etree
         debug.printDebug("DEBUG", "running with cElementTree")
     except ImportError:
-    try:
-        # Python 2.5
-        import xml.etree.ElementTree as etree
-        debug.printDebug("DEBUG", "running with ElementTree")
-    except ImportError:
         try:
-          # normal cElementTree install
-          import cElementTree as etree
-          debug.printDebug("DEBUG", "running with cElementTree")
-      except ImportError:
-          try:
-              # normal ElementTree install
-              import elementtree.ElementTree as etree
-              debug.printDebug("DEBUG", "running with ElementTree")
-          except ImportError:
-              debug.prefix("FATAL", "Failed to import ElementTree")
-              os._exit(1)
+            # Python 2.5
+            import xml.etree.ElementTree as etree
+            debug.printDebug("DEBUG", "running with ElementTree")
+        except ImportError:
+            try:
+              # normal cElementTree install
+              import cElementTree as etree
+              debug.printDebug("DEBUG", "running with cElementTree")
+            except ImportError:
+                try:
+                    # normal ElementTree install
+                    import elementtree.ElementTree as etree
+                    debug.printDebug("DEBUG", "running with ElementTree")
+                except ImportError:
+                    debug.prefix("FATAL", "Failed to import ElementTree")
+                    os._exit(1)
 
 
 def parseModels(models) :
