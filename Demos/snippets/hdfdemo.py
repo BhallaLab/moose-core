a74f108181db0f8d77876726aabd2b771738d2ce
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2013-09-10 09:54:19 +0000

    Cleaned up and documented hdfdemo.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/buildQ@4761 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- Demos/snippets/hdfdemo.py
+++ Demos/snippets/hdfdemo.py
@@ -1,17 +1,33 @@
-# import moose.utils as mu
-# mu.printtree('/')
+#!/usr/bin/env python
+# Author: Subhasis Ray
+
+"""
+Demonstrates the use of HDF5DataWriter class to save simulated data in
+HDF5 file.
+
+In this example a passive neuronal compartment `c` is created and its
+membrane voltage Vm and membrane current Im are recorded in two tables
+`t` and `t1` respectively. Both the tables are in turn connected to a
+single HDF5DataWriter object, which saves the table contents into an
+HDF5 file `output.h5`. 
+
+The `mode` of the HDF5DataWriter is set to `2` which means if a file
+of the same name exists, it will be overwritten.
+"""
 import sys
 sys.path.append('../../python')
 import os
 os.environ['NUMPTHREADS'] = '1'
 import numpy
 import moose
+
 c = moose.Compartment('c')
 t = moose.Table('t')
 t1 = moose.Table('t1')
 moose.connect(t, 'requestData', c, 'get_Vm')
 moose.connect(t1, 'requestData', c, 'get_Im')
 h = moose.HDF5DataWriter('h')
+h.mode = 2 # Truncate existing file
 moose.connect(h, 'requestData', t, 'get_vec')
 moose.connect(h, 'requestData', t1, 'get_vec')
 h.filename = 'output.h5'
@@ -24,7 +40,7 @@ moose.useClock(2, '/##[TYPE=HDF5DataWriter]', 'process')
 moose.reinit()
 c.inject = 0.1
 moose.start(30.0)
-print 'Here'
+print 'Finished simulation. Data was saved in', h.filename
 # print numpy.array(t.vec)
 # moose.start(0.5)
 # print numpy.array(t.vec)
