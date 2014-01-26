401afd9a8aeff28ba5be52b92cd12e99412a2c7f
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-01-26 13:00:04 +0000

    async13: Started diffusion directory. Coded test framework for Hines-ordered fast elimination solution for electrical cable equations and 1-d branching diffusion equations.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5025 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 18dc2eff49e940920c4021ec6b8405db4990c9c9

---
+++ diffusion/fastElim.cpp
@@ -0,0 +1,257 @@
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <functional>
+#include <iostream>
+using namespace std;
+#include "../basecode/SparseMatrix.h"
+
+const unsigned int SM_MAX_ROWS = 200000;
+const unsigned int SM_MAX_COLUMNS = 200000;
+const unsigned int SM_RESERVE = 8;
+
+class Unroll
+{
+	public:
+		Unroll( double diag, double off, unsigned int i, unsigned int j )
+			: 
+				diagVal( diag ),
+				offDiagVal( off ),
+				row( i ),
+				col( j )
+		{;}
+		double diagVal;
+		double offDiagVal;
+		unsigned int row; // On which the diagonal is located
+		unsigned int col; // Col on which the offDiagVal is located.
+};
+
+class FastElim: public SparseMatrix< double >
+{
+	public:
+		void makeTestMatrix();
+		/*
+		void rowElim( unsigned int row1, unsigned int row2, 
+						vector< double >& rhs );
+						*/
+		void buildForwardElim( vector< unsigned int >& diag,
+				vector< Triplet< double > >& fops );
+		void buildBackwardSub( vector< unsigned int >& diag,
+				vector< Unroll >& bops );
+		void orderBranches();
+};
+
+
+/*
+void FastElim::rowElim( unsigned int row1, unsigned int row2, 
+						vector< double >& rhs )
+{
+	unsigned int rs1 = rowStart_[row1];
+	unsigned int rs2 = rowStart_[row2];
+	unsigned int n1 = rowStart_[row1+1] - rs1;
+	if ( n1 < 2 ) return;
+	assert( colIndex_[rs2] == row1 );
+	double diag1 = N_[diagIndex_[row1]];
+	double temp = N_[rs2];
+	double r = temp/diag1;
+
+	const double* p1 = &(N_[ diagIndex_[row1] + 1 ] );
+	double* p2 = N_ + 
+	for ( unsigned int i = 
+	N_[rs2+1]
+
+	double* v1 = N_ + rs1;
+	double* v2 = N_ + rs2;
+	
+	
+			v2' - v1'*v2/v1
+
+}
+*/
+
+void FastElim::makeTestMatrix()
+{
+	static double test[] = {
+		1,  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+		3,  4,  0,  0,  0,  0,  5,  0,  0,  0,  0,
+		0,  0,  6,  7,  0,  0,  0,  0,  0,  0,  0,
+		0,  0,  8,  9,  0,  0, 10,  0,  0,  0,  0,
+		0,  0,  0,  0, 11, 12,  0,  0,  0,  0,  0,
+		0,  0,  0,  0, 13, 14,  0,  0, 15,  0,  0,
+		0, 16,  0, 17,  0,  0, 18, 19,  0,  0,  0,
+		0,  0,  0,  0,  0,  0, 20, 21, 22,  0,  0,
+		0,  0,  0,  0,  0, 23,  0, 24, 25, 26,  0,
+		0,  0,  0,  0,  0,  0,  0,  0, 27, 28, 29,
+		0,  0,  0,  0,  0,  0,  0,  0,  0, 30, 31,
+	};
+	const unsigned int numCompts = 11;
+	setSize( numCompts, numCompts );
+	vector< double > row( numCompts, ~0 );
+	unsigned int i = 1;
+	for ( unsigned int i = 0; i < numCompts; ++i ) {
+		for ( unsigned int j = 0; j < numCompts; ++j ) {
+			unsigned int k = i * numCompts + j;
+			if ( test[k] < 0.1 ) {
+			} else {
+				N_.push_back( test[k] );
+				colIndex_.push_back( j );
+			}
+		}
+		rowStart_[i + 1] = N_.size();
+	}
+}
+
+/*
+I need an outer function to fill the vector of ops for forward elim.
+Then I need another outer function to fill another set of ops for
+back-substitution.
+*/
+
+/**
+ * Builds the vector of forward ops: ratio, i, j 
+ * RHS[i] = RHS[i] - RHS[j] * ratio 
+ * This vec tells the routine which rows below have to be eliminated.
+ * This includes the rows if any in the tridiagonal band and also 
+ * rows, if any, on branches.
+ */
+void FastElim::buildForwardElim( vector< unsigned int >& diag,
+	vector< Triplet< double > >& fops )
+{
+	vector< vector< unsigned int > > rowsToElim( nrows_ );
+	diag.clear();
+	for ( unsigned int i = 0; i < nrows_; ++i ) {
+		unsigned int rs = rowStart_[i];
+		unsigned int re = rowStart_[i+1];
+		for ( unsigned int j = rs; j < re; ++j ) {
+			unsigned int k = colIndex_[j];
+			if ( k == i ) {
+				diag.push_back(j);
+			} else if ( k > i ) {
+				rowsToElim[ i ].push_back( k );
+			}
+		}
+	}
+	for ( unsigned int i = 0; i < nrows_; ++i ) {
+		double d = N_[diag[i]];
+		unsigned int diagend = rowStart_[ i + 1 ];
+		assert( diag[i] < diagend );
+		vector< unsigned int >& elim = rowsToElim[i];
+		for ( unsigned int j = 0; j < elim.size(); ++j ) {
+			unsigned int erow = elim[j];
+			if ( erow == i ) continue;
+			unsigned int rs = rowStart_[ erow ];
+			unsigned int re = rowStart_[ erow+1 ];
+			// assert( colIndex_[rs] == i );
+			double ratio = N_[rs]/d;
+			for ( unsigned int k = diag[i]+1; k < diagend; ++k ) {
+				unsigned int col = colIndex_[k];
+				// findElimEntry
+				for ( unsigned int q = rs; q < re; ++q ) {
+					if ( colIndex_[q] == col ) {
+						N_[q] -= N_[k] * ratio;
+						fops.push_back( Triplet< double >( ratio, i, col) );
+					}
+				}
+			}
+		}
+	}
+	for ( unsigned int i = 0; i < rowsToElim.size(); ++i ) {
+		cout << i << " :		";
+		for ( unsigned int j = 0; j < rowsToElim[i].size(); ++j ) {
+			cout << rowsToElim[i][j] << "	";
+		}
+		cout << endl;
+	}
+	for ( unsigned int i = 0; i < fops.size(); ++i ) {
+		cout << i << "		" << fops[i].b_ << "	" << fops[i].c_ << 
+				"	" << fops[i].a_ << endl;
+	}
+}
+
+/** 
+ * Operations to be done on the RHS for the back sub are generated and
+ * put into the bops (backward ops) vector. 
+ * col > row here, row is the entry being operated on, and col is given by 
+ * rowsToSub.
+ * offDiagVal is the value on the off-diagonal at row,col.  
+ * diagVal is the value on the diagonal at [row][row].  
+ * RHS[row] = ( RHS[row] - offDiagVal * RHS[col] ) / diagVal
+ */
+void FastElim::buildBackwardSub( vector< unsigned int >& diag,
+	vector< Unroll >& bops )
+{
+	// This vec tells the routine which rows below have to be back-subbed.
+	// This includes the rows if any in the tridiagonal band and also 
+	// rows, if any, on branches.
+	vector< vector< unsigned int > > rowsToSub( nrows_ );
+
+
+	for ( unsigned int i = 0; i < nrows_; ++i ) {
+		unsigned int d = diag[i] + 1;
+		unsigned int re = rowStart_[i+1];
+		for ( unsigned int j = d; j < re; ++j ) {
+			unsigned int k = colIndex_[j];
+			rowsToSub[ i ].push_back( k );
+		}
+	}
+	for ( unsigned int i = 0; i < rowsToSub.size(); ++i ) {
+		cout << i << " :		";
+		for ( unsigned int j = 0; j < rowsToSub[i].size(); ++j ) {
+			cout << rowsToSub[i][j] << "	";
+		}
+		cout << endl;
+	}
+
+	// for the last entry we just want to divide by its diagonal.
+	unsigned int k = nrows_ -1;
+	bops.push_back( Unroll( N_[diag[ k ]], 0.0, k, k ) );
+	for ( unsigned int i = nrows_-2; i != -1 ; --i ) {
+		double diagval = N_[diag[i]];
+		for ( unsigned int j = rowsToSub[i].size() - 1; j != -1; --j ) {
+			k = rowsToSub[i][j];
+			double val = get( i, k );
+			bops.push_back( Unroll( diagval, val, i, k) );
+		}
+	}
+	for ( unsigned int i = 0; i < bops.size(); ++i ) {
+		cout << i << ":		" << bops[i].diagVal << "	" << 
+				bops[i].offDiagVal << "	" << 
+				bops[i].row <<  "	" <<
+				bops[i].col <<
+				endl;
+	}
+}
+
+void advance( vector< double >& y,
+	   const vector< Triplet< double > >& fops,
+	   const vector< Unroll >& bops )
+{
+	for ( vector< Triplet< double > >::const_iterator
+				i = fops.begin(); i != fops.end(); ++i )
+		y[i->b_] -= y[i->c_] * i->a_;
+	for ( vector< Unroll >::const_iterator
+				i = bops.begin(); i != bops.end(); ++i )
+		y[i->row] = (y[i->row] - i->offDiagVal * y[i->col] ) / i->diagVal;
+}
+
+void FastElim::orderBranches()
+{
+}
+
+main()
+{
+	FastElim fe;
+	vector< Triplet< double > > fops;
+	vector< Unroll > bops;
+	fe.makeTestMatrix();
+	fe.print();
+	cout << endl << endl;
+	vector< unsigned int > diag;
+	fe.buildForwardElim( diag, fops );
+	fe.print();
+	fe.buildBackwardSub( diag, bops );
+	vector< double > y( 11, 1.0 );
+	advance( y, fops, bops );
+	for ( int i = 0; i < 11; ++i )
+		cout << i << "	" << y[i] << endl;
+}
