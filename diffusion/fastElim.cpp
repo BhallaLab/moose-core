f207beead71f781035fe3b53194f67891a1c2fd6
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-01-26 17:49:16 +0000

    async13: Further fixes on diffusion/branched nerve eqn solver  prototype. Seems to work OK now.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5026 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 22edce24aade8e38821203d4c379079dd7e4c683

--- diffusion/fastElim.cpp
+++ diffusion/fastElim.cpp
@@ -3,6 +3,7 @@
 #include <cassert>
 #include <functional>
 #include <iostream>
+#include <gsl/gsl_linalg.h>
 using namespace std;
 #include "../basecode/SparseMatrix.h"
 
@@ -29,7 +30,7 @@ class Unroll
 class FastElim: public SparseMatrix< double >
 {
 	public:
-		void makeTestMatrix();
+		void makeTestMatrix( const double* test, unsigned int numCompts );
 		/*
 		void rowElim( unsigned int row1, unsigned int row2, 
 						vector< double >& rhs );
@@ -69,22 +70,8 @@ void FastElim::rowElim( unsigned int row1, unsigned int row2,
 }
 */
 
-void FastElim::makeTestMatrix()
+void FastElim::makeTestMatrix( const double* test, unsigned int numCompts )
 {
-	static double test[] = {
-		1,  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-		3,  4,  0,  0,  0,  0,  5,  0,  0,  0,  0,
-		0,  0,  6,  7,  0,  0,  0,  0,  0,  0,  0,
-		0,  0,  8,  9,  0,  0, 10,  0,  0,  0,  0,
-		0,  0,  0,  0, 11, 12,  0,  0,  0,  0,  0,
-		0,  0,  0,  0, 13, 14,  0,  0, 15,  0,  0,
-		0, 16,  0, 17,  0,  0, 18, 19,  0,  0,  0,
-		0,  0,  0,  0,  0,  0, 20, 21, 22,  0,  0,
-		0,  0,  0,  0,  0, 23,  0, 24, 25, 26,  0,
-		0,  0,  0,  0,  0,  0,  0,  0, 27, 28, 29,
-		0,  0,  0,  0,  0,  0,  0,  0,  0, 30, 31,
-	};
-	const unsigned int numCompts = 11;
 	setSize( numCompts, numCompts );
 	vector< double > row( numCompts, ~0 );
 	unsigned int i = 1;
@@ -142,10 +129,11 @@ void FastElim::buildForwardElim( vector< unsigned int >& diag,
 			unsigned int rs = rowStart_[ erow ];
 			unsigned int re = rowStart_[ erow+1 ];
 			// assert( colIndex_[rs] == i );
-			double ratio = N_[rs]/d;
+			double ratio = get( erow, i ) / d;
+			// double ratio = N_[rs]/N_[diag[i]];
 			for ( unsigned int k = diag[i]+1; k < diagend; ++k ) {
 				unsigned int col = colIndex_[k];
-				// findElimEntry
+				// findElimEntry, subtract it out.
 				for ( unsigned int q = rs; q < re; ++q ) {
 					if ( colIndex_[q] == col ) {
 						N_[q] -= N_[k] * ratio;
@@ -163,7 +151,7 @@ void FastElim::buildForwardElim( vector< unsigned int >& diag,
 		cout << endl;
 	}
 	for ( unsigned int i = 0; i < fops.size(); ++i ) {
-		cout << i << "		" << fops[i].b_ << "	" << fops[i].c_ << 
+		cout << "fops[" << i << "]=		" << fops[i].b_ << "	" << fops[i].c_ << 
 				"	" << fops[i].a_ << endl;
 	}
 }
@@ -175,7 +163,7 @@ void FastElim::buildForwardElim( vector< unsigned int >& diag,
  * rowsToSub.
  * offDiagVal is the value on the off-diagonal at row,col.  
  * diagVal is the value on the diagonal at [row][row].  
- * RHS[row] = ( RHS[row] - offDiagVal * RHS[col] ) / diagVal
+ * RHS[row] = ( RHS[row] - offDiagVal * RHS[col] ) / diagVyal
  */
 void FastElim::buildBackwardSub( vector< unsigned int >& diag,
 	vector< Unroll >& bops )
@@ -228,7 +216,7 @@ void advance( vector< double >& y,
 {
 	for ( vector< Triplet< double > >::const_iterator
 				i = fops.begin(); i != fops.end(); ++i )
-		y[i->b_] -= y[i->c_] * i->a_;
+		y[i->c_] -= y[i->b_] * i->a_;
 	for ( vector< Unroll >::const_iterator
 				i = bops.begin(); i != bops.end(); ++i )
 		y[i->row] = (y[i->row] - i->offDiagVal * y[i->col] ) / i->diagVal;
@@ -238,20 +226,106 @@ void FastElim::orderBranches()
 {
 }
 
+double checkAns( 
+	const double* m, unsigned int numCompts, 
+	const double* ans, const double* rhs )
+{
+	vector< double > check( numCompts, 0.0 );
+	for ( unsigned int i = 0; i < numCompts; ++i ) {
+		for ( unsigned int j = 0; j < numCompts; ++j )
+			check[i] += m[i*numCompts + j] * ans[j];
+	}
+	double ret = 0.0;
+	for ( unsigned int i = 0; i < numCompts; ++i )
+		ret += (check[i] - rhs[i]) * (check[i] - rhs[i] );
+	return ret;
+}
 main()
 {
+		/*
+	*/
+	static double test[] = {
+		1,  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+		3,  4,  0,  0,  0,  0,  5,  0,  0,  0,  0,
+		0,  0,  6,  7,  0,  0,  0,  0,  0,  0,  0,
+		0,  0,  8,  9,  0,  0, 10,  0,  0,  0,  0,
+		0,  0,  0,  0, 11, 12,  0,  0,  0,  0,  0,
+		0,  0,  0,  0, 13, 14,  0,  0, 15,  0,  0,
+		0, 16,  0, 17,  0,  0, 18, 19,  0,  0,  0,
+		0,  0,  0,  0,  0,  0, 20, 21, 22,  0,  0,
+		0,  0,  0,  0,  0, 23,  0, 24, 25, 26,  0,
+		0,  0,  0,  0,  0,  0,  0,  0, 27, 28, 29,
+		0,  0,  0,  0,  0,  0,  0,  0,  0, 30, 31,
+	};
+	const unsigned int numCompts = 11;
+		/*
+	static double test[] = {
+		1,  2,
+		3,  4
+	};
+	const unsigned int numCompts = 2;
+	static double test[] = {
+		1, 2, 0, 0,
+		3, 4, 5, 0,
+		0, 6, 7, 8,
+		0, 0, 9, 10
+	};
+	const unsigned int numCompts = 4;
+	static double test[] = {
+		1,  2,  0,  0,  0,  0,
+		3,  4,  5,  0,  0,  0,
+		0,  6,  7,  8,  0,  0,
+		0,  0,  9, 10, 11,  0,
+		0,  0,  0, 12, 13, 14,
+		0,  0,  0,  0, 15, 16,
+	};
+	const unsigned int numCompts = 6;
+	static double test[] = {
+		1,  2,  0,  0,  0,  0,
+		3,  4,  0,  0,  1,  0,
+		0,  0,  7,  8,  0,  0,
+		0,  0,  9, 10, 11,  0,
+		0,  1,  0, 12, 13, 14,
+		0,  0,  0,  0, 15, 16,
+	};
+	const unsigned int numCompts = 6;
+	*/
 	FastElim fe;
 	vector< Triplet< double > > fops;
 	vector< Unroll > bops;
-	fe.makeTestMatrix();
+	fe.makeTestMatrix( test, numCompts );
 	fe.print();
 	cout << endl << endl;
 	vector< unsigned int > diag;
 	fe.buildForwardElim( diag, fops );
 	fe.print();
 	fe.buildBackwardSub( diag, bops );
-	vector< double > y( 11, 1.0 );
+	vector< double > y( numCompts, 1.0 );
+	vector< double > ones( numCompts, 1.0 );
 	advance( y, fops, bops );
-	for ( int i = 0; i < 11; ++i )
-		cout << i << "	" << y[i] << endl;
+	for ( int i = 0; i < numCompts; ++i )
+		cout << "y" << i << "]=	" << y[i] << endl;
+
+	// Here we verify the answer
+	cout << "myCode: " << checkAns( test, numCompts, &y[0], &ones[0] ) << endl;
+
+	/////////////////////////////////////////////////////////////////////
+	// Here we do the gsl test.
+	vector< double > temp( &test[0], &test[numCompts*numCompts] );
+	gsl_matrix_view m = gsl_matrix_view_array( &temp[0], numCompts, numCompts );
+
+	vector< double > z( numCompts, 1.0 );
+	gsl_vector_view b = gsl_vector_view_array( &z[0], numCompts );
+	gsl_vector* x = gsl_vector_alloc( numCompts );
+	int s;
+	gsl_permutation* p = gsl_permutation_alloc( numCompts );
+	gsl_linalg_LU_decomp( &m.matrix, p, &s );
+	gsl_linalg_LU_solve( &m.matrix, p, &b.vector, x);
+	vector< double > gslAns( numCompts );
+	for ( int i = 0; i < numCompts; ++i ) {
+		gslAns[i] = gsl_vector_get( x, i );
+		cout << "x[" << i << "]=	" << gslAns[i] << endl;
+	}
+	cout << "GSL: " << checkAns( test, numCompts, &gslAns[0], &ones[0] ) << endl;
+	gsl_vector_free( x );
 }
