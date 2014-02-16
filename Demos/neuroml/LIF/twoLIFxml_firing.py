46f562ffd62705f3537757f463b6c10857e6cc72
aditya-g <aditya-g@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-02-16 10:56:44 +0000

    testing two LIFs loading by copying from /library via NeuroML reader. BUG: the last LIF created controls all previous LIFs! inject for other LIFs doesn't work. The inject set for the last LIF takes over all cells.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5085 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- Demos/neuroml/LIF/twoLIFxml_firing.py
+++ Demos/neuroml/LIF/twoLIFxml_firing.py
@@ -27,15 +27,15 @@ import math
 ## import numpy and matplotlib in matlab style commands
 from pylab import *
 
-def create_LIF():
-    neuromlR = NeuroML()
-    neuromlR.readNeuroMLFromFile('cells_channels/LIF.morph.xml')
-    libcell = moose.Neuron('/library/LIF')
-    LIFCellid = moose.copy(libcell,moose.Neutral('/cells'),'IF1')
-    LIFCell = moose.Neuron(LIFCellid)
-    return LIFCell
+def create_twoLIFs():
+    NML = NetworkML({'temperature':37.0,'model_dir':'.'})
+    ## Below returns populationDict = { 'populationname1':(cellname,{instanceid1:moosecell, ... }) , ... }
+    ## and projectionDict = { 'projectionname1':(source,target,[(syn_name1,pre_seg_path,post_seg_path),...]) , ... }
+    (populationDict,projectionDict) = \
+        NML.readNetworkMLFromFile('twoLIFs.net.xml', {}, params={})
+    return populationDict,projectionDict
 
-def run_LIF():
+def run_twoLIFs():
 	## reset and run the simulation
 	print "Reinit MOOSE."
 	## from moose_utils.py sets clocks and resets
@@ -44,11 +44,15 @@ def run_LIF():
 	moose.start(RUNTIME)
 
 if __name__ == '__main__':
-    IF1 = create_LIF()
-    printCellTree(IF1)
-    IF1Soma = moose.Compartment(IF1.path+'/soma_0')
+    populationDict,projectionDict = create_twoLIFs()
+    ## element returns the right element and error if not present
+    IF1Soma = moose.element(populationDict['LIFs'][1][0].path+'/soma_0')
     IF1Soma.inject = injectI
+    IF2Soma = moose.element(populationDict['LIFs'][1][1].path+'/soma_0')
+    IF2Soma.inject = 0.0#injectI*2.0
+    IF2Soma.inject = injectI
     IF1vmTable = setupTable("vmTableIF1",IF1Soma,'Vm')
+    IF2vmTable = setupTable("vmTableIF2",IF2Soma,'Vm')
 
     ## edge-detect the spikes using spike-gen (table does not have edge detect)
     ## IaF_spikegen is already present for compartments having IaF mechanisms
@@ -57,14 +61,15 @@ if __name__ == '__main__':
     IF1spikesTable = moose.Table(table_path+'/spikesTable')
     moose.connect(spikeGen,'spikeOut',IF1spikesTable,'input') ## spikeGen gives spiketimes
 
-    run_LIF()
+    run_twoLIFs()
     print "Spiketimes :",IF1spikesTable.vector
     ## plot the membrane potential of the neuron
     timevec = arange(0.0,RUNTIME+PLOTDT/2.0,PLOTDT)
-    ## Something is crazy! Why twice the number of table entries compared to time!!??
     figure(facecolor='w')
-    print IF1vmTable
-    plot(timevec, IF1vmTable.vector)
+    print IF1vmTable,IF2vmTable
+    plot(timevec, IF1vmTable.vector,'r-')
+    figure(facecolor='w')
+    plot(timevec, IF2vmTable.vector,'b-')
     show()
 
     ## At the end, some issue with Func (as per Subha) gives below or core dump error
