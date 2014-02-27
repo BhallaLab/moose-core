5155b7cc2bdb811a3bb5e46cd029633f3ebf8535
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-02-27 15:37:00 +0000

    Async13: Added ZombieFuncPool to the ksolver family. Yet to test.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5121 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 516a36e608e76594d48fc81b529699008fd8f323
    Former-commit-id: 3210ca56183647d9f93e476c13a7efb8f2008614

---
+++ ksolve/ZombieFuncPool.cpp
@@ -0,0 +1,47 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2010 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+#include "header.h"
+#include "PoolBase.h"
+#include "ZombiePoolInterface.h"
+#include "ZombiePool.h"
+#include "ZombieFuncPool.h"
+
+// Derived from ZombiePool.
+const Cinfo* ZombieFuncPool::initCinfo()
+{
+	static DestFinfo input( "input",
+		"Handles input to control value of n_",
+		new OpFunc1< ZombieFuncPool, double >( &ZombieFuncPool::input ) );
+	
+	static Finfo* zombieFuncPoolFinfos[] = {
+		&input,             // DestFinfo
+	};
+
+	static Cinfo zombieFuncPoolCinfo (
+		"ZombieFuncPool",
+		ZombiePool::initCinfo(),
+		zombieFuncPoolFinfos,
+		sizeof( zombieFuncPoolFinfos ) / sizeof( const Finfo* ),
+		new Dinfo< ZombieFuncPool >()
+	);
+
+	return &zombieFuncPoolCinfo;
+}
+
+//////////////////////////////////////////////////////////////
+// Class definitions
+//////////////////////////////////////////////////////////////
+static const Cinfo* zombieFuncPoolCinfo = ZombieFuncPool::initCinfo();
+
+ZombieFuncPool::ZombieFuncPool()
+{;}
+
+void ZombieFuncPool::input( double v )
+{;}
+
---
+++ ksolve/ZombieFuncPool.h
@@ -0,0 +1,24 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2010 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+#ifndef _ZOMBIE_FUNC_POOL_H
+#define _ZOMBIE_FUNC_POOL_H
+
+class ZombieFuncPool: public ZombiePool
+{
+	public: 
+		ZombieFuncPool();
+
+		void input( double v );
+
+		static const Cinfo* initCinfo();
+	private:
+};
+
+#endif	// _ZOMBIE_FUNC_POOL_H
