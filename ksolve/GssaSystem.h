e27ad99a0f0bad66ee0b72c0d8136ca16d926b9b
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-03-16 12:40:15 +0000

    async13: Added and compiled in GssaVoxelPools class, which does the per-voxel storage and calculations for the Gillespie stochastic systems algorithm. Yet to bring in the setup and solver class, Gsolve.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5168 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ ksolve/GssaSystem.h
@@ -0,0 +1,43 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2014 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+#ifndef _GSSA_SYSTEM_H
+#define _GSSA_SYSTEM_H
+
+/**
+ * This class stores the common data needed across all voxels for doing
+ * GSSA calculations.
+ */
+
+class GssaSystem
+{
+	public: 
+		vector< vector< unsigned int > > dependency;
+		vector< vector< unsigned int > > dependentMathExpn;
+		vector< vector< unsigned int > > ratesDependentOnPool;
+
+		/// Transpose of stoichiometry matrix.
+		KinSparseMatrix transposeStoich;
+		const Stoich* stoich;
+
+		/**
+		 * Flag: True when using probabilistic (random) rounding.
+		 * When initializing the mol# from floating-point Sinit values,
+		 * we have two options. One is to look at each Sinit, and round
+		 * to the nearest integer. The other is to look at each Sinit,
+		 * and probabilistically round up or down depending on the 
+		 * value. For example, if we had a Sinit value of 1.49, 
+		 * this would always be rounded to 1.0 if the flag is false,
+		 * and would be rounded to 1.0 and 2.0 in the ratio 51:49 if
+		 * the flag is true.
+		 */
+		bool useRandInit;
+};
+
+#endif	// _GSSA_SYSTEM_H
---
+++ ksolve/GssaVoxelPools.cpp
@@ -0,0 +1,169 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2010 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+#include "header.h"
+#include "RateTerm.h"
+#include "FuncTerm.h"
+#include "SparseMatrix.h"
+#include "KinSparseMatrix.h"
+#include "Stoich.h"
+#include "GssaSystem.h"
+#include "VoxelPoolsBase.h"
+#include "GssaVoxelPools.h"
+#include "../randnum/randnum.h"
+
+/**
+ * The SAFETY_FACTOR Protects against the total propensity exceeding
+ * the cumulative
+ * sum of propensities, atot. We do a lot of adding and subtracting of
+ * dependency terms from atot. Roundoff error will eventually cause
+ * this to drift from the true sum. To guarantee that we never lose
+ * the propensity of the last reaction, this safety factor scales the
+ * first calculation of atot to be slightly larger. Periodically this
+ * will cause the reaction picking step to exceed the last reaction 
+ * index. This is safe, we just pick another random number.  
+ * This will happen rather infrequently.
+ * That is also a good time to update the cumulative sum.
+ * A double should have >15 digits, so cumulative error will be much
+ * smaller than this.
+ */
+const double SAFETY_FACTOR = 1.0 + 1.0e-9;
+
+//////////////////////////////////////////////////////////////
+// Class definitions
+//////////////////////////////////////////////////////////////
+
+GssaVoxelPools::GssaVoxelPools()
+	: 
+			VoxelPoolsBase()
+{;}
+
+GssaVoxelPools::~GssaVoxelPools()
+{;}
+
+//////////////////////////////////////////////////////////////
+// Solver ops
+//////////////////////////////////////////////////////////////
+
+/*
+double GssaSystem::updateDependentRates( unsigned int rindex, double atot, 
+			   double* v, const double* S )
+{
+	const vector< unsigned int >& deps = dependency_[ rindex ];
+	for ( vector< unsigned int >::const_iterator
+			i = deps.begin(); i != deps.end(); ++i ) {
+		atot -= v[ *i ];
+		// atot_ += ( v[ *i ] = ( *rates_[ *i ] )( S() );
+		atot += ( v[ *i ] = stoich_->getReacVelocity( *i, ( S() ) );
+	}
+	return atot;
+}
+*/
+
+void GssaVoxelPools::updateDependentMathExpn( double t, unsigned int index )
+{
+}
+
+
+void GssaVoxelPools::updateDependentRates( 
+	const vector< unsigned int >& deps, const Stoich* stoich )
+{
+	for ( vector< unsigned int >::const_iterator
+			i = deps.begin(); i != deps.end(); ++i ) {
+		atot_ -= v_[ *i ];
+		// atot_ += ( v[ *i ] = ( *rates_[ *i ] )( S() );
+		atot_ += ( v_[ *i ] = stoich->getReacVelocity( *i, S() ) );
+	}
+}
+
+unsigned int GssaVoxelPools::pickReac() const
+{
+	// double r =  gsl_rng_uniform( rng ) * atot_;
+	double r = mtrand() * atot_;
+	double sum = 0.0;
+
+	// This is an inefficient way to do it. Can easily get to 
+	// log time or thereabouts by doing one or two levels of 
+	// subsidiary tables. Too many levels causes slow-down because
+	// of overhead in managing the tree. 
+	// Slepoy, Thompson and Plimpton 2008
+	// report a linear time version.
+	for ( vector< double >::const_iterator 
+			i = v_.begin(); i != v_.end(); ++i ) {
+		if ( r < ( sum += *i ) )
+			return static_cast< unsigned int >( i - v_.begin() );
+	}
+	return v_.size();
+}
+
+void GssaVoxelPools::advance( const ProcInfo* p, const GssaSystem* g )
+{
+	double nextt = p->currTime;
+	while ( t_ < nextt ) {
+		if ( atot_ <= 0.0 ) { // reac system is stuck, will not advance.
+			t_ = nextt;
+			return;
+		}
+		unsigned int rindex = pickReac();
+		if ( rindex >= g->stoich->getNumRates() ) {
+			// probably cumulative roundoff error here. 
+			// Recalculate atot to avoid, and redo.
+			g->stoich->updateRates( S(), &v_[0] );
+			atot_ = 0;
+			for ( vector< double >::const_iterator 
+					i = v_.begin(); i != v_.end(); ++i )
+				atot_ += *i;
+			atot_ *= SAFETY_FACTOR;
+		}
+
+		g->transposeStoich.fireReac( rindex, Svec() );
+		updateDependentMathExpn( t_, rindex );
+		// atot_ = g->updateDependentRates( atot_, rinidex );
+		updateDependentRates( g->dependency[ rindex ], g->stoich );
+		// double r = gsl_rng_uniform( rng );
+		double r = mtrand();
+		while ( r <= 0.0 ) {
+			// r = gsl_rng_uniform( rng )
+			r = mtrand();
+		}
+		t_ -= ( 1.0 / atot_ ) * log( r );
+	}
+}
+
+void GssaVoxelPools::reinit( const ProcInfo* p, const GssaSystem* g )
+{
+	VoxelPoolsBase::reinit(); // Assigns S = Sinit;
+	g->stoich->updateFuncs( varS(), 0 );
+
+	unsigned int numVarPools = g->stoich->getNumVarPools();
+	double* n = varS();
+	if ( g->useRandInit ) { 
+		// round up or down probabilistically depending on fractional 
+		// num molecules.
+		for ( unsigned int i = 0; i < numVarPools; ++i ) {
+			double base = floor( n[i] );
+			double frac = n[i] - base;
+			// if ( gsl_rng_uniform( rng ) > frac )
+			if ( mtrand() > frac )
+				n[i] = base;
+			else
+				n[i] = base + 1.0;
+		}
+	} else { // Just round to the nearest int.
+		for ( unsigned int i = 0; i < numVarPools; ++i ) {
+			n[i] = round( n[i] );
+		}
+	}
+	t_ = 0.0;
+	g->stoich->updateRates( S(), &v_[0] );
+	atot_ = 0;
+	for ( vector< double >::const_iterator 
+				i = v_.begin(); i != v_.end(); ++i )
+		atot_ += *i;
+	atot_ *= SAFETY_FACTOR;
+}
