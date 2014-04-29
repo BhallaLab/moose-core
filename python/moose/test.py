3d66df3d1963e1fcc0338c6fca83461fd2f3f17f
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-04-29 09:36:47 +0000

    Added sanity tests. Loading moose failing with seg_fault.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5297 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ python/moose/test.py
@@ -0,0 +1,10 @@
+try:
+    import _moose
+except Exception as e:
+    raise Exception("Failed to import _moose module with error {}".format(e))
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
