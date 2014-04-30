d33fa325472ea9e6c375fa06f430c64faf92ee2f
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-04-30 10:02:09 +0000

    Rallpacks is removed from cpp core.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5314 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: fae3ce6028225a25013da27e6a1ab4e818301ead
    Former-commit-id: 4f6c815669327eaf3c4e67dc14f0eaaffbec57a1

--- hsolve/testHSolve.cpp
+++ hsolve/testHSolve.cpp
@@ -25,7 +25,6 @@ void testHSolve()
 	testHSolveUtils();
 	testHinesMatrix();
 	testHSolvePassive();
-        runRallpackBenchmarks();
 }
 
 //////////////////////////////////////////////////////////////////////////////
