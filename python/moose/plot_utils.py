94da182bd5f748681675e37c98be15a0d8c23b77
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-05-01 09:40:50 +0000

    Some utility function related with plotting numpy vector in consolve using gnuplot are added.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5331 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 69ac6ea33cb2d48a2fa3a598426c8d73c5b3460e

---
+++ python/moose/plot_utils.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""plot_utils.py: Some utility function for plotting data in moose.
+
+Last modified: Sat Jan 18, 2014  05:01PM
+
+"""
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2013, NCBS Bangalore"
+__credits__          = ["NCBS Bangalore", "Bhalla Lab"]
+__license__          = "GPL"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@iitb.ac.in"
+__status__           = "Development"
+
+def plotAscii(yvec, xvec = None):
+    """docstring for plotAscii"""
+    assert type(yvec) == type(xvec)
+    if xvec in None:
+        plotInTerminal(yvec, xvec = range( len(yvec) ))
+    else:
+        plotInTerminal(yvec, xvec)
+
+def plotInTerminal(yvec, xvec = None):
+    '''
+    Plot given vectors in terminal using gnuplot.
+    '''
+    import subprocess
+    g = subprocess.Popen(["gnuplot"], stdin=subprocess.PIPE)
+    g.stdin.write("set term dumb 79 25\n")
+    g.stdin.write("plot '-' using 1:2 title 'Line1' with linespoints \n")
+    for i,j in zip(xvec, yvec):
+        g.stdin.write("%f %f\n" % (i, j))
+    g.stdin.write("e\n")
+    g.stdin.flush()
