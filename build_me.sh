87fb3aefa4df41890d7f6daeb6b4b1d3ac8b0ef5
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-05-01 06:07:20 +0000

    Few changes in compile script. Compilation and sanity tests are stable now
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5327 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: d793d886e9e9282fdd6bf43ff8e61abb50af8cd6

--- build_me.sh
+++ build_me.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
 set -e 
+#function runPythonTest
+#{
+#    ( cd python/moose && python test.py )
+#}
+#
+#function runTest 
+#{
+#    ( echo "q" | ./moose )
+#    runPythonTest
+#}
+#
+#function makeMoose
+#{
+#    make BUILD="$1"
+#}
+#
+#function cleanCode 
+#{
+#    if [ $# -eq 1 ]; then
+#        cd "$1" && make clean
+#    else
+#        make clean
+#    fi
+#}
+#
+#if [ "$1" = "c" ]; then
+#    echo "Cleaning $2 and building moose again"
+#    cleanCode "$2"
+#    makeMoose developer
+#    runTest 
+#elif [ "$1" = "t" ]; then
+#    echo "Running tests"
+#    runTest
+#    exit
+#else
+#    makeMoose developer
+#    #runTest
+#    ( cd tests && ./test_hsolve_in_python.sh )
+#    #echo "Running python test only"
+#    #runPythonTest
+#fi
 
-function runPythonTest
-{
-    ( cd python/moose && python test.py )
-}
-
-function runTest 
-{
-    ( echo "q" | ./moose )
-    runPythonTest
-}
-
-function makeMoose
-{
-    make BUILD="$1"
-}
-
-function cleanCode 
-{
-    if [ $# -eq 1 ]; then
-        cd "$1" && make clean
-    else
-        make clean
-    fi
-}
-
-if [ "$1" = "c" ]; then
-    echo "Cleaning $2 and building moose again"
-    cleanCode "$2"
-    makeMoose developer
-    runTest 
-elif [ "$1" = "t" ]; then
-    echo "Running tests"
-    runTest
-    exit
-else
-    makeMoose developer
-    #runTest
-    ( cd tests && ./test_hsolve_in_python.sh )
-    #echo "Running python test only"
-    #runPythonTest
-fi
+( cd ./buildMooseUsingCmake && ./build_me.sh )
 
