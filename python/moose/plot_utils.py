c557a13a2c536a4f6f5f594bbf81a10824f74c0c
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-05-01 11:21:43 +0000

    Ascii plot using gnuplot also standalone gnuplot script is generated if file argument is not None in function plotAscii.
    Only works with two data-set which are list-like.
    
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5334 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: d51b35ad856b2f6adad20259479035dc270f5883

--- python/moose/plot_utils.py
+++ python/moose/plot_utils.py
@@ -15,23 +15,66 @@ __maintainer__       = "Dilawar Singh"
 __email__            = "dilawars@iitb.ac.in"
 __status__           = "Development"
 
-def plotAscii(yvec, xvec = None):
-    """docstring for plotAscii"""
+def plotAscii(yvec, xvec = None, file=None):
+    """Plot two list-like object in terminal using gnuplot.
+    If file is given then save data to file as well.
+    """
     if xvec is None:
-        plotInTerminal(yvec, xvec = range( len(yvec) ))
+        plotInTerminal(yvec, range( len(yvec) ), file=file)
     else:
-        assert type(yvec) == type(xvec)
-        plotInTerminal(yvec, xvec)
+        plotInTerminal(yvec, xvec, file=file)
 
-def plotInTerminal(yvec, xvec = None):
+def plotInTerminal(yvec, xvec = None, file=None):
     '''
     Plot given vectors in terminal using gnuplot.
+
+    If file is not None then write the data to a file.
     '''
     import subprocess
     g = subprocess.Popen(["gnuplot"], stdin=subprocess.PIPE)
-    g.stdin.write("set term dumb 79 25\n")
-    g.stdin.write("plot '-' using 1:2 title 'Line1' with linespoints \n")
+    g.stdin.write("set term dumb 100 25\n")
+    g.stdin.write("plot '-' using 1:2 title '{}' with linespoints\n".format(file))
+    if file:
+        saveAsGnuplot(yvec, xvec, file=file)
     for i,j in zip(xvec, yvec):
         g.stdin.write("%f %f\n" % (i, j))
-    g.stdin.write("e\n")
+    g.stdin.write("\n")
     g.stdin.flush()
+
+def listsToString( yvec, xvec, sepby = ' '):
+    """ Given two list-like objects, returns a text string. 
+    """
+    textLines = []
+    for y, x in zip( yvec, xvec ):
+        textLines.append("{}{}{}".format(y, sepby, x))
+    return "\n".join(textLines)
+
+
+def saveNumpyVec( yvec, xvec, file):
+    """save the numpy vectors to a data-file
+    
+    """
+    if file is None:
+        return
+    print("[INFO] Saving plot data to file {}".format(file))
+    textLines = listsToString( yvec, xvec)
+    with open(file, "w") as dataF:
+        dataF.write(textLines)
+
+def saveAsGnuplot( yvec, xvec, file):
+    ''' Save the plot as stand-alone gnuplot script '''
+    if file is None:
+        return
+    print("[INFO] Saving plot data to a gnuplot-script: {}".format(file))
+    dataText = listsToString( yvec, xvec )
+    text = []
+    text.append("#!/bin/bash")
+    text.append("gnuplot << EOF")
+    text.append("set term post eps")
+    text.append("set output \"{0}.eps\"".format(file))
+    text.append("plot '-' using 0:1 title '{0}'".format(file))
+    text.append(dataText)
+    text.append("EOF")
+    with open(file+".gnuplot","w") as gnuplotF:
+        gnuplotF.write("\n".join(text))
+
