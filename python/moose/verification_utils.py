36ab2f4135b805aa25ad17e711c72fb114ff250f
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-05-08 10:40:58 +0000

    Added some more utils libraries. A sinple verification before doing simulation and development related dump messages are added. Some fixing. [skip ci]
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5370 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: adc423204a70d4dcfa9604b656159d198fdc7878

---
+++ python/moose/verification_utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""verification_utils.py: Do some verification.
+
+Last modified: Sat Jan 18, 2014  05:01PM
+
+"""
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2013, NCBS Bangalore"
+__credits__          = ["NCBS Bangalore", "Bhalla Lab"]
+__license__          = "GPL"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@iitb.ac.in"
+__status__           = "Development"
+
+import print_utils as debug
+
+import sys
+import sys
+import _moose
+
+def verify( *args, **kwargs):
+    '''Verify the current moose setup. Emit errors and warnings '''
+    pass
+
