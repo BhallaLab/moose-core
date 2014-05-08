37be8ef64f336b178b516b7aefcfa4e25eee4a90
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-05-08 09:08:56 +0000

    Each Rallpacks benchmarks gets its own class.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5359 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 454d0b64fb28ec519bc72f1deb6f5ba20e8067f5
    Former-commit-id: 04cd6ed61ebc53074b9b027a2cc662ee3a2aa8a6

--- tests/python/Rallpacks/rallpacks_benchmark.py
+++ tests/python/Rallpacks/rallpacks_benchmark.py
@@ -20,69 +20,94 @@ __status__           = "Development"
 
 import sys
 import os
+import time
 
 sys.path.append('../../python')
 import moose
 import moose.utils
 import pylab
 
+startTime = time.time()
 simTime = 100
 simDt = 1e-2
 plotDt = 0.5e-2
-cableSize = 10
+
+# Record from compartments at stepSize away
+cableSize = 100
+stepSize = int(cableSize / 10)
 
 def connectTwoComp(comp1, comp2):
+    """ Connecting two compartments """
     print("[CONNECT] Connecting {} and {}".format(comp1.path, comp2.path))
     comp1.connect('raxial', comp2, 'axial')
 
 class RallPacks():
+    ''' Run Rallpacks. '''
 
     def __init__(self):
         moose.Neutral('/cable')
+        moose.Neutral('/data')
         self.cable = []
 
     def runBenchmarks(self):
+        ''' Run all benchmarks 
+        '''
         self.runFirstBenchmark()
         #self.runSecondBenchMark()
         #self.runThirdBenchMark()
     
     def runFirstBenchmark(self):
-        print("[STEP] Setting up first benchmark... ", endby='')
-        self.setupFirstBenchmarks()
-        print("done")
+        print("[STEP] Setting up first benchmark... ")
+        inputTable = self.setupFirstBenchmarks()
+        self.setupClocks()
+        moose.start(simTime)
+        print("[RALLPACKS] Cable with {} compartments took {} sec.".format(
+            cableSize
+            , time.time() - startTime)
+            )
+        moose.utils.plotTable(inputTable, file='input.eps')
+        referenceVec = self.tables[0].vector
+        for t in self.tables[1:]:
+            pylab.plot( t.vector - referenceVec )
+        pylab.savefig('rallpacks1.eps')
 
     def setupFirstBenchmarks(self):
+        ''' Setup cable with cableSize compartments.
+        Return input and output tables after simulation.
+        '''
         self.buildCable(cableSize)
-        outputTable, inputTable = self.setupDUT()
+
+        # Pulse for input
+        pulse = moose.PulseGen('/cable/pulsegen')
+        pulse.delay[0] = 10
+        pulse.width[0] = 20
+        pulse.level[0] = 3e-5
+        moose.connect(pulse, 'output', self.cable[0], 'injectMsg')
+
+        # Read the input from stimulus
+        stimTable = moose.Table('/data/stim')
+        moose.connect(stimTable, 'requestOut', pulse, 'getOutputValue')
+
+        # Do recording at each compartment.
+        self.tables = []
+        for i in range(1, len(self.cable), stepSize):
+            table = moose.Table('/data/table{}'.format(i))
+            moose.connect(table, 'requestOut', self.cable[i], 'getVm')
+            self.tables.append(table)
+
+        return stimTable
 
 
     def buildCable(self, noOfCompartments):
         ''' Build a cable of noOfCompartments comparments '''
         for i in range(noOfCompartments):
             comp = moose.Compartment("/cable/comp{}".format(i))
-            comp.Rm = 7.6e6
-            comp.Cm = 7e-9
+            #comp.Rm = 1e6
+            #comp.Cm = 7e-9
             self.cable.append(comp)
         # Connect these compartments linearly.
         for c in range(1, len(self.cable)):
             connectTwoComp(self.cable[c-1], self.cable[c])
-
-    def setupDUT(self):
-        ''' Attach a pulse generator at the first compartment and record from
-        the last'''
-        pulse = moose.PulseGen('/cable/pulsegen')
-        pulse.delay[0] = 2
-        pulse.width[0] = 10
-        pulse.level[0] = 3e-6
-        moose.connect(pulse, 'output', self.cable[0], 'injectMsg')
-
-        # Do recording here
-        moose.Neutral('/data')
-        recordingAtStim = moose.Table('/data/stim')
-        moose.connect(recordingAtStim, 'requestOut', pulse, 'getOutputValue')
-        vmTab = moose.Table('/data/cable_vm')
-        moose.connect(vmTab, 'requestOut', self.cable[1], 'getVm')
-        return vmTab, recordingAtStim
 
     def setupClocks(self):
         """ Setup clocks for simulation 
@@ -93,7 +118,7 @@ class RallPacks():
 
         # Initializing is very important
         moose.useClock(0, '/cable/##', 'init')
-        moose.useClock(1, '/data/#[TYPE=Table]', 'process')
+        moose.useClock(1, '/data/##', 'process')
         moose.reinit()
 
 def main():
