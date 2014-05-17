2e4bbc868deec2a818d19d28c5c9f275ed414aa8
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-05-17 10:45:55 +0000

    async13: Further fixes to HSolve and updates to the Shell::dropClockMsgs. Now HSolve seems to do something in the right general range.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5424 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- hsolve/HSolve.cpp
+++ hsolve/HSolve.cpp
@@ -23,6 +23,7 @@
 #include "../biophysics/ChanBase.h"
 #include "../biophysics/HHChannel.h"
 #include "ZombieHHChannel.h"
+#include "../shell/Shell.h"
 
 const Cinfo* HSolve::initCinfo()
 {
@@ -206,14 +207,27 @@ void HSolve::reinit( const Eref& hsolve, ProcPtr p )
 void HSolve::zombify( Eref hsolve ) const
 {
     vector< Id >::const_iterator i;
+	vector< ObjId > temp;
 
+    for ( i = compartmentId_.begin(); i != compartmentId_.end(); ++i )
+		temp.push_back( ObjId( *i, 0 ) );
+	Shell::dropClockMsgs( temp, "init" );
+	Shell::dropClockMsgs( temp, "process" );
     for ( i = compartmentId_.begin(); i != compartmentId_.end(); ++i )
         CompartmentBase::zombify( i->eref().element(),
 					   ZombieCompartment::initCinfo(), hsolve.id() );
 
+	temp.clear();
+    for ( i = caConcId_.begin(); i != caConcId_.end(); ++i )
+		temp.push_back( ObjId( *i, 0 ) );
+	Shell::dropClockMsgs( temp, "process" );
     for ( i = caConcId_.begin(); i != caConcId_.end(); ++i )
         ZombieCaConc::zombify( hsolve.element(), i->eref().element() );
 
+	temp.clear();
+    for ( i = channelId_.begin(); i != channelId_.end(); ++i )
+		temp.push_back( ObjId( *i, 0 ) );
+	Shell::dropClockMsgs( temp, "process" );
     for ( i = channelId_.begin(); i != channelId_.end(); ++i )
         ZombieHHChannel::zombify( hsolve.element(), i->eref().element() );
 }
@@ -223,8 +237,8 @@ void HSolve::setup( Eref hsolve )
     // Setup solver.
     this->HSolveActive::setup( seed_, dt_ );
 
-    zombify( hsolve );
     mapIds();
+    zombify( hsolve );
 }
 
 ///////////////////////////////////////////////////
