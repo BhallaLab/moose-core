b441c3d6af24adcc736b61a75a0b949b5ee83aad
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-05-18 11:55:52 +0000

    async13: Added and cleaned up a number of tutorials for oscillatory chemical systems.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5426 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: b1a5728458fae48b0ca301ee15dc73e4553ec285

--- Demos/tutorials/repressillator.py
+++ Demos/tutorials/repressillator.py
@@ -6,29 +6,26 @@
 ## GNU Lesser General Public License version 2.1
 ## See the file COPYING.LIB for the full notice.
 #########################################################################
-# This example illustrates loading, running, and saving a kinetic model 
-# defined in kkit format. It uses a default kkit model but you can
+# This example illustrates loading, and running a kinetic model 
+# for a relaxation oscillator, defined in kkit format. It uses a default 
+# kkit model but you can
 # specify another using the command line 
 #     python filename runtime solver
 # We use the gsl solver here. The model already
-# defines a couple of plots and sets the runtime to 100 seconds. The
-# script dumps the output into an xplot file called data.plot and the
-# saved version into saveReaction.g
-# Since it is dealing with kkit models which may span compartments, the
-# 'solver' argument is prefixed with 'old_'. This tells the readKkit
-# function to put the entire model tree on the 'kinetics' compartment
-# which confuses the volume scaling but lets the solver handle the
-# entire model.
+# defines some plots and sets the runtime to 4000 seconds.
 
 import moose
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import pylab
 import numpy
 import sys
 
 def main():
-        solver = "old_gsl"  # Pick any of gsl, gssa, ee..
-	mfile = '../Genesis_files/kkit_objects_example.g'
-	runtime = 20.0
+        #solver = "gsl"  # Pick any of gsl, gssa, ee..
+        solver = "gssa"  # Pick any of gsl, gssa, ee..
+	mfile = '../Genesis_files/Repressillator.g'
+	runtime = 6000.0
 	if ( len( sys.argv ) >= 3 ):
 		mfile = '../Genesis_files/' + sys.argv[1]
 		runtime = float( sys.argv[2] )
@@ -37,31 +34,27 @@ def main():
 	modelId = moose.loadModel( mfile, 'model', solver )
         # Increase volume so that the stochastic solver gssa 
         # gives an interesting output
-        #compt = moose.element( '/model/kinetics' )
-        #compt.volume = 1e-19 
+        compt = moose.element( '/model/kinetics' )
+        compt.volume = 1e-19 
+        dt = moose.element( '/clock' ).dt
 
 	moose.reinit()
 	moose.start( runtime ) 
 
-        # Report parameters
-        '''
-	for x in moose.wildcardFind( '/model/kinetics/##[ISA=PoolBase]' ):
-		print x.name, x.nInit, x.concInit
-	for x in moose.wildcardFind( '/model/kinetics/##[ISA=ReacBase]' ):
-		print x.name, 'num: (', x.numKf, ', ',  x.numKb, '), conc: (', x.Kf, ', ', x.Kb, ')'
-	for x in moose.wildcardFind('/model/kinetics/##[ISA=EnzBase]'):
-		print x.name, '(', x.Km, ', ',  x.numKm, ', ', x.kcat, ')'
-                '''
-
 	# Display all plots.
-        dt = moose.element( '/clock' ).dt
+        img = mpimg.imread( 'repressillator_tut.png' )
+        fig = plt.figure( figsize=(12, 10 ) )
+        png = fig.add_subplot( 211 )
+        imgplot = plt.imshow( img )
+        ax = fig.add_subplot( 212 )
+	x = moose.wildcardFind( '/model/#graphs/conc#/#' )
+        plt.ylabel( 'Conc (mM)' )
+        plt.xlabel( 'Time (seconds)' )
 	for x in moose.wildcardFind( '/model/#graphs/conc#/#' ):
             t = numpy.arange( 0, x.vector.size, 1 ) * dt
             pylab.plot( t, x.vector, label=x.name )
         pylab.legend()
         pylab.show()
-
-	quit()
 
 # Run the 'main' if this script is executed standalone.
 if __name__ == '__main__':
