51c3946fc6fb718b15966b70c61a46707cfefe0d
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-05-18 11:55:52 +0000

    async13: Added and cleaned up a number of tutorials for oscillatory chemical systems.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5426 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: fbab5ca38f14c82c3ca70f5ac33844577c284284
    Former-commit-id: 51eb7630247d5251301c414e3ff99011391ab4c7

---
+++ Demos/tutorials/slowFbOsc.py
@@ -0,0 +1,63 @@
+#########################################################################
+## This program is part of 'MOOSE', the
+## Messaging Object Oriented Simulation Environment.
+##           Copyright (C) 2014 Upinder S. Bhalla. and NCBS
+## It is made available under the terms of the
+## GNU Lesser General Public License version 2.1
+## See the file COPYING.LIB for the full notice.
+#########################################################################
+# This example illustrates loading, and running a kinetic model 
+# for a relaxation oscillator, defined in kkit format. It uses a default 
+# kkit model but you can
+# specify another using the command line 
+#     python filename runtime solver
+# We use the gsl solver here. The model already
+# defines some plots and sets the runtime to 4000 seconds.
+
+import moose
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import pylab
+import numpy
+import sys
+
+def main():
+        #solver = "gsl"  # Pick any of gsl, gssa, ee..
+        solver = "gsl"  # Pick any of gsl, gssa, ee..
+	mfile = '../Genesis_files/Kholodenko.g'
+	runtime = 5000.0
+	if ( len( sys.argv ) >= 3 ):
+		mfile = '../Genesis_files/' + sys.argv[1]
+		runtime = float( sys.argv[2] )
+	if ( len( sys.argv ) == 4 ):
+                solver = sys.argv[3]
+	modelId = moose.loadModel( mfile, 'model', solver )
+        # Increase volume so that the stochastic solver gssa 
+        # gives an interesting output
+        compt = moose.element( '/model/kinetics' )
+        compt.volume = 1e-19 
+        dt = moose.element( '/clock' ).dt
+
+	moose.reinit()
+	moose.start( runtime ) 
+
+	# Display all plots.
+        img = mpimg.imread( 'Kholodenko_tut.png' )
+        fig = plt.figure( figsize=( 12, 10 ) )
+        png = fig.add_subplot( 211 )
+        imgplot = plt.imshow( img )
+        ax = fig.add_subplot( 212 )
+	x = moose.wildcardFind( '/model/#graphs/conc#/#' )
+        t = numpy.arange( 0, x[0].vector.size, 1 ) * dt
+        ax.plot( t, x[0].vector * 100, 'b-', label='Ras-MKKK * 100' )
+        ax.plot( t, x[1].vector, 'y-', label='MKKK-P' )
+        ax.plot( t, x[2].vector, 'm-', label='MKK-PP' )
+        ax.plot( t, x[3].vector, 'r-', label='MAPK-PP' )
+        plt.ylabel( 'Conc (mM)' )
+        plt.xlabel( 'Time (seconds)' )
+        pylab.legend()
+        pylab.show()
+
+# Run the 'main' if this script is executed standalone.
+if __name__ == '__main__':
+	main()
