24058050d48c19826d4e5f87154c0510a7773276
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-05-19 14:45:05 +0000

    Added a dummy function in DifShell.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5433 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- biophysics/DifShell.h
+++ biophysics/DifShell.h
@@ -63,6 +63,8 @@ class DifShell
 
                 void process_1(const Eref & e, ProcPtr p );
 
+                void reinit_1(const Eref & e, ProcPtr p ); // dummyFunc
+                
 		void buffer(
 		 double kf,
 		 double kb,
