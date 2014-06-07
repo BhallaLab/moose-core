0d41573a24c5c57ca51433ede550a5de0c885126
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-06-07 09:21:25 +0000

    CMake tested on gulabjamun
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5499 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: dc2f4e44a10c6ffa33aa1acd06cecf15b39508a2
    Former-commit-id: 1d09f6e3c6d288ed6c98ad69e2fdeefaef17ba8e

--- buildMooseUsingCmake/build_me.sh
+++ buildMooseUsingCmake/build_me.sh
@@ -56,6 +56,8 @@ if [ $# -gt 0 ]; then
     colorPrint "INPUT" "Building for distribution" 
     BUILD_TYPE=distribution 
 fi
+rm -rf ../CMakeFiles/ 
+rm -f ../CMakeCache.txt
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
 make VERBOSE=1
 make check_python
--- build_me.sh
+++ build_me.sh
@@ -1,55 +1,11 @@
 #!/bin/bash
 set -e 
-# Definitions of colors in bash
-RESTORE='\033[0m'
+source ./buildMooseUsingCmake/build_me.sh
 
-RED='\033[00;31m'
-GREEN='\033[00;32m'
-YELLOW='\033[00;33m'
-BLUE='\033[00;34m'
-PURPLE='\033[00;35m'
-CYAN='\033[00;36m'
-LIGHTGRAY='\033[00;37m'
-
-LRED='\033[01;31m'
-LGREEN='\033[01;32m'
-LYELLOW='\033[01;33m'
-LBLUE='\033[01;34m'
-LPURPLE='\033[01;35m'
-LCYAN='\033[01;36m'
-WHITE='\033[01;37m'
-
-function colorPrint
-{
-    case $1 in 
-        "WARN")
-            echo -e "[WARN] ${LRED} $2 ${RESTORE} $3"
-            ;;
-        "INFO")
-            echo -e "[INFO] ${LGREEN} $2 ${RESTORE} $3"
-            ;;
-        "ERROR")
-            echo -e "[ERROR] ${RED} $2 ${RESTORE} $3"
-            ;;
-        "DEBUG")
-            echo -e "[DEBUG] ${YELLOW} $2 ${RESTORE} $3"
-            ;;
-        "STEP")
-            echo -e "[STEP] ${BLUE} $2 ${RESTORE} $3"
-            ;;
-        "TODO")
-            echo -e "[TODO] ${CYAN} $2 ${RESTORE} $3"
-            ;;
-        *)
-            echo -e "[$1] $2 $3"
-            ;;
-    esac
-}
-colorPrint "INFO" "Beware, O unsuspecting developer!" 
-colorPrint "INFO" "This script builds moose using cmake. If you want to use standard "
-colorPrint "INFO" "version, run make"
-colorPrint "INFO" "You may like to customize some varibales in Makefile."
-colorPrint "INFO" ""
+colorPrint "||" "Beware, O unsuspecting developer!" 
+colorPrint "||" "This script builds moose using cmake. If you want to use standard "
+colorPrint "||" "version, run make"
+colorPrint "||" "You may like to customize some varibales in Makefile."
 
 BUILD_TYPE=Debug
 if [ $# -gt 0 ]; then
@@ -57,6 +13,5 @@ if [ $# -gt 0 ]; then
     export CXX_FLAGS=-O3
     BUILD_TYPE=RELEASE 
 fi
-( cd ./buildMooseUsingCmake && \
-    ./build_me.sh -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DQUIET_MODE 
+( cd ./buildMooseUsingCmake && ./build_me.sh
 )
