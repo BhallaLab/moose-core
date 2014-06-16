0846308bd8bce8070708f98aba58e915634663ac
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-06-16 18:08:12 +0000

    Write moose messages to a graphviz file.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5541 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: ac8b42af9074088bfb77b5b07428614f0aefce00

--- python/moose/backend/backend.py
+++ python/moose/backend/backend.py
@@ -1,18 +1,31 @@
-#!/usr/bin/env python
+# This file is part of MOOSE simulator: http://moose.ncbs.res.in.
 
-"""backend.py: Convert moose data-structure to some other backend.
+# MOOSE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-Last modified: Mon May 12, 2014  11:21PM
+# MOOSE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with MOOSE.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""backend.py: 
+
+Last modified: Sat Jan 18, 2014  05:01PM
 
 """
     
 __author__           = "Dilawar Singh"
-__copyright__        = "Copyright 2013, NCBS Bangalore"
-__credits__          = ["NCBS Bangalore", "Bhalla Lab"]
-__license__          = "GPL"
+__copyright__        = "Copyright 2013, Dilawar Singh and NCBS Bangalore"
+__credits__          = ["NCBS Bangalore"]
+__license__          = "GNU GPL"
 __version__          = "1.0.0"
 __maintainer__       = "Dilawar Singh"
-__email__            = "dilawars@iitb.ac.in"
+__email__            = "dilawars@ncbs.res.in"
 __status__           = "Development"
 
 
@@ -21,15 +34,9 @@ import os
 thisDir = os.path.dirname( __file__ )
 sys.path.append( os.path.join( thisDir, '..') )
 
-import _moose 
-import print_utils as debug
+from .. import _moose 
+from .. import print_utils 
 from collections import defaultdict
-
-# Plot in terminal uses gnuplot. So use it as a backend.
-import plot_utils 
-plotInTerminal = plot_utils.plotInTerminal 
-plotAscii = plot_utils.plotAscii
-
 
 class Backend(object):
     """ Base class for all backend """
@@ -61,7 +68,7 @@ class Backend(object):
         """ Populate all data-structures related with Compartments, Tables, and
         pulse generators.
         """
-        debug.dump("INFO", "Populating data-structures to write spice netlist")
+        print_utils.dump("INFO", "Populating data-structures to write spice netlist")
         self.getComparments(**kwargs)
         self.getTables(**kwargs)
         self.getPulseGens(**kwargs)
