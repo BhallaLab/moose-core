5fe28730fe747f874445db88ea02114e889c38e9
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-07-13 03:54:09 +0000

    Added cmake-install scripts. Moose python related packages are here.
    gui is fixed and tested. moosegui launches the gui.
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5639 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 4a7ca669c6a98ad3a6a918efd073695c664c97a2

---
+++ python/InstallPyMoose.cmake
@@ -0,0 +1,7 @@
+# CMake script to install python.
+MESSAGE("Instlling python scripts: ${CMAKE_INSTALL_PREFIX}")
+set(PYTHON_DIR ${CMAKE_CURRENT_LIST_DIR})
+execute_process(COMMAND python setup.py install --prefix=${CMAKE_INSTALL_PREFIX} 
+    WORKING_DIRECTORY ${PYTHON_DIR}
+    )
+FILE(REMOVE_RECURSE ${PYTHON_DIR}/build)
