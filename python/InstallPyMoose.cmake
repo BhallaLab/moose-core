7391cc507018ff289074862226cc6b217b164cf6
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-07-18 11:05:08 +0000

    Launchpad script cleans the repo before making the tar-ball.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5669 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 6b20da83e5078fdc35f3068351cc1b9922413e05

--- python/InstallPyMoose.cmake
+++ python/InstallPyMoose.cmake
@@ -1,7 +1,9 @@
 # CMake script to install python.
 MESSAGE("Instlling python scripts: ${CMAKE_INSTALL_PREFIX}")
 set(PYTHON_DIR ${CMAKE_CURRENT_LIST_DIR})
-execute_process(COMMAND python setup.py build_py install --prefix=${CMAKE_INSTALL_PREFIX} 
+execute_process(COMMAND 
+    python setup.py build_py install --build-base=/tmp --force \
+        --prefix=${CMAKE_INSTALL_PREFIX} 
     WORKING_DIRECTORY ${PYTHON_DIR}
     )
 FILE(REMOVE_RECURSE ${PYTHON_DIR}/build)
