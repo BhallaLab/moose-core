823125f896cd548e60de8846900a34e59a6c6f90
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-07-23 08:19:20 +0000

    Some progress in copying python files into package.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5680 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 2b3ae52d504f2494111326de2bd8e0559fef16c1
    Former-commit-id: 9dae148f37bf8c4bf7e0169aed5eda50a370d2e9

--- python/InstallPyMoose.cmake
+++ python/InstallPyMoose.cmake
@@ -1,8 +1,20 @@
 # CMake script to install python.
-MESSAGE("Instlling python scripts: ${CMAKE_INSTALL_PREFIX}")
+if(CPACK_TEMPORARY_INSTALL_DIR)
+    MESSAGE("++ Using cpack to generate package")
+    set(CMAKE_INSTALL_PREFIX ${CPACK_TEMPORARY_INSTALL_DIR})
+endif()
+execute_process(COMMAND 
+    python -c "from distutils.sysconfig import get_python_lib; print get_python_lib(prefix=\"${CMAKE_INSTALL_PREFIX}\")" 
+    OUTPUT_VARIABLE PYTHON_SITE_PACKAGES OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+MESSAGE("++ Installing python scripts: ${CMAKE_INSTALL_PREFIX}")
+FILE(MAKE_DIRECTORY ${PYTHON_SITE_PACKAGES})
+MESSAGE("+++ Updating PYTHONPATH: ${PYTHON_SITE_PACKAGES}")
+set(ENV{PYTHONPATH} ${PYTHON_SITE_PACKAGES})
 set(PYTHON_DIR ${CMAKE_CURRENT_LIST_DIR})
 execute_process(COMMAND 
-    python setup.py build_py build -b /tmp install --user --prefix=${CMAKE_INSTALL_PREFIX} 
+    python setup.py build_py build -b /tmp install
+        --prefix=${CMAKE_INSTALL_PREFIX} 
     WORKING_DIRECTORY ${PYTHON_DIR}
     )
 FILE(REMOVE_RECURSE ${PYTHON_DIR}/build)
