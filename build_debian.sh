fb5043be60bb400a6dac5fb23f843a7d5655f04d
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-07-25 11:32:48 +0000

    Adding scripts needed to make package on launchpad.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5685 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: a83701f6396201b39aa074a37d241da33e8527fc

---
+++ build_debian.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+set -o xtrace
+
+if [ $# -gt 0 ]; then
+    if [[ "$1" == "update" ]]; then
+        echo "Fetching repo and creating tar-ball"
+        bzr dailydeb --allow-fallback-to-native moose.recipe ..
+    elif [[ "$1" == "pbuilder" ]]; then
+        bzr dailydeb --allow-fallback-to-native moose.recipe ..
+        sudo -E pbuilder build ../*.dsc
+        exit
+    fi
+fi
+
+tarFile=`find .. -type f -maxdepth 1 -name "moose_3.0*.tar.gz"`
+echo "Found tarfiles are $tarFile"
+if [[ ! $tarFile ]]; then
+    echo "I could not file a tar.gz file. Can't continue"
+    echo "++ Cleaning previous mess"
+    rm -rf ../moose_3.0* ../moose-{*
+    echo "++ Let me download a fresh one"
+    bzr dailydeb --allow-fallback-to-native moose.recipe ..
+fi
+
+echo "Building debian package"
+bzr builddeb -- -uc -us
