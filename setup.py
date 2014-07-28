94cb68883ebb6fb288fd6ba03135e24e0329d62c
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-07-28 06:17:24 +0000

    LOCAL_GSL is compiled using CMake now.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5696 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: f269ed2a789fb5bbaa255162530644e11cf6834c

--- setup.py
+++ setup.py
@@ -1,102 +1,109 @@
-#!/usr/bin/env python
 
-import distutils.core as dcore
+"""setup.py: This scripts prepare MOOSE for PyPI.
 
-#------------------------------------------------------------------------------
-# Metadata
-#------------------------------------------------------------------------------
+Last modified: Mon Jul 28, 2014  12:52AM
+
+"""
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2013, Dilawar Singh and NCBS Bangalore"
+__credits__          = ["NCBS Bangalore"]
+__license__          = "GNU GPL"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+__status__           = "Development"
+
+import os
+import sys
+
+from distutils.core import setup, Command
+from distutils.command.install import install as _install
+
+import distutils.spawn as ds
+
+class ConfigureCommand(Command):
+    """Configure MOOSE """
+    user_options = []
+
+    def initialize_options(self):
+        self.cwd = os.getcwd()
+        self.new_dir = os.path.join(os.path.split(__file__)[0], 'buildMooseUsingCmake')
+    
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        if ds.find_executable('cmake') is None:
+            msg = [ "Error: Unable to configure MOOSE."
+                    , " "
+                    , " This application relies on CMake build tool (www.cmake.org)"
+                    , " Once cmake is installed, you can continue "
+                    ]
+            print("\n".join(msg))
+            sys.exit(0)
+
+        print("Configuring MOOSE using CMake")
+        os.chdir(self.new_dir)
+        try:
+            ds.spawn(['cmake', '..'])
+        except ds.DistutilsExecError:
+            print("Error: error occurred while running CMake to configure MOOSE.")
+            os.chdir(self.cwd)
+            sys.exit(-1)
+        os.chdir(self.cwd)
+
+class BuildCommand(Command):
+    user_options = []
+    def initialize_options(self):
+        self.cwd = os.getcwd()
+        self.new_dir = os.path.join(os.path.split(__file__)[0], 'buildMooseUsingCmake')
+    
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        print("Building PyMOOSE")
+        os.chdir(self.new_dir)
+        try:
+            ds.spawn(['make'])
+        except ds.DistutilsExecError as e:
+            print("Can't build MOOSE")
+            print(e)
+            os.chdir(self.cwd)
+            sys.exit(-1)
+        os.chdir(self.cwd)
+
+##
+# @brief FUnction to read a file.
+#
+# @param fname Name of the file.
+#
+# @return  A string content of the file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 name          = 'moose'
-version       = '1.4'
+version       = '3.0'
 description   = (
-	'MOOSE is the Multiscale Object-Oriented Simulation Environment. '
-	'It is the base and numerical core for large, detailed simulations '
-	'including Computational Neuroscience and Systems Biology.' )
-author        = 'Upinder S. Bhalla'
-author_email  = 'bhalla@ncbs.res.in'
+        'MOOSE is the Multiscale Object-Oriented Simulation Environment. '
+        'It is the base and numerical core for large, detailed simulations '
+        'including Computational Neuroscience and Systems Biology.' )
 url           = 'http://moose.ncbs.res.in/'
 
-#------------------------------------------------------------------------------
-# Script parameters
-#------------------------------------------------------------------------------
-#============
-# Defaults
-#============
-mode = 'debug'
 
-
-#============
-# Overriding defaults: command line
-#============
-
-
-#============
-# Auto-detect
-#============
-#------------------------------------------------------------------------------
-# Sources
-#------------------------------------------------------------------------------
-from setup_ import sources
-
-#------------------------------------------------------------------------------
-# Include directories
-#------------------------------------------------------------------------------
-include_dirs = [ 'basecode', 'msg', 'kinetics', 'mesh', 'shell' ]
-
-#------------------------------------------------------------------------------
-# Libs, lib dirs
-#------------------------------------------------------------------------------
-libraries = [ 'gsl', 'gslcblas' ]
-library_dirs = []
-
-#------------------------------------------------------------------------------
-# Macros
-#------------------------------------------------------------------------------
-define_macros = [
-	( 'PYMOOSE', None ),
-	( 'USE_GSL', 1 ),
-]
-
-#------------------------------------------------------------------------------
-# Compile/Link args
-#------------------------------------------------------------------------------
-extra_compile_args = [ '-O0', '-Wall', '-pedantic', '-Wno-long-long' ]
-extra_link_args = []
-
-#------------------------------------------------------------------------------
-# Dependencies
-#------------------------------------------------------------------------------
-#============
-# Parse
-#============
-
-#============
-# Load
-#============
-
-#============
-# Save
-#============
-
-#------------------------------------------------------------------------------
-# Setup
-#------------------------------------------------------------------------------
-moose = dcore.Extension(
-	'moose',
-	sources,
-	define_macros      = define_macros,
-	include_dirs       = include_dirs,
-	library_dirs       = library_dirs,
-	libraries          = libraries,
-	extra_compile_args = extra_compile_args,
-	extra_link_args    = extra_link_args,
-)
-
-dcore.setup(
-	name          = name,
-	version       = version,
-	description   = description,
-	author        = author,
-	author_email  = author_email,
-	url           = url,
-	ext_modules   = [ moose ]
-)
+setup(
+        name = name
+        , version = version 
+        , author = [ "Upinder Bhalla et. al." ]
+        , author_email = "bhalla@ncbs.res.in"
+        , description = description 
+        , license = "LGPL"
+        , keywords = "neural simulation"
+        , url = url
+        , packages = []
+        , long_description = read('README')
+        , cmdclass = { 'configure' : ConfigureCommand 
+            , 'build' : BuildCommand 
+            }
+        )
