2dc554b3c5f8ad47cb841b88c9703ff0a0a69d38
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-07-31 17:30:31 +0000

    Removed the local gsl. PyPI is ready. Think of a better way to pack it on precise.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5710 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 026b27a495cf9efe57e7d29b64fe651f409ad3d9

--- cmake_modules/FindGSL.cmake
+++ cmake_modules/FindGSL.cmake
@@ -60,6 +60,8 @@ ELSE(WIN32)
     IF (GSL_CONFIG) 
       # set CXXFLAGS to be fed into CXX_FLAGS by the user:
       SET(GSL_CXX_FLAGS "`${GSL_CONFIG} --cflags`")
+      SET(GSL_VERSION "`${GSL_CONFIG} --version`")
+
       
       # set INCLUDE_DIRS to prefix+include
       EXEC_PROGRAM(${GSL_CONFIG}
