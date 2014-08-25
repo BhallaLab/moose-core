d67d90bb6011c9e115f8f4e4a3dd062efbefc4d0
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-08-25 14:16:11 +0000

    Fixes to Dsolve and to rate terms to correct bugs with running multiscale models.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5763 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- diffusion/Dsolve.cpp
+++ diffusion/Dsolve.cpp
@@ -627,16 +627,25 @@ double Dsolve::getNinit( const Eref& e ) const
 
 void Dsolve::setDiffConst( const Eref& e, double v )
 {
+	unsigned int pid = convertIdToPoolIndex( e );
+	if ( pid >= pools_.size() )   // Ignore silently, out of range.
+		return;
 	pools_[ convertIdToPoolIndex( e ) ].setDiffConst( v );
 }
 
 double Dsolve::getDiffConst( const Eref& e ) const
 {
+	unsigned int pid = convertIdToPoolIndex( e );
+	if ( pid >= pools_.size() )   // Ignore silently, out of range.
+		return 0.0;
 	return pools_[ convertIdToPoolIndex( e ) ].getDiffConst();
 }
 
 void Dsolve::setMotorConst( const Eref& e, double v )
 {
+	unsigned int pid = convertIdToPoolIndex( e );
+	if ( pid >= pools_.size() )   // Ignore silently, out of range.
+		return;
 	pools_[ convertIdToPoolIndex( e ) ].setMotorConst( v );
 }
 
