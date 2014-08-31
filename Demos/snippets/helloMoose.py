06a4753e6c63e51a2c3d55edaf1200b9308f2c69
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-08-31 15:49:14 +0000

    Lots of documentation stuff. Fixed up the docs to include link to source file. Added a couple more documentation examples, for steady state and for dose-responses.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/branches/async13@5776 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 0b434123931c1d2429fd5bf3f97e0c64a0fb1601
    Former-commit-id: a27d4604f1be532a6d9a207164b6536e79186dfc

--- Demos/snippets/helloMoose.py
+++ Demos/snippets/helloMoose.py
@@ -6,24 +6,25 @@
 ## GNU Lesser General Public License version 2.1
 ## See the file COPYING.LIB for the full notice.
 #########################################################################
-"""
-This is the Hello MOOSE program. It shows how to get MOOSE to do
-its most basic operations: to load, run, and graph a model defined 
-in an external model definition file.
-
-The loadModel function is the core of this example. It can accept
-a range of file and model types, including kkit, cspace and GENESIS .p
-files. It autodetects the file type and loads in the simulation.
-
-The wildcardFind function finds all objects matching the specified path,
-in this case Table objects hoding the simulation results. They were 
-all defined in the model file.
-"""
 
 import moose
 import pylab
 import numpy
-if __name__ == '__main__':
+
+def main():
+    """
+    This is the Hello MOOSE program. It shows how to get MOOSE to do
+    its most basic operations: to load, run, and graph a model defined 
+    in an external model definition file.
+    
+    The loadModel function is the core of this example. It can accept
+    a range of file and model types, including kkit, cspace and GENESIS .p
+    files. It autodetects the file type and loads in the simulation.
+    
+    The wildcardFind function finds all objects matching the specified path,
+    in this case Table objects hoding the simulation results. They were 
+    all defined in the model file.
+    """
     mfile = '../Genesis_files/kkit_objects_example.g'
     modelId = moose.loadModel( mfile, 'model', 'gsl' ) # Load it
     moose.reinit()          # Set initial conditions
@@ -33,3 +34,6 @@ if __name__ == '__main__':
         pylab.plot( x.vector, label=x.name )
     pylab.legend()
     pylab.show()
+
+if __name__ == '__main__':
+	main()
