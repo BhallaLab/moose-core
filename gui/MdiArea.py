72c6d20849477b0991730ec8b02829eb28cb99e6
aviralg <aviralg@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-12-02 09:52:29 +0000

    Moose icon appears, Moose background appears, Lower limit of X and Y axes in plots set to 0.0
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@6136 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 3b9f37040e4d7c60efc81fd659e68d83537c2a4f
    Former-commit-id: 69263a0530e33695948c81775e80a2f2ab233b5c
    Former-commit-id: aee4630a4c693a34ad261daaffb2700dc31fa7ba

---
+++ gui/MdiArea.py
@@ -0,0 +1,27 @@
+from PyQt4 import Qt, QtCore, QtGui
+from PyQt4.QtGui import *
+import os
+
+APPLICATION_BACKGROUND_PATH = os.path.join( os.path.dirname(os.path.realpath(__file__))
+                                          , "icons/moose_icon_large.png"
+                                          )
+
+
+class MdiArea(QMdiArea):
+    def __init__(self):
+        super(MdiArea, self).__init__()
+        print()
+        self.backgroundImage = QImage(APPLICATION_BACKGROUND_PATH)
+        self.background      = None
+
+    def resizeEvent(self,event):
+        # http://qt-project.org/faq/answer/when_setting_a_background_pixmap_for_a_widget_it_is_tiled_if_the_pixmap_is_
+        self.background = QImage(event.size(), QImage.Format_ARGB32_Premultiplied)
+        painter = QPainter(self.background)
+        painter.fillRect(self.background.rect(), QColor(255, 255, 255, 255))
+        scaled = self.backgroundImage.scaled(event.size() , QtCore.Qt.KeepAspectRatio)
+        scaledRect = scaled.rect()
+        scaledRect.moveCenter(self.background.rect().center())
+        painter.drawImage(scaledRect, scaled)
+        self.setBackground(QBrush(self.background))
+        super(MdiArea, self).resizeEvent(event)
