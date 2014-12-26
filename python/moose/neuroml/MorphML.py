dd9bdf14356c709b91afc9fcd6397c867192d316
aditya-g <aditya-g@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-12-26 07:00:31 +0000

    To the neuroml reader, I added a boolean-argument to turn off the combining of multiple sections in a cable. Now by default, the reader does not combine segments, whereas perviously it did.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@6287 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 87523b357f5b60ca82961856e2d85532d850de58
    Former-commit-id: 0888eec9270c165b3ffa9d7a2dd1c11e2b86d020
    Former-commit-id: 0a165c0cc1c1103d3253dde0e28a82698de7f908

--- python/moose/neuroml/MorphML.py
+++ python/moose/neuroml/MorphML.py
@@ -57,7 +57,7 @@ class MorphML():
             cellsDict.update(cellDict)
         return cellsDict
 
-    def readMorphML(self,cell,params={},lengthUnits="micrometer"):
+    def readMorphML(self,cell,params={},lengthUnits="micrometer",combine_segments=False):
         """
         returns {cellname:segDict}
         where segDict = { segid1 : [ segname,(proximalx,proximaly,proximalz),
@@ -156,7 +156,7 @@ class MorphML():
         ############################################################
         #### load morphology and connections between compartments
         ## Many neurons exported from NEURON have multiple segments in a section
-        ## Combine those segments into one Compartment / section
+        ## Combine those segments into one Compartment / section, if combine_segments = True
         ## assume segments of a compartment/section are in increasing order and
         ## assume all segments of a compartment/section have the same cableid
         ## findall() returns elements in document order:
@@ -172,8 +172,9 @@ class MorphML():
             ## cable is an optional attribute. WARNING: Here I assume it is always present.
             cableid = segment.attrib['cable']
             segmentid = segment.attrib['id']
-            ## old cableid still running, hence don't start a new compartment, skip to next segment
-            if cableid == running_cableid:
+            ## if old cableid still running AND combine_segments == True,
+            ## then don't start a new compartment, skip to next segment
+            if cableid == running_cableid and combine_segments:
                 self.cellDictBySegmentId[cellname][1][segmentid] = running_comp
                 proximal = segment.find('./{'+self.mml+'}proximal')
                 if proximal is not None:
