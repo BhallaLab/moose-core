8acfdd8a10adc01a0794d2dff06792a2285a51c0
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-12-26 10:58:33 +0000

    Adding old cymoose work from my branch to trunk.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@6300 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 05aeb1924017541df67a68d5cd027fa7d1d1a03d

---
+++ cymoose/PyObjId.pyx
@@ -0,0 +1,31 @@
+# Definition to this class.
+cimport ObjId as _ObjId
+cimport Id as _Id
+
+
+from cython.operator import dereference as deref
+from libc.stdio cimport printf
+
+cdef class PyObjId:
+
+    cdef _ObjId.ObjId* thisptr
+    cdef _ObjId.ObjId objId
+    cdef public string path_
+
+    def __repr__(self):
+        msg = "<type: ObjId> at {}"
+        if len(self.path_) < 1:
+            self.path_ = self.objId.path()
+        return msg.format(self.path_)
+
+    def __cinit__(self, id_ = None, dataIndex = 0, fieldIndex = 0):
+        self.thisptr = new _ObjId.ObjId()
+
+    def path(self):
+        """Get the path"""
+        if len(self.path_) > 0:
+            return self.path_
+        else:
+            return self.objId.path()
+
+
---
+++ cymoose/cymoose.pyx
@@ -0,0 +1,66 @@
+# distutils: language = c++
+# distutils: include_dirs = ., cymoose
+# distutils: extra_compile_args = -DCYTHON
+
+cimport Id as _Id
+cimport ObjId as _ObjId 
+cimport Neutral as _Neutral
+cimport Compartment as _Compartment
+cimport Shell as _Shell
+#from libcpp.map cimport map
+from libcpp.vector cimport vector
+
+shell = PyShell()
+
+## CyMoose functions
+
+def wildcardFind(pattern):
+    cdef vector[_ObjId.ObjId] paths
+    cdef int ret = _Shell.wildcardFind(pattern, paths)
+    pypath = []
+    for p in paths:
+        obj = PyObjId()
+        obj.objId = p
+        pypath.append(obj)
+    return pypath
+
+cdef class Neutral:
+    """Neutral class """
+
+    cdef public PyId obj
+
+    def __cinit__(self, path):
+        self.obj =  shell.create("Neutral", path, 1)
+
+    def __repr__(self):
+        return "Id: {0}, Type: Neutral, Path: {1}".format(None, self.obj.path)
+
+    def path(self):
+        return self.obj.path
+
+
+cdef class Compartment:
+
+    """ Compartment class """
+
+    cdef public PyId obj
+    cdef public PyCompartment comp_ 
+
+    def __cinit__(self, path, numData = 1):
+        self.obj = shell.create("Compartment", path, numData)
+        # Wrap this id in python compartment
+        self.comp_ = PyCompartment(self.obj)
+
+    def __repr__(self):
+        return "Id: {0}, Type: Compartment, Path: {1}".format(1, self.obj.path)
+
+    def path(self):
+        return self.obj.path
+
+    property Vm:
+        def __get__(self):
+            return self.comp_.getVm(self.obj.eref_)
+
+        def __set__(self, value):
+            self.comp_.setVm(self.obj.eref_, value)
+
