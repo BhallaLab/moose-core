149baeaf1e7e503434954f7da5277a19554b83fa
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-12-28 04:19:58 +0000

    Some changes are made to basecode to get the cython port working. Addition code is put between CYMOOSE macros. Does not break anything.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@6309 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 2eb51ab1768f184f1f387c7ab659d09c92be7c22
    Former-commit-id: e5c23a57bd15332d1c92fa84de80fe58ce981e57
    Former-commit-id: b53563ff1909b6f93f6970e325d5ce84aabd8a0d

--- shell/Neutral.h
+++ shell/Neutral.h
@@ -10,6 +10,15 @@
 #ifndef _NEUTRAL_H
 #define _NEUTRAL_H
 
+
+#ifdef  CYMOOSE
+
+#include "../basecode/ProcInfo.h"
+
+class Cinfo;
+
+#endif     /* -----  CYMOOSE  ----- */
+
 class Neutral
 {
 	public:
