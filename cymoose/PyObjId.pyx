978551cfb0092c811ef650a8669cf60e26c76df1
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-12-30 06:55:55 +0000

    Connect seems to sending message to c++ core. Need to fix the type errors.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@6329 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: c006b21d7cac72730d0f86d57ddf13654fece93e

--- cymoose/PyObjId.pyx
+++ cymoose/PyObjId.pyx
@@ -14,22 +14,31 @@ from libc.stdio cimport printf
 cdef class PyObjId:
 
     cdef _ObjId.ObjId* thisptr
+
+    cdef _Id.Id* pId 
+
+    cdef PyId id_
     cdef public string path_
 
     def __repr__(self):
         msg = "<type: ObjId> at {}"
         if len(self.path_) < 1:
-            self.path_ = self.objId.path()
+            self.path_ = self.path
         return msg.format(self.path_)
 
     def __cinit__(self, id_ = None, dataIndex = 0, fieldIndex = 0):
         self.thisptr = new _ObjId.ObjId()
+        if id_:
+            self.id_ = id_
 
     property path:
         def __get__(self): 
             if len(self.path_) > 0:
                 return self.path_
             else:
-                return self.objId.path()
+                return self.id_.path
 
+    property id_:
+        def __get__(self): return self.id_ 
+        def __set__(self, id_): self.id_ = id_
 
