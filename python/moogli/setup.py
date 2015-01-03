0eaf7e80668edeb77228b582a979ff1aee575a30
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2015-01-03 11:15:07 +0000

    Added missing package_dir option in setup.py .
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@6393 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 894a2039b8066bc590b769edfd883f7c8ab676a6
    Former-commit-id: 374bcaed69bc8b33cc7a19bf46063af8c22012ec
    Former-commit-id: 262b8da1b69a2272d4c940e20b065224dff9a029

--- python/moogli/setup.py
+++ python/moogli/setup.py
@@ -42,5 +42,6 @@ setup(
         url='http://moose.ncbs.res.in/moogli',
         options={'build' : {'build_base' : '/tmp' } },
         packages=[ 'moogli'] ,
+        package_dir= { 'moogli' : '.' },
         package_data = { 'moogli' : ['_moogli.so'] },
     ) 
