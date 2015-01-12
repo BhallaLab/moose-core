845b2a5f2e0f85a155bbb4ad6bbf39b4981ae321
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2015-01-12 07:06:50 +0000

    Added the missing __init__.py file. Essential file post-installation. Also updated cmake file. Now moogli can be installed as standalone package.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/tags/gulab_jamun_3.0.1@6482 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 7bb7a22e61ea5e4b67620c9f0be389f8969c2503

---
+++ python/moogli/__init__.py
@@ -0,0 +1,16 @@
+"""__init__.py: 
+
+    Import required components from moogli.
+
+"""
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2015, Dilawar Singh and NCBS Bangalore"
+__credits__          = ["NCBS Bangalore"]
+__license__          = "GNU GPL"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+__status__           = "Development"
+
+from moogli import *
