985823c66f54b3796532b403dab32c98f8b87e4c
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2015-02-21 18:43:26 +0000

    Added checks for valid object names.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@6571 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: 9709ece58c9876a4104be9dc946433b41f6efb89
    Former-commit-id: c2f06828e48955650598ff0f9fce4343a3a087fb
    Former-commit-id: da5ebb1b2b2e93b0b2c8930d99b974416810f434

--- shell/Shell.cpp
+++ shell/Shell.cpp
@@ -223,7 +223,7 @@ Id Shell::doCreate( string type, ObjId parent, string name,
     clock_t t = clock();
 #endif
 	const Cinfo* c = Cinfo::find( type );
-	if ( name.find_first_of( "[] #?\"/\\" ) != string::npos ) {
+	if ( !isNameValid( name ) ) {
 		stringstream ss;
 		ss << "Shell::doCreate: bad character in name'" << name << 
 				"'. No Element created";
@@ -560,6 +560,15 @@ bool Shell::chopString( const string& path, vector< string >& ret,
 		ret.push_back( temp.substr( 0, pos ) );
 	}
 	return isAbsolute;
+}
+
+/**
+ * Static func to check if an object name is legal. True if legal.
+ */
+bool Shell::isNameValid( const string& name )
+{
+	return ( name.length() > 0 && 
+				name.find_first_of( "[] #?\"/\\" ) == string::npos );
 }
 
 /**
