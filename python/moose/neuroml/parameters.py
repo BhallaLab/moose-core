6554ec66be74a5669ab88e654ba0bcbab94a6c99
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2015-02-22 12:21:13 +0000

    Making `/library` into a global paramters. This should be changed only in one place.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@6574 d1858a74-f715-0410-99c9-c66fe9cce3d6

---
+++ python/moose/neuroml/parameters.py
@@ -0,0 +1,15 @@
+"""paramters.py: All global paramters are here.
+
+"""
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2015, Dilawar Singh and NCBS Bangalore"
+__credits__          = ["NCBS Bangalore"]
+__license__          = "GNU GPL"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+__status__           = "Development"
+
+libraryPath = '/library'
+modelPath = '/model'
