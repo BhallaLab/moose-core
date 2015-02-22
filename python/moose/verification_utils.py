902ea574f57b9bf89f715b27a8f9db918af0c85c
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2015-02-22 09:09:18 +0000

    Added a verification test to check synchan.Gbar is not negative or zero.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@6573 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: ce15b0e774aa4a0f9ce7175299ec30eeb3499b0b

--- python/moose/verification_utils.py
+++ python/moose/verification_utils.py
@@ -83,6 +83,13 @@ class MooseTestCase( unittest.TestCase ):
     def test_synchans(self):
         self.dump("Checking if any synapse is dead")
         for synchan in self.mooseElems.synchans:
+            if synchan.Gbar <= 0.0:
+                debug.dump("WARN"
+                        , [ synchan.path
+                        , "Gbar value is zero or negative: %s" % synchan.Gbar
+                        , "Not cool!"
+                        ]
+                        )
             # Check the output of synchan.
             if not synchan.neighbors['channel']:
                 debug.dump("FAIL"
