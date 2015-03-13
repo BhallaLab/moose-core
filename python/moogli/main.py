4c303838c20149ce49def607e42ecb58a07cbf3e
aviralg <aviralg@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2015-03-13 07:17:49 +0000

    Cylinders can now have different radii at the 2 end points.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@6637 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: ae98a77fc127f96c5563f29b36787c895bbde29c
    Former-commit-id: 2b9fc28d8af6bd5ed0014b0af01baa463b910c66
    Former-commit-id: 110004aff9ed48a93563113ff8b880487f060fdc

--- python/moogli/main.py
+++ python/moogli/main.py
@@ -68,16 +68,31 @@ def main():
 if __name__ == "__main__":
     main()
 """
-def read_morphology_from_moose(name = "", path = ""):
+
+DISTAL          = 0
+AVERAGED        = 1
+PROXIMAL_DISTAL = 2
+
+
+def read_morphology_from_moose(name = "", path = "", radius = DISTAL):
     import moose
     morphology = _moogli.Morphology(name, 1)
     compartments = moose.wildcardFind(path + "/##[ISA=CompartmentBase]")
     for compartment in compartments:
+        distal_diameter = compartment.diameter
         try:
             parent_compartment = compartment.neighbors["raxial"][0]
             proximal_diameter  = parent_compartment.diameter
         except IndexError:
-            proximal_diameter = compartment.diameter
+            proximal_diameter = distal_diameter
+
+        if   radius == DISTAL          :
+            proximal_diameter = distal_diameter
+        elif radius == AVERAGED        :
+            distal_diameter = proximal_diameter =  ( distal_diameter
+                                                   + proximal_diameter
+                                                   ) / 2.0
+
         morphology.add_compartment(     compartment.path
                                        , compartment.parent.path
                                        , compartment.x0          * 10000000
@@ -87,6 +102,6 @@ def read_morphology_from_moose(name = "", path = ""):
                                        , compartment.x           * 10000000
                                        , compartment.y           * 10000000
                                        , compartment.z           * 10000000
-                                       , compartment.diameter    * 10000000
+                                       , distal_diameter         * 10000000
                                        )
     return morphology
