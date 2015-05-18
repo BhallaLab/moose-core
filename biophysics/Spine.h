09cc2d288c7c6b4b3259d65148cafa367a02d3bf
bhalla <bhalla@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2015-05-18 04:29:24 +0000

    Added a couple of missing files Additional skeleton work on spine resizing compiled in. Fixed some unit tests that broke with changes to NeuroMesh.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@6717 d1858a74-f715-0410-99c9-c66fe9cce3d6
    
    Former-commit-id: d415756de6ef100c8bab4efa73dde4a71d435c56

---
+++ biophysics/Spine.h
@@ -0,0 +1,72 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment.
+**           Copyright (C) 2003-2015 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser General Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+
+#ifndef _SPINE_H
+#define _SPINE_H
+
+class Neuron;
+
+/**
+ * The Spine is a dendritic spine, typically with 2 compartments, the
+ * shaft and the head.
+ * It is a FieldElement, so it gets all its values from the parent
+ * Neuron.
+ * Later we can put in a maturation profile, so that a linear number
+ * from 0 to 1 will define all the stages of spine growth from stub
+ * to filopodium to mature spine.
+ */
+class Spine
+{
+	public: 
+		Spine();
+		Spine( const Neuron* parent );
+		//////////////////////////////////////////////////////////////////
+		// Field assignment stuff
+		//////////////////////////////////////////////////////////////////
+
+		Id getShaft( const Eref& e ) const;
+		Id getHead( const Eref& e ) const;
+
+		void setShaftLength( const Eref& e, double len );
+		double getShaftLength( const Eref& e ) const;
+		void setShaftDiameter( const Eref& e, double dia );
+		double getShaftDiameter( const Eref& e ) const;
+		void setHeadLength( const Eref& e, double len );
+		double getHeadLength( const Eref& e ) const;
+		void setHeadDiameter( const Eref& e, double dia );
+		double getHeadDiameter( const Eref& e ) const;
+
+		void setTotalLength( const Eref& e, double len );
+		double getTotalLength( const Eref& e ) const;
+
+		// rotate around dend, but still at right angles to it.
+		void setAngle( const Eref& e, double theta ); 
+		double getAngle( const Eref& e ) const;
+
+		// Incline to dend, radians. Default is normal to dend and is 0.
+		void setInclination( const Eref& e, double phi ); 
+		double getInclination( const Eref& e ) const;
+
+		// Assign to specific vector. Length of vector does size scaling.
+		//void setVectorDirection( const Eref& e, vector< double > dir ); 
+		//vector< double > getVectorDirection( const Eref& e ) const;
+
+		//////////////////////////////////////////////////////////////////
+		// Dest funcs
+		//////////////////////////////////////////////////////////////////
+		//////////////////////////////////////////////////////////////////
+		// Lookup funcs for Boundary
+		//////////////////////////////////////////////////////////////////
+
+		static const Cinfo* initCinfo();
+	private:
+		const Neuron* parent_;
+};
+
+#endif	// _SPINE_H
