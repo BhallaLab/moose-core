5af6c04a5ad8d4a2b480b34b71cec9d58fb72eb3
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2015-07-01 06:30:25 +0000

    NSDFWriter works for Uniform data.
    git-svn-id: https://svn.code.sf.net/p/moose/code/moose/trunk@6904 d1858a74-f715-0410-99c9-c66fe9cce3d6

--- builtins/HDF5WriterBase.h
+++ builtins/HDF5WriterBase.h
@@ -36,7 +36,6 @@
 hid_t require_attribute(hid_t file_id, string path,
                         hid_t data_type, hid_t data_id);
 
-hid_t createDataset2D(hid_t parent, string name, unsigned int rows);
 
 class HDF5WriterBase
 {
@@ -77,6 +76,7 @@ class HDF5WriterBase
     
   protected:
     herr_t openFile();
+    hid_t createDataset2D(hid_t parent, string name, unsigned int rows);
     
     /// map from element path to nodes in hdf5file.  Multiple MOOSE
     /// tables can be written to the single file corresponding to a
