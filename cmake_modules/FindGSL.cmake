29250093e06de6647f69fd467ea3436fee81d333
virtualuser <virtualuser@ubuntu-12.04>
2015-07-05 14:39:54 +0530

    Fixed. If GSL_HOME is set, get the gsl-prefix from there, else search the system paths.
    Former-commit-id: 38da1ae1614d5278dbbec3456f15167432f59e26

--- cmake_modules/FindGSL.cmake
+++ cmake_modules/FindGSL.cmake
@@ -49,14 +49,23 @@ IF(WIN32)
 ELSE(WIN32)
   IF(UNIX) 
     SET(GSL_CONFIG_PREFER_PATH "$ENV{GSL_HOME}/bin")
-    FIND_PROGRAM(GSL_CONFIG gsl-config
-      ${GSL_CONFIG_PREFER_PATH}
-      $ENV{LIB_DIR}/bin
-      /usr/local/bin/
-      /usr/bin/
-      )
+    MESSAGE("++ DEBUG: GSL_CONFIG_PREFER_PATH: ${GSL_CONFIG_PREFER_PATH}")
+     
+    # GSL_CONFIG must be cleared. This script may be called again after installing
+    # the proper version of gsl
+    unset(GSL_CONFIG CACHE)
+    set(GSL_CONFIG "GSL_CONFIG-NOTFOUND")
+    if($ENV{GSL_HOME})
+	set(GSL_CONFIG "$ENV{GSL_HOME}/bin/gsl-config")
+    else()
+	FIND_PROGRAM(GSL_CONFIG gsl-config
+			${GSL_CONFIG_PREFER_PATH}
+			$ENV{LIB_DIR}/bin
+			/usr/local/bin/
+			/usr/bin/
+		    )
+    endif()
     MESSAGE("DBG GSL_CONFIG ${GSL_CONFIG}")
-    
     IF (GSL_CONFIG) 
       # set CXXFLAGS to be fed into CXX_FLAGS by the user:
       SET(GSL_CXX_FLAGS "`${GSL_CONFIG} --cflags`")
