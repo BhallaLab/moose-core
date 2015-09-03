0c3ba0f02135c0873bd75f38278aa4334bd6e2e0
Dilawar Singh <dilawars@ncbs.res.in>
2015-09-03 11:09:51 +0530

    Fixing permission issues on logger. Creating a tempfile for now. Remove the logger and change the statements later.

--- python/moose/neuroml/__init__.py
+++ python/moose/neuroml/__init__.py
@@ -2,12 +2,14 @@ from .NeuroML import NeuroML, loadNeuroML_L123
 from .NetworkML import NetworkML
 from .MorphML import MorphML
 from .ChannelML import ChannelML
+
+import tempfile 
 import logging
+
 logging.basicConfig(level=logging.DEBUG,
     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
     datefmt='%m-%d %H:%M',
-    filename='/tmp/moose.log',
-    filemode='w'
+    filename='__moose.nml__.log'
     )
 
 # define a Handler which writes INFO messages or higher to the sys.stderr
@@ -18,5 +20,3 @@ console.setFormatter(formatter)
 logging.getLogger('moose.nml').addHandler(console)
 _logger = logging.getLogger('moose.nml')
 _logger.debug("Loading NML library")
-
-
