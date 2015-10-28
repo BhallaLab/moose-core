bbdb12f3e53ab11501f51aa821a77088c1b61ad3
Dilawar Singh <dilawars@ncbs.res.in>
2015-10-28 16:59:51 +0530

    Added executable moose script.

--- scripts/build_macosx_bundle.sh
+++ scripts/build_macosx_bundle.sh
@@ -2,7 +2,11 @@
 mkdir -p _build 
 (
     cd _build
-    cmake -DCMAKE_INSTALL_PREFIX=_install ..
+    rm -f moose*.dmg
+    rm -rf moose-3.0.1-Linux*
+    cmake -DCMAKE_INSTALL_PREFIX=/Moose.app ..
     make -j4
-    cpack -G Bundle 
+    #cpack -G Bundle 
+    cpack -G DragNDrop -V
+    7z x moose-3.0.1-Linux-.dmg
 )
