9afa3d47a5cf1f77be0a88581d5d460b34a06436
Dilawar Singh <dilawars@ncbs.res.in>
2016-03-27 22:11:12 +0530

    Added simple benchmark for random number generation.

--- benchmarks/benchmark_random_numbers.cpp
+++ benchmarks/benchmark_random_numbers.cpp
@@ -15,11 +15,27 @@
 
 
 #include <iostream>
+#include <vector>
+#include <random>
+#include <ctime>
+#include "../randnum/randnum.h"
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+    vector<double> randVec( 500000000 );
     cerr << "here we are" << endl;
+    clock_t startT = clock();
+    for( unsigned int i = 0; i < 500000000; ++i )
+        randVec[i] = mtrand();
+    cerr << "Time taken (MOOSE) " << clock() - startT << endl;
+
+    // Using c++ library
+    mt19937 rng( 0 );
+    startT = clock();
+    for( unsigned int i = 0; i < 500000000; ++i )
+        randVec[i] = mtrand();
+    cerr << "Time taken (STL) " << clock() - startT << endl;
     
     return 0;
 }
