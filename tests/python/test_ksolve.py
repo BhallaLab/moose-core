4f255a55b4f1b9dca2782cb9720074aab2f85a01
Dilawar Singh <dilawars@ncbs.res.in>
2016-04-10 21:02:45 +0530

    Added a test script for ksolve-boost debugging purpose.

---
+++ tests/python/test_ksolve.py
@@ -0,0 +1,25 @@
+import moose
+
+compt = moose.CubeMesh( '/compt' )
+compt.volume = 1e-20
+
+a = moose.Pool( '/compt/a')
+a.concInit = 1.0
+b = moose.Pool( '/compt/b' )
+b.concInit = 0.6
+r = moose.Reac( '/compt/reac' )
+r.Kf = 1.0
+r.Kb = 0.2
+
+moose.connect( r, 'sub', a, 'reac' )
+moose.connect( r, 'prd', b, 'reac' )
+
+stoich = moose.Stoich( '/compt/stoich' )
+ksolve = moose.Ksolve( '/compt/ksolve' )
+stoich.ksolve = ksolve 
+stoich.compartment = compt
+stoich.path = '/compt/##'
+
+moose.reinit()
+moose.start( 1.0 )
+
