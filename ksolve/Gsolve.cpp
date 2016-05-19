625e4ff495d7af07cd011e19ba9f2dc7bd399650
Rahul <rahulgayatri84@gmail.com>
2016-05-19 12:05:15 +0530

    Merging with rahul/rboost

--- ksolve/Gsolve.cpp
+++ ksolve/Gsolve.cpp
@@ -402,9 +402,9 @@ void Gsolve::process( const Eref& e, ProcPtr p )
 	}
 	// Fifth, update the mol #s.
 	// First we advance the simulation.
-	for ( vector< GssaVoxelPools >::iterator 
-					i = pools_.begin(); i != pools_.end(); ++i ) {
-		i->advance( p, &sys_ );
+	for ( vector< GssaVoxelPools >::iterator i = pools_.begin(); i != pools_.end(); ++i ) 
+   {
+           i->advance( p, &sys_ );
 	}
 	if ( useClockedUpdate_ ) { // Check if a clocked stim is to be updated
 		for ( vector< GssaVoxelPools >::iterator 
