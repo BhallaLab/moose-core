b943072e1ef247f87931aad5b43e77e912e1da95
HarshaRani <ranishashi@gmail.com>
2016-07-05 12:41:49 +0530

    added networkx's Graph for layout the co-ordinates when model is save to SBML at commandline, if model saved at GUI level then pyqt's scenepos which is used for layout in GUI, those co-ordinates are used

--- python/moose/SBML/writeSBML.py
+++ python/moose/SBML/writeSBML.py
@@ -23,6 +23,8 @@ from libsbml import *
 import re
 from collections import Counter
 #from moose import wildcardFind, element, loadModel, ChemCompt, exists, Annotator, Pool, ZombiePool,PoolBase,CplxEnzBase,Function,ZombieFunction
+import networkx as nx
+#import matplotlib.pyplot as plt
 
 #ToDo:
 #	Table should be written
@@ -30,7 +32,6 @@ from collections import Counter
 #   x and y cordinates shd be added if exist
 #	boundary condition for buffer pool having assignment statment constant shd be false
 def mooseWriteSBML(modelpath,filename,sceneitems=None):
-	print ' write ',sceneitems
 	sbmlDoc = SBMLDocument(3, 1)
 	filepath,filenameExt = os.path.split(filename)
 	if filenameExt.find('.') != -1:
@@ -55,7 +56,13 @@ def mooseWriteSBML(modelpath,filename,sceneitems=None):
 	cremodel_.setTimeUnits("second")
 	cremodel_.setExtentUnits("substance")
 	cremodel_.setSubstanceUnits("substance")
-	
+
+	if sceneitems == None:
+		srcdesConnection = {}
+        setupItem(modelpath,srcdesConnection)
+        meshEntry = setupMeshObj(modelpath)
+        cmin,cmax,sceneitems = autoCoordinates(meshEntry,srcdesConnection)
+
 	writeUnits(cremodel_)
 	modelAnno = writeSimulationAnnotation(modelpath)
 	if modelAnno:
@@ -151,8 +158,6 @@ def writeEnz(modelpath,cremodel_,sceneitems):
 				#rec_order = rec_order + noofSub
 				rec_order = noofSub
 				rate_law = rate_law +"*"+sRateLaw
-				
-		   
 
 			enzPrd = enz.neighbors["cplxDest"]
 			if not enzPrd:
@@ -737,7 +742,174 @@ def validateModel( sbmlDoc ):
 		print validationMessages;
 	return ( numConsistencyErrors == 0 and numValidationErrors == 0)
 	#return ( numConsistencyErrors == 0 and numValidationErrors == 0, consistencyMessages)
+def setupItem(modelPath,cntDict):
+    '''This function collects information of what is connected to what. \
+    eg. substrate and product connectivity to reaction's and enzyme's \
+    sumtotal connectivity to its pool are collected '''
+    #print " setupItem"
+    sublist = []
+    prdlist = []
+    zombieType = ['ReacBase','EnzBase','Function','StimulusTable']
+    for baseObj in zombieType:
+        path = '/##[ISA='+baseObj+']'
+        if modelPath != '/':
+            path = modelPath+path
+        if ( (baseObj == 'ReacBase') or (baseObj == 'EnzBase')):
+            for items in wildcardFind(path):
+                sublist = []
+                prdlist = []
+                uniqItem,countuniqItem = countitems(items,'subOut')
+                subNo = uniqItem
+                for sub in uniqItem: 
+                    sublist.append((element(sub),'s',countuniqItem[sub]))
 
+                uniqItem,countuniqItem = countitems(items,'prd')
+                prdNo = uniqItem
+                if (len(subNo) == 0 or len(prdNo) == 0):
+                    print "Substrate Product is empty ",path, " ",items
+                    
+                for prd in uniqItem:
+                    prdlist.append((element(prd),'p',countuniqItem[prd]))
+                
+                if (baseObj == 'CplxEnzBase') :
+                    uniqItem,countuniqItem = countitems(items,'toEnz')
+                    for enzpar in uniqItem:
+                        sublist.append((element(enzpar),'t',countuniqItem[enzpar]))
+                    
+                    uniqItem,countuniqItem = countitems(items,'cplxDest')
+                    for cplx in uniqItem:
+                        prdlist.append((element(cplx),'cplx',countuniqItem[cplx]))
+
+                if (baseObj == 'EnzBase'):
+                    uniqItem,countuniqItem = countitems(items,'enzDest')
+                    for enzpar in uniqItem:
+                        sublist.append((element(enzpar),'t',countuniqItem[enzpar]))
+                cntDict[items] = sublist,prdlist
+        elif baseObj == 'Function':
+            for items in wildcardFind(path):
+                sublist = []
+                prdlist = []
+                item = items.path+'/x[0]'
+                uniqItem,countuniqItem = countitems(item,'input')
+                for funcpar in uniqItem:
+                    sublist.append((element(funcpar),'sts',countuniqItem[funcpar]))
+                
+                uniqItem,countuniqItem = countitems(items,'valueOut')
+                for funcpar in uniqItem:
+                    prdlist.append((element(funcpar),'stp',countuniqItem[funcpar]))
+                cntDict[items] = sublist,prdlist
+        else:
+            for tab in wildcardFind(path):
+                tablist = []
+                uniqItem,countuniqItem = countitems(tab,'output')
+                for tabconnect in uniqItem:
+                    tablist.append((element(tabconnect),'tab',countuniqItem[tabconnect]))
+                cntDict[tab] = tablist
+def countitems(mitems,objtype):
+    items = []
+    #print "mitems in countitems ",mitems,objtype
+    items = element(mitems).neighbors[objtype]
+    uniqItems = set(items)
+    countuniqItems = Counter(items)
+    return(uniqItems,countuniqItems)
+
+def setupMeshObj(modelRoot):
+    ''' Setup compartment and its members pool,reaction,enz cplx under self.meshEntry dictionaries \ 
+    self.meshEntry with "key" as compartment, 
+    value is key2:list where key2 represents moose object type,list of objects of a perticular type
+    e.g self.meshEntry[meshEnt] = { 'reaction': reaction_list,'enzyme':enzyme_list,'pool':poollist,'cplx': cplxlist }
+    '''
+    meshEntry = {}
+    if meshEntry:
+        meshEntry.clear()
+    else:
+        meshEntry = {}
+    meshEntryWildcard = '/##[ISA=ChemCompt]'
+    if modelRoot != '/':
+        meshEntryWildcard = modelRoot+meshEntryWildcard
+    for meshEnt in wildcardFind(meshEntryWildcard):
+        mollist = []
+        cplxlist = []
+        mol_cpl  = wildcardFind(meshEnt.path+'/##[ISA=PoolBase]')
+        funclist = wildcardFind(meshEnt.path+'/##[ISA=Function]')
+        enzlist  = wildcardFind(meshEnt.path+'/##[ISA=EnzBase]')
+        realist  = wildcardFind(meshEnt.path+'/##[ISA=ReacBase]')
+        tablist  = wildcardFind(meshEnt.path+'/##[ISA=StimulusTable]')
+        if mol_cpl or funclist or enzlist or realist or tablist:
+            for m in mol_cpl:
+                if isinstance(element(m.parent),CplxEnzBase):
+                    cplxlist.append(m)
+                elif isinstance(element(m),PoolBase):
+                    mollist.append(m)
+                    
+            meshEntry[meshEnt] = {'enzyme':enzlist,
+                                  'reaction':realist,
+                                  'pool':mollist,
+                                  'cplx':cplxlist,
+                                  'table':tablist,
+                                  'function':funclist
+                                  }
+    return(meshEntry)
+def autoCoordinates(meshEntry,srcdesConnection):
+    
+    G = nx.Graph()
+    for cmpt,memb in meshEntry.items():
+        for enzObj in find_index(memb,'enzyme'):
+            G.add_node(enzObj.path)
+    for cmpt,memb in meshEntry.items():
+        for poolObj in find_index(memb,'pool'):
+            G.add_node(poolObj.path)
+        for cplxObj in find_index(memb,'cplx'):
+            G.add_node(cplxObj.path)
+            G.add_edge((cplxObj.parent).path,cplxObj.path)
+        for reaObj in find_index(memb,'reaction'):
+            G.add_node(reaObj.path)
+        
+    for inn,out in srcdesConnection.items():
+        if (inn.className =='ZombieReac'): arrowcolor = 'green'
+        elif(inn.className =='ZombieEnz'): arrowcolor = 'red'
+        else: arrowcolor = 'blue'
+        if isinstance(out,tuple):
+            if len(out[0])== 0:
+                print inn.className + ':' +inn.name + "  doesn't have input message"
+            else:
+                for items in (items for items in out[0] ):
+                    G.add_edge(element(items[0]).path,inn.path)
+            if len(out[1]) == 0:
+                print inn.className + ':' + inn.name + "doesn't have output mssg"
+            else:
+                for items in (items for items in out[1] ):
+                    G.add_edge(inn.path,element(items[0]).path)
+        elif isinstance(out,list):
+            if len(out) == 0:
+                print "Func pool doesn't have sumtotal"
+            else:
+                for items in (items for items in out ):
+                    G.add_edge(element(items[0]).path,inn.path)
+    
+    nx.draw(G,pos=nx.spring_layout(G))
+
+    #plt.savefig('/home/harsha/Trash/netwrokXtest.png')
+    
+    position = nx.spring_layout(G)
+    sceneitems = {}
+    xycord = []
+
+    for key,value in position.items():
+        xycord.append(value[0])
+        xycord.append(value[1])
+        sceneitems[element(key)] = {'x':value[0],'y':value[1]}
+    cmin = min(xycord)
+    cmax = max(xycord)
+    
+    return cmin,cmax,sceneitems
+
+def find_index(value, key):
+    """ Value.get(key) to avoid expection which would raise if empty value in dictionary for a given key """
+    if value.get(key) != None:
+        return value.get(key)
+    else:
+        raise ValueError('no dict with the key found')
 if __name__ == "__main__":
 
 	filepath = sys.argv[1]
