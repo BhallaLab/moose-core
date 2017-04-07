985c42600e6bcfe9e5f5de872c9c515208a3ec83
Dilawar Singh <dilawars@ncbs.res.in>
2017-04-07 18:06:30 +0530

    Fixes for python3.

--- python/rdesigneur/rdesigneur.py
+++ python/rdesigneur/rdesigneur.py
@@ -198,7 +198,7 @@ class rdesigneur:
             self._configureSolvers()
             self.buildAdaptors()
             self._buildPlots()
-#self._buildMoogli()
+            self._buildMoogli()
             self._buildStims()
             self._configureClocks()
             self._printModelStats()
