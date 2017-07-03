ad2c63da12abec490709d83655585c7062612b14
Dilawar Singh <dilawars@ncbs.res.in>
2017-07-03 10:42:02 +0530

    Use QUIET_MODE=ON on travis.

--- .travis/travis_build_osx.sh
+++ .travis/travis_build_osx.sh
@@ -21,7 +21,7 @@ set -o nounset                              # Treat unset variables as an error
 set -e
 
 (
-    mkdir -p _GSL_BUILD && cd _GSL_BUILD && cmake -DDEBUG=ON -DPYTHON_EXECUTABLE=`which python` ..
+    mkdir -p _GSL_BUILD && cd _GSL_BUILD && cmake -DQUIET_MODE=ON -DDEBUG=ON -DPYTHON_EXECUTABLE=`which python` ..
     make && ctest --output-on-failure
     sudo make install
     cd .. # Now with boost.
