1e7ebbe3c1d4104ec54d58922d63257c03f2defd
HarshaRani <ranishashi@gmail.com>
2017-08-16 15:00:05 +0530

    unwanted statements

--- python/moose/genesis/writeKkit.py
+++ python/moose/genesis/writeKkit.py
@@ -590,14 +590,12 @@ def getColorCheck(color,GENESIS_COLOR_SEQUENCE):
         raise Exception("Invalid Color Value!")
 
 def getRandColor():
-    print " getColorCheck "
     ignoreColor= ["mistyrose","antiquewhite","aliceblue","azure","bisque","black","blanchedalmond","blue","cornsilk","darkolivegreen","darkslategray","dimgray","floralwhite","gainsboro","ghostwhite","honeydew","ivory","lavender","lavenderblush","lemonchiffon","lightcyan","lightgoldenrodyellow","lightgray","lightyellow","linen","mediumblue","mintcream","navy","oldlace","papayawhip","saddlebrown","seashell","snow","wheat","white","whitesmoke","aquamarine","lightsalmon","moccasin","limegreen","snow","sienna","beige","dimgrey","lightsage"]
     matplotcolor = {}
     for name,hexno in matplotlib.colors.cnames.items():
         matplotcolor[name]=hexno
 
     k = random.choice(list(matplotcolor.keys()))
-    print " k ",k
     if k in ignoreColor:
         return getRandColor()
     else:
