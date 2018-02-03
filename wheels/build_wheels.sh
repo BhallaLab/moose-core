c353ca606389ec3b4f5e3037f98ac008fddfaafc
Dilawar Singh <dilawar.s.rajput@gmail.com>
2018-02-03 06:26:23 +0000

    Added dependecies.

--- wheels/build_wheels.sh
+++ wheels/build_wheels.sh
@@ -21,20 +21,21 @@ mkdir -p $WHEELHOUSE
 for PYDIR in /opt/python/cp27-cp27m/ /opt/python/cp34-cp34m/ /opt/python/cp36-cp36m/; do
     PYVER=$(basename $PYDIR)
     mkdir -p $PYVER
-    cd $PYVER
-    echo "Building using $PYDIR in $PYVER"
-    PYTHON=$(ls $PYDIR/bin/python?.?)
-    $PYTHON -m pip install numpy
-    $CMAKE -DPYTHON_EXECUTABLE=$PYTHON  \
-        -DGSL_STATIC_LIBRARIES=$GSL_STATIC_LIBS \
-        -DMOOSE_VERSION="3.2rc1" ${MOOSE_SOURCE_DIR}
-    make -j4
-    
-    # Now build bdist_wheel
-    cd python
-    cp setup.cmake.py setup.py
-    $PYDIR/bin/pip wheel . -w $WHEELHOUSE
-    cd $SCRIPT_DIR
+    (
+        cd $PYVER
+        echo "Building using $PYDIR in $PYVER"
+        PYTHON=$(ls $PYDIR/bin/python?.?)
+        $PYTHON -m pip install numpy
+        $CMAKE -DPYTHON_EXECUTABLE=$PYTHON  \
+            -DGSL_STATIC_LIBRARIES=$GSL_STATIC_LIBS \
+            -DMOOSE_VERSION="3.2rc1" ${MOOSE_SOURCE_DIR}
+        make -j4
+        
+        # Now build bdist_wheel
+        cd python
+        cp setup.cmake.py setup.py
+        $PYDIR/bin/pip wheel . -w $WHEELHOUSE
+    )
 done
 
 # now check the wheels.
