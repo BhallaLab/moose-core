c21d002ef1cdd2138654c4397695228215a26e80
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-02 17:46:15 +0530

    Added Makefile to build wheel for pymoose.

---
+++ scripts/build_wheels/README.md
@@ -0,0 +1,6 @@
+Run  'make' to build wheel.
+
+# Dependencies
+
+- docker
+-  And thats all
--- scripts/build_wheels/build_wheels.sh
+++ scripts/build_wheels/build_wheels.sh
@@ -10,7 +10,7 @@ set -x
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-MOOSE_SOURCE_DIR=${SCRIPT_DIR}/..
+MOOSE_SOURCE_DIR=/tmp/moose
 
 # Clone git or update.
 if [ ! -d $MOOSE_SOURCE_DIR ]; then
