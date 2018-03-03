53213c8d379235f07e1f31da58850101de110e48
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-03 17:33:59 +0530

    merged travis scripts from master.

--- .travis/travis_build_osx.sh
+++ .travis/travis_build_osx.sh
@@ -1,12 +1,12 @@
-#!/bin/bash - 
+#!/bin/bash -
 #===============================================================================
 #
 #          FILE: travis_build_linux.sh
-# 
-#         USAGE: ./travis_build_linux.sh 
-# 
+#
+#         USAGE: ./travis_build_linux.sh
+#
 #   DESCRIPTION:  Build  on linux environment.
-# 
+#
 #       OPTIONS: ---
 #  REQUIREMENTS: ---
 #          BUGS: ---
@@ -21,10 +21,19 @@ set -o nounset                              # Treat unset variables as an error
 set -e
 
 (
-    mkdir -p _GSL_BUILD && cd _GSL_BUILD && cmake -DDEBUG=ON -DPYTHON_EXECUTABLE=`which python` ..
+    mkdir -p _GSL_BUILD && cd _GSL_BUILD \
+        && cmake -DQUIET_MODE=ON -DDEBUG=ON \
+        -DPYTHON_EXECUTABLE=`which python` ..
     make && ctest --output-on-failure
     cd .. # Now with boost.
-    mkdir -p _BOOST_BUILD && cd _BOOST_BUILD && cmake -DWITH_BOOST=ON -DDEBUG=ON -DPYTHON_EXECUTABLE=`which python` ..
+    mkdir -p _BOOST_BUILD && cd _BOOST_BUILD \
+        && cmake -DWITH_BOOST=ON -DDEBUG=ON \
+        -DPYTHON_EXECUTABLE=`which python` ..
     make && ctest --output-on-failure
-    cd .. 
+    cd ..
+
+    # Now test the brew formula
+    cd ~
+    brew tap BhallaLab/moose
+    brew install moose
 )
--- .travis/travis_prepare_osx.sh
+++ .travis/travis_prepare_osx.sh
@@ -1,12 +1,12 @@
-#!/bin/bash - 
+#!/bin/bash -
 #===============================================================================
 #
 #          FILE: travis_prepare_osx.sh
-# 
-#         USAGE: ./travis_prepare_osx.sh 
-# 
+#
+#         USAGE: ./travis_prepare_osx.sh
+#
 #   DESCRIPTION: Script to prepare OSX build on Travis CI.
-# 
+#
 #       OPTIONS: ---
 #  REQUIREMENTS: ---
 #          BUGS: ---
@@ -19,16 +19,21 @@
 
 set -o nounset                              # Treat unset variables as an error
 set +e
-brew update
+#rvm get head
+#brew update
 #brew outdated cmake || brew install cmake
 brew install gsl
-brew outdated hdf5 || brew install homebrew/science/hdf5
-brew outdated libsbml || brew install homebrew/science/libsbml
+brew install hdf5
+brew install python
+brew install numpy
 #brew outdated python || brew install python
 #brew outdated numpy || brew install homebrew/python/numpy
-#brew unlink numpy && brew link numpy || echo "Failed to link numpy"
+brew unlink numpy && brew link numpy || echo "Failed to link numpy"
 # Numpy caveats
 mkdir -p $HOME/Library/Python/2.7/lib/python/site-packages
 echo 'import sys; sys.path.insert(1, "/usr/local/lib/python2.7/site-packages")' >> $HOME/Library/Python/2.7/lib/python/site-packages/homebrew.pth
-pip install matplotlib
-
+# ensurepip
+#python -m ensurepip
+pip2 install matplotlib --user
+pip2 install pyNeuroML libNeuroML --user
+pip2 install scipy --user
