9fe69ca3c22c7cf66054197ffa07d2ec0b178483
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-05 14:21:38 +0530

    updated version in wheel file.

--- python/setup.cmake.py
+++ python/setup.cmake.py
@@ -1,43 +1,51 @@
 # -*- coding: utf-8 -*-
-# This file is part of MOOSE simulator: http://moose.ncbs.res.in.
 
-# MOOSE is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+"""setup.py:
+Script to install python targets.
 
-# MOOSE is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with MOOSE.  If not, see <http://www.gnu.org/licenses/>.
+NOTE: This script is to be called by CMake. Not intended to be used standalone.
+
+"""
 
 __author__           = "Dilawar Singh"
-__copyright__        = "Copyright 2013-17, Dilawar Singh"
+__copyright__        = "Copyright 2013, Dilawar Singh and NCBS Bangalore"
 __credits__          = ["NCBS Bangalore"]
-__license__          = "GNU GPLv3"
+__license__          = "GNU GPL"
+__version__          = "1.0.0"
 __maintainer__       = "Dilawar Singh"
 __email__            = "dilawars@ncbs.res.in"
+__status__           = "Development"
 
 import os
-from distutils.core import setup
+import sys
+
+try:
+	from setuptools import setup
+except Exception as e:
+	from distutils.core import setup
 
 script_dir = os.path.dirname( os.path.abspath( __file__ ) )
+version = '3.1.3'
 
-version = '3.1.2'
+try:
+    with open( os.path.join( script_dir, 'VERSION'), 'r' ) as f:
+        version = f.read( )
+except Exception as e:
+    print( 'Failed to read VERSION %s' % e )
+    print( 'Using default %s' % version )
 
 try:
     import importlib.machinery
-    suffix = importlib.machinery.EXTENSION_SUFFIXES[-1]
+    suffix = importlib.machinery.EXTENSION_SUFFIXES[0]
 except Exception as e:
+    print( '[WARN] Failed to determine importlib suffix' )
     suffix = '.so'
 
 setup(
-        name='moose',
+        name='pymoose',
         version=version,
-        description='MOOSE python scripting module.',
-        author='See AUTHORS file',
+        description='Python scripting interface of MOOSE Simulator (https://moose.ncbs.res.in)',
+        author='MOOSERes',
         author_email='bhalla@ncbs.res.in',
         maintainer='Dilawar Singh',
         maintainer_email='dilawars@ncbs.res.in',
@@ -49,10 +57,11 @@ setup(
             , 'moose.neuroml'
             , 'moose.genesis'
             , 'moose.chemUtil'
+            , 'moose.chemMerge'
             ],
+	install_requires = [ 'python-libsbml', 'numpy' ],
         package_dir = {
-            'moose' : 'moose'
-            , 'rdesigneur' : 'rdesigneur'
+            'moose' : 'moose', 'rdesigneur' : 'rdesigneur'
             },
-        package_data = { 'moose' : ['_moose' + suffix] },
+        package_data = { 'moose' : ['_moose' + suffix ] },
     )
