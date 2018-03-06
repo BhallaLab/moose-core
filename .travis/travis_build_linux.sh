162ca4744efacf63847491aace127daca1dc75f7
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-06 15:02:26 +0530

    Passed all test on ubuntu-16.04. If this fails on Travis, its travis

--- .travis/travis_build_linux.sh
+++ .travis/travis_build_linux.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 #===============================================================================
 #
 #          FILE: travis_build_linux.sh
@@ -17,47 +18,57 @@
 #      REVISION:  ---
 #===============================================================================
 
-set -e
+set -e -x
 
 PYTHON2="/usr/bin/python2"
 PYTHON3="/usr/bin/python3"
 MAKEFLAGS="-j`nproc`"
 
-if [ -z MAKE ]; then
-    MAKE=/usr/bin/make
+if [ ! -n "$MAKE" ]; then
+    MAKE="make -j`nproc`"
+else
+    MAKE="$MAKE -j`nproc`"
 fi
+
+unset PYTHONPATH
 
 # Bug: `which python` returns /opt/bin/python* etc on travis. For which numpy
 # many not be available. Therefore, it is neccessary to use fixed path for
 # python executable.
 
-(
-    $PYTHON2 -m compileall -q .
-    if type $PYTHON3 > /dev/null; then $PYTHON3 -m compileall -q . ; fi
+$PYTHON2 -m compileall -q .
+if type $PYTHON3 > /dev/null; then $PYTHON3 -m compileall -q . ; fi
 
-    mkdir -p _GSL_BUILD && cd _GSL_BUILD && \
-        cmake -DDEBUG=ON -DPYTHON_EXECUTABLE="$PYTHON2" ..
+echo "Currently in `pwd`"
+(
+    mkdir -p _GSL_BUILD && cd _GSL_BUILD 
+    cmake -DDEBUG=ON -DPYTHON_EXECUTABLE="$PYTHON2" ..
     $MAKE && ctest --output-on-failure
-    cd .. # Now with boost.
+)
+
+(
+    # Now with boost.
     mkdir -p _BOOST_BUILD && cd _BOOST_BUILD && \
         cmake -DWITH_BOOST=ON -DDEBUG=ON -DQUIET_MODE=ON -DPYTHON_EXECUTABLE="$PYTHON2" ..
     $MAKE && ctest --output-on-failure
-    cd ..
+)
 
-    # This is only applicable on linux build.
-    echo "Python3: Removed python2-networkx and install python3"
-    if type $PYTHON3 > /dev/null; then
-        sudo apt-get remove -qq python-networkx
-        sudo apt-get install -qq python3-networkx
+# This is only applicable on linux build.
+echo "Python3: Removed python2-networkx and install python3"
+if type $PYTHON3 > /dev/null; then
+    sudo apt-get remove -qq python-networkx
+    sudo apt-get install -qq python3-networkx
+    (
         mkdir -p _GSL_BUILD2 && cd _GSL_BUILD2 && \
             cmake -DDEBUG=ON -DPYTHON_EXECUTABLE="$PYTHON3" ..
         $MAKE && ctest --output-on-failure
-        cd .. # Now with BOOST and python3
+    )
+    (
         mkdir -p _BOOST_BUILD2 && cd _BOOST_BUILD2 && \
             cmake -DWITH_BOOST=ON -DDEBUG=ON -DPYTHON_EXECUTABLE="$PYTHON3" ..
         $MAKE && ctest --output-on-failure
-        cd .. && echo "All done"
-    else
-        echo "Python3 is not found. Build disabled"
-    fi
-)
+    )
+    echo "All done"
+else
+    echo "Python3 is not found. Build disabled"
+fi
