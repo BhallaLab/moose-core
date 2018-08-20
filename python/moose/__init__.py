a274786dffa2536330acedaa6b251121293ce93a
Dilawar Singh <dilawars@ncbs.res.in>
2018-08-20 10:06:48 +0530

    Try to load matplotlib backend only if matplotlib has not been loaded by the user yet.

--- python/moose/__init__.py
+++ python/moose/__init__.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
+import sys
 
 # Bring moose.py functions into global namespace.
 # Try to set the backend. This must be called before any instance of matplotlib
 # is imported i.e. import 'moose' after this.
-import matplotlib
-try:
-    matplotlib.use( 'TkAgg' )
-except Exception as e:
+if 'matplotlib' not in sys.modules:
+    import matplotlib
     try:
-        matplotlib.use( 'Qt5Agg')
+        matplotlib.use( 'TkAgg' )
     except Exception as e:
         try:
-            matplotlib.use( 'Qt4Agg')
+            matplotlib.use( 'Qt5Agg')
         except Exception as e:
-            pass
+            try:
+                matplotlib.use( 'Qt4Agg')
+            except Exception as e:
+                pass
 
 from moose.moose import *
 
