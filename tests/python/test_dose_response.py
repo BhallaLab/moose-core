BhallaLab/moose-core

commit d229eba6bbc957c97bac44d24d513543956942c1

Parallel solvers (#293)
* Merge bhallalab.

* Using std::copysign to get the sign of a number.

* DO NOT use round() method to get to the nearest integer. It violates
mass-conservation in small system. The probabilistic algorithm is better and
does not cause extra molecules in system.

* Force disable round() method. Fixed error message in plotutil import failure.

* Disable rounding off in reinit function.
1.2 1.2 ... 1.2
will be converted to 1.0 1.0 ..2.0... 1.0 etc. Both sequences will have
different sum sometime and will cause mass-conservation error.

* Fixed error in utility function.

* Problem is unresolvable.

* removed test logger.

* Merged upstraems and tweaks to cmake policy. Enforcing NEW by default.

* Absolute path in header.

* Show extra molecule warning when number is significant.

* Fixes to allow tests to pass under Fedora 29 (#282)

* Do not try to access position -1 in string

This ain't python ;(

* Avoid open-coded strdup that cause compilation failure with new gcc

/builddir/build/BUILD/moose-3.1.3/moose-core/pymoose/moosemodule.cpp:2724:32: error: invalid conversion from 'const char*' to 'char*' [-fpermissive]
         strncpy(vec[currIndex].name,

* Avoid open-coded strdup that causes a warning

/home/zbyszek/python/moose/moose-core/pymoose/mfield.cpp: In function 'int moose_Field_init(_Field*, PyObject*, PyObject*)':
/home/zbyszek/python/moose/moose-core/pymoose/mfield.cpp:122:12: warning: 'char* strncpy(char*, const char*, size_t)' output truncated before terminating nul copying as many bytes from a string as its length [-Wstringop-truncation]
     strncpy(name, fieldName, size);
     ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~
/home/zbyszek/python/moose/moose-core/pymoose/mfield.cpp:120:25: note: length computed here
     size_t size = strlen(fieldName);
                   ~~~~~~^~~~~~~~~~~

Add error handling while at it.

* Use single thread by default unless all benchamarking is done. For small

ksolve/Ksolve.cpp
@@ -7,6 +7,7 @@
 ** See the file COPYING.LIB for the full notice.
 **********************************************************************/
 #include "../basecode/header.h"
+#include "../basecode/global.h"
 #ifdef USE_GSL
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_matrix.h>
@@ -31,9 +32,21 @@
 #include "../mesh/ChemCompt.h"
 #include "Ksolve.h"
 
+#include <chrono>
+#include <algorithm>
+
+#ifdef USE_BOOST_ASYNC
+#define BOOST_THREAD_PROVIDES_FUTURE
+#include <boost/thread.hpp>
+#include <boost/thread/future.hpp>
+#endif
+
 #include <future>
+#include <functional>
 #include <atomic>
 #include <thread>
+
+using namespace std::chrono;
 
 const unsigned int OFFNODE = ~0;
 
@@ -42,7 +55,6 @@ const Cinfo* Ksolve::initCinfo()
     ///////////////////////////////////////////////////////
     // Field definitions
     ///////////////////////////////////////////////////////
-
     static ValueFinfo< Ksolve, string > method (
         "method",
         "Integration method, using GSL. So far only explict. Options are:"
@@ -84,13 +96,14 @@ const Cinfo* Ksolve::initCinfo()
         "current solver. ",
         &Ksolve::getNumLocalVoxels
     );
-    static LookupValueFinfo<
-    Ksolve, unsigned int, vector< double > > nVec(
+
+    static LookupValueFinfo< Ksolve, unsigned int, vector< double > > nVec(
         "nVec",
         "vector of pool counts. Index specifies which voxel.",
         &Ksolve::setNvec,
         &Ksolve::getNvec
     );
+
     static ValueFinfo< Ksolve, unsigned int > numAllVoxels(
         "numAllVoxels",
         "Number of voxels in the entire reac-diff system, "
@@ -99,14 +112,12 @@ const Cinfo* Ksolve::initCinfo()
         &Ksolve::getNumAllVoxels
     );
 
-#if PARALLELIZE_KSOLVE_WITH_CPP11_ASYNC
     static ValueFinfo< Ksolve, unsigned int > numThreads (
         "numThreads",
         "Number of threads to use (applicable in deterministic case)",
         &Ksolve::setNumThreads,
         &Ksolve::getNumThreads
     );
-#endif
 
     static ValueFinfo< Ksolve, unsigned int > numPools(
         "numPools",
@@ -121,6 +132,7 @@ const Cinfo* Ksolve::initCinfo()
         "Estimated timestep for reac system based on Euler error",
         &Ksolve::getEstimatedDt
     );
+
     static ReadOnlyValueFinfo< Ksolve, Id > stoich(
         "stoich",
         "Id for stoichiometry object tied to this Ksolve",
@@ -134,17 +146,23 @@ const Cinfo* Ksolve::initCinfo()
 
     static DestFinfo process( "process",
                               "Handles process call from Clock",
-                              new ProcOpFunc< Ksolve >( &Ksolve::process ) );
+                              new ProcOpFunc< Ksolve >( &Ksolve::process )
+                            );
+
     static DestFinfo reinit( "reinit",
                              "Handles reinit call from Clock",
-                             new ProcOpFunc< Ksolve >( &Ksolve::reinit ) );
+                             new ProcOpFunc< Ksolve >( &Ksolve::reinit )
+                           );
 
     static DestFinfo initProc( "initProc",
                                "Handles initProc call from Clock",
-                               new ProcOpFunc< Ksolve >( &Ksolve::initProc ) );
+                               new ProcOpFunc< Ksolve >( &Ksolve::initProc )
+                             );
+
     static DestFinfo initReinit( "initReinit",
                                  "Handles initReinit call from Clock",
-                                 new ProcOpFunc< Ksolve >( &Ksolve::initReinit ) );
+                                 new ProcOpFunc< Ksolve >( &Ksolve::initReinit )
+                               );
 
     static DestFinfo voxelVol( "voxelVol",
                                "Handles updates to all voxels. Comes from parent "
@@ -152,6 +170,7 @@ const Cinfo* Ksolve::initCinfo()
                                new OpFunc1< Ksolve, vector< double > >(
                                    &Ksolve::updateVoxelVol )
                              );
+
     ///////////////////////////////////////////////////////
     // Shared definitions
     ///////////////////////////////////////////////////////
@@ -159,16 +178,19 @@ const Cinfo* Ksolve::initCinfo()
     {
         &process, &reinit
     };
+
     static SharedFinfo proc( "proc",
                              "Shared message for process and reinit. These are used for "
                              "all regular Ksolve calculations including interfacing with "
                              "the diffusion calculations by a Dsolve.",
                              procShared, sizeof( procShared ) / sizeof( const Finfo* )
                            );
+
     static Finfo* initShared[] =
     {
         &initProc, &initReinit
     };
+
     static SharedFinfo init( "init",
                              "Shared message for initProc and initReinit. This is used"
                              " when the system has cross-compartment reactions. ",
@@ -177,22 +199,20 @@ const Cinfo* Ksolve::initCinfo()
 
     static Finfo* ksolveFinfos[] =
     {
-        &method,			// Value
-        &epsAbs,			// Value
-        &epsRel ,			// Value
-#if PARALLELIZE_KSOLVE_WITH_CPP11_ASYNC
-        &numThreads,                    // Value
-#endif
-        &compartment,		// Value
-        &numLocalVoxels,	// ReadOnlyValue
-        &nVec,				// LookupValue
-        &numAllVoxels,		// ReadOnlyValue
-        &numPools,			// Value
-        &estimatedDt,		// ReadOnlyValue
-        &stoich,			// ReadOnlyValue
-        &voxelVol,			// DestFinfo
-        &proc,				// SharedFinfo
-        &init,				// SharedFinfo
+        &method,                         // Value
+        &epsAbs,                         // Value
+        &epsRel ,                         // Value
+        &numThreads,                            // Value
+        &compartment,                           // Value
+        &numLocalVoxels,                 // ReadOnlyValue
+        &nVec,                                 // LookupValue
+        &numAllVoxels,                         // ReadOnlyValue
+        &numPools,                         // Value
+        &estimatedDt,                         // ReadOnlyValue
+        &stoich,                         // ReadOnlyValue
+        &voxelVol,                         // DestFinfo
+        &proc,                                 // SharedFinfo
+        &init,                                 // SharedFinfo
     };
 
     static Dinfo< Ksolve > dinfo;
@@ -222,20 +242,25 @@ Ksolve::Ksolve()
 #endif
     epsAbs_( 1e-7 ),
     epsRel_( 1e-7 ),
-#if PARALLELIZE_KSOLVE_WITH_CPP11_ASYNC
-    numThreads_( 3 ),
-#endif
+    numThreads_( 1 ),
     pools_( 1 ),
     startVoxel_( 0 ),
     dsolve_(),
     dsolvePtr_( 0 )
 {
-    ;
 }
 
 Ksolve::~Ksolve()
 {
-    ;
+#if 0
+    char* p = getenv( "MOOSE_SHOW_SOLVER_PERF" );
+    if( p != NULL )
+    {
+        cout << "Info: Ksolve (+Dsolve) took " << totalTime_ << " seconds and took " << numSteps_
+             << " steps." << endl;
+
+    }
+#endif
 }
 
 //////////////////////////////////////////////////////////////
@@ -302,7 +327,6 @@ void Ksolve::setEpsRel( double epsRel )
     }
 }
 
-#if PARALLELIZE_KSOLVE_WITH_CPP11_ASYNC
 void Ksolve::setNumThreads( unsigned int x )
 {
     numThreads_ = x;
@@ -312,7 +336,6 @@ unsigned int Ksolve::getNumThreads(  ) const
 {
     return numThreads_;
 }
-#endif
 
 Id Ksolve::getStoich() const
 {
@@ -365,10 +388,11 @@ void Ksolve::setStoich( Id stoich )
         ode.method = method_;
 #ifdef USE_GSL
         ode.gslSys.dimension = stoichPtr_->getNumAllPools();
-        if ( ode.gslSys.dimension == 0 ) {
-			stoichPtr_ = 0;
+        if ( ode.gslSys.dimension == 0 )
+        {
+            stoichPtr_ = 0;
             return; // No pools, so don't bother.
-		}
+        }
         innerSetMethod( ode, method_ );
         ode.gslSys.function = &VoxelPools::gslFunc;
         ode.gslSys.jacobian = 0;
@@ -382,15 +406,11 @@ void Ksolve::setStoich( Id stoich )
         }
 #elif USE_BOOST_ODE
         ode.dimension = stoichPtr_->getNumAllPools();
-        ode.boostSys.epsAbs = epsAbs_;
-        ode.boostSys.epsRel = epsRel_;
-        ode.boostSys.method = method_;
         if ( ode.dimension == 0 )
             return; // No pools, so don't bother.
         unsigned int numVoxels = pools_.size();
         for ( unsigned int i = 0 ; i < numVoxels; ++i )
         {
-            ode.boostSys.params = &pools_[i];
             pools_[i].setStoich( stoichPtr_, &ode );
         }
 #endif
@@ -496,8 +516,12 @@ double Ksolve::getEstimatedDt() const
 //////////////////////////////////////////////////////////////
 void Ksolve::process( const Eref& e, ProcPtr p )
 {
+
+
     if ( isBuilt_ == false )
         return;
+
+    t0_ = high_resolution_clock::now();
 
     // First, handle incoming diffusion values, update S with those.
     if ( dsolvePtr_ )
@@ -509,29 +533,30 @@ void Ksolve::process( const Eref& e, ProcPtr p )
         dvalues[3] = stoichPtr_->getNumVarPools();
 
         dsolvePtr_->getBlock( dvalues );
-		// Second, set the prev_ value in DiffPoolVec
-		dsolvePtr_->setPrev();
+        // Second, set the prev_ value in DiffPoolVec
+        dsolvePtr_->setPrev();
         setBlock( dvalues );
     }
 
     size_t nvPools = pools_.size( );
 
-#ifdef PARALLELIZE_KSOLVE_WITH_CPP11_ASYNC
     // Third, do the numerical integration for all reactions.
-    size_t grainSize = min( nvPools, 1 + (nvPools / numThreads_ ) );
-    size_t nWorkers = nvPools / grainSize;
+    size_t grainSize = max( (size_t)1, min( nvPools, nvPools / numThreads_));
+    size_t nWorkers = std::max(1, (int)(nvPools/grainSize) );
+
+    // Just to be sure. Its not very costly computation.
+    while( nWorkers * grainSize < nvPools )
+        grainSize += 1;
 
     if( 1 == nWorkers || 1 == nvPools )
     {
         if( numThreads_ > 1 )
         {
-#ifndef NDEBUG
-            cout << "Debug: Reset to 1 threads " << endl;
-#endif
+            cerr << "Warn: Not enough voxels or threads. Reverting to serial mode. " << endl;
             numThreads_ = 1;
         }
 
-        for ( size_t i = 0; i < nvPools; i++ )
+        for ( unsigned int i = 0; i < nvPools; i++ )
             pools_[i].advance( p );
     }
     else
@@ -540,15 +565,19 @@ void Ksolve::process( const Eref& e, ProcPtr p )
          *  Somewhat complicated computation to compute the number of threads. 1
          *  thread per (at least) voxel pool is ideal situation.
          *-----------------------------------------------------------------------------*/
-        //cout << "Grain size " << grainSize <<  " Workers : " << nWorkers << endl;
-        for (size_t i = 0; i < nWorkers; i++)
-            parallel_advance( i * grainSize, (i+1) * grainSize, nWorkers, p );
-    }
-#else
-    for ( size_t i = 0; i < nvPools; i++ )
-        pools_[i].advance( p );
-#endif
+        vector<std::thread> vecThreads;
+        // cout << nWorkers << " grain size " << grainSize << endl;
 
+        // lambdas are faster than std::bind
+        for (size_t i = 0; i < nWorkers; i++)
+        {
+            std::thread t( &Ksolve::advance_chunk, this, i*grainSize, (i+1)*grainSize, p );
+            vecThreads.push_back( std::move(t) );
+        }
+
+        for (auto &v : vecThreads )
+            v.join();
+    }
 
     // Assemble and send the integrated values off for the Dsolve.
     if ( dsolvePtr_ )
@@ -561,40 +590,24 @@ void Ksolve::process( const Eref& e, ProcPtr p )
         getBlock( kvalues );
         dsolvePtr_->setBlock( kvalues );
 
-		// Now use the values in the Dsolve to update junction fluxes
-		// for diffusion, channels, and xreacs
-        dsolvePtr_->updateJunctions( p->dt );
+        // Now use the values in the Dsolve to update junction fluxes
+        // for diffusion, channels, and xreacs
+        dsolvePtr_->updateJunctions( p->dt ); 
     }
+    t1_ = high_resolution_clock::now();
+    moose::addSolverProf( "Ksolve", duration_cast<duration<double>> (t1_ - t0_ ).count(), 1 );
 }
 
-
-#if PARALLELIZE_KSOLVE_WITH_CPP11_ASYNC
-/**
- * @brief Advance voxels pools using parallel Ksolve.
- *
- * @param begin
- * @param end
- * @param p
- */
-void Ksolve::parallel_advance(int begin, int end, size_t nWorkers, ProcPtr p)
+void Ksolve::advance_pool( const size_t i, ProcPtr p )
 {
-    std::atomic<int> idx( begin );
-    for (size_t cpu = 0; cpu != nWorkers; ++cpu)
-    {
-        std::async( std::launch::async
-                , [this, &idx, end, p]() {
-                    for (;;)
-                    {
-                        int i = idx++;
-                        if (i >= end)
-                            break;
-                        pools_[i].advance( p );
-                    }
-                }
-            );
-    }
+    pools_[i].advance(p);
 }
-#endif
+
+void Ksolve::advance_chunk( const size_t begin, const size_t end, ProcPtr p )
+{
+    for (size_t i = begin; i < std::min(end, pools_.size() ); i++)
+        pools_[i].advance( p );
+}
 
 
 void Ksolve::reinit( const Eref& e, ProcPtr p )
@@ -613,10 +626,8 @@ void Ksolve::reinit( const Eref& e, ProcPtr p )
         return;
     }
 
-#if PARALLELIZE_KSOLVE_WITH_CPP11_ASYNC
     if( 1 < getNumThreads( ) )
-        cout << "Debug: User wants Ksolve with " << numThreads_ << " threads" << endl;
-#endif
+        cout << "Info: Setting up ksolve with " << numThreads_ << " threads" << endl;
 
 }
 

ksolve/OdeSystem.h
@@ -37,8 +37,6 @@ class OdeSystem {
         double epsRel; // Relative error
 
 #if  USE_BOOST_ODE
-        //BoostSys* pBoostSys;
-        BoostSys boostSys;
         size_t dimension;
 #endif     /* -----  USE_BOOST_ODE  ----- */
 };

commit 081dd1409dc65b2aeb6d9e5dec032245ae8411c4

Fixed a regression to StreadyState solver by #293. - SteadyState called VoxelPools::setStoich with null OdeSystem; I assumed that  OdeSystem is never a NULL. - Added a test to avoid such thing in future.

ksolve/Ksolve.cpp
@@ -378,6 +378,7 @@ void Ksolve::setStoich( Id stoich )
     assert( stoich.element()->cinfo()->isA( "Stoich" ) );
     stoich_ = stoich;
     stoichPtr_ = reinterpret_cast< Stoich* >( stoich.eref().data() );
+
     if ( !isBuilt_ )
     {
         OdeSystem ode;
@@ -386,6 +387,7 @@ void Ksolve::setStoich( Id stoich )
         // ode.initStepSize = getEstimatedDt();
         ode.initStepSize = 0.01; // This will be overridden at reinit.
         ode.method = method_;
+
 #ifdef USE_GSL
         ode.gslSys.dimension = stoichPtr_->getNumAllPools();
         if ( ode.gslSys.dimension == 0 )
@@ -416,6 +418,7 @@ void Ksolve::setStoich( Id stoich )
 #endif
         isBuilt_ = true;
     }
+
 }
 
 Id Ksolve::getDsolve() const

ksolve/OdeSystem.h
@@ -26,15 +26,16 @@ class OdeSystem {
     {;}
 
         std::string method;
-        // GSL stuff
 
-#ifdef USE_GSL
-        gsl_odeiv2_system gslSys;
-        const gsl_odeiv2_step_type* gslStep;
-#endif
         double initStepSize;
         double epsAbs; // Absolute error
         double epsRel; // Relative error
+
+#ifdef USE_GSL
+        // GSL stuff
+        gsl_odeiv2_system gslSys;
+        const gsl_odeiv2_step_type* gslStep;
+#endif
 
 #if  USE_BOOST_ODE
         size_t dimension;

ksolve/SteadyStateGsl.cpp
@@ -382,9 +382,10 @@ void SteadyState::setStoich( Id value )
     double vol = LookupField< unsigned int, double >::get(
                      stoichPtr->getCompartment(), "oneVoxelVolume", 0 );
     pool_.setVolume( vol );
-    pool_.setStoich( stoichPtr, 0 );
-    pool_.updateAllRateTerms( stoichPtr->getRateTerms(),
-                              stoichPtr->getNumCoreRates() );
+
+    pool_.setStoich( stoichPtr, nullptr );
+
+    pool_.updateAllRateTerms( stoichPtr->getRateTerms(), stoichPtr->getNumCoreRates() );
     isInitialized_ = 1;
 }
 

ksolve/VoxelPools.cpp
@@ -66,9 +66,12 @@ void VoxelPools::reinit( double dt )
 void VoxelPools::setStoich( Stoich* s, const OdeSystem* ode )
 {
     stoichPtr_ = s;
-    absTol_ = ode->epsAbs;
-    relTol_ = ode->epsRel;
-    method_ = ode->method;
+    if( ode )
+    {
+        absTol_ = ode->epsAbs;
+        relTol_ = ode->epsRel;
+        method_ = ode->method;
+    }
 
 #ifdef USE_GSL
     if ( ode )

tests/python/chem_models/19085.cspace
@@ -1,0 +1,1 @@
+M101: |DabX|Jbca| 5.59269 0.0157641 0.172865 0.361005 4.72728 1.08558 0.0982933

tests/python/test_dose_response.py
@@ -1,0 +1,105 @@
+# -*- coding: utf-8 -*-
+## Makes and plots the dose response curve for bistable models
+## Author: Sahil Moza
+## June 26, 2014
+## Update:
+## Friday 14 September 2018 05:48:42 PM IST
+## Tunrned into a light-weight test by Dilawar Singh 
+
+import os
+import sys
+import moose
+import numpy as np
+
+sdir_ = os.path.dirname( os.path.realpath( __file__ ) )
+
+def setupSteadyState(simdt,plotDt):
+
+    ksolve = moose.Ksolve( '/model/kinetics/ksolve' )
+    stoich = moose.Stoich( '/model/kinetics/stoich' )
+    stoich.compartment = moose.element('/model/kinetics')
+    stoich.ksolve = ksolve
+    stoich.path = "/model/kinetics/##"
+    state = moose.SteadyState( '/model/kinetics/state' )
+    moose.reinit()
+    state.stoich = stoich
+    state.showMatrices()
+    state.convergenceCriterion = 1e-8
+    return ksolve, state
+
+def parseModelName(fileName):
+    pos1=fileName.rfind('/')
+    pos2=fileName.rfind('.')
+    directory=fileName[:pos1]
+    prefix=fileName[pos1+1:pos2]
+    suffix=fileName[pos2+1:len(fileName)]
+    return directory, prefix, suffix
+
+# Solve for the steady state
+def getState( ksolve, state, vol):
+    scale = 1.0 / ( vol * 6.022e23 )
+    moose.reinit()
+    state.randomInit() # Removing random initial condition to systematically make Dose reponse curves.
+    moose.start( 2.0 ) # Run the model for 2 seconds.
+    state.settle()
+    
+    vector = []
+    a = moose.element( '/model/kinetics/a' ).conc
+    for x in ksolve.nVec[0]:
+        vector.append( x * scale)
+    failedSteadyState = any([np.isnan(x) for x in vector])
+    if not (failedSteadyState):
+        return state.stateType, state.solutionStatus, a, vector
+
+
+def main():
+    # Setup parameters for simulation and plotting
+    simdt= 1e-2
+    plotDt= 1
+
+    # Factors to change in the dose concentration in log scale
+    factorExponent = 10  ## Base: ten raised to some power.
+    factorBegin = -10
+    factorEnd = 11
+    factorStepsize = 2
+    factorScale = 10.0 ## To scale up or down the factors
+
+    # Load Model and set up the steady state solver.
+    # model = sys.argv[1] # To load model from a file.
+    model = os.path.join( sdir_, 'chem_models/19085.cspace' )
+    modelPath, modelName, modelType = parseModelName(model)
+    outputDir = modelPath
+    
+    modelId = moose.loadModel(model, 'model', 'ee')
+    dosePath = '/model/kinetics/b/DabX' # The dose entity
+
+    ksolve, state = setupSteadyState( simdt, plotDt)
+    vol = moose.element( '/model/kinetics' ).volume
+    iterInit = 100
+    solutionVector = []
+    factorArr = []
+    
+    enz = moose.element(dosePath)
+    init = float(enz.kcat) # Dose parameter
+    
+    # Change Dose here to .
+    for factor in range(factorBegin, factorEnd, factorStepsize ):
+        scale = factorExponent ** (factor/factorScale) 
+        enz.kcat = init * scale     
+        print( "scale={:.3f}\tkcat={:.3f}".format( scale, enz.kcat) )
+        for num in range(iterInit):
+            stateType, solStatus, a, vector = getState( ksolve, state, vol)
+            if solStatus == 0:
+                #solutionVector.append(vector[0]/sum(vector))
+                solutionVector.append(a)
+                factorArr.append(scale)   
+                
+    joint = np.array([factorArr, solutionVector])
+    joint = joint[:,joint[1,:].argsort()]
+    got = np.mean( joint ), np.std( joint )
+    expected = (1.2247, 2.46)
+    assert np.isclose(got, expected, 1e-3, 1e-3).all(), "Got %s, expected %s" % (got, expected)
+    print( joint )
+
+if __name__ == '__main__':
+    main()