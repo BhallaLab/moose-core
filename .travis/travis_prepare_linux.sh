c4387cc17f83aaa669c32fe37027410dc63246a2
Dilawar Singh <dilawars@ncbs.res.in>
2018-09-19 11:16:24 +0530

    install lapack-dev as well. Required for BOOST.

--- .travis/travis_prepare_linux.sh
+++ .travis/travis_prepare_linux.sh
@@ -21,15 +21,19 @@ set -o nounset                              # Treat unset variables as an error
 set +e  # Let installation fail in some command
 
 apt-get install -qq libxml2-dev libbz2-dev
-apt-get install -qq libhdf5-serial-dev
 apt-get install -qq make cmake
 apt-get install -qq python-numpy python-matplotlib python-networkx python-pip
 apt-get install -qq python3-lxml python-lxml
 apt-get install -qq python3-numpy python3-matplotlib python3-dev
-apt-get install -qq libboost-all-dev
-apt-get install -qq libgsl0-dev
 apt-get install -qq python-pip python3-pip
 apt-get install -qq libgraphviz-dev
+
+# Gsl
+apt-get install -qq libgsl0-dev || apt-get install -qq libgsl-dev
+
+# Boost related.
+apt-get install -qq liblapack-dev
+apt-get install -qq libboost-all-dev
 
 # Dependencies for NML2
 apt-get install -qq python-scipy python3-scipy
