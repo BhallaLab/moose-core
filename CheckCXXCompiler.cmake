4627d4c771a26c4ec1875698eaba818c9c1fe9f9
Dilawar Singh <dilawars@ncbs.res.in>
2018-09-23 09:49:44 +0530

    TIDY UP: basecode and benchmarks can be built independantly. [skip ci]

--- CheckCXXCompiler.cmake
+++ CheckCXXCompiler.cmake
@@ -50,4 +50,17 @@ else(COMPILER_SUPPORTS_CXX11)
         ")
 endif(COMPILER_SUPPORTS_CXX11)
 
+################################ LINT  ##################################
+# This is only available in cmake3.6+
+find_program(
+    CLANG_TIDY_EXE
+    NAMES "clang-tidy"
+    DOC "Path to clang-tidy executable"
+    )
+if(NOT CLANG_TIDY_EXE)
+    message(STATUS "clang-tidy not found.")
+else()
+    message(STATUS "clang-tidy found: ${CLANG_TIDY_EXE}")
+    set(DO_CLANG_TIDY "${CLANG_TIDY_EXE}" "-checks=*,-clang-analyzer-alpha.*")
+endif()
 
