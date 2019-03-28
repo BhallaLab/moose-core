BhallaLab/moose-core

commit a1a256bd20614acb9c457145fd7fa753ba039d3c

Efficient Network Streamer for MOOSE (#352)
* This version shows images perfectly well. do not use timestamp since their value
will not compare with timestamp of artifacts on different machines.

* Temp commit.

* CTRL+C is still not behaving well.

* Socket information is separated and stored in one class.

* os.file.isfile does not work with UNIX_DOMAIN_SOCKET. use os.path.exists.

* Working version. Good for demo.

* Updated a little.

* Streaming is now flawless.

* passes.

* Local tests passes.

* Good for demo.

* Squashed commit of the following:

commit 33fd75bf051e4799d2c37d3dc6cd59d3bbd73f42
Author: Dilawar Singh <dilawars@ncbs.res.in>
Date:   Sat Feb 9 17:48:43 2019 +0530

    Working fantastically with multiprocessing.

commit 3fd5a8724e7b9cc0c681f44f4019ecb3844129eb
Author: Dilawar Singh <dilawars@ncbs.res.in>
Date:   Sat Feb 9 16:29:59 2019 +0530

    Split connect and stream function into two.

* A decent working version.

* updated a little bit more.

* Small simulation do not steam all the data. Large simulations are
working fine.

* Working version. May be SocketStreamer is ticking too fast.

* faster tests; non-blocking client.

* use absolute import | python3 error in build.

* folder name changed for reflect if PYTHON3 is being used.

* Added some delay after start()

* Fixes for OSX.

* The last table is allowed to have slightly fewer elements.

* Some more improvement.

shell/Shell.cpp
@@ -63,73 +63,68 @@ const Cinfo* Shell::initCinfo()
         &Shell::setCwe,
         &Shell::getCwe );
 
-////////////////////////////////////////////////////////////////
-// Dest Finfos: Functions handled by Shell
-////////////////////////////////////////////////////////////////
+
+    // Dest Finfos: Functions handled by Shell
     static DestFinfo handleUseClock( "useClock"
-                                     , "Deals with assignment of path to a given clock."
-                                     " Arguments: path, field, tick number. "
-                                     , new EpFunc4< Shell, string, string, unsigned int, unsigned int >(&Shell::handleUseClock )
-                                   );
+            , "Deals with assignment of path to a given clock."
+            " Arguments: path, field, tick number. "
+            , new EpFunc4< Shell, string, string, unsigned int, unsigned int >(&Shell::handleUseClock )
+            );
 
     static DestFinfo handleCreate( "create"
-                                   , "create( class, parent, newElm, name, numData, isGlobal )"
-                                   , new EpFunc6<Shell, string, ObjId, Id, string, NodeBalance, unsigned int>(&Shell::handleCreate)
-                                 );
+            , "create( class, parent, newElm, name, numData, isGlobal )"
+            , new EpFunc6<Shell, string, ObjId, Id, string, NodeBalance, unsigned int>(&Shell::handleCreate)
+            );
 
     static DestFinfo handleDelete( "delete"
-                                   , "When applied to a regular object, this function operates "
-                                   "on the Id (element) specified by the ObjId argument. "
-                                   "The function deletes the entire object "
-                                   "array on this Id, including all dataEntries on it,"
-                                   "all its messages, and all its children. The DataIndex here "
-                                   "is ignored, and all dataEntries are destroyed. \n"
-                                   "When applied to a message: Destroys only that one specific "
-                                   "message identified by the full ObjId. \n"
-                                   "Args: ObjId\n"
-                                   , new EpFunc1< Shell, ObjId >( & Shell::destroy )
-                                 );
+            , "When applied to a regular object, this function operates "
+            "on the Id (element) specified by the ObjId argument. "
+            "The function deletes the entire object "
+            "array on this Id, including all dataEntries on it,"
+            "all its messages, and all its children. The DataIndex here "
+            "is ignored, and all dataEntries are destroyed. \n"
+            "When applied to a message: Destroys only that one specific "
+            "message identified by the full ObjId. \n"
+            "Args: ObjId\n"
+            , new EpFunc1< Shell, ObjId >( & Shell::destroy )
+            );
 
     static DestFinfo handleAddMsg( "addMsg"
-                                   , "Makes a msg. Arguments are:"
-                                   " msgtype, src object, src field, dest object, dest field"
-                                   , new EpFunc6< Shell, string, ObjId, string, ObjId, string, unsigned int >(&Shell::handleAddMsg)
-                                 );
+            , "Makes a msg. Arguments are:"
+            " msgtype, src object, src field, dest object, dest field"
+            , new EpFunc6< Shell, string, ObjId, string, ObjId, string, unsigned int >(&Shell::handleAddMsg)
+            );
 
     static DestFinfo handleQuit( "quit"
-                                 , "Stops simulation running and quits the simulator"
-                                 , new OpFunc0< Shell >( & Shell::handleQuit )
-                               );
+            , "Stops simulation running and quits the simulator"
+            , new OpFunc0< Shell >( & Shell::handleQuit )
+            );
+
     static DestFinfo handleMove( "move"
-                                 , "handleMove( Id orig, Id newParent ): "
-                                 "moves an Element to a new parent"
-                                 , new EpFunc2<Shell, Id, ObjId>( & Shell::handleMove )
-                               );
+            , "handleMove( Id orig, Id newParent ): "
+            "moves an Element to a new parent"
+            , new EpFunc2<Shell, Id, ObjId>( & Shell::handleMove )
+            );
 
     static DestFinfo handleCopy( "copy"
-                                 , "handleCopy( vector< Id > args, string newName, unsigned int nCopies, bool toGlobal, bool copyExtMsgs ): "
-                                 " The vector< Id > has Id orig, Id newParent, Id newElm. "
-                                 "This function copies an Element and all its children to a new parent."
-                                 " May also expand out the original into nCopies copies."
-                                 " Normally all messages within the copy tree are also copied. "
-                                 " If the flag copyExtMsgs is true, then all msgs going out are also copied."
-                                 , new EpFunc5< Shell, vector< ObjId >, string, unsigned int, bool, bool >(
-                                     & Shell::handleCopy )
-                               );
+            , "handleCopy( vector< Id > args, string newName, unsigned int nCopies, bool toGlobal, bool copyExtMsgs ): "
+            " The vector< Id > has Id orig, Id newParent, Id newElm. "
+            "This function copies an Element and all its children to a new parent."
+            " May also expand out the original into nCopies copies."
+            " Normally all messages within the copy tree are also copied. "
+            " If the flag copyExtMsgs is true, then all msgs going out are also copied."
+            , new EpFunc5< Shell, vector< ObjId >, string, unsigned int, bool, bool >(
+                & Shell::handleCopy )
+            );
 
     static DestFinfo setclock( "setclock"
-                               , "Assigns clock ticks. Args: tick#, dt"
-                               , new OpFunc2< Shell, unsigned int, double >( & Shell::doSetClock )
-                             );
+            , "Assigns clock ticks. Args: tick#, dt"
+            , new OpFunc2< Shell, unsigned int, double >( & Shell::doSetClock )
+            );
 
     static Finfo* shellFinfos[] =
     {
         &setclock,
-        ////////////////////////////////////////////////////////////////
-        //  Shared msg
-        ////////////////////////////////////////////////////////////////
-        // &master,
-        // &worker,
         &handleCreate,
         &handleDelete,
         &handleCopy,
@@ -190,9 +185,8 @@ void Shell::setShellElement( Element* shelle )
  *
  */
 Id Shell::doCreate( string type, ObjId parent, string name,
-                    unsigned int numData,
-                    NodePolicy nodePolicy,
-                    unsigned int preferredNode )
+    unsigned int numData, NodePolicy nodePolicy, unsigned int preferredNode
+    )
 {
 
     const Cinfo* c = Cinfo::find( type );
@@ -650,8 +644,8 @@ bool Shell::isRunning() const
  * This gets a bit complicated if the Element is a multidim array.
  */
 void Shell::handleCreate( const Eref& e,
-                          string type, ObjId parent, Id newElm, string name,
-                          NodeBalance nb, unsigned int parentMsgIndex )
+        string type, ObjId parent, Id newElm, string name,
+        NodeBalance nb, unsigned int parentMsgIndex )
 {
     innerCreate( type, parent, newElm, name, nb, parentMsgIndex );
 }
@@ -702,8 +696,7 @@ bool Shell::adopt( Id parent, Id child, unsigned int msgIndex )
  * Assumes we've already done all the argument checking.
  */
 void Shell::innerCreate( string type, ObjId parent, Id newElm, string name,
-                         const NodeBalance& nb, unsigned int msgIndex )
-// unsigned int numData, bool isGlobal
+        const NodeBalance& nb, unsigned int msgIndex )
 {
     const Cinfo* c = Cinfo::find( type );
     if ( c )
@@ -720,7 +713,6 @@ void Shell::innerCreate( string type, ObjId parent, Id newElm, string name,
         case MooseSingleNode:
             cout << "Error: Shell::innerCreate: Yet to implement SingleNodeDataElement. Making BlockBalance.\n";
             ret = new LocalDataElement( newElm, c, name, nb.numData );
-            // ret = new SingleNodeDataElement( newElm, c, name, numData, nb.preferredNode );
             break;
         };
         assert( ret );
@@ -728,9 +720,7 @@ void Shell::innerCreate( string type, ObjId parent, Id newElm, string name,
         ret->setTick( Clock::lookupDefaultTick( c->name() ) );
     }
     else
-    {
         assert( 0 );
-    }
 }
 
 void Shell::destroy( const Eref& e, ObjId oid)

.travis/travis_build_linux.sh
@@ -58,14 +58,14 @@ if type $PYTHON3 > /dev/null; then
     sudo apt-get install -qq python3-networkx || echo "Error with apt"
     # GSL.
     (
-        mkdir -p _GSL_BUILD2 && cd _GSL_BUILD2 && \
+        mkdir -p _GSL_BUILD_PY3 && cd _GSL_BUILD_PY3 && \
             cmake -DPYTHON_EXECUTABLE="$PYTHON3" -DDEBUG=ON ..
         $MAKE && ctest --output-on-failure
     )
 
     # BOOST
     (
-        mkdir -p _BOOST_BUILD2 && cd _BOOST_BUILD2 && \
+        mkdir -p _BOOST_BUILD_PY3 && cd _BOOST_BUILD_PY3 && \
             cmake -DWITH_BOOST_ODE=ON -DPYTHON_EXECUTABLE="$PYTHON3" ..
         $MAKE && ctest --output-on-failure
     )

basecode/ObjId.cpp
@@ -112,8 +112,9 @@ bool ObjId::isOffNode() const
 
 char* ObjId::data() const
 {
-    return id.element()->data( id.element()->rawIndex( dataIndex ),
-                               fieldIndex );
+    return id.element()->data(
+            id.element()->rawIndex( dataIndex ), fieldIndex 
+        );
 }
 
 string ObjId::path() const

builtins/MooseSocketInfo.h
@@ -1,0 +1,83 @@
+/*
+ *    Description:  Utility class for handling socket.
+ *         Author:  Dilawar Singh (), dilawars@ncbs.res.in
+ *   Organization:  NCBS Bangalore
+ */
+
+#ifndef MOOSESOCKETINFO_H
+#define MOOSESOCKETINFO_H
+
+typedef enum t_socket_type_ {TCP_SOCKET, UNIX_DOMAIN_SOCKET} SocketType; // Type of socket.
+
+class MooseSocketInfo
+{
+public:
+    MooseSocketInfo( const string& addr = "" )
+        : address(addr), valid(false)
+    {
+        if( addr.size() > 0 )
+            init();
+    }
+
+    ~MooseSocketInfo() { ; }
+
+    void setAddress( const string addr )
+    {
+        address = addr;
+        init();
+    }
+
+    void init( void )
+    {
+        if( "file://" == address.substr(0, 7))
+        {
+            type = UNIX_DOMAIN_SOCKET;
+            filepath = address.substr(7);
+            valid = true;
+        }
+        else if( "http://" == address.substr(0, 7))
+        {
+            type = TCP_SOCKET;
+            auto colPos = address.find_last_of(':');
+            if( colPos == string::npos )
+            {
+                port = 0;
+                host = address;
+            }
+            else
+            {
+                host = address.substr(0, colPos);
+                port = std::stoi(address.substr(colPos+1));
+            }
+            valid = true;
+        }
+        else if( '/' == address[0] )
+        {
+            type = UNIX_DOMAIN_SOCKET;
+            filepath = address;
+            valid = true;
+        }
+    }
+
+    // C++ is so stupid: https://stackoverflow.com/q/476272/1805129
+    friend std::ostream& operator<<(ostream& os, const MooseSocketInfo& info)
+    {
+        os << "ADDRESS= " << info.address << " TYPE= " << info.type;
+        if( info.type == UNIX_DOMAIN_SOCKET)
+            os << ". FILEPATH " << info.filepath;
+        else
+            os << ". HOST:PORT=" << info.host << ":" << info.port;
+        return os;
+    }
+
+public:
+    SocketType type;
+    string address;
+    string filepath;
+    string host;
+    bool valid;
+    size_t port;
+};
+
+
+#endif /* end of include guard: MOOSESOCKETINFO_H */

commit 7d40c10b3131b1267fdcd42716d1f0f44de29c7f

Fixes to issue #360. Regression caused by #352 .

basecode/header.h
@@ -99,7 +99,6 @@ class Neutral;
 #include "SharedFinfo.h"
 #include "FieldElementFinfo.h"
 #include "FieldElement.h"
-#include "../builtins/Streamer.h"
 #include "../shell/Neutral.h"
 
 

builtins/Streamer.cpp
@@ -1,17 +1,8 @@
 /***
- *       Filename:  Streamer.cpp
- *
  *    Description:  Stream table data.
- *
- *        Version:  0.0.1
- *        Created:  2016-04-26
-
- *       Revision:  none
  *
  *         Author:  Dilawar Singh <dilawars@ncbs.res.in>
  *   Organization:  NCBS Bangalore
- *
- *        License:  GNU GPL2
  */
 
 #include <algorithm>
@@ -45,10 +36,16 @@ const Cinfo* Streamer::initCinfo()
         , &Streamer::getFormat
     );
 
-    static ReadOnlyValueFinfo< Streamer, size_t > numTables (
+    static ReadOnlyValueFinfo<Streamer, size_t> numTables (
         "numTables"
         , "Number of Tables handled by Streamer "
         , &Streamer::getNumTables
+    );
+
+    static ReadOnlyValueFinfo<Streamer, size_t> numWriteEvents(
+        "numWriteEvents"
+        , "Number of time streamer was called to write. (For debugging/performance reason only)"
+        , &Streamer::getNumWriteEvents
     );
 
     /*-----------------------------------------------------------------------------
@@ -96,7 +93,7 @@ const Cinfo* Streamer::initCinfo()
      *-----------------------------------------------------------------------------*/
     static Finfo* procShared[] =
     {
-        &process , &reinit , &addTable, &addTables, &removeTable, &removeTables
+        &process, &reinit, &addTable, &addTables, &removeTable, &removeTables
     };
 
     static SharedFinfo proc(
@@ -107,7 +104,7 @@ const Cinfo* Streamer::initCinfo()
 
     static Finfo * tableStreamFinfos[] =
     {
-        &outfile, &format, &proc, &numTables
+        &outfile, &format, &proc, &numTables, &numWriteEvents
     };
 
     static string doc[] =
@@ -140,8 +137,9 @@ Streamer::Streamer()
 {
     // Not all compilers allow initialization during the declaration of class
     // methods.
-    format_ = "npy";
-    columns_.push_back( "time" );               /* First column is time. */
+    format_ = "csv";
+    numWriteEvents_ = 0;
+    columns_.push_back("time");               /* First column is time. */
     tables_.resize(0);
     tableIds_.resize(0);
     tableTick_.resize(0);
@@ -238,7 +236,7 @@ void Streamer::reinit(const Eref& e, ProcPtr p)
     // write now.
     currTime_ = 0.0;
     zipWithTime( );
-    StreamerBase::writeToOutFile( outfilePath_, format_, "w", data_, columns_);
+    StreamerBase::writeToOutFile(outfilePath_, format_, "w", data_, columns_);
     data_.clear( );
 }
 
@@ -261,15 +259,11 @@ void Streamer::cleanUp( )
  */
 void Streamer::process(const Eref& e, ProcPtr p)
 {
-    //LOG( moose::debug, "Writing to table" );
+    // LOG( moose::debug, "Writing Streamer data to file." );
     zipWithTime( );
-
-    // Write only if there are more than 100 entry in first table.
-    if( tables_[0]->getVecSize() > 100 )
-    {
-        StreamerBase::writeToOutFile( outfilePath_, format_, "a", data_, columns_ );
-        data_.clear( );
-    }
+    StreamerBase::writeToOutFile( outfilePath_, format_, "a", data_, columns_ );
+    data_.clear();
+    numWriteEvents_ += 1;
 }
 
 
@@ -354,6 +348,19 @@ void Streamer::removeTables( vector<Id> tables )
 size_t Streamer::getNumTables( void ) const
 {
     return tables_.size();
+}
+
+/* --------------------------------------------------------------------------*/
+/**
+ * @Synopsis  Get number of write events in streamer. Useful for debugging and
+ * performance measuerments.
+ *
+ * @Returns   
+ */
+/* ----------------------------------------------------------------------------*/
+size_t Streamer::getNumWriteEvents( void ) const
+{
+    return numWriteEvents_;
 }
 
 

builtins/Streamer.h
@@ -49,6 +49,7 @@ public:
     void setFormat( string format );
 
     size_t getNumTables( void ) const;
+    size_t getNumWriteEvents( void ) const;
 
     void addTable( Id table );
     void addTables( vector<Id> tables);
@@ -74,6 +75,9 @@ private:
 
     string outfilePath_;
     string format_;
+
+    size_t numWriteEvents_;
+
     bool isOutfilePathSet_;
 
     // dt_ and tick number of Table's clock

builtins/StreamerBase.cpp
@@ -91,14 +91,14 @@ void StreamerBase::writeToCSVFile( const string& filepath, const string& openmod
     }
 
     // If writing in "w" mode, write the header first.
-    if( openmode == "w" )
+    if(openmode == "w")
     {
         string headerText = "";
         for( vector<string>::const_iterator it = columns.begin();
             it != columns.end(); it++ )
             headerText += ( *it + delimiter_ );
         headerText += eol;
-        fprintf( fp, "%s", headerText.c_str() );
+        fprintf(fp, "%s", headerText.c_str());
     }
 
     string text = "";
@@ -111,8 +111,8 @@ void StreamerBase::writeToCSVFile( const string& filepath, const string& openmod
         // At the end of each row, we remove the delimiter_ and append newline_.
         *(text.end()-1) = eol;
     }
-    fprintf( fp, "%s", text.c_str() );
-    fclose( fp );
+    fprintf(fp, "%s", text.c_str() );
+    fclose(fp);
 }
 
 /*  write data to a numpy file */

shell/Shell.cpp
@@ -22,6 +22,7 @@
 #include "../msg/OneToAllMsg.h"
 #include "../msg/SparseMsg.h"
 #include "../builtins/SocketStreamer.h"
+#include "../builtins/Streamer.h"
 
 #include "Shell.h"
 #include "Wildcard.h"

tests/python/test_streamer.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
+
 """test_streamer.py:
-
 Test script for Streamer class.
-
 """
 
 __author__           = "Dilawar Singh"
@@ -14,13 +14,17 @@ __maintainer__       = "Dilawar Singh"
 __email__            = "dilawars@ncbs.res.in"
 __status__           = "Development"
 
+import moose
+import threading
+import numpy as np
+import time
 import os
 import sys
-import time
-import moose
-import numpy as np
-print(( '[INFO] Using moose form %s' % moose.__file__ ))
+print('[INFO] Using moose form %s' % moose.__file__)
 
+all_done_ = False
+
+# Poll the file to see that we are really writing to it.
 def sanity_test( ):
     a = moose.Table( '/t1' )
     b = moose.Table( '/t1/t1' )
@@ -64,6 +68,7 @@ def sanity_test( ):
 
 def test( ):
     compt = moose.CubeMesh( '/compt' )
+    assert compt
     r = moose.Reac( '/compt/r' )
     a = moose.Pool( '/compt/a' )
     a.concInit = 1
@@ -77,6 +82,10 @@ def test( ):
     r.Kf = 0.1
     r.Kb = 0.01
 
+    outfile = 'streamer_test.csv'
+    if os.path.exists(outfile):
+        os.remove(outfile)
+
     tabA = moose.Table2( '/compt/a/tab' )
     tabB = moose.Table2( '/compt/tabB' )
     tabC = moose.Table2( '/compt/tabB/tabC' )
@@ -88,20 +97,21 @@ def test( ):
 
     # Now create a streamer and use it to write to a stream
     st = moose.Streamer( '/compt/streamer' )
-    st.outfile = os.path.join( os.getcwd(), 'temp.npy' )
-    print(("outfile set to: %s " % st.outfile ))
-    assert st.outfile  == os.path.join( os.getcwd(), 'temp.npy' ), st.outfile
+    st.outfile = outfile
 
+    print("outfile set to: %s " % st.outfile )
     st.addTable( tabA )
     st.addTables( [ tabB, tabC ] )
-
     assert st.numTables == 3
 
     moose.reinit( )
-    print( '[INFO] Running for 57 seconds' )
-    moose.start( 57 )
+    t = 100
+    print( '[INFO] Running for %d seconds' % t )
+    moose.start(t)
     outfile = st.outfile
     moose.quit() # Otherwise Streamer won't flush the rest of entries.
+
+    print('Moose is done. Waiting for monitor to shut down...')
 
     # Now read the table and verify that we have written
     print( '[INFO] Reading file %s' % outfile )
@@ -110,11 +120,9 @@ def test( ):
     else:
         data = np.load( outfile )
     # Total rows should be 58 (counting zero as well).
-    # print(data)
+    #  print(data)
     # print( data.dtype )
-    time = data['time']
-    print( time )
-    assert data.shape >= (58,), data.shape
+    assert data.shape >= (101,), data.shape
     print( '[INFO] Test 2 passed' )
     return 0
 

tests/python/test_table_streaming_support.py
@@ -59,7 +59,7 @@ def test( ):
     moose.reinit( )
     [ print_table( x) for x in [tabA, tabB, tabC] ]
     runtime = 1000
-    print( 'Starting moose for %s' % runtime )
+    print( 'Starting moose for %d secs' % runtime )
     moose.start( runtime, 1 )
     print( ' MOOSE is done' )
 