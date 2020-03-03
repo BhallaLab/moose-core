BhallaLab/moose-core

commit 9489bbdda85904a736bb55b957135ff54630c659

Named variables in Function expression  (#390)
* - Tests are passing locally.
- MooseParser is a pointer in Function. Simplifies header include.
- Added wrapper.py which generates a derived class moose.A for each
  _moose.A. `moose.A` provides `connect` function. Instead of,
        moose.connect(a, 'f1', b, 'f2')
    One can do,
        a.connect('f1', b, 'f2')

* Developers emails are back in travis config file.

* If a symbol already exists, do not add it again. Fixed typo.

* If sympy not found, allow failure.  Added sympy on travis but not on OSX. Don't install sympy with python2.

* removed custom OrderedDict. Python2.6 is not needed to be supported.
Centos6 is EOL.

python/moose/utils.py
@@ -16,8 +16,12 @@ from datetime import datetime
 from collections import defaultdict
 import re
 
+import logging
+logger = logging.getLogger('moose')
+
 from moose.moose_constants import *
 from moose.print_utils import *
+
 # Print and Plot utilities.
 try:
     from moose.plot_utils import *
@@ -519,15 +523,12 @@ def assignDefaultTicks(modelRoot='/model', dataRoot='/data', solver='hsolve'):
             if len(tab.neighbors['input']) == 0:
                 moose.useClock(9, tab.path, 'process')
 
-def stepRun(simtime, steptime, verbose=True, logger=None):
+def stepRun(simtime, steptime, verbose=True):
     """Run the simulation in steps of `steptime` for `simtime`."""
     clock = moose.element('/clock')
     if verbose:
         msg = 'Starting simulation for %g' % (simtime)
-        if logger is None:
-            print(msg)
-        else:
-            logger.info(msg)
+        logger.info(msg)
     ts = datetime.now()
     while clock.currentTime < simtime - steptime:
         ts1 = datetime.now()
@@ -536,29 +537,20 @@ def stepRun(simtime, steptime, verbose=True, logger=None):
         td = te - ts1
         if verbose:
             msg = 'Simulated till %g. Left: %g. %g of simulation took: %g s' % (clock.currentTime, simtime - clock.currentTime, steptime, td.days * 86400 + td.seconds + 1e-6 * td.microseconds)
-            if logger is None:
-                print(msg)
-            else:
-                logger.info(msg)
+            logger.info(msg)
 
     remaining = simtime - clock.currentTime
     if remaining > 0:
         if verbose:
             msg = 'Running the remaining %g.' % (remaining)
-            if logger is None:
-                print(msg)
-            else:
-                logger.info(msg)
+            logger.info(msg)
         moose.start(remaining)
     te = datetime.now()
     td = te - ts
     dt = min([t for t in moose.element('/clock').dts if t > 0.0])
     if verbose:
         msg = 'Finished simulation of %g with minimum dt=%g in %g s' % (simtime, dt, td.days * 86400 + td.seconds + 1e-6 * td.microseconds)
-        if logger is None:
-            print(msg)
-        else:
-            logger.info(msg)
+        logger.info(msg)
 
 
 

python/moose/moose_test.py
@@ -25,13 +25,6 @@ test_dir_ = os.path.join(tempfile.gettempdir(), 'moose-examples')
 test_data_url_ = 'https://github.com/BhallaLab/moose-examples/archive/master.zip'
 test_repo_url_ = 'https://github.com/BhallaLab/moose-examples'
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-    datefmt='%m-%d %H:%M',
-    filename=os.path.join(test_dir_, '_moose_test.log'),
-    filemode='a')
-
 ignored_dict_ = defaultdict(list)
 test_status_ = defaultdict(list)
 total_ = 0

.ci/travis_build_linux.sh
@@ -21,6 +21,9 @@ $PYTHON2 -m pip install libNeuroML pyNeuroML python-libsbml --upgrade --user
 $PYTHON3 -m pip install pip --upgrade --user
 $PYTHON3 -m pip install libNeuroML pyNeuroML python-libsbml --upgrade --user
 
+# sympy is only needed for pretty-priting for one test.
+$PYTHON3 -m pip install sympy --upgrade --user   
+
 NPROC=$(nproc)
 MAKE="make -j$NPROC"
 

.travis.yml
@@ -11,13 +11,13 @@ matrix:
     osx_image: xcode11.3
 
 notifications:
-  email:
-    recipients:
-    - bhalla@ncbs.res.in
-    - dilawar.s.rajput@gmail.com
-    - hrani@ncbs.res.in
-    on_success: change
-    on_failure: always
+    email:
+        recipients:
+            - bhalla@ncbs.res.in
+            - dilawar.s.rajput@gmail.com
+            - hrani@ncbs.res.in
+        on_success: change
+        on_failure: always
 
 addons:
   apt:

CMakeLists.txt
@@ -110,7 +110,6 @@ add_executable(moose.bin basecode/main.cpp)
 
 
 ################################### SETUP BUILD ################################
-
 # default include paths.
 include_directories( ${CMAKE_CURRENT_SOURCE_DIR} )
 

basecode/FieldElement.h
@@ -90,8 +90,7 @@ class FieldElement: public Element
 		 *
 		 * Returns 0 if either index is out of range.
 		 */
-		char* data( unsigned int rawIndex,
-						unsigned int fieldIndex = 0 ) const;
+		char* data( unsigned int rawIndex, unsigned int fieldIndex = 0 ) const;
 
 		/**
 		 * virtual

builtins/Function.h
@@ -12,12 +12,13 @@ class Variable;
 class Eref;
 class Cinfo;
 
-#include "../builtins/MooseParser.h"
+namespace moose { 
+    class MooseParser;
+};
 
-/**
-   Expression parser and evaluator based on ExprTK. 
- */
-// double *functionAddVar_(const char *name, void *data);
+
+// Symbol types.
+enum VarType {XVAR_INDEX, XVAR_NAMED, YVAR, TVAR, CONSTVAR};
 
 class Function 
 {
@@ -39,13 +40,13 @@ public:
     string getExpr(const Eref& e) const;
 
     // get a list of variable identifiers.
-    // this is created by the parser
     vector<string> getVars() const;
     void setVarValues(vector<string> vars, vector<double> vals);
 
     // get/set the value of variable `name`
     void setVar(unsigned int index, double value);
-    Variable * getVar(unsigned int ii);
+
+    Variable* getVar(unsigned int ii);
 
     // get function eval result
     double getValue() const;
@@ -63,16 +64,22 @@ public:
     void setDoEvalAtReinit(bool doEvalAtReinit);
     bool getDoEvalAtReinit() const;
 
+    void setAllowUnknownVariable(bool value);
+    bool getAllowUnknowVariable() const;
+
     void setNumVar(unsigned int num);
     unsigned int getNumVar() const;
 
     void setConst(string name, double value);
     double getConst(string name) const;
 
+    void setVarIndex(string name, unsigned int val);
+    unsigned int getVarIndex(string name) const;
+
     void setIndependent(string index);
     string getIndependent() const;
 
-    vector < double > getY() const;
+    vector<double> getY() const;
 
     double getDerivative() const;
 
@@ -84,10 +91,22 @@ public:
     void process(const Eref& e, ProcPtr p);
     void reinit(const Eref& e, ProcPtr p);
 
+    // This is also used as callback.
     void addVariable(const string& name);
 
-    void showError(moose::Parser::exception_type &e) const;
+    // Add unknown variable.
+    void callbackAddSymbol(const string& name);
 
+    bool symbolExists(const string& name) const;
+
+    void addXByIndex(const unsigned int index);
+    void addXByName(const string& name);
+
+    void addY(const unsigned int index);
+
+    VarType getVarType(const string& name) const;
+
+    void clearAll();
 
 protected:
 
@@ -99,6 +118,7 @@ protected:
     unsigned int mode_;
     bool useTrigger_;
     bool doEvalAtReinit_;
+    bool allowUnknownVar_;
 
     double t_;                             // local storage for current time
     string independent_;                   // To take derivative.
@@ -107,14 +127,19 @@ protected:
     // the form x{i} are included in this
     vector<shared_ptr<Variable>> xs_;
 
+    // Keep the index of x's.
+    map<string, unsigned int> varIndex_;
+
     // this stores variable values pulled by sending request. identifiers of
     // the form y{i} are included in this
     vector<shared_ptr<double>> ys_;
+    map<string, shared_ptr<double>> consts_;
 
     // Used by kinetic solvers when this is zombified.
     void* stoich_;
 
-    moose::MooseParser parser_;
+    // pointer to the MooseParser
+    shared_ptr<moose::MooseParser> parser_;
 
 };
 

commit 6a7a7b27f1f075af50609dd375eacc6764ab8194

Hotfix | some old scripts in moose-examples failed because of regression caused by #390.

python/moose/moose_constants.py
@@ -21,13 +21,9 @@ Last modified: Sat Jan 18, 2014  05:01PM
 """
 
 __author__           = "Dilawar Singh"
-__copyright__        = "Copyright 2013, Dilawar Singh, NCBS Bangalore"
-__credits__          = ["NCBS Bangalore"]
 __license__          = "GNU GPL"
-__version__          = "1.0.0"
 __maintainer__       = "Dilawar Singh"
 __email__            = "dilawars@ncbs.res.in"
-__status__           = "Development"
 
 ## for Ca Pool
 # FARADAY = 96154.0
@@ -66,4 +62,3 @@ POOLCLOCK = 3
 LOOKUPCLOCK = 6
 STIMCLOCK = 7
 PLOTCLOCK = 8
-

python/moose/utils.py
@@ -17,7 +17,7 @@ from collections import defaultdict
 import re
 
 import logging
-logger = logging.getLogger('moose')
+logger_ = logging.getLogger('moose')
 
 from moose.moose_constants import *
 from moose.print_utils import *
@@ -523,8 +523,11 @@ def assignDefaultTicks(modelRoot='/model', dataRoot='/data', solver='hsolve'):
             if len(tab.neighbors['input']) == 0:
                 moose.useClock(9, tab.path, 'process')
 
-def stepRun(simtime, steptime, verbose=True):
+def stepRun(simtime, steptime, verbose=True, logger=None):
     """Run the simulation in steps of `steptime` for `simtime`."""
+    global logger_
+    if logger is None:
+        logger = logger_
     clock = moose.element('/clock')
     if verbose:
         msg = 'Starting simulation for %g' % (simtime)

python/moose/moose_test.py
@@ -62,6 +62,10 @@ class Command(object):
         try:
             timer.start()
             output, errors = self.process.communicate()
+            try:
+                errors = errors.decode('utf8')
+            except Exception:
+                pass
             logging.warn('%s %s' % (output, errors))
         finally:
             timer.cancel()