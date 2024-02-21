201bc017ae5e2f532513ab70a6e3a0dd65c1d1b1
Subhasis Ray <ray.subhasis@gmail.com>
2024-02-21 15:21:27 +0530

    This actually fixes scheduling for channels and compartments #467
    The previous commit only updated the docs. This one modifies the
    actual tick assignments.
    Now electrical compartments are scheduled on ticks 2 and 3 and
    channels (all derived from ChanBase) on 4.

--- scheduling/Clock.cpp
+++ scheduling/Clock.cpp
@@ -370,7 +370,6 @@ const Cinfo* Clock::initCinfo()
         "    MgBlock             1       50e-6\n"
         "    Nernst              1       50e-6\n"
         "    RandSpike           1       50e-6\n"
-        "    ChanBase            2       50e-6\n"
         "    IntFire             2       50e-6\n"
         "    IntFireBase         2       50e-6\n"
         "    LIF                 2       50e-6\n"
@@ -391,6 +390,7 @@ const Cinfo* Clock::initCinfo()
         "    Compartment          3      50e-6\n"
         "    CompartmentBase      3      50e-6\n"
         "    SymCompartment       3      50e-6\n"
+        "    ChanBase            4       50e-6\n"
         "    SynChan             4       50e-6\n"
         "    NMDAChan            4       50e-6\n"
         "    GapJunction         4       50e-6\n"
@@ -890,7 +890,6 @@ void Clock::buildDefaultTick()
     defaultTick_["MgBlock"] = 1;
     defaultTick_["Nernst"] = 1;
     defaultTick_["RandSpike"] = 1;
-    defaultTick_["ChanBase"] = 2;
     defaultTick_["IntFire"] = 2;
     defaultTick_["IntFireBase"] = 2;
     defaultTick_["LIF"] = 2;
@@ -900,21 +899,22 @@ void Clock::buildDefaultTick()
     defaultTick_["AdThreshIF"] = 2;
     defaultTick_["IzhIF"] = 2;
     defaultTick_["IzhikevichNrn"] = 2;
-    defaultTick_["SynChan"] = 2;
-    defaultTick_["NMDAChan"] = 2;
-    defaultTick_["GapJunction"] = 2;
-    defaultTick_["HHChannel"] = 2;
-    defaultTick_["HHChannel2D"] = 2;
-    defaultTick_["Leakage"] = 2;
-    defaultTick_["MarkovChannel"] = 2;
     defaultTick_["MarkovOdeSolver"] = 2;
     defaultTick_["MarkovRateTable"] = 2;
     defaultTick_["MarkovSolver"] = 2;
     defaultTick_["MarkovSolverBase"] = 2;
     defaultTick_["RC"] = 2;
-    defaultTick_["Compartment"] = 4; // Note these use an 'init', at t-1.
-    defaultTick_["CompartmentBase"] = 4; // Uses 'init'
-    defaultTick_["SymCompartment"] = 4; // Uses 'init'
+    defaultTick_["Compartment"] = 3; // Note these use an 'init', at t-1.
+    defaultTick_["CompartmentBase"] = 3; // Uses 'init'
+    defaultTick_["SymCompartment"] = 3; // Uses 'init'
+    defaultTick_["ChanBase"] = 4;
+    defaultTick_["SynChan"] = 4;
+    defaultTick_["NMDAChan"] = 4;
+    defaultTick_["GapJunction"] = 4;
+    defaultTick_["HHChannel"] = 4;
+    defaultTick_["HHChannel2D"] = 4;
+    defaultTick_["Leakage"] = 4;
+    defaultTick_["MarkovChannel"] = 4;
     defaultTick_["SpikeGen"] = 5;
     defaultTick_["HSolve"] = 6;
     defaultTick_["SpikeStats"] = 7;
